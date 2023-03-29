2a72558a31c58292dbe20133919c6c3dddd37d08
Torantulino <toran.richards@gmail.com>
2023-03-29 08:32:48 +0100

    Ensures custom print function handles empty content.

--- AutonomousAI/main.py
+++ AutonomousAI/main.py
@@ -10,16 +10,17 @@ import time
 
 def print_to_console(title, title_color, content, min_typing_speed=0.05, max_typing_speed=0.01):
     print(title_color + title + " " + Style.RESET_ALL, end="")
-    words = content.split()
-    for i, word in enumerate(words):
-        print(word, end="", flush=True)
-        if i < len(words) - 1:
-            print(" ", end="", flush=True)
-        typing_speed = random.uniform(min_typing_speed, max_typing_speed)
-        time.sleep(typing_speed)
-        # type faster after each word
-        min_typing_speed = min_typing_speed * 0.95
-        max_typing_speed = max_typing_speed * 0.95
+    if content:
+        words = content.split()
+        for i, word in enumerate(words):
+            print(word, end="", flush=True)
+            if i < len(words) - 1:
+                print(" ", end="", flush=True)
+            typing_speed = random.uniform(min_typing_speed, max_typing_speed)
+            time.sleep(typing_speed)
+            # type faster after each word
+            min_typing_speed = min_typing_speed * 0.95
+            max_typing_speed = max_typing_speed * 0.95
     print()
 
 def print_assistant_thoughts(assistant_reply):
