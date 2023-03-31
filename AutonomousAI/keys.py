cbcbe26e67d0751fdbfc46dcecec22804089441e
Toran Bruce Richards <toran.richards@gmail.com>
2023-03-31 01:09:52 +0100

    Create keys.py

---
+++ AutonomousAI/keys.py
@@ -0,0 +1,2 @@
+OPENAI_API_KEY="YOUR-OPENAI-KEY" # Get yours from:  https://beta.openai.com/account/api-keys
+ELEVENLABS_API_KEY="YOUR-ELEVENLABS-KEY" # To access your ElevenLabs API key, head to https://elevenlabs.io, you can view your xi-api-key using the 'Profile' tab on the website.
