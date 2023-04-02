9a086197a1dd9f3f6bd07e08460e759ea1e21406
DataBassGit <126125195+DataBassGit@users.noreply.github.com>
2023-04-02 15:41:05 -0400

    Update ai_function_lib.py

--- scripts/ai_function_lib.py
+++ scripts/ai_function_lib.py
@@ -2,9 +2,21 @@ import json
 import openai
 import configparser
 
-# Load the configuration file - changed to only perform read once to save performance.
 config = configparser.ConfigParser()
 config.read("config.ini")
+model = config.get("AI", "Chosen_Model", fallback="gpt-4")
+
+gpt4all_model = None
+
+# Load the GPT4All model conditionally
+def load_gpt4all_model():
+    global gpt4all_model
+    from nomic.gpt4all import GPT4All
+    gpt4all_model = GPT4All()
+    gpt4all_model.open()
+
+if model == "gpt4all":
+    load_gpt4all_model()
 
 def call_ai_function(function, args, description):
     # Get the chosen model from the config file
@@ -20,12 +32,13 @@ def call_ai_function(function, args, description):
         {"role": "user", "content": args},
     ]
 
-    # Use different AI APIs based on the chosen model
+    # If the chosen model is GPT-4, call the OpenAI API with the appropriate parameters
     if model == "gpt-4":
         response = openai.ChatCompletion.create(
             model=model, messages=messages, temperature=0
         )
-    # GPT-3.5 with a json verification loop.
+
+    # If the chosen model is GPT-3.5, check if messages are valid JSON before calling the OpenAI API
     elif model == "gpt-3.5":
         try:
             json.dumps(messages)
@@ -36,11 +49,20 @@ def call_ai_function(function, args, description):
                 model=model, messages=messages, temperature=0
             )
             response = response.choices[0].message["content"]
-    # Future model template
+
+    # If the chosen model is some other API, call that API with the appropriate parameters
     elif model == "some_other_api":
-        # Add code to call another AI API with the appropriate parameters
         response = some_other_api_call(parameters)
+
+    # If the chosen model is unsupported, raise an error
     else:
         raise ValueError(f"Unsupported model: {model}")
 
     return response
+
+
+# Call the GPT4All model to process the function, arguments, and description
+def call_gpt4all(function, args, description):
+    prompt = f"{description}. The function should have the following signature:\n{function}\n\nExample input: {', '.join(args)}"
+    result = gpt4all_model.prompt(prompt)
+    return result.strip()
