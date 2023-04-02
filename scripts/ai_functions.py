0a290927a0cd1b8548ac5cf635cce105f24515b4
Torantulino <toran.richards@gmail.com>
2023-04-02 09:55:44 +0100

    Reformats with "Black" formatter.

--- scripts/ai_functions.py
+++ scripts/ai_functions.py
@@ -6,18 +6,20 @@ import openai
 def call_ai_function(function, args, description, model="gpt-4"):
     # parse args to comma seperated string
     args = ", ".join(args)
-    messages = [{"role": "system",
-                 "content": f"You are now the following python function: ```# {description}\n{function}```\n\nOnly respond with your `return` value."},
-                {"role": "user",
-                 "content": args}]
+    messages = [
+        {
+            "role": "system",
+            "content": f"You are now the following python function: ```# {description}\n{function}```\n\nOnly respond with your `return` value.",
+        },
+        {"role": "user", "content": args},
+    ]
 
     response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0
+        model=model, messages=messages, temperature=0
     )
 
     return response.choices[0].message["content"]
+
 
 # Evaluating code
 
@@ -33,8 +35,11 @@ def evaluate_code(code: str) -> List[str]:
 
 # Improving code
 
+
 def improve_code(suggestions: List[str], code: str) -> str:
-    function_string = "def generate_improved_code(suggestions: List[str], code: str) -> str:"
+    function_string = (
+        "def generate_improved_code(suggestions: List[str], code: str) -> str:"
+    )
     args = [json.dumps(suggestions), code]
     description_string = """Improves the provided code based on the suggestions provided, making no other changes."""
 
@@ -44,8 +49,11 @@ def improve_code(suggestions: List[str], code: str) -> str:
 
 # Writing tests
 
+
 def write_tests(code: str, focus: List[str]) -> str:
-    function_string = "def create_test_cases(code: str, focus: Optional[str] = None) -> str:"
+    function_string = (
+        "def create_test_cases(code: str, focus: Optional[str] = None) -> str:"
+    )
     args = [code, json.dumps(focus)]
     description_string = """Generates test cases for the existing code, focusing on specific areas if required."""
 
