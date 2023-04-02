968305123b95a20ef0f7c51d2eb009efabed17cd
Torantulino <toran.richards@gmail.com>
2023-04-02 09:56:53 +0100

    Adds comment to call_ai_function.

--- scripts/ai_functions.py
+++ scripts/ai_functions.py
@@ -3,6 +3,7 @@ import json
 import openai
 
 
+# This is a magic function that can do anything with no-code. See https://github.com/Torantulino/AI-Functions for more info. 
 def call_ai_function(function, args, description, model="gpt-4"):
     # parse args to comma seperated string
     args = ", ".join(args)
