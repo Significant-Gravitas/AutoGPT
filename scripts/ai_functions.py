958ec64fdc5dc0938412c203d58b813620d15f24
DataBassGit <126125195+DataBassGit@users.noreply.github.com>
2023-04-02 09:30:36 -0400

    Update ai_functions.py

--- scripts/ai_functions.py
+++ scripts/ai_functions.py
@@ -1,26 +1,22 @@
 from typing import List, Optional
 import json
 import openai
-
+# Insert AI endpoint library
+from ai_function_lib import call_ai_function
 
 # This is a magic function that can do anything with no-code. See
 # https://github.com/Torantulino/AI-Functions for more info.
-def call_ai_function(function, args, description, model="gpt-4"):
-    # parse args to comma seperated string
-    args = ", ".join(args)
-    messages = [
-        {
-            "role": "system",
-            "content": f"You are now the following python function: ```# {description}\n{function}```\n\nOnly respond with your `return` value.",
-        },
-        {"role": "user", "content": args},
-    ]
 
-    response = openai.ChatCompletion.create(
-        model=model, messages=messages, temperature=0
-    )
 
-    return response.choices[0].message["content"]
+# Define the function, arguments, and description
+function = "def add(a, b):\n    return a + b"
+args = ["3", "5"]
+description = "Adds two numbers together"
+
+# Call the AI function with the parameters
+result = call_ai_function(function, args, description)
+
+print("AI's function output:", result)
 
 
 # Evaluating code
