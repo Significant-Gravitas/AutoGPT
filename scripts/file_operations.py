82e798d7ebc22a48fa32e2993c2097c2f36fe6ae
yousefissa <yousef@u.northwestern.edu>
2023-04-03 09:19:40 -0700

    rewrite file_operations to use pathlib

--- scripts/file_operations.py
+++ scripts/file_operations.py
@@ -1,60 +1,48 @@
-import os
-import os.path
+from pathlib import Path
 
 # Set a dedicated folder for file I/O
-working_directory = "auto_gpt_workspace"
-
-if not os.path.exists(working_directory):
-    os.makedirs(working_directory)
-
+working_directory = Path("auto_gpt_workspace")
+working_directory.mkdir(exist_ok=True)
 
 def safe_join(base, *paths):
-    new_path = os.path.join(base, *paths)
-    norm_new_path = os.path.normpath(new_path)
+    new_path = base.joinpath(*paths)
+    norm_new_path = new_path.resolve()
 
-    if os.path.commonprefix([base, norm_new_path]) != base:
+    if base not in norm_new_path.parents:
         raise ValueError("Attempted to access outside of working directory.")
 
     return norm_new_path
 
-
 def read_file(filename):
     try:
         filepath = safe_join(working_directory, filename)
-        with open(filepath, "r") as f:
-            content = f.read()
+        content = filepath.read_text()
         return content
     except Exception as e:
         return "Error: " + str(e)
 
-
 def write_to_file(filename, text):
     try:
         filepath = safe_join(working_directory, filename)
-        directory = os.path.dirname(filepath)
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        with open(filepath, "w") as f:
-            f.write(text)
+        filepath.parent.mkdir(parents=True, exist_ok=True)
+        filepath.write_text(text)
         return "File written to successfully."
     except Exception as e:
         return "Error: " + str(e)
 
-
 def append_to_file(filename, text):
     try:
         filepath = safe_join(working_directory, filename)
-        with open(filepath, "a") as f:
+        with filepath.open("a") as f:
             f.write(text)
         return "Text appended successfully."
     except Exception as e:
         return "Error: " + str(e)
 
-
 def delete_file(filename):
     try:
         filepath = safe_join(working_directory, filename)
-        os.remove(filepath)
+        filepath.unlink()
         return "File deleted successfully."
     except Exception as e:
         return "Error: " + str(e)
