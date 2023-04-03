326da40a2ffa6e6d995d449f9785fb40956eeca6
astelmach01 <astelmach20@gmail.com>
2023-04-02 21:03:18 -0400

    fixed requirements, updated gitignore, python version to 3.10

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.11
+FROM python:3.10
 
 WORKDIR /app
 
--- scripts/execute_code.py
+++ scripts/execute_code.py
@@ -16,11 +16,11 @@ def execute_python_file(file):
     try:
         client = docker.from_env()
 
-        # You can replace 'python:3.8' with the desired Python image/version
+        # You can replace 'python:3.11' with the desired Python image/version
         # You can find available Python images on Docker Hub:
         # https://hub.docker.com/_/python
         container = client.containers.run(
-            'python:3.11',
+            'python:3.10',
             f'python {file}',
             volumes={
                 os.path.abspath(workspace_folder): {
