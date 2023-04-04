1b4a5301d0d4406732b8e903af1bfda8e7552a6d
Andres Caicedo <Andrezz1997@gmail.com>
2023-04-04 10:49:28 +0200

    Update ai_functions.py
    Just import dumps from JSON library.

--- scripts/ai_functions.py
+++ scripts/ai_functions.py
@@ -1,5 +1,5 @@
 from typing import List, Optional
-import json
+from json import dumps
 from config import Config
 from call_ai_function import call_ai_function
 from json_parser import fix_and_parse_json
@@ -23,7 +23,7 @@ def improve_code(suggestions: List[str], code: str) -> str:
     function_string = (
         "def generate_improved_code(suggestions: List[str], code: str) -> str:"
     )
-    args = [json.dumps(suggestions), code]
+    args = [dumps(suggestions), code]
     description_string = """Improves the provided code based on the suggestions provided, making no other changes."""
 
     result_string = call_ai_function(function_string, args, description_string)
@@ -36,7 +36,7 @@ def write_tests(code: str, focus: List[str]) -> str:
     function_string = (
         "def create_test_cases(code: str, focus: Optional[str] = None) -> str:"
     )
-    args = [code, json.dumps(focus)]
+    args = [code, dumps(focus)]
     description_string = """Generates test cases for the existing code, focusing on specific areas if required."""
 
     result_string = call_ai_function(function_string, args, description_string)
