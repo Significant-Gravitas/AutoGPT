8ecbe7278c44a7001b3fd2cdcd858e53bc01dd48
Andres Caicedo <Andrezz1997@gmail.com>
2023-04-04 11:34:28 +0200

    Update json_parser.py
    Introduces spaces between code blocks.

--- scripts/json_parser.py
+++ scripts/json_parser.py
@@ -2,7 +2,6 @@ import json
 from typing import Any, Dict, Union
 from call_ai_function import call_ai_function
 from config import Config
-from json_utils import correct_json
 
 cfg = Config()
 
@@ -38,38 +37,25 @@ def fix_and_parse_json(
     except json.JSONDecodeError as _:  # noqa: F841
         json_str = correct_json(json_str)
         try:
-            return json.loads(json_str)
-        except json.JSONDecodeError as _:  # noqa: F841
-            pass
-    # Let's do something manually:
-    # sometimes GPT responds with something BEFORE the braces:
-    # "I'm sorry, I don't understand. Please try again."
-    # {"text": "I'm sorry, I don't understand. Please try again.",
-    #  "confidence": 0.0}
-    # So let's try to find the first brace and then parse the rest
-    #  of the string
-    try:
-        brace_index = json_str.index("{")
-        json_str = json_str[brace_index:]
-        last_brace_index = json_str.rindex("}")
-        json_str = json_str[:last_brace_index+1]
-        return json.loads(json_str)
-    except json.JSONDecodeError as e:  # noqa: F841
-        if try_to_fix_with_gpt:
-            print("Warning: Failed to parse AI output, attempting to fix."
-                  "\n If you see this warning frequently, it's likely that"
-                  " your prompt is confusing the AI. Try changing it up"
-                  " slightly.")
+          brace_index = json_str.index("{")
+          json_str = json_str[brace_index:]
+          last_brace_index = json_str.rindex("}")
+          json_str = json_str[:last_brace_index+1]
+          return json.loads(json_str)
+        except Exception as e:
+
+          if try_to_fix_with_gpt:
+            print(f"Warning: Failed to parse AI output, attempting to fix.\n If you see this warning frequently, it's likely that your prompt is confusing the AI. Try changing it up slightly.")
             # Now try to fix this up using the ai_functions
-            ai_fixed_json = fix_json(json_str, JSON_SCHEMA, cfg.debug)
+            ai_fixed_json = fix_json(json_str, json_schema, False)
+
             if ai_fixed_json != "failed":
                 return json.loads(ai_fixed_json)
             else:
-                # This allows the AI to react to the error message,
-                #   which usually results in it correcting its ways.
-                print("Failed to fix ai output, telling the AI.")
-                return json_str
-        else:
+              print(f"Failed to fix ai output, telling the AI.") # This allows the AI to react to the error message, which usually results in it correcting its ways.
+              return json_str
+            
+          else:
             raise e
 
 
@@ -86,16 +72,19 @@ def fix_json(json_str: str, schema: str, debug=False) -> str:
 
     # If it doesn't already start with a "`", add one:
     if not json_str.startswith("`"):
-        json_str = "```json\n" + json_str + "\n```"
+      json_str = "```json\n" + json_str + "\n```"
+
     result_string = call_ai_function(
         function_string, args, description_string, model=cfg.fast_llm_model
     )
+
     if debug:
         print("------------ JSON FIX ATTEMPT ---------------")
         print(f"Original JSON: {json_str}")
         print("-----------")
         print(f"Fixed JSON: {result_string}")
         print("----------- END OF FIX ATTEMPT ----------------")
+
     try:
         json.loads(result_string)  # just check the validity
         return result_string
