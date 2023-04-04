6f211d945167cd5834b9be2c190f1c4d4865d005
Toran Bruce Richards <toran.richards@gmail.com>
2023-04-04 13:09:33 +0100

    Fixes: TypeError: eleven_labs_speech() missing 1 required positional argument: 'text'

--- scripts/speak.py
+++ scripts/speak.py
@@ -42,7 +42,7 @@ def say_text(text, voice_index=0):
     if not cfg.elevenlabs_api_key:
         gtts_speech(text)
     else:
-        success = eleven_labs_speech()
+        success = eleven_labs_speech(text)
         if not success:
             gtts_speech(text)
 
