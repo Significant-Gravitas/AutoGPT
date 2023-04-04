f5541d95f0c1d4a62dab4f229c90c54cafa4b8b4
Andres Caicedo <Andrezz1997@gmail.com>
2023-04-04 10:49:28 +0200

    Update ai_functions.py
    Just import dumps from JSON library.

--- scripts/ai_functions.py
+++ scripts/ai_functions.py
@@ -1,27 +1,9 @@
 from typing import List, Optional
-import json
-import openai
-
-
-# This is a magic function that can do anything with no-code. See
-# https://github.com/Torantulino/AI-Functions for more info.
-def call_ai_function(function, args, description, model="gpt-4"):
-    """Calls an AI function and returns the result."""
-    args = ", ".join(args)
-    messages = [
-        {
-            "role": "system",
-            "content": f"You are now the following python function: ```# {description}\n{function}```\n\nOnly respond with your `return` value.",
-        },
-        {"role": "user", "content": args},
-    ]
-
-    response = openai.ChatCompletion.create(
-        model=model, messages=messages, temperature=0
-    )
-
-    return response.choices[0].message["content"]
-
+from json import dumps
+from config import Config
+from call_ai_function import call_ai_function
+from json_parser import fix_and_parse_json
+cfg = Config()
 
 # Evaluating code
 def evaluate_code(code: str) -> List[str]:
@@ -41,7 +23,7 @@ def improve_code(suggestions: List[str], code: str) -> str:
     function_string = (
         "def generate_improved_code(suggestions: List[str], code: str) -> str:"
     )
-    args = [json.dumps(suggestions), code]
+    args = [dumps(suggestions), code]
     description_string = """Improves the provided code based on the suggestions provided, making no other changes."""
 
     result_string = call_ai_function(function_string, args, description_string)
@@ -54,7 +36,7 @@ def write_tests(code: str, focus: List[str]) -> str:
     function_string = (
         "def create_test_cases(code: str, focus: Optional[str] = None) -> str:"
     )
-    args = [code, json.dumps(focus)]
+    args = [code, dumps(focus)]
     description_string = """Generates test cases for the existing code, focusing on specific areas if required."""
 
     result_string = call_ai_function(function_string, args, description_string)
