dea2bef66a15c8e3248655acf847eac474d454fd
Bituq <d.noorland9@hotmail.com>
2023-04-05 15:50:28 +0200

    fixed speech blocking main thread

--- scripts/speak.py
+++ scripts/speak.py
@@ -4,6 +4,8 @@ import requests
 from config import Config
 cfg = Config()
 import gtts
+import threading
+from threading import Lock
 
 
 # TODO: Nicer names for these ids
@@ -14,18 +16,21 @@ tts_headers = {
     "xi-api-key": cfg.elevenlabs_api_key
 }
 
+mutex_lock = Lock() # Ensure only one sound is played at a time
+
 def eleven_labs_speech(text, voice_index=0):
     tts_url = "https://api.elevenlabs.io/v1/text-to-speech/{voice_id}".format(
         voice_id=voices[voice_index])
-    formatted_message = {"text": text}
+    formatted_message = {"text": text, "voice_settings": {"stability": 0.05, "similarity_boost": 0.8}}
     response = requests.post(
         tts_url, headers=tts_headers, json=formatted_message)
 
     if response.status_code == 200:
-        with open("speech.mpeg", "wb") as f:
-            f.write(response.content)
-        playsound("speech.mpeg")
-        os.remove("speech.mpeg")
+        with mutex_lock:
+            with open("speech.mpeg", "wb") as f:
+                f.write(response.content)
+            playsound("speech.mpeg", True)
+            os.remove("speech.mpeg")
         return True
     else:
         print("Request failed with status code:", response.status_code)
@@ -34,15 +39,19 @@ def eleven_labs_speech(text, voice_index=0):
 
 def gtts_speech(text):
     tts = gtts.gTTS(text)
-    tts.save("speech.mp3")
-    playsound("speech.mp3")
-    os.remove("speech.mp3")
+    with mutex_lock:
+        tts.save("speech.mp3")
+        playsound("speech.mp3", True)
+        os.remove("speech.mp3")
 
 def say_text(text, voice_index=0):
-    if not cfg.elevenlabs_api_key:
-        gtts_speech(text)
-    else:
-        success = eleven_labs_speech(text)
-        if not success:
+    def speak():
+        if not cfg.elevenlabs_api_key:
             gtts_speech(text)
+        else:
+            success = eleven_labs_speech(text)
+            if not success:
+                gtts_speech(text)
 
+    thread = threading.Thread(target=speak)
+    thread.start()
