60e18a741815cedd501f8f2c8babfdb40bae33fc
gannonh <gannonh@users.noreply.github.com>
2023-04-05 17:40:32 -0700

    fixfor: issue #90

--- scripts/commands.py
+++ scripts/commands.py
@@ -19,15 +19,15 @@ cfg = Config()
 def get_command(response):
     try:
         response_json = fix_and_parse_json(response)
-        
+
         if "command" not in response_json:
-            return "Error:" , "Missing 'command' object in JSON"
-        
+            return "Error:", "Missing 'command' object in JSON"
+
         command = response_json["command"]
 
         if "name" not in command:
             return "Error:", "Missing 'name' field in 'command' object"
-        
+
         command_name = command["name"]
 
         # Use an empty dictionary if 'args' field is not present in 'command' object
@@ -47,7 +47,7 @@ def get_command(response):
 def execute_command(command_name, arguments):
     try:
         if command_name == "google":
-            
+
             # Check if the Google API key is set and use the official search method
             # If the API key is not set or has only whitespaces, use the unofficial search method
             if cfg.google_api_key and (cfg.google_api_key.strip() if cfg.google_api_key else None):
@@ -117,6 +117,7 @@ def google_search(query, num_results=8):
 
     return json.dumps(search_results, ensure_ascii=False, indent=4)
 
+
 def google_official_search(query, num_results=8):
     from googleapiclient.discovery import build
     from googleapiclient.errors import HttpError
@@ -129,20 +130,21 @@ def google_official_search(query, num_results=8):
 
         # Initialize the Custom Search API service
         service = build("customsearch", "v1", developerKey=api_key)
-        
+
         # Send the search query and retrieve the results
-        result = service.cse().list(q=query, cx=custom_search_engine_id, num=num_results).execute()
+        result = service.cse().list(q=query, cx=custom_search_engine_id,
+                                    num=num_results).execute()
 
         # Extract the search result items from the response
         search_results = result.get("items", [])
-        
+
         # Create a list of only the URLs from the search results
         search_results_links = [item["link"] for item in search_results]
 
     except HttpError as e:
         # Handle errors in the API call
         error_details = json.loads(e.content.decode())
-        
+
         # Check if the error is related to an invalid or missing API key
         if error_details.get("error", {}).get("code") == 403 and "invalid API key" in error_details.get("error", {}).get("message", ""):
             return "Error: The provided Google API key is invalid or missing."
@@ -151,6 +153,7 @@ def google_official_search(query, num_results=8):
 
     # Return the list of search result URLs
     return search_results_links
+
 
 def browse_website(url, question):
     summary = get_text_summary(url, question)
@@ -194,7 +197,7 @@ def delete_memory(key):
 
 
 def overwrite_memory(key, string):
-    if int(key) >= 0 and key < len(mem.permanent_memory):
+    if isinstance(key, int) and key >= 0 and key < len(mem.permanent_memory):
         _text = "Overwriting memory with key " + \
             str(key) + " and string " + string
         mem.permanent_memory[key] = string
