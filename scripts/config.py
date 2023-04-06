34b6f47f7128c1eb69309beddab6dac65a0d1236
Peter Edwards <sweetlilmre@gmail.com>
2023-04-06 09:15:45 +0200

    Fix for boolean eval from .env

--- scripts/config.py
+++ scripts/config.py
@@ -35,7 +35,7 @@ class Config(metaclass=Singleton):
         
         self.openai_api_key = os.getenv("OPENAI_API_KEY")
         self.use_azure = False
-        self.use_azure = os.getenv("USE_AZURE")
+        self.use_azure = os.getenv("USE_AZURE") == 'True'
         if self.use_azure:
             self.openai_api_base = os.getenv("OPENAI_API_BASE")
             self.openai_api_version = os.getenv("OPENAI_API_VERSION")
