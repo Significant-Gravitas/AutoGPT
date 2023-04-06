978ca2c0da46c3cc12269b59d218a993749bd2d4
root <root@denicomp.redmond.corp.microsoft.com>
2023-04-06 10:23:59 -0700

    updating retry functions and re-formatting code

--- scripts/llm_utils.py
+++ scripts/llm_utils.py
@@ -14,3 +14,12 @@ def create_chat_completion(messages, model=None, temperature=None, max_tokens=No
     )
 
     return response.choices[0].message["content"]
+
+
+@retry(wait=wait_exponential(multiplier=1, min=4, max=30), stop=stop_after_attempt(5))
+def create_chat_completion_with_retry(messages):
+    return create_chat_completion(
+        model=cfg.fast_llm_model,
+        messages=messages,
+        max_tokens=300,
+    )
