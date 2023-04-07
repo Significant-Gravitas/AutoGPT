855f3b10be30cf440599ec9e1083ab5237474557
Luis Lechuga Ruiz <luislechugaruiz@gmail.com>
2023-04-07 23:07:45 +0200

    Improve modularity:  - New AutoGPT class to encapsulate the Auto-GPT creation and execution.  - Run_utils to save utils which lived on main.py  - Main.py calls AutoGPT for backward compatibility

--- scripts/ai_config.py
+++ scripts/ai_config.py
@@ -17,17 +17,20 @@ class AIConfig:
         try:
             with open(config_file) as file:
                 config_params = yaml.load(file, Loader=yaml.FullLoader)
+                ai_name = config_params.get("ai_name", "")
+                ai_role = config_params.get("ai_role", "")
+                ai_goals = config_params.get("ai_goals", [])
+
+                return cls(ai_name, ai_role, ai_goals)
         except FileNotFoundError:
-            config_params = {}
-
-        ai_name = config_params.get("ai_name", "")
-        ai_role = config_params.get("ai_role", "")
-        ai_goals = config_params.get("ai_goals", [])
-
-        return cls(ai_name, ai_role, ai_goals)
+            return None
 
     def save(self, config_file=SAVE_FILE):
-        config = {"ai_name": self.ai_name, "ai_role": self.ai_role, "ai_goals": self.ai_goals}
+        config = {
+            "ai_name": self.ai_name,
+            "ai_role": self.ai_role,
+            "ai_goals": self.ai_goals,
+        }
         with open(config_file, "w") as file:
             yaml.dump(config, file)
 
@@ -35,7 +38,9 @@ class AIConfig:
         prompt_start = """Your decisions must always be made independently without seeking user assistance. Play to your strengths as an LLM and pursue simple strategies with no legal complications."""
 
         # Construct full prompt
-        full_prompt = f"You are {self.ai_name}, {self.ai_role}\n{prompt_start}\n\nGOALS:\n\n"
+        full_prompt = (
+            f"You are {self.ai_name}, {self.ai_role}\n{prompt_start}\n\nGOALS:\n\n"
+        )
         for i, goal in enumerate(self.ai_goals):
             full_prompt += f"{i+1}. {goal}\n"
 
