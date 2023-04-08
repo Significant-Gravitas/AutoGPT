fe985139c4347f36e8ae29618e54c1bc2001db71
kinance <kinance@gmail.com>
2023-04-08 12:39:57 +0900

    Revised to support debug mode from command line

--- scripts/config.py
+++ scripts/config.py
@@ -35,8 +35,9 @@ class Config(metaclass=Singleton):
         """Initialize the configuration class."""
         self.continuous_mode = False
         self.speak_mode = False
-        # TODO - make these models be self-contained, using langchain, so we can configure them once and call it good
-        self.fast_llm_model = os.getenv("FAST_LLM_MODEL", "gpt-3.5-turbo")
+        self.debug_mode = False
+        # TODO - make these models be self-contained, using langchain, so we can configure them once and call it good 
+        self.fast_llm_model = os.getenv("FAST_LLM_MODEL", "gpt-3.5-turbo") 
         self.smart_llm_model = os.getenv("SMART_LLM_MODEL", "gpt-4")
         self.fast_token_limit = int(os.getenv("FAST_TOKEN_LIMIT", 4000))
         self.smart_token_limit = int(os.getenv("SMART_TOKEN_LIMIT", 8000))
@@ -85,6 +86,9 @@ class Config(metaclass=Singleton):
     def set_speak_mode(self, value: bool):
         """Set the speak mode value."""
         self.speak_mode = value
+
+    def set_debug_mode(self, value: bool):
+        self.debug_mode = value
 
     def set_fast_llm_model(self, value: str):
         """Set the fast LLM model value."""
