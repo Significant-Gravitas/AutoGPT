cbcb90569610305c2744b382f9dc752b0f721b2e
Eric Park <47099787+sw5park@users.noreply.github.com>
2023-04-09 00:50:57 +0000

    create, message, list, delete agents

--- scripts/agent_manager.py
+++ scripts/agent_manager.py
@@ -1,73 +1,103 @@
+from typing import Dict, List
 import openai
 
-next_key = 0
-agents = {}  # key, (task, full_message_history, model)
 
-# Create new GPT agent
+class Agent:
+    """
+    The Agent class represents an individual chat agent that interacts with OpenAI's GPT model.
+    It manages the agent's conversation history and GPT model selection.
+    """
 
+    def __init__(self, model: str = "gpt-3.5-turbo") -> None:
+        """
+        Initializes an Agent instance with a specified model.
 
-def create_agent(task, prompt, model):
-    global next_key
-    global agents
+        :param model: The name of the GPT model to be used by the agent (default: "gpt-3.5-turbo").
+        """
+        self.model = model
+        self.message_history: List[Dict[str, str]] = []
 
-    messages = [{"role": "user", "content": prompt}, ]
+    def add_message(self, role: str, content: str) -> None:
+        """
+        Adds a message to the agent's conversation history.
 
-    # Start GTP3 instance
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-    )
+        :param role: The role of the sender in the conversation ("system" or "assistant").
+        :param content: The content of the message.
+        """
+        self.message_history.append({"role": role, "content": content})
 
-    agent_reply = response.choices[0].message["content"]
+    def get_history(self) -> List[Dict[str, str]]:
+        """
+        Returns the agent's conversation history.
 
-    # Update full message history
-    messages.append({"role": "assistant", "content": agent_reply})
-
-    key = next_key
-    # This is done instead of len(agents) to make keys unique even if agents
-    # are deleted
-    next_key += 1
-
-    agents[key] = (task, messages, model)
-
-    return key, agent_reply
-
-
-def message_agent(key, message):
-    global agents
-
-    task, messages, model = agents[int(key)]
-
-    # Add user message to message history before sending to agent
-    messages.append({"role": "user", "content": message})
-
-    # Start GTP3 instance
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-    )
-
-    # Get agent response
-    agent_reply = response.choices[0].message["content"]
-
-    # Update full message history
-    messages.append({"role": "assistant", "content": agent_reply})
-
-    return agent_reply
+        :return: A list of dictionaries representing the conversation history.
+        """
+        return self.message_history
 
 
-def list_agents():
-    global agents
+class AgentManager:
+    """
+    The AgentManager class is designed to manage multiple instances of the Agent class.
+    It provides methods for creating agents, sending messages to agents, listing agents, and deleting agents.
+    """
 
-    # Return a list of agent keys and their tasks
-    return [(key, task) for key, (task, _, _) in agents.items()]
+    def __init__(self, api_key: str) -> None:
+        """
+        Initializes an AgentManager instance with a specified API key.
 
+        :param api_key: The OpenAI API key.
+        """
+        openai.api_key = api_key
+        self.agents: Dict[str, Agent] = {}
 
-def delete_agent(key):
-    global agents
+    def create_agent(self, agent_name: str, model: str = "gpt-3.5-turbo") -> None:
+        """
+        Creates a new agent and stores it in the agents dictionary.
 
-    try:
-        del agents[int(key)]
-        return True
-    except KeyError:
-        return False
+        :param agent_name: A unique name for the agent.
+        :param model: The name of the GPT model to be used by the agent (default: "gpt-3.5-turbo").
+        """
+        self.agents[agent_name] = Agent(model)
+
+    def send_message(self, agent_name: str, message: str) -> str:
+        """
+        Sends a message to the specified agent and returns its response.
+
+        :param agent_name: The name of the agent to send the message to.
+        :param message: The message to be sent to the agent.
+        :return: The agent's response.
+        :raises ValueError: If the agent is not found.
+        """
+        agent = self.agents.get(agent_name)
+        if agent is not None:
+            agent.add_message("system", message)
+            response = openai.ChatCompletion.create(
+                model=agent.model,
+                messages=agent.get_history(),
+                max_tokens=150
+            )
+            agent_response = response.choices[0].text.strip()
+            agent.add_message("assistant", agent_response)
+            return agent_response
+        else:
+            raise ValueError(f"Agent '{agent_name}' not found.")
+
+    def list_agents(self) -> List[str]:
+        """
+        Returns a list of agent names.
+
+        :return: A list of agent names.
+        """
+        return list(self.agents.keys())
+
+    def delete_agent(self, agent_name: str) -> None:
+        """
+        Deletes an agent from the agents dictionary.
+
+        :param agent_name: The name of the agent to be deleted.
+        :raises ValueError: If the agent is not found.
+        """
+        if agent_name in self.agents:
+            del self.agents[agent_name]
+        else:
+            raise ValueError(f"Agent '{agent_name}' not found.")
