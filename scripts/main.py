d05146c6c5766a17d7b40b932d3de4056e142123
Andres Caicedo <Andrezz1997@gmail.com>
2023-04-09 15:48:17 +0200

    Delete unrelated changes with PR

--- scripts/main.py
+++ scripts/main.py
@@ -170,10 +170,10 @@ def construct_prompt():
             f"Would you like me to return to being {config.ai_name}?",
             speak_text=True)
         should_continue = input(f"""Continue with the last settings? 
-            Name:  {config.ai_name}
-            Role:  {config.ai_role}
-            Goals: {config.ai_goals}  
-            Continue (y/n): """)        
+Name:  {config.ai_name}
+Role:  {config.ai_role}
+Goals: {config.ai_goals}  
+Continue (y/n): """)        
         if should_continue.lower() == "n":
             config = AIConfig()
 
