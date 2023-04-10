90b7ebd032b1e161873c3ceb037faca9847f78af
Richard Beales <rich@richbeales.net>
2023-04-10 08:11:54 +0100

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -395,5 +395,5 @@ while True:
             chat.create_chat_message(
                 "system", "Unable to execute command"))
         print_to_console("SYSTEM: ", Fore.YELLOW, "Unable to execute command")
-
-
+        
+        
