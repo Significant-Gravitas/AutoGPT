4d0c8889f381a91920f9a5880e3fe32dccbc9a87
Mike M <mikeones42@gmail.com>
2023-04-10 18:00:13 -0500

    user_input prompt - request that the response should only be in JSON format

--- scripts/main.py
+++ scripts/main.py
@@ -316,7 +316,7 @@ full_message_history = []
 result = None
 next_action_count = 0
 # Make a constant:
-user_input = "Determine which next command to use, and respond using the format specified above:"
+user_input = "Determine which next command to use, respond exclusively in JSON and only JSON:"
 
 # Initialize memory and make sure it is empty.
 # this is particularly important for indexing and referencing pinecone memory
