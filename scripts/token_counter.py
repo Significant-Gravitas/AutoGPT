41b3e70bb1fa60dc477bf4876b35b4f69e9bff45
Phoebe Bright <phoebebright310@gmail.com>
2023-04-10 14:25:22 +0100

    Update token_counter.py
    Add additional clarification to error message as this is the first check of the model name.  An alternative approach would be to add a verification of the model before this is called.

--- scripts/token_counter.py
+++ scripts/token_counter.py
@@ -30,7 +30,7 @@ def count_message_tokens(messages : List[Dict[str, str]], model : str = "gpt-3.5
         tokens_per_message = 3
         tokens_per_name = 1
     else:
-        raise NotImplementedError(f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.""")
+        raise NotImplementedError(f"""model {model} is not a reconised model or num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.""")
     num_tokens = 0
     for message in messages:
         num_tokens += tokens_per_message
