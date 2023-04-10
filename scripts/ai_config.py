b84db4e558aaa065ecd19bc3f177dcaa7b031d01
Andy Melnikov <andy.melnikov@gmail.com>
2023-04-10 18:46:40 +0200

    Fix flake8 W293 and W391

--- scripts/ai_config.py
+++ scripts/ai_config.py
@@ -76,4 +76,3 @@ class AIConfig:
 
         full_prompt += f"\n\n{data.load_prompt()}"
         return full_prompt
-
--- scripts/chat.py
+++ scripts/chat.py
@@ -62,7 +62,7 @@ def chat_with_ai(
             """
             model = cfg.fast_llm_model # TODO: Change model from hardcode to argument
             # Reserve 1000 tokens for the response
-            
+
             if cfg.debug:
                 print(f"Token limit: {token_limit}")
             send_token_limit = token_limit - 1000
