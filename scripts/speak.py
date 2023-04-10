316ac16e0822909df51abf737e8ed77ba96fb8c8
Dylan N <65708398+bituq@users.noreply.github.com>
2023-04-10 16:44:01 +0200

    Remove voice_settings parameters
    I removed the voice_settings parameters as it was an irrelevant change.

--- scripts/speak.py
+++ scripts/speak.py
@@ -22,7 +22,7 @@ def eleven_labs_speech(text, voice_index=0):
     """Speak text using elevenlabs.io's API"""
     tts_url = "https://api.elevenlabs.io/v1/text-to-speech/{voice_id}".format(
         voice_id=voices[voice_index])
-    formatted_message = {"text": text, "voice_settings": {"stability": 0.05, "similarity_boost": 0.8}}
+    formatted_message = {"text": text}
     response = requests.post(
         tts_url, headers=tts_headers, json=formatted_message)
 
