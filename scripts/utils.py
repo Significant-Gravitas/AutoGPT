b3fa363417256585e24561fa36c51cb44312cef9
Sma Das <sd4598@rit.edu>
2023-04-10 11:31:43 -0400

    Added exit message on `KeyboardInterrupt`

--- scripts/utils.py
+++ scripts/utils.py
@@ -2,5 +2,7 @@ def clean_input(prompt: str=''):
     try:
         return input(prompt)
     except KeyboardInterrupt:
+        print("You interrupted Auto-GPT")
+        print("Quitting...")
         exit(0)
 
