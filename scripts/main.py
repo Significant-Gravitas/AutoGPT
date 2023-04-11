8aa0856663c6d3032a24b6e956ff9c66d0007baa
cs0lar <cristiano.solarino@brightminded.com>
2023-04-11 13:51:31 +0100

    fixed formatting

--- scripts/main.py
+++ scripts/main.py
@@ -415,3 +415,4 @@ while True:
             chat.create_chat_message(
                 "system", "Unable to execute command"))
         print_to_console("SYSTEM: ", Fore.YELLOW, "Unable to execute command")
+
