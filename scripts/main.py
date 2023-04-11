b19eb74874a91b0c8e372ffb37d34e833de07cad
Alrik Olson <10505065+AlrikOlson@users.noreply.github.com>
2023-04-11 09:09:59 -0700

    Refactor the seed prompt to be generated programmatically
    This removes the tedium of having to re-number every numbered item in the prompt.txt if you want to add/remove commands.

--- scripts/main.py
+++ scripts/main.py
@@ -18,6 +18,7 @@ import traceback
 import yaml
 import argparse
 import logging
+from prompt import get_prompt
 
 cfg = Config()
 
@@ -171,7 +172,7 @@ def load_variables(config_file="config.yaml"):
     with open(config_file, "w") as file:
         documents = yaml.dump(config, file)
 
-    prompt = data.load_prompt()
+    prompt = get_prompt()
     prompt_start = """Your decisions must always be made independently without seeking user assistance. Play to your strengths as an LLM and pursue simple strategies with no legal complications."""
 
     # Construct full prompt
