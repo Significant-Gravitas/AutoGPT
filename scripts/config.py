f3b513d6b55a12a4e5963a64bf0007dda53f820b
kinance <kinance@gmail.com>
2023-04-11 19:26:23 +0900

    Remove duplicates of set debug mode func

--- scripts/config.py
+++ scripts/config.py
@@ -89,9 +89,6 @@ class Config(metaclass=Singleton):
         """Set the speak mode value."""
         self.speak_mode = value
 
-    def set_debug_mode(self, value: bool):
-        self.debug_mode = value
-
     def set_fast_llm_model(self, value: str):
         """Set the fast LLM model value."""
         self.fast_llm_model = value
