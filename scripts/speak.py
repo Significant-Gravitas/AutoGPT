0cf790b633b417c092f84b52715f5ec057c045aa
meta-fx <nmd.devs.1@gmail.com>
2023-04-10 20:00:43 -0500

    Added new env variable and speech function for alternative TTS voice

--- scripts/speak.py
+++ scripts/speak.py
@@ -1,9 +1,9 @@
+import gtts
 import os
 from playsound import playsound
 import requests
 from config import Config
 cfg = Config()
-import gtts
 
 
 # TODO: Nicer names for these ids
@@ -13,6 +13,7 @@ tts_headers = {
     "Content-Type": "application/json",
     "xi-api-key": cfg.elevenlabs_api_key
 }
+
 
 def eleven_labs_speech(text, voice_index=0):
     """Speak text using elevenlabs.io's API"""
@@ -33,23 +34,46 @@ def eleven_labs_speech(text, voice_index=0):
         print("Response content:", response.content)
         return False
 
+
+def brian_speech(text):
+    """Speak text using Brian with the streamelements API"""
+    tts_url = f"https://api.streamelements.com/kappa/v2/speech?voice=Brian&text={text}"
+    response = requests.get(tts_url)
+
+    if response.status_code == 200:
+        with open("speech.mp3", "wb") as f:
+            f.write(response.content)
+        playsound("speech.mp3")
+        os.remove("speech.mp3")
+        return True
+    else:
+        print("Request failed with status code:", response.status_code)
+        print("Response content:", response.content)
+        return False
+
+
 def gtts_speech(text):
     tts = gtts.gTTS(text)
     tts.save("speech.mp3")
     playsound("speech.mp3")
     os.remove("speech.mp3")
 
+
 def macos_tts_speech(text):
     os.system(f'say "{text}"')
+
 
 def say_text(text, voice_index=0):
     if not cfg.elevenlabs_api_key:
         if cfg.use_mac_os_tts == 'True':
             macos_tts_speech(text)
+        elif cfg.use_brian_tts == 'True':
+            success = brian_speech(text)
+            if not success:
+                gtts_speech(text)
         else:
             gtts_speech(text)
     else:
         success = eleven_labs_speech(text, voice_index)
         if not success:
             gtts_speech(text)
-
