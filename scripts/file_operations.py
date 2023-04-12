b7d2acdcbee2fba330fcbde75ca50274f1dd4b0f
Gull Man <gullmann@gmail.com>
2023-04-12 12:13:18 +0200

    add encoding to open file

--- scripts/file_operations.py
+++ scripts/file_operations.py
@@ -38,7 +38,7 @@ def write_to_file(filename, text):
         directory = os.path.dirname(filepath)
         if not os.path.exists(directory):
             os.makedirs(directory)
-        with open(filepath, "w") as f:
+        with open(filepath, "w", encoding='utf-8') as f:
             f.write(text)
         return "File written to successfully."
     except Exception as e:
