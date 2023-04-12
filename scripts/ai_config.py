b0cfa42cfb0c8edf2665108835657c0c76ab8ad5
profound <cairunqiang@gmail.com>
2023-04-12 23:38:30 +0800

    fix read config file encoding that broke Chinese

--- scripts/ai_config.py
+++ scripts/ai_config.py
@@ -46,7 +46,7 @@ class AIConfig:
         """
 
         try:
-            with open(config_file) as file:
+            with open(config_file, encoding='utf-8') as file:
                 config_params = yaml.load(file, Loader=yaml.FullLoader)
         except FileNotFoundError:
             config_params = {}
