096a3e874bb432f02413fafc73511df8635f6492
Warren Moxley <warren@reliablegun.com>
2023-04-12 01:54:47 -0700

    This is a fix for the missing debug command line argument processing. Without this, you can't enable debug mode to see what Auto-GPT is saying with GPT. It seems like at one point someone went to remove 1, because there were 2, and both got removed.

--- scripts/main.py
+++ scripts/main.py
@@ -297,7 +297,9 @@ def parse_arguments():
         print_to_console("GPT4 Only Mode: ", Fore.GREEN, "ENABLED")
         cfg.set_fast_llm_model(cfg.smart_llm_model)
 
-
+    if args.debug:
+        print_to_console("Debug Mode: ", Fore.GREEN, "ENABLED")
+        cfg.set_debug_mode(True)
 
 # TODO: fill in llm values here
 check_openai_api_key()
