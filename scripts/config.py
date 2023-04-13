a0cffead3a2801ef4460f289f07be75aac7f42a4
Eesa Hamza <eesa.hamza@gmail.com>
2023-04-13 14:02:42 +0300

    Implemented the '--ai-settings' flag

--- scripts/config.py
+++ scripts/config.py
@@ -39,6 +39,7 @@ class Config(metaclass=Singleton):
         self.speak_mode = False
         self.skip_reprompt = False
 
+        self.ai_settings_file = os.getenv("AI_SETTINGS_FILE", "ai_settings.yaml")
         self.fast_llm_model = os.getenv("FAST_LLM_MODEL", "gpt-3.5-turbo")
         self.smart_llm_model = os.getenv("SMART_LLM_MODEL", "gpt-4")
         self.fast_token_limit = int(os.getenv("FAST_TOKEN_LIMIT", 4000))
