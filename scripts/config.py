6471adfbe444ad2ab68626a3e418ebe95cb1bec5
Bernhard Mueller <bernhardmueller@multipoarneuron.lan>
2023-04-13 12:43:25 +0700

    Fix linter errors

--- scripts/config.py
+++ scripts/config.py
@@ -46,7 +46,7 @@ class Config(metaclass=Singleton):
         self.use_azure = False
         self.use_azure = os.getenv("USE_AZURE") == 'True'
         self.execute_local_commands = os.getenv('EXECUTE_LOCAL_COMMANDS', 'False') == 'True'
-    
+
         if self.use_azure:
             self.openai_api_base = os.getenv("OPENAI_AZURE_API_BASE")
             self.openai_api_version = os.getenv("OPENAI_AZURE_API_VERSION")
