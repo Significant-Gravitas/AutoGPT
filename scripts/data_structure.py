887db5c0ad358889494f0410cf6cd167ebe985ea
Kenny <88726944+Nnnsightnnn@users.noreply.github.com>
2023-04-13 18:58:32 +0000

    modified:   scripts/data_structure.py 	new file:   scripts/database/restrict_act.txt 	new file:   scripts/database/work_week.txt 	modified:   scripts/llm_handler.py 	modified:   scripts/static/styles.css 	new file:   scripts/templates/index.html

--- scripts/data_structure.py
+++ scripts/data_structure.py
@@ -1,47 +1,47 @@
-"""this module structures and splits the data then embeds it into Pinecone"""
 import os
+from typing import List, Union
 import pinecone
-import yaml
 from dotenv import load_dotenv
-# from config import Config
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 
-# cfg = Config()
 
 load_dotenv()
 
-def get_text_loader(filename, directory='scripts/static'):
-    """this fuction takes filename and searches a directory for file then loads with TextLoader"""
-    filepath = os.path.join(directory,filename)
-    if os.path.isfile(filepath):
-        return TextLoader(filepath, encoding='utf-8')
-    else:
-        return None
 
-# Utilize .yaml settings to find directory of database
-SETTINGS_FILE = "ai_settings.yaml"
+def get_text_loaders(directory='scripts/database'):
+    """this function searches a directory for files and returns a list of TextLoader objects"""
+    loaders = []
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+        if os.path.isfile(filepath):
+            loaders.append(TextLoader(filepath, encoding='utf-8'))
+    return loaders
 
-with open(SETTINGS_FILE, encoding='utf-8') as file:
-    config_params = yaml.load(file, Loader=yaml.FullLoader)
- 
-knowledgebase = config_params.get("knowledgebase", "")
-# Instance of TextLoader with the knowledgebase from yaml
-loader = get_text_loader(knowledgebase)
-# Produces a string to be split
-documents = loader.load()
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-document_chunks = text_splitter.split_documents(documents)
 
-#Can utilize config file better
 embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
 INDEX_NAME = "auto-gpt"
-
 pinecone.init(api_key=os.environ.get("PINECONE_API_KEY"), environment="us-central1-gcp")
 
-docsearch = Pinecone.from_documents(document_chunks, embeddings, index_name=INDEX_NAME)
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+
+docsearch = None
+
+loaders = get_text_loaders()
+
+# Iterate over each TextLoader object
+for loader in loaders:
+    documents = loader.load()
+    document_chunks = text_splitter.split_documents(documents)
+    
+    # Create or update Pinecone index
+    if docsearch is None:
+        docsearch = Pinecone.from_documents(document_chunks, embeddings, index_name=INDEX_NAME, upsert=True)
+    else:
+        docsearch.add_documents(document_chunks, upsert=True)
+
 
 def get_best_matching_document_content(question):
     """This function returns the best matching document content based on the user's query."""   
--- scripts/llm_handler.py
+++ scripts/llm_handler.py
@@ -2,7 +2,7 @@
 import os
 import openai
 from dotenv import load_dotenv
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from data_structure import get_best_matching_document_content
 
 load_dotenv()
@@ -20,11 +20,10 @@ def embed_text(elements):
 def generate_summary(document_content):
     """this function generates a summary from document content"""
     openai.api_key = os.environ.get("OPENAI_API_KEY")
-    llm = OpenAI(temperature=0.3, model_name='gpt-3.5-turbo', max_tokens=500, api_key=openai.api_key)
-
+    llm = ChatOpenAI(temperature=0.3, model_name='gpt-3.5-turbo', max_tokens=500, api_key=openai.api_key)
     summary_prompt = (f"Please provide a brief summary of the"
                       f"following information:\n{document_content}")
-    summary = llm(summary_prompt)
+    summary = llm.call_as_llm(summary_prompt)
     return summary
 
 
@@ -32,7 +31,7 @@ def communicate_with_llm(user_message):
     """Your code to communicate with the language model (e.g., GPT-4)"""
 #initialize llm
     openai.api_key = os.environ.get("OPENAI_API_KEY")
-    llm = OpenAI(temperature=0.5, model_name='gpt-3.5-turbo', max_tokens=500, api_key=openai.api_key)
+    llm = ChatOpenAI(temperature=0.3, model_name='gpt-3.5-turbo', max_tokens=500, api_key=openai.api_key)
 #create document content for llm
     document_content = get_best_matching_document_content(user_message)
 #logic for document content
@@ -40,7 +39,7 @@ def communicate_with_llm(user_message):
         summary = generate_summary(document_content)
         new_prompt = (f"{user_message}\n\nBased on the summary of the relevant information:"
                       f"\n{summary}\n\nPlease provide an informed and natural response: ")
-        response = llm(new_prompt)
+        response = llm.call_as_llm(new_prompt)
 #logic for no document content
     else:
         response = llm(user_message)
