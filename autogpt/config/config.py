a2590301c67b83fa706f22dc5f41ae55233379c7
Summit Suen <summit.suen@gmail.com>
2023-04-14 15:33:43 +0800

    Cleanup azure parameters in env.template and remove unused env in config.load_azure_config().

--- autogpt/config/config.py
+++ autogpt/config/config.py
@@ -127,15 +127,9 @@ class Config(metaclass=Singleton):
                 config_params = yaml.load(file, Loader=yaml.FullLoader)
         except FileNotFoundError:
             config_params = {}
-        self.openai_api_type = os.getenv(
-            "OPENAI_API_TYPE", config_params.get("azure_api_type", "azure")
-        )
-        self.openai_api_base = os.getenv(
-            "OPENAI_AZURE_API_BASE", config_params.get("azure_api_base", "")
-        )
-        self.openai_api_version = os.getenv(
-            "OPENAI_AZURE_API_VERSION", config_params.get("azure_api_version", "")
-        )
+        self.openai_api_type = config_params.get("azure_api_type") or "azure"
+        self.openai_api_base = config_params.get("azure_api_base") or ""
+        self.openai_api_version = config_params.get("azure_api_version") or "2023-03-15-preview"
         self.azure_model_to_deployment_id_map = config_params.get("azure_model_map", [])
 
     def set_continuous_mode(self, value: bool) -> None:
