17bb13e13c1e21a7493fd18cf23f71c1d461a3b9
cybersword112 <pembertoncy@gmail.com>
2023-04-14 20:15:21 -0700

    corrected the function return names for defining strings to pass to the the AI.

--- autogpt/ai_functions.py
+++ autogpt/ai_functions.py
@@ -19,7 +19,7 @@ def evaluate_code(code: str) -> List[str]:
             improve the code.
     """
 
-    function_string = "def analyze_code(code: str) -> List[str]:"
+    function_string = "def evaluate_code(code: str) -> List[str]:"
     args = [code]
     description_string = (
         "Analyzes the given code and returns a list of suggestions" " for improvements."
@@ -41,7 +41,7 @@ def improve_code(suggestions: List[str], code: str) -> str:
     """
 
     function_string = (
-        "def generate_improved_code(suggestions: List[str], code: str) -> str:"
+        "def improve_code(suggestions: List[str], code: str) -> str:"
     )
     args = [json.dumps(suggestions), code]
     description_string = (
@@ -66,7 +66,7 @@ def write_tests(code: str, focus: List[str]) -> str:
     """
 
     function_string = (
-        "def create_test_cases(code: str, focus: Optional[str] = None) -> str:"
+        "def write_tests(code: str, focus: Optional[str] = None) -> str:"
     )
     args = [code, json.dumps(focus)]
     description_string = (
