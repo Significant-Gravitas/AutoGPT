9eb34bd5886c2e84437c1d2346a79ef9314f57b5
BillSchumacher <34168009+BillSchumacher@users.noreply.github.com>
2023-04-15 18:09:35 -0500

    Add test.

--- autogpt/commands/git_operations.py
+++ autogpt/commands/git_operations.py
@@ -1,5 +1,5 @@
 """Git operations for autogpt"""
-import git
+from git.repo import Repo
 from autogpt.config import Config
 
 CFG = Config()
@@ -16,5 +16,5 @@ def clone_repository(repo_url: str, clone_path: str) -> str:
         str: The result of the clone operation"""
     split_url = repo_url.split("//")
     auth_repo_url = f"//{CFG.github_username}:{CFG.github_api_key}@".join(split_url)
-    git.Repo.clone_from(auth_repo_url, clone_path)
+    Repo.clone_from(auth_repo_url, clone_path)
     return f"""Cloned {repo_url} to {clone_path}"""
---
+++ tests/test_git_operations.py
@@ -0,0 +1,12 @@
+import unittest
+from autogpt.commands.git_operations import clone_repository
+
+
+class TestTokenCounter(unittest.TestCase):
+    def test_clone_repository(self):
+        result = clone_repository(
+            "https://github.com/BillSchumacher/Auto-GPT.git", "auto-gpt-cloned"
+        )
+        self.assertEqual(
+            result, 
+            "Cloned https://github.com/BillSchumacher/Auto-GPT.git to auto-gpt-cloned")
