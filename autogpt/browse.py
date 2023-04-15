923d379dc2e6055a3bf3bb74095e6684a00829f8
HDKiller <hdkiller@gmail.com>
2023-04-15 06:28:14 +0000

    remove "please" from prompt in browser.py

--- autogpt/browse.py
+++ autogpt/browse.py
@@ -145,9 +145,9 @@ def create_message(chunk, question):
     """Create a message for the user to summarize a chunk of text"""
     return {
         "role": "user",
-        "content": f'"""{chunk}""" Using the above text, please answer the following'
+        "content": f'"""{chunk}""" Using the above text, answer the following'
         f' question: "{question}" -- if the question cannot be answered using the'
-        " text, please summarize the text.",
+        " text, summarize the text.",
     }
 
 
