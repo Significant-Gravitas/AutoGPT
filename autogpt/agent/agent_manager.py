05bafb983844d09180d5f33753e53ca2472e0c0e
BillSchumacher <34168009+BillSchumacher@users.noreply.github.com>
2023-04-16 00:40:00 -0500

    Fix fstring bug.

--- autogpt/agent/agent_manager.py
+++ autogpt/agent/agent_manager.py
@@ -47,7 +47,8 @@ class AgentManager(metaclass=Singleton):
         for i, plugin in enumerate(self.cfg.plugins):
             plugin_result = plugin.on_instruction(messages)
             if plugin_result:
-                plugins_reply = f"{plugins_reply}{'' if not i else '\n'}{plugin_result}"
+                sep = '' if not i else '\n'
+                plugins_reply = f"{plugins_reply}{sep}{plugin_result}"
 
         if plugins_reply and plugins_reply != "":
             messages.append({"role": "assistant", "content": plugins_reply})
@@ -96,7 +97,8 @@ class AgentManager(metaclass=Singleton):
         for i, plugin in enumerate(self.cfg.plugins):
             plugin_result = plugin.on_instruction(messages)
             if plugin_result:
-                plugins_reply = f"{plugins_reply}{'' if not i else '\n'}{plugin_result}"
+                sep = '' if not i else '\n'
+                plugins_reply = f"{plugins_reply}{sep}{plugin_result}"
         # Update full message history
         if plugins_reply and plugins_reply != "":
             messages.append({"role": "assistant", "content": plugins_reply})
