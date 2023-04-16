7c89208ccd9cad91e94d4aa502a6db83a772b51f
Andres Caicedo <andres.felipe.caicedo.ultengo@outlook.com>
2023-04-16 16:31:52 +0200

    Update token_counter.py
    [TyPo]: Mistake in tiktoken call.

--- autogpt/token_counter.py
+++ autogpt/token_counter.py
@@ -25,7 +25,7 @@ def count_message_tokens(
         encoding = tiktoken.encoding_for_model(model)
     except KeyError:
         logger.warn("Warning: model not found. Using cl100k_base encoding.")
-        encoding = get_encoding("cl100k_base")
+        encoding = tiktoken.get_encoding("cl100k_base")
     if model == "gpt-3.5-turbo":
         # !Note: gpt-3.5-turbo may change over time.
         # Returning num tokens assuming gpt-3.5-turbo-0301.")
