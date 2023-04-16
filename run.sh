b969beda6fb1915647d7a490c6c346de4c3a4d02
Andres Caicedo <andres.felipe.caicedo.ultengo@outlook.com>
2023-04-16 19:51:42 +0200

    Update run.sh
    Introduces security checks to pip and venv modules.

--- run.sh
+++ run.sh
@@ -1,21 +1,50 @@
 #!/bin/bash
 
 # This script is used to run the main script in a virtual environment.
-if ! command -v python3 -m venv &> /dev/null    # Check if venv is installed and install it if not
-then
-echo "venv is not found. Installing venv..."
-sudo apt-get update
-sudo apt-get install python3-venv
-fi
 
-python3 -m venv venv # Create a virtual environment
-source env/bin/activate # Activate the virtual environment
+set_venv() {
+    if [ -d "venv" ]; then # Check if the virtual environment exists
+        source venv/bin/activate # Activate the virtual environment
+    else
+        echo "venv is not found. Creating venv..."
+        create_venv
+    fi
+}
+
+create_venv() {
+    python3 -m venv venv && # Create a virtual environment
+    source venv/bin/activate # Activate the virtual environment
+}
+
+{ # try
+    set_venv # Set the virtual environment
+} || { # catch
+    echo "An error occurred while creating the virtual environment."
+    echo "venv is not found. Installing venv..."
+    sudo apt-get update
+    sudo apt-get install python3-venv
+    set_venv
+}
 
 set -e # Exit immediately if a command exits with a non-zero status.
-python scripts/check_requirements.py requirements.txt # Check if all the required packages are installed
-if [ $? -eq 1 ]; then # If not, install them
-echo "Installing missing packages..."
-pip install -r requirements.txt
-fi
-python -m autogpt "$@" # Run the main script
+# Check if all the required packages are installed
+
+install_packages() {
+    echo "Installing missing packages..."
+    pip3 install -r requirements.txt
+}
+
+sudo apt install python3-pip
+{ # try
+    echo "Installing missing packages..."
+    install_packages # Install the required packages
+} || { # catch
+    echo "An error occurred while installing the required packages."
+    echo "Installing missing packages..."
+    sudo apt-get update
+    sudo apt-get install python3-pip
+    install_packages # Install the required packages
+}
+
+python3 -m autogpt "$@" # Run the main script
 read -n1 -rsp $'Press any key to continue...\n' # Wait for user input before exiting
