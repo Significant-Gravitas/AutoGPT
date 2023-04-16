9bb958dd7c9754bd104053ac5d91361babe2fc47
EthanJohnson97 <ethan.johnson1997@outlook.com>
2023-04-16 20:44:50 +0000

    Refactored Auth and API for future expansion

--- autogpt/commands/twitter.py
+++ autogpt/commands/twitter.py
@@ -4,19 +4,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+#Grab keys from .env file
+consumer_key = os.environ.get("TW_CONSUMER_KEY")
+consumer_secret = os.environ.get("TW_CONSUMER_SECRET")
+access_token = os.environ.get("TW_ACCESS_TOKEN")
+access_token_secret = os.environ.get("TW_ACCESS_TOKEN_SECRET")
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Create API object
+api = tweepy.API(auth)
+
 
 def send_tweet(tweet_text):
-    consumer_key = os.environ.get("TW_CONSUMER_KEY")
-    consumer_secret = os.environ.get("TW_CONSUMER_SECRET")
-    access_token = os.environ.get("TW_ACCESS_TOKEN")
-    access_token_secret = os.environ.get("TW_ACCESS_TOKEN_SECRET")
-    # Authenticate to Twitter
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-
-    # Create API object
-    api = tweepy.API(auth)
-
     # Send tweet
     try:
         api.update_status(tweet_text)
