8832cecec07966772cbe6533bd666fd94c71c9e5
Marcel Domingus <domingus@xs4all.nl>
2023-04-17 23:45:05 +0200

    Make headless Chrome more crash-resistent

--- autogpt/commands/web_selenium.py
+++ autogpt/commands/web_selenium.py
@@ -67,6 +67,10 @@ def scrape_text_with_selenium(url: str) -> tuple[WebDriver, str]:
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.49 Safari/537.36"
     )
 
+    if CFG.selenium_web_browser == "chrome":
+        options.add_argument('--headless')
+        options.add_argument('--disable-dev-shm-usage') # To reduce DevToolsActivePort errors
+
     if CFG.selenium_web_browser == "firefox":
         driver = webdriver.Firefox(
             executable_path=GeckoDriverManager().install(), options=options
