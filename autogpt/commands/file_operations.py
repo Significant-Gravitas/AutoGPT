370545835d2db87ac6285fbd6b163ce95191b9b4
CalCameron <cjcam97@ymail.com>
2023-04-16 19:14:10 -0500

    Logging File Changes, Append if duplicate write

--- autogpt/commands/file_operations.py
+++ autogpt/commands/file_operations.py
@@ -133,7 +133,8 @@ def write_to_file(filename: str, text: str) -> str:
         str: A message indicating success or failure
     """
     if check_duplicate_operation("write", filename):
-        return "Error: File has already been updated."
+        append_to_file(filename, text)
+        return "Append to file complete!"
     try:
         filepath = path_in_workspace(filename)
         directory = os.path.dirname(filepath)
