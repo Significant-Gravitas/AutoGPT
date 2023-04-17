f4eda398eeecb75477c7fec89952dc740616a834
Merwane Hamadi <merwanehamadi@gmail.com>
2023-04-17 13:47:38 -0700

    black

--- autogpt/utils.py
+++ autogpt/utils.py
@@ -32,7 +32,7 @@ def readable_file_size(size, decimal_places=2):
         size: Size in bytes
         decimal_places (int): Number of decimal places to display
     """
-    for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
+    for unit in ["B", "KB", "MB", "GB", "TB"]:
         if size < 1024.0:
             break
         size /= 1024.0
