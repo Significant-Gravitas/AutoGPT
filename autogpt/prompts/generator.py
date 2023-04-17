90920b331b4adba7ce2f8ccebb2e2d5cc63eeb1d
BillSchumacher <34168009+BillSchumacher@users.noreply.github.com>
2023-04-16 22:51:39 -0500

    blacked

--- autogpt/prompts/generator.py
+++ autogpt/prompts/generator.py
@@ -123,7 +123,9 @@ class PromptGenerator:
             command_strings = []
             if self.command_registry:
                 command_strings += [
-                    str(item) for item in self.command_registry.commands.values() if item.enabled
+                    str(item)
+                    for item in self.command_registry.commands.values()
+                    if item.enabled
                 ]
             # These are the commands that are added manually, do_nothing and terminate
             command_strings += [self._generate_command_string(item) for item in items]
