d5527d846b7281db14d2fb7bdf33f803b008d6ab
Reinier van der Leer <github@pwuts.nl>
2023-04-18 16:15:37 +0200

    consolidate browser settings

--- autogpt/commands/web_selenium.py
+++ autogpt/commands/web_selenium.py
@@ -83,7 +83,7 @@ def scrape_text_with_selenium(url: str) -> tuple[WebDriver, str]:
             options.add_argument("--remote-debugging-port=9222")
 
         options.add_argument("--no-sandbox")
-        if CFG.headless_browser:
+        if CFG.selenium_headless:
             options.add_argument("--headless")
             options.add_argument("--disable-gpu")
 
