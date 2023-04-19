3f7f090ff931541eb17dd291f77e48e4b00dc172
Toran Bruce Richards <toran.richards@gmail.com>
2023-04-19 20:47:17 +1200

    Add path sanitization to prevent path traversal
    - Added sanitize_path function to remove NULL bytes, prevent use of '..', and avoid symbolic links in user-provided paths.
    - Modified safe_path_join to sanitize input paths and compare real paths to ensure no path traversal occurs.

--- autogpt/workspace.py
+++ autogpt/workspace.py
@@ -37,11 +37,45 @@ def safe_path_join(base: Path, *paths: str | Path) -> Path:
     Returns:
         Path: The joined path
     """
-    joined_path = base.joinpath(*paths).resolve()
+    sanitized_paths = [sanitize_path(path) for path in paths]
+    joined_path = base.joinpath(*sanitized_paths).resolve(strict=False)
+    real_joined_path = Path(os.path.realpath(joined_path))
 
-    if CFG.restrict_to_workspace and not joined_path.is_relative_to(base):
+    if CFG.restrict_to_workspace and not real_joined_path.is_relative_to(base):
         raise ValueError(
-            f"Attempted to access path '{joined_path}' outside of workspace '{base}'."
+            f"Attempted to access path '{real_joined_path}' outside of workspace '{base}'."
         )
 
-    return joined_path
+    return real_joined_path
+
+def sanitize_path(path: str | Path) -> str:
+    """Sanitize user-provided path to prevent path traversal.
+
+    Args:
+        path (str | Path): The path to sanitize
+
+    Returns:
+        str: The sanitized path
+    """
+    if '\x00' in str(path):
+        raise ValueError(f"Path contains NULL bytes: '{path}'")
+
+    sanitized_path = os.path.normpath(str(path))
+    if '..' in sanitized_path or os.path.isabs(sanitized_path):
+        raise ValueError(f"Invalid relative path '{sanitized_path}' detected.")
+
+    # Check for symbolic links in the joined path
+    try:
+        # Join the input path to the current working directory before calling os.path.realpath()
+        real_path = os.path.realpath(os.path.join(os.getcwd(), sanitized_path))
+        if os.path.islink(real_path):
+            raise ValueError(f"Symbolic link detected in path '{sanitized_path}'")
+    except OSError:
+        pass
+
+    # Ensure the path does not contain symbolic links
+    path_parts = Path(sanitized_path).parts
+    if any(os.path.islink(part) for part in path_parts):
+        raise ValueError(f"Symbolic link detected in path '{sanitized_path}'")
+
+    return sanitized_path
