21c8e78d4bf4eda3b1bdd572ce606ac36875a25a
Mohamad Zamini <32536264+mzamini92@users.noreply.github.com>
2023-04-18 21:11:38 -0600

    Update base.py
    Use static methods instead of instance methods for the MemoryProviderSingleton class. Since MemoryProviderSingleton is an abstract class, it doesn't make sense to create an instance of it. Instead, you can define its methods as static methods that can be called without instantiating the class. This will reduce unnecessary memory usage and improve performance.
    
    Cache the results of get_ada_embedding() function. Since the get_ada_embedding() function is called repeatedly, it can be a good idea to cache its results to avoid making redundant API calls. One way to do this is to use a dictionary to store the embeddings with the text as the key.
    
    Use list comprehension instead of for loops. List comprehension can be a more efficient way to create a list than using a for loop with an append() statement.
    
    Use f-strings instead of string concatenation. F-strings can make the code more readable and faster than string concatenation.

--- autogpt/memory/base.py
+++ autogpt/memory/base.py
@@ -6,38 +6,44 @@ import openai
 from autogpt.config import AbstractSingleton, Config
 
 cfg = Config()
-
+embedding_cache = {}
 
 def get_ada_embedding(text):
     text = text.replace("\n", " ")
-    if cfg.use_azure:
-        return openai.Embedding.create(
-            input=[text],
-            engine=cfg.get_azure_deployment_id_for_model("text-embedding-ada-002"),
-        )["data"][0]["embedding"]
-    else:
-        return openai.Embedding.create(input=[text], model="text-embedding-ada-002")[
-            "data"
-        ][0]["embedding"]
-
+    if text not in embedding_cache:
+        if cfg.use_azure:
+            embedding_cache[text] = openai.Embedding.create(
+                input=[text],
+                engine=cfg.get_azure_deployment_id_for_model("text-embedding-ada-002"),
+            )["data"][0]["embedding"]
+        else:
+            embedding_cache[text] = openai.Embedding.create(
+                input=[text], model="text-embedding-ada-002"
+            )["data"][0]["embedding"]
+    return embedding_cache[text]
 
 class MemoryProviderSingleton(AbstractSingleton):
+    @staticmethod
     @abc.abstractmethod
-    def add(self, data):
+    def add(data):
         pass
 
+    @staticmethod
     @abc.abstractmethod
-    def get(self, data):
+    def get(data):
         pass
 
+    @staticmethod
     @abc.abstractmethod
-    def clear(self):
+    def clear():
         pass
 
+    @staticmethod
     @abc.abstractmethod
-    def get_relevant(self, data, num_relevant=5):
+    def get_relevant(data, num_relevant=5):
         pass
 
+    @staticmethod
     @abc.abstractmethod
-    def get_stats(self):
+    def get_stats():
         pass
