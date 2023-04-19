eb03bb3336597baaf35fd07f752e287a40539871
shashankboosi <shashank1872@gmail.com>
2023-04-19 16:33:48 +1000

    Remove the duplicate command as it is already declared in the end, and also to save token space

--- autogpt/prompt.py
+++ autogpt/prompt.py
@@ -1,6 +1,5 @@
 from colorama import Fore
 
-from autogpt.config import Config
 from autogpt.config.ai_config import AIConfig
 from autogpt.config.config import Config
 from autogpt.logs import logger
@@ -85,7 +84,6 @@ def get_prompt() -> str:
             {"code": "<full_code_string>", "focus": "<list_of_focus_areas>"},
         ),
         ("Execute Python File", "execute_python_file", {"file": "<file>"}),
-        ("Task Complete (Shutdown)", "task_complete", {"reason": "<reason>"}),
         ("Generate Image", "generate_image", {"prompt": "<prompt>"}),
         ("Send Tweet", "send_tweet", {"text": "<text>"}),
     ]
