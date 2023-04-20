30799c02a26d3ce870d051942135a17b2b2891dc
Robert Luke <code@robertluke.net>
2023-04-20 15:15:47 +1000

    Add embedding completion cost

--- autogpt/api_manager.py
+++ autogpt/api_manager.py
@@ -15,7 +15,7 @@ COSTS = {
     "gpt-3.5-turbo-0301": {"prompt": 0.002, "completion": 0.002},
     "gpt-4-0314": {"prompt": 0.03, "completion": 0.06},
     "gpt-4": {"prompt": 0.03, "completion": 0.06},
-    "text-embedding-ada-002": {"prompt_tokens": 0.0004}
+    "text-embedding-ada-002": {"prompt": 0.0004, "completion": 0.0}
 }
 
 
