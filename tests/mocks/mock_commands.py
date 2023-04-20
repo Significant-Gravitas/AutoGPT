40ce19582f9667d825e056565833ddcd2c29f1d6
Andres Caicedo <andres.felipe.caicedo.ultengo@outlook.com>
2023-04-20 20:58:38 +0200

    Update mock_commands.py

--- tests/mocks/mock_commands.py
+++ tests/mocks/mock_commands.py
@@ -3,4 +3,6 @@ from autogpt.commands.command import command
 
 @command("function_based", "Function-based test command")
 def function_based(arg1: int, arg2: str) -> str:
+    """Test function-based command."""
+    # Return a string with the two arguments separated by a dash
     return f"{arg1} - {arg2}"
