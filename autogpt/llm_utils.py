999990b614f9c5e32dbfb560ab9516755d212884
BillSchumacher <34168009+BillSchumacher@users.noreply.github.com>
2023-04-20 01:15:46 -0500

    Revert "Merge branch 'master' into stable"
    This reverts commit c4008971f7a85c2a82744aa6fd940de44b137591, reversing
    changes made to fe855fef13156aaa22153baaa953f55e0fdb6294.

--- autogpt/llm_utils.py
+++ autogpt/llm_utils.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import time
-from typing import List, Optional
+from ast import List
 
 import openai
 from colorama import Fore, Style
@@ -9,7 +9,6 @@ from openai.error import APIError, RateLimitError
 
 from autogpt.config import Config
 from autogpt.logs import logger
-from autogpt.types.openai import Message
 
 CFG = Config()
 
@@ -38,8 +37,8 @@ def call_ai_function(
     # For each arg, if any are None, convert to "None":
     args = [str(arg) if arg is not None else "None" for arg in args]
     # parse args to comma separated string
-    args: str = ", ".join(args)
-    messages: List[Message] = [
+    args = ", ".join(args)
+    messages = [
         {
             "role": "system",
             "content": f"You are now the following python function: ```# {description}"
@@ -54,15 +53,15 @@ def call_ai_function(
 # Overly simple abstraction until we create something better
 # simple retry mechanism when getting a rate error or a bad gateway
 def create_chat_completion(
-    messages: List[Message],  # type: ignore
-    model: Optional[str] = None,
+    messages: list,  # type: ignore
+    model: str | None = None,
     temperature: float = CFG.temperature,
-    max_tokens: Optional[int] = None,
+    max_tokens: int | None = None,
 ) -> str:
     """Create a chat completion using the OpenAI API
 
     Args:
-        messages (List[Message]): The messages to send to the chat completion
+        messages (list[dict[str, str]]): The messages to send to the chat completion
         model (str, optional): The model to use. Defaults to None.
         temperature (float, optional): The temperature to use. Defaults to 0.9.
         max_tokens (int, optional): The max tokens to use. Defaults to None.
@@ -70,28 +69,15 @@ def create_chat_completion(
     Returns:
         str: The response from the chat completion
     """
+    response = None
     num_retries = 10
     warned_user = False
     if CFG.debug_mode:
         print(
-            f"{Fore.GREEN}Creating chat completion with model {model}, temperature {temperature}, max_tokens {max_tokens}{Fore.RESET}"
+            Fore.GREEN
+            + f"Creating chat completion with model {model}, temperature {temperature},"
+            f" max_tokens {max_tokens}" + Fore.RESET
         )
-    for plugin in CFG.plugins:
-        if plugin.can_handle_chat_completion(
-            messages=messages,
-            model=model,
-            temperature=temperature,
-            max_tokens=max_tokens,
-        ):
-            message = plugin.handle_chat_completion(
-                messages=messages,
-                model=model,
-                temperature=temperature,
-                max_tokens=max_tokens,
-            )
-            if message is not None:
-                return message
-    response = None
     for attempt in range(num_retries):
         backoff = 2 ** (attempt + 2)
         try:
@@ -114,7 +100,8 @@ def create_chat_completion(
         except RateLimitError:
             if CFG.debug_mode:
                 print(
-                    f"{Fore.RED}Error: ", f"Reached rate limit, passing...{Fore.RESET}"
+                    Fore.RED + "Error: ",
+                    f"Reached rate limit, passing..." + Fore.RESET,
                 )
             if not warned_user:
                 logger.double_check(
@@ -123,14 +110,16 @@ def create_chat_completion(
                 )
                 warned_user = True
         except APIError as e:
-            if e.http_status != 502:
+            if e.http_status == 502:
+                pass
+            else:
                 raise
             if attempt == num_retries - 1:
                 raise
         if CFG.debug_mode:
             print(
-                f"{Fore.RED}Error: ",
-                f"API Bad gateway. Waiting {backoff} seconds...{Fore.RESET}",
+                Fore.RED + "Error: ",
+                f"API Bad gateway. Waiting {backoff} seconds..." + Fore.RESET,
             )
         time.sleep(backoff)
     if response is None:
@@ -145,12 +134,8 @@ def create_chat_completion(
             raise RuntimeError(f"Failed to get response after {num_retries} retries")
         else:
             quit(1)
-    resp = response.choices[0].message["content"]
-    for plugin in CFG.plugins:
-        if not plugin.can_handle_on_response():
-            continue
-        resp = plugin.on_response(resp)
-    return resp
+
+    return response.choices[0].message["content"]
 
 
 def create_embedding_with_ada(text) -> list:
@@ -173,13 +158,15 @@ def create_embedding_with_ada(text) -> list:
         except RateLimitError:
             pass
         except APIError as e:
-            if e.http_status != 502:
+            if e.http_status == 502:
+                pass
+            else:
                 raise
             if attempt == num_retries - 1:
                 raise
         if CFG.debug_mode:
             print(
-                f"{Fore.RED}Error: ",
-                f"API Bad gateway. Waiting {backoff} seconds...{Fore.RESET}",
+                Fore.RED + "Error: ",
+                f"API Bad gateway. Waiting {backoff} seconds..." + Fore.RESET,
             )
         time.sleep(backoff)
--- tests/mocks/mock_commands.py
+++
@@ -1,6 +0,0 @@
-from autogpt.commands.command import command
-
-
-@command("function_based", "Function-based test command")
-def function_based(arg1: int, arg2: str) -> str:
-    return f"{arg1} - {arg2}"
