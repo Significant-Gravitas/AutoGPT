87f2ecd93db930ad4cb9977726f5648d4da6e23d
Andres Caicedo <andres.felipe.caicedo.ultengo@outlook.com>
2023-04-20 21:25:01 +0200

    Update context.py

--- tests/context.py
+++ tests/context.py
@@ -1,3 +1,7 @@
-import sys, os 
+import os
+import sys
+
 # This is the path to the autogpt package.
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../scripts")))
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../scripts"))
+)
