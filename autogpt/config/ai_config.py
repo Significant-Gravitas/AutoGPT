abf66191bfd6d519116783cec01a6c6bcb4f480c
Mikel Calvo <admin@mikelcalvo.net>
2023-04-21 03:23:21 +0200

    Fixed lint issues

--- autogpt/config/ai_config.py
+++ autogpt/config/ai_config.py
@@ -5,9 +5,10 @@ A module that contains the AIConfig class object that contains the configuration
 from __future__ import annotations
 
 import os
-import platform
 from pathlib import Path
+from platform import platform
 from typing import Optional, Type
+
 import yaml
 
 from autogpt.prompts.generator import PromptGenerator
