6c5d21cbfc4b649bcdab13d6e3bb898bfd2e6576
coditamar <108689937+coditamar@users.noreply.github.com>
2023-04-21 13:24:26 +0300

    config.py: make load_dotenv(override=True) (#2788)

--- autogpt/config/config.py
+++ autogpt/config/config.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 
 from autogpt.config.singleton import Singleton
 
-load_dotenv(verbose=True)
+load_dotenv(verbose=True, override=True)
 
 
 class Config(metaclass=Singleton):
