21f191534213bcd5232834fd980aaf7e2ce2cbf2
EpicMo <1982742309@qq.com>
2023-04-22 23:56:18 +0800

    add env var and improve code

--- run.bat
+++ run.bat
@@ -1,11 +1,13 @@
 @echo off
-if not exist env_check.lock (
+SET EnvCheckLock=env_check.lock
+
+if not exist %EnvCheckLock% (
 	python scripts/check_requirements.py requirements.txt
 	if errorlevel 1 (
     	echo Installing missing packages...
     	pip install -r requirements.txt
 	)
-	echo lock => env_check.lock
+	echo lock > %EnvCheckLock%
 )
 python -m autogpt %*
 pause
--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-if [ ! -d "./env_check.lock" ];then
+EnvCheckLock=env_check.lock
+if [ ! -r $EnvCheckLock ];then
     python scripts/check_requirements.py requirements.txt
     if [ $? -eq 1 ]
     then
     echo Installing missing packages...
     pip install -r requirements.txt
     fi
-    touch env_check.lock
+    touch $EnvCheckLock
 fi
-python -m autogpt $@
+python -m autogpt "$@"
 read -p "Press any key to continue..."
