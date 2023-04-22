5d7fcb8c15909bec68e477c47de970f656c64a52
lengweiping1983 <lengweiping1983@163.com>
2023-04-22 13:26:21 +0800

    optimization validate_yaml_file function formatted and exception

--- autogpt/utils.py
+++ autogpt/utils.py
@@ -26,21 +26,27 @@ def validate_yaml_file(file: str):
         with open(file, encoding="utf-8") as fp:
             yaml.load(fp.read(), Loader=yaml.FullLoader)
     except FileNotFoundError:
-        return (False, f"The file {Fore.CYAN}`{file}`{Fore.RESET} wasn't found")
-    except yaml.YAMLError as e:
         return (
             False,
-            f"There was an issue while trying to read with your AI Settings file: {e}",
+            f"The file {Fore.CYAN}`{file}`{Fore.RESET} wasn't found!",
+        )
+    except Exception as e:
+        return (
+            False,
+            f"There was an issue while trying to read with the file {Fore.CYAN}`{file}`{Fore.RESET}: {e}",
         )
 
-    return (True, f"Successfully validated {Fore.CYAN}`{file}`{Fore.RESET}!")
+    return (
+        True,
+        f"Successfully validated {Fore.CYAN}`{file}`{Fore.RESET}!",
+    )
 
 
 def readable_file_size(size, decimal_places=2):
     """Converts the given size in bytes to a readable format.
     Args:
-        size: Size in bytes
-        decimal_places (int): Number of decimal places to display
+        size: Size in bytes.
+        decimal_places (int): Number of decimal places to display.
     """
     for unit in ["B", "KB", "MB", "GB", "TB"]:
         if size < 1024.0:
