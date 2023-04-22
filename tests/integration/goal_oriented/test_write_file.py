5f2d897426a3afe01675e1980b76592225d1e5b8
Merwane Hamadi <merwane.hamadi@redica.com>
2023-04-22 12:39:56 -0700

    add decorator to tests
    Signed-off-by: Merwane Hamadi <merwanehamadi@gmail.com>

--- tests/integration/goal_oriented/test_write_file.py
+++ tests/integration/goal_oriented/test_write_file.py
@@ -13,6 +13,7 @@ from autogpt.memory import get_memory
 
 # from autogpt.prompt import Prompt
 from autogpt.workspace import WORKSPACE_PATH
+from tests.integration.goal_oriented.decorators import requires_openai_api_key
 from tests.integration.goal_oriented.vcr_helper import before_record_request
 
 current_file_dir = os.path.dirname(os.path.abspath(__file__))
@@ -27,8 +28,11 @@ my_vcr = vcr.VCR(
 CFG = Config()
 
 
+@requires_openai_api_key
 @pytest.mark.integration_test
 def test_write_file() -> None:
+    # Your test code here
+
     # if file exist
     file_name = "hello_world.txt"
 
