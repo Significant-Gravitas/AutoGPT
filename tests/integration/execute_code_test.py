82fe25c3d07b7558bc084dae5df9635012552aa3
Reinier van der Leer <github@pwuts.nl>
2023-04-22 04:44:00 +0200

    Fix linting errors

--- tests/integration/execute_code_test.py
+++ tests/integration/execute_code_test.py
@@ -5,14 +5,15 @@ import tempfile
 from unittest import TestCase
 from unittest.mock import patch
 
-from autogpt.execute_code import execute_python_file
+from autogpt.commands.execute_code import execute_python_file
 
 
 class TestExecuteCode(TestCase):
-
     def test_execute_python_file(self):
-        random_string = ''.join(random.choice(string.ascii_lowercase) for _ in range(10))
-        with tempfile.NamedTemporaryFile(delete=True, suffix='.py') as temp_file:
+        random_string = "".join(
+            random.choice(string.ascii_lowercase) for _ in range(10)
+        )
+        with tempfile.NamedTemporaryFile(delete=True, suffix=".py") as temp_file:
             temp_file.write(str.encode(f"print('Hello {random_string}!')"))
             temp_file.flush()
             temp_file_path = pathlib.Path(temp_file.name).parent
