f462674e32279f383a4d34f2307709566c4a975f
Toran Bruce Richards <toran.richards@gmail.com>
2023-04-23 17:36:10 +1200

    Automatic prompting (#2896)
    * Add automatic ai prompting
    
    * Tweak the default prompt.
    
    * Print agent info upon creation.
    
    * Improve system prompt
    
    * Switch to fast_llm_model by default
    
    * Add format output command to user prompt.
    
    This vastly improves formatting success rate.
    
    * Add fallback to manual mode if llm output cannot be parsed (or other error).
    
    * Add unit test to cover ai creation setup.
    
    * Replace redundent prompt with manual mode instructions.
    
    * Add missing docstrings and typing.
    
    * Runs black on changes.
    
    * Runs isort
    
    * Update Python version and benchmark file in benchmark.yml
    
    * Refactor main function and imports in cli.py
    
    * Update import statement in ai_config.py
    
    * Add set_temperature and set_memory_backend methods in config.py
    
    * Remove unused import in prompt.py
    
    * Add goal oriented tasks workflow
    
    * Added agent_utils to create agent
    
    * added pytest and vcrpy
    
    * added write file cassette
    
    * created goal oriented task write file with cassettes to not pay openai tokens
    
    * solve conflicts
    
    * add ability set azure because github workflow needs it off
    
    * solve conflicts in cli.py
    
    * black because linter fails
    
    * solve conflict
    
    * setup github action to v3
    
    Signed-off-by: Merwane Hamadi <merwanehamadi@gmail.com>
    
    * fix conflicts
    
    Signed-off-by: Merwane Hamadi <merwanehamadi@gmail.com>
    
    * Plugins: debug line always printed in plugin load
    
    * add decorator to tests
    
    Signed-off-by: Merwane Hamadi <merwanehamadi@gmail.com>
    
    * move decorator higher up
    
    Signed-off-by: Merwane Hamadi <merwanehamadi@gmail.com>
    
    * merge
    
    ---------
    
    Signed-off-by: Merwane Hamadi <merwanehamadi@gmail.com>
    Co-authored-by: Merwane Hamadi <merwanehamadi@gmail.com>
    Co-authored-by: Merwane Hamadi <merwane.hamadi@redica.com>
    Co-authored-by: Richard Beales <rich@richbeales.net>
    Co-authored-by: Nicholas Tindle <nick@ntindle.com>
    Co-authored-by: BillSchumacher <34168009+BillSchumacher@users.noreply.github.com>

---
+++ tests/unit/test_setup.py
@@ -0,0 +1,71 @@
+import unittest
+from io import StringIO
+from unittest.mock import patch
+
+from autogpt.config.ai_config import AIConfig
+from autogpt.setup import (
+    generate_aiconfig_automatic,
+    generate_aiconfig_manual,
+    prompt_user,
+)
+
+
+class TestAutoGPT(unittest.TestCase):
+    def test_generate_aiconfig_automatic_default(self):
+        user_inputs = [""]
+        with patch("builtins.input", side_effect=user_inputs):
+            ai_config = prompt_user()
+
+        self.assertIsInstance(ai_config, AIConfig)
+        self.assertIsNotNone(ai_config.ai_name)
+        self.assertIsNotNone(ai_config.ai_role)
+        self.assertGreaterEqual(len(ai_config.ai_goals), 1)
+        self.assertLessEqual(len(ai_config.ai_goals), 5)
+
+    def test_generate_aiconfig_automatic_typical(self):
+        user_prompt = "Help me create a rock opera about cybernetic giraffes"
+        ai_config = generate_aiconfig_automatic(user_prompt)
+
+        self.assertIsInstance(ai_config, AIConfig)
+        self.assertIsNotNone(ai_config.ai_name)
+        self.assertIsNotNone(ai_config.ai_role)
+        self.assertGreaterEqual(len(ai_config.ai_goals), 1)
+        self.assertLessEqual(len(ai_config.ai_goals), 5)
+
+    def test_generate_aiconfig_automatic_fallback(self):
+        user_inputs = [
+            "T&GF£OIBECC()!*",
+            "Chef-GPT",
+            "an AI designed to browse bake a cake.",
+            "Purchase ingredients",
+            "Bake a cake",
+            "",
+        ]
+        with patch("builtins.input", side_effect=user_inputs):
+            ai_config = prompt_user()
+
+        self.assertIsInstance(ai_config, AIConfig)
+        self.assertEqual(ai_config.ai_name, "Chef-GPT")
+        self.assertEqual(ai_config.ai_role, "an AI designed to browse bake a cake.")
+        self.assertEqual(ai_config.ai_goals, ["Purchase ingredients", "Bake a cake"])
+
+    def test_prompt_user_manual_mode(self):
+        user_inputs = [
+            "--manual",
+            "Chef-GPT",
+            "an AI designed to browse bake a cake.",
+            "Purchase ingredients",
+            "Bake a cake",
+            "",
+        ]
+        with patch("builtins.input", side_effect=user_inputs):
+            ai_config = prompt_user()
+
+        self.assertIsInstance(ai_config, AIConfig)
+        self.assertEqual(ai_config.ai_name, "Chef-GPT")
+        self.assertEqual(ai_config.ai_role, "an AI designed to browse bake a cake.")
+        self.assertEqual(ai_config.ai_goals, ["Purchase ingredients", "Bake a cake"])
+
+
+if __name__ == "__main__":
+    unittest.main()
