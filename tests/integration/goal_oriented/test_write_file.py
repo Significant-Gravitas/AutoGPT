9bc61706e0476509785b57c39d750c5e1417c426
Jaz Garewal <jazgarewal@gmail.com>
2023-04-22 17:25:12 -0700

    Update test_write_file.py

--- tests/integration/goal_oriented/test_write_file.py
+++ tests/integration/goal_oriented/test_write_file.py
@@ -15,31 +15,6 @@ from autogpt.workspace import WORKSPACE_PATH
 from tests.integration.goal_oriented.vcr_helper import before_record_request
 from tests.utils import requires_api_key
 
-
-def replace_timestamp_in_request(request):
-    # Check if the request body contains a JSON object
-
-    try:
-        if not request.body:
-            return request
-        body = json.loads(request.body)
-    except ValueError:
-        return request
-
-    if "messages" not in body:
-        return request
-
-    for message in body["messages"]:
-        if "content" in message and "role" in message and message["role"] == "system":
-            timestamp_regex = re.compile(r"\w{3} \w{3} \d{2} \d{2}:\d{2}:\d{2} \d{4}")
-            message["content"] = timestamp_regex.sub(
-                "Tue Jan 01 00:00:00 2000", message["content"]
-            )
-
-    request.body = json.dumps(body)
-    return request
-
-
 current_file_dir = os.path.dirname(os.path.abspath(__file__))
 # tests_directory = os.path.join(current_file_dir, 'tests')
 
