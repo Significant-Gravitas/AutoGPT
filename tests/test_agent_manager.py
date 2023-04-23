d921c2d4cefd9ce47f454f55dd4c1a8325602d56
DaoAdvocate <12145726+rihp@users.noreply.github.com>
2023-04-23 12:19:31 +0200

    decorator for api key

--- tests/test_agent_manager.py
+++ tests/test_agent_manager.py
@@ -2,9 +2,7 @@ import unittest
 from typing import List
 
 from autogpt.agent.agent_manager import AgentManager
-from autogpt.config.config import Config
-from autogpt.llm_utils import create_chat_completion
-from autogpt.types.openai import Message
+from tests.utils import requires_api_key
 
 
 class TestAgentManager(unittest.TestCase):
@@ -16,6 +14,7 @@ class TestAgentManager(unittest.TestCase):
         )
         self.model = "gpt-4"
 
+    @requires_api_key("OPENAI_API_KEY")
     def test_create_agent(self):
         key, agent_reply = self.agent_manager.create_agent(
             self.task, self.prompt, self.model
@@ -24,18 +23,21 @@ class TestAgentManager(unittest.TestCase):
         self.assertIsInstance(agent_reply, str)
         self.assertIn(key, self.agent_manager.agents)
 
+    @requires_api_key("OPENAI_API_KEY")
     def test_message_agent(self):
         key, _ = self.agent_manager.create_agent(self.task, self.prompt, self.model)
         user_message = "Please translate 'Good morning' to French."
         agent_reply = self.agent_manager.message_agent(key, user_message)
         self.assertIsInstance(agent_reply, str)
 
+    @requires_api_key("OPENAI_API_KEY")
     def test_list_agents(self):
         key, _ = self.agent_manager.create_agent(self.task, self.prompt, self.model)
         agents_list = self.agent_manager.list_agents()
         self.assertIsInstance(agents_list, list)
         self.assertIn((key, self.task), agents_list)
 
+    @requires_api_key("OPENAI_API_KEY")
     def test_delete_agent(self):
         key, _ = self.agent_manager.create_agent(self.task, self.prompt, self.model)
         success = self.agent_manager.delete_agent(key)
