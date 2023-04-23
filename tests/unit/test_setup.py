d6d258c803faf7c4891ec5397134f66041c80280
James Collins <collijk@uw.edu>
2023-04-22 23:34:00 -0700

    Add api key requirements to new tests

--- tests/unit/test_setup.py
+++ tests/unit/test_setup.py
@@ -3,14 +3,12 @@ from io import StringIO
 from unittest.mock import patch
 
 from autogpt.config.ai_config import AIConfig
-from autogpt.setup import (
-    generate_aiconfig_automatic,
-    generate_aiconfig_manual,
-    prompt_user,
-)
+from autogpt.setup import generate_aiconfig_automatic, prompt_user
+from tests.utils import requires_api_key
 
 
 class TestAutoGPT(unittest.TestCase):
+    @requires_api_key("OPENAI_API_KEY")
     def test_generate_aiconfig_automatic_default(self):
         user_inputs = [""]
         with patch("builtins.input", side_effect=user_inputs):
@@ -22,6 +20,7 @@ class TestAutoGPT(unittest.TestCase):
         self.assertGreaterEqual(len(ai_config.ai_goals), 1)
         self.assertLessEqual(len(ai_config.ai_goals), 5)
 
+    @requires_api_key("OPENAI_API_KEY")
     def test_generate_aiconfig_automatic_typical(self):
         user_prompt = "Help me create a rock opera about cybernetic giraffes"
         ai_config = generate_aiconfig_automatic(user_prompt)
@@ -32,6 +31,7 @@ class TestAutoGPT(unittest.TestCase):
         self.assertGreaterEqual(len(ai_config.ai_goals), 1)
         self.assertLessEqual(len(ai_config.ai_goals), 5)
 
+    @requires_api_key("OPENAI_API_KEY")
     def test_generate_aiconfig_automatic_fallback(self):
         user_inputs = [
             "T&GF£OIBECC()!*",
@@ -49,6 +49,7 @@ class TestAutoGPT(unittest.TestCase):
         self.assertEqual(ai_config.ai_role, "an AI designed to browse bake a cake.")
         self.assertEqual(ai_config.ai_goals, ["Purchase ingredients", "Bake a cake"])
 
+    @requires_api_key("OPENAI_API_KEY")
     def test_prompt_user_manual_mode(self):
         user_inputs = [
             "--manual",
