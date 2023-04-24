576f9a125e0afe3f4331d02a7d25001b017f561f
Daniel Chen <daniel.young.chen@gmail.com>
2023-04-23 18:46:13 -0700

    Install plugin dependencies from check_requirements.py

--- scripts/check_requirements.py
+++ scripts/check_requirements.py
@@ -1,8 +1,16 @@
 import re
 import sys
+import os
+import zipfile
+from pathlib import Path
+import subprocess
+import sys
 
 import pkg_resources
 
+
+def _install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 def main():
     requirements_file = sys.argv[1]
@@ -12,6 +20,17 @@ def main():
         ]
 
     installed_packages = [package.key for package in pkg_resources.working_set]
+
+    # Plugin packages
+    plugins_dir = Path(os.getenv("PLUGINS_DIR", "plugins"))
+    for plugin in plugins_dir.glob("*.zip"):
+        with zipfile.ZipFile(str(plugin), "r") as zfile:
+            basedir = zfile.namelist()[0]
+            basereqs = os.path.join(basedir, 'requirements.txt')
+            required_packages += [
+                line.decode('utf-8').strip().split("#")[0].strip()
+                for line in zfile.open(basereqs).readlines()
+            ]
 
     missing_packages = []
     for package in required_packages:
@@ -24,6 +43,8 @@ def main():
     if missing_packages:
         print("Missing packages:")
         print(", ".join(missing_packages))
+        for package in missing_packages:
+            _install(package)
         sys.exit(1)
     else:
         print("All packages are installed.")
