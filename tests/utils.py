27742e9d871aa2672a220599b6f869ab10c435fc
Reinier van der Leer <github@pwuts.nl>
2023-04-24 21:26:35 +0200

    fix test_json_parser.py

--- tests/utils.py
+++ tests/utils.py
@@ -25,3 +25,10 @@ def skip_in_ci(test_function):
         os.environ.get("CI") == "true",
         reason="This test doesn't work on GitHub Actions.",
     )(test_function)
+
+def skip_if_dumb_llm(test_function):
+    """Skip a test if a very smart LLM is not available"""
+    return pytest.mark.skipif(
+        os.environ.get("SMART_LLM_MODEL") != "gpt-4",
+        reason="This test only works with GPT-4",
+    )(test_function)
