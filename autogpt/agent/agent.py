d4e172516d6186c1f854e988f94238dbb301f2ba
AbTrax <45964236+AbTrax@users.noreply.github.com>
2023-04-25 15:28:06 +1000

    Feature: Added Self Feedback (#3013)
    * Feature: Added Self Feedback
    
    * minor fix: complied to flake8
    
    * Add: Self Feedback To Usage.md
    
    * Add: role/goal allignment
    
    * Added: warning to usage.md
    
    * fix: Formatted with black
    
    ---------
    
    Co-authored-by: Richard Beales <rich@richbeales.net>

--- autogpt/agent/agent.py
+++ autogpt/agent/agent.py
@@ -1,3 +1,4 @@
+import yaml
 from colorama import Fore, Style
 
 from autogpt.app import execute_command, get_command
@@ -5,6 +6,7 @@ from autogpt.chat import chat_with_ai, create_chat_message
 from autogpt.config import Config
 from autogpt.json_utils.json_fix_llm import fix_json_using_multiple_techniques
 from autogpt.json_utils.utilities import validate_json
+from autogpt.llm_utils import create_chat_completion
 from autogpt.logs import logger, print_assistant_thoughts
 from autogpt.speech import say_text
 from autogpt.spinner import Spinner
@@ -129,8 +131,8 @@ class Agent:
                     f"ARGUMENTS = {Fore.CYAN}{arguments}{Style.RESET_ALL}",
                 )
                 print(
-                    "Enter 'y' to authorise command, 'y -N' to run N continuous "
-                    "commands, 'n' to exit program, or enter feedback for "
+                    "Enter 'y' to authorise command, 'y -N' to run N continuous commands, 's' to run self-feedback commands"
+                    "'n' to exit program, or enter feedback for "
                     f"{self.ai_name}...",
                     flush=True,
                 )
@@ -140,6 +142,24 @@ class Agent:
                     )
                     if console_input.lower().strip() == "y":
                         user_input = "GENERATE NEXT COMMAND JSON"
+                        break
+                    elif console_input.lower().strip() == "s":
+                        logger.typewriter_log(
+                            "-=-=-=-=-=-=-= THOUGHTS, REASONING, PLAN AND CRITICISM WILL NOW BE VERIFIED BY AGENT -=-=-=-=-=-=-=",
+                            Fore.GREEN,
+                            "",
+                        )
+                        thoughts = assistant_reply_json.get("thoughts", {})
+                        self_feedback_resp = self.get_self_feedback(thoughts)
+                        logger.typewriter_log(
+                            f"SELF FEEDBACK: {self_feedback_resp}",
+                            Fore.YELLOW,
+                            "",
+                        )
+                        if self_feedback_resp[0].lower().strip() == "y":
+                            user_input = "GENERATE NEXT COMMAND JSON"
+                        else:
+                            user_input = self_feedback_resp
                         break
                     elif console_input.lower().strip() == "":
                         print("Invalid input format.")
@@ -245,3 +265,33 @@ class Agent:
                         self.workspace.get_path(command_args[pathlike])
                     )
         return command_args
+
+    @staticmethod
+    def get_self_feedback(thoughts: dict) -> str:
+        """Generates a feedback response based on the provided thoughts dictionary.
+        This method takes in a dictionary of thoughts containing keys such as 'reasoning',
+        'plan', 'thoughts', and 'criticism'. It combines these elements into a single
+        feedback message and uses the create_chat_completion() function to generate a
+        response based on the input message.
+        Args:
+            thoughts (dict): A dictionary containing thought elements like reasoning,
+            plan, thoughts, and criticism.
+        Returns:
+            str: A feedback response generated using the provided thoughts dictionary.
+        """
+
+        with open("ai_settings.yaml", "r") as yaml_file:
+            parsed_yaml = yaml.safe_load(yaml_file)
+            ai_role = parsed_yaml["ai_role"]
+
+        feedback_prompt = f"Below is a message from an AI agent with the role of {ai_role}. Please review the provided Thought, Reasoning, Plan, and Criticism. If these elements accurately contribute to the successful execution of the assumed role, respond with the letter 'Y' followed by a space, and then explain why it is effective. If the provided information is not suitable for achieving the role's objectives, please provide one or more sentences addressing the issue and suggesting a resolution."
+        reasoning = thoughts.get("reasoning", "")
+        plan = thoughts.get("plan", "")
+        thought = thoughts.get("thoughts", "")
+        criticism = thoughts.get("criticism", "")
+        feedback_thoughts = thought + reasoning + plan + criticism
+        feedback_response = create_chat_completion(
+            [{"role": "user", "content": feedback_prompt + feedback_thoughts}],
+            "gpt-3.5-turbo",
+        )  # * This hardcodes the model to use GPT3.5. should be an argument
+        return feedback_response
