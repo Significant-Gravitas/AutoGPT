9744250b3979deea01947023d6efb5b594193cd5
jacobyoby <45674026+jacobyoby@users.noreply.github.com>
2023-04-25 19:36:11 -0400

    Update workspace.py
    Instead of using os.makedirs for creating the workspace directory if it doesn't exist,  use mkdir from the Path class like this: WORKSPACE_PATH.mkdir(parents=True, exist_ok=True).
    
    I added type hints to the functions to show what types of input they take and what they return.
    
    I included comments (docstrings) for each function to explain their actions
    
    I made a new function called is_path_within_workspace that checks if a path is inside the workspace

--- autogpt/workspace.py
+++ autogpt/workspace.py
@@ -11,18 +11,16 @@ CFG = Config()
 WORKSPACE_PATH = Path(os.getcwd()) / "auto_gpt_workspace"
 
 # Create the directory if it doesn't exist
-if not os.path.exists(WORKSPACE_PATH):
-    os.makedirs(WORKSPACE_PATH)
-
+WORKSPACE_PATH.mkdir(parents=True, exist_ok=True)
 
 def path_in_workspace(relative_path: str | Path) -> Path:
-    """Get full path for item in workspace
+    """Get full path for item in workspace.
 
-    Parameters:
-        relative_path (str | Path): Path to translate into the workspace
+    Args:
+        relative_path (str | Path): Path to translate into the workspace.
 
     Returns:
-        Path: Absolute path for the given path in the workspace
+        Path: Absolute path for the given path in the workspace.
     """
     return safe_path_join(WORKSPACE_PATH, relative_path)
 
@@ -31,18 +29,35 @@ def safe_path_join(base: Path, *paths: str | Path) -> Path:
     """Join one or more path components, asserting the resulting path is within the workspace.
 
     Args:
-        base (Path): The base path
-        *paths (str): The paths to join to the base path
+        base (Path): The base path.
+        *paths (str | Path): The paths to join to the base path.
 
     Returns:
-        Path: The joined path
+        Path: The joined path.
     """
     base = base.resolve()
     joined_path = base.joinpath(*paths).resolve()
 
-    if CFG.restrict_to_workspace and not joined_path.is_relative_to(base):
+    if CFG.restrict_to_workspace and not is_path_within_workspace(base, joined_path):
         raise ValueError(
             f"Attempted to access path '{joined_path}' outside of workspace '{base}'."
         )
 
     return joined_path
+
+
+def is_path_within_workspace(base: Path, path: Path) -> bool:
+    """Check if a given path is within the workspace.
+
+    Args:
+        base (Path): The base path (workspace path).
+        path (Path): The path to check.
+
+    Returns:
+        bool: True if the path is within the workspace, False otherwise.
+    """
+    try:
+        path.relative_to(base)
+        return True
+    except ValueError:
+        return False
