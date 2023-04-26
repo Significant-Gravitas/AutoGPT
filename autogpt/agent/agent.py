cd8fdb31efef2d6b755011f491863a17eead1bdd
WladBlank <wlad.c@me.com>
2023-04-26 22:08:39 +0200

    Chat plugin capability (#2929)
    Co-authored-by: BillSchumacher <34168009+BillSchumacher@users.noreply.github.com>

--- autogpt/agent/agent.py
+++ autogpt/agent/agent.py
@@ -9,7 +9,7 @@ from autogpt.llm_utils import create_chat_completion
 from autogpt.logs import logger, print_assistant_thoughts
 from autogpt.speech import say_text
 from autogpt.spinner import Spinner
-from autogpt.utils import clean_input
+from autogpt.utils import clean_input, send_chat_message_to_user
 from autogpt.workspace import Workspace
 
 
@@ -84,8 +84,11 @@ class Agent:
                 logger.typewriter_log(
                     "Continuous Limit Reached: ", Fore.YELLOW, f"{cfg.continuous_limit}"
                 )
+                send_chat_message_to_user(
+                    f"Continuous Limit Reached: \n {cfg.continuous_limit}"
+                )
                 break
-
+            send_chat_message_to_user("Thinking... \n")
             # Send message to AI, get response
             with Spinner("Thinking... "):
                 assistant_reply = chat_with_ai(
@@ -114,6 +117,8 @@ class Agent:
                     command_name, arguments = get_command(assistant_reply_json)
                     if cfg.speak_mode:
                         say_text(f"I want to execute {command_name}")
+
+                    send_chat_message_to_user("Thinking... \n")
                     arguments = self._resolve_pathlike_command_args(arguments)
 
                 except Exception as e:
@@ -123,6 +128,11 @@ class Agent:
                 # ### GET USER AUTHORIZATION TO EXECUTE COMMAND ###
                 # Get key press: Prompt the user to press enter to continue or escape
                 # to exit
+                self.user_input = ""
+                send_chat_message_to_user(
+                    "NEXT ACTION: \n " + f"COMMAND = {command_name} \n "
+                    f"ARGUMENTS = {arguments}"
+                )
                 logger.typewriter_log(
                     "NEXT ACTION: ",
                     Fore.CYAN,
@@ -136,9 +146,13 @@ class Agent:
                     flush=True,
                 )
                 while True:
-                    console_input = clean_input(
-                        Fore.MAGENTA + "Input:" + Style.RESET_ALL
-                    )
+                    console_input = ""
+                    if cfg.chat_messages_enabled:
+                        console_input = clean_input("Waiting for your response...")
+                    else:
+                        console_input = clean_input(
+                            Fore.MAGENTA + "Input:" + Style.RESET_ALL
+                        )
                     if console_input.lower().strip() == "y":
                         user_input = "GENERATE NEXT COMMAND JSON"
                         break
@@ -193,10 +207,16 @@ class Agent:
                         "",
                     )
                 elif user_input == "EXIT":
+                    send_chat_message_to_user("Exiting...")
                     print("Exiting...", flush=True)
                     break
             else:
                 # Print command
+                send_chat_message_to_user(
+                    "NEXT ACTION: \n " + f"COMMAND = {command_name} \n "
+                    f"ARGUMENTS = {arguments}"
+                )
+
                 logger.typewriter_log(
                     "NEXT ACTION: ",
                     Fore.CYAN,
