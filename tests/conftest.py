feef0812832996a3da09e0933a065fa49acbfd28
Merwane Hamadi <merwanehamadi@gmail.com>
2023-04-25 20:24:57 -0700

    use pytest-recording with VCR

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,7 @@ from autogpt.api_manager import ApiManager
 from autogpt.api_manager import api_manager as api_manager_
 from autogpt.config import Config
 from autogpt.workspace import Workspace
+from tests.vcr.openai_filter import before_record_request
 
 
 @pytest.fixture()
