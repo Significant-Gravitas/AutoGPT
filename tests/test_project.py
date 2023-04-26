7cfb3fc92a0573eec59901ca6d395b629e06bb20
ph-ausseil <ph.ausseil@gmail.com>
2023-04-26 08:55:22 +0200

    Unstable : More test cases

--- tests/test_project.py
+++ tests/test_project.py
@@ -3,6 +3,16 @@ from autogpt.config.project.agent.config import AgentConfig
 from autogpt.config.project import Project
 
 
+@pytest.fixture
+def lead_agent():
+    return AgentConfig(agent_name="Lead Agent", agent_role="Leader", agent_goals=["Goal1"])
+
+
+@pytest.fixture
+def delegated_agent():
+    return AgentConfig(agent_name="Delegated Agent", agent_role="Delegated", agent_goals=["Goal1"])
+
+
 def test_project_init():
     lead_agent = AgentConfig(
         agent_name="Agent1",
@@ -53,6 +63,36 @@ def test_project_to_dict():
     assert project_dict["delegated_agents"][0]["agent_role"] == "Role2"
     assert project_dict["delegated_agents"][0]["agent_goals"] == ["Goal3", "Goal4"]
 
+
+def test_project_with_fixture(lead_agent, delegated_agent):
+    project = Project("Test Project", 100.0, lead_agent, [delegated_agent])
+
+    assert project.project_name == "Test Project"
+    assert project.api_budget == 100.0
+    assert project.lead_agent == lead_agent
+    assert project.delegated_agents == [delegated_agent]
+
+
+def test_project_to_dict_with_fixture(lead_agent, delegated_agent):
+    project = Project("Test Project", 100.0, lead_agent, [delegated_agent])
+    project_dict = project.to_dict()
+
+    assert project_dict["project_name"] == "Test Project"
+    assert project_dict["api_budget"] == 100.0
+    assert project_dict["lead_agent"] == lead_agent.to_dict()
+    assert project_dict["delegated_agents"] == [delegated_agent.to_dict()]
+
+
+def test_project_without_delegated_agents(lead_agent):
+    project = Project("Test Project", 100.0, lead_agent)
+
+    assert project.project_name == "Test Project"
+    assert project.api_budget == 100.
+
+def test_project_str(lead_agent, delegated_agent):
+    project = Project("Test Project", 100.0, lead_agent, [delegated_agent])
+
+    assert str(project) == str(project.to_dict())
 
 if __name__ == "__main__":
     pytest.main([__file__])
--- tests/test_project_config_broker.py
+++ tests/test_project_config_broker.py
@@ -36,10 +36,42 @@ def test_create_project(project_config_broker):
     )
     assert result
 
+    # Test creating a project with the same ID without overwrite
+    with pytest.raises(ValueError):
+        project_config_broker.create_project(
+            project_id=0,
+            api_budget=1.0,
+            agent_name="Test Agent",
+            agent_role="Test Role",
+            agent_goals=["Test Goal 1", "Test Goal 2"],
+            prompt_generator="Test Prompt Generator",
+            command_registry="Test Command Registry",
+            overwrite=False,
+            project_name="Test Project"
+        )
+
+    # Test creating a project with an invalid ID
+    with pytest.raises(ValueError):
+        project_config_broker.create_project(
+            project_id=-1,
+            api_budget=1.0,
+            agent_name="Test Agent",
+            agent_role="Test Role",
+            agent_goals=["Test Goal 1", "Test Goal 2"],
+            prompt_generator="Test Prompt Generator",
+            command_registry="Test Command Registry",
+            overwrite=True,
+            project_name="Test Project"
+        )
+
 
 def test_set_project_number(project_config_broker):
     result = project_config_broker.set_project_number(new_project_id=0)
     assert result
+
+    # Test setting an invalid project number
+    with pytest.raises(ValueError):
+        project_config_broker.set_project_number(new_project_id=-1)
 
 
 def test_get_current_project_id(project_config_broker):
@@ -56,7 +88,24 @@ def test_get_project(project_config_broker):
     project = project_config_broker.get_project(project_number=0)
     assert isinstance(project, AgentConfig)
 
+    # Test getting a project with an invalid number
+    with pytest.raises(ValueError):
+        project_config_broker.get_project(project_number=-1)
+
 
 def test_get_projects(project_config_broker):
     projects = project_config_broker.get_projects()
     assert isinstance(projects, list)
+
+
+def test_delete_project(project_config_broker):
+    project_count = len(project_config_broker.get_projects())
+
+    # Test deleting a project
+    project_config_broker.delete_project(project_number=0)
+    new_project_count = len(project_config_broker.get_projects())
+    assert new_project_count == project_count - 1
+
+    # Test deleting a project with an invalid number
+    with pytest.raises(ValueError):
+        project_config_broker.delete_project(project_number=-1)
