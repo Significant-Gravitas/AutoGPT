96f972171cb7cce7422525320941f76f6885075c
Reinier van der Leer <github@pwuts.nl>
2023-04-27 19:16:56 +0200

    Fix imports, type hints and fixtures for goal oriented tests (#3415)

--- tests/integration/goal_oriented/test_write_file.py
+++ tests/integration/goal_oriented/test_write_file.py
@@ -1,27 +1,18 @@
-import os
-
 import pytest
 
+from autogpt.agent import Agent
 from autogpt.commands.file_operations import read_file
-from autogpt.config import Config
-from tests.integration.agent_factory import create_writer_agent
 from tests.integration.agent_utils import run_interaction_loop
 from tests.utils import requires_api_key
-
-CFG = Config()
 
 
 @requires_api_key("OPENAI_API_KEY")
 @pytest.mark.vcr
-def test_write_file(workspace) -> None:
-    CFG.workspace_path = workspace.root
-    CFG.file_logger_path = os.path.join(workspace.root, "file_logger.txt")
-
-    file_name = str(workspace.get_path("hello_world.txt"))
-    agent = create_writer_agent(workspace)
+def test_write_file(writer_agent: Agent) -> None:
+    file_path = str(writer_agent.workspace.get_path("hello_world.txt"))
     try:
-        run_interaction_loop(agent, 40)
+        run_interaction_loop(writer_agent, 40)
     # catch system exit exceptions
     except SystemExit:  # the agent returns an exception when it shuts down
-        content = read_file(file_name)
+        content = read_file(file_path)
         assert content == "Hello World", f"Expected 'Hello World', got {content}"
