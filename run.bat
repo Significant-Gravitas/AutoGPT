2788e955979dd99c91ca8c0e91665022988677f3
Dylan Lau <dylanlz@gmail.com>
2023-04-28 19:21:39 +0800

    Revert "Update run batch to support either python or python 3"
    This reverts commit a3d48b852f3a63bbdbfe3112352a82e0c51b33ac.

--- run.bat
+++ run.bat
@@ -1,15 +1,8 @@
 @echo off
-
-setlocal
-
-set PY_CMD=python3
-where %PY_CMD% >nul 2>nul || set PY_CMD=python
-
-%PY_CMD% scripts/check_requirements.py requirements.txt
+python scripts/check_requirements.py requirements.txt
 if errorlevel 1 (
     echo Installing missing packages...
     pip install -r requirements.txt
 )
-
-%PY_CMD% -m autogpt %*
+python -m autogpt %*
 pause
