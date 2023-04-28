937bce514f1a119b5dbe33b690ab319c5cb95762
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2023-04-28 02:11:55 +0200

    Fix `milvus_memory_test.py` mock `Config`

--- tests/milvus_memory_test.py
+++ tests/milvus_memory_test.py
@@ -5,22 +5,21 @@ import sys
 import unittest
 
 try:
+    from autogpt.config import Config
     from autogpt.memory.milvus import MilvusMemory
 
-    def mock_config() -> dict:
+    def mock_config() -> Config:
         """Mock the config object for testing purposes."""
+
         # Return a mock config object with the required attributes
-        return type(
-            "MockConfig",
-            (object,),
-            {
-                "debug_mode": False,
-                "continuous_mode": False,
-                "speak_mode": False,
-                "milvus_collection": "autogpt",
-                "milvus_addr": "localhost:19530",
-            },
-        )
+        class MockConfig(Config):
+            debug_mode = False
+            continuous_mode = False
+            speak_mode = False
+            milvus_collection = "autogpt"
+            milvus_addr = "localhost:19530"
+
+        return MockConfig()
 
     class TestMilvusMemory(unittest.TestCase):
         """Tests for the MilvusMemory class."""
