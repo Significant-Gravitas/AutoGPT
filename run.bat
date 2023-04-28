a3d48b852f3a63bbdbfe3112352a82e0c51b33ac
Dylan Lau <dylanlz@gmail.com>
2023-04-28 19:21:39 +0800

    Update run batch to support either python or python 3

--- run.bat
+++ run.bat
@@ -1,8 +1,15 @@
 @echo off
-python scripts/check_requirements.py requirements.txt
+
+setlocal
+
+set PY_CMD=python3
+where %PY_CMD% >nul 2>nul || set PY_CMD=python
+
+%PY_CMD% scripts/check_requirements.py requirements.txt
 if errorlevel 1 (
     echo Installing missing packages...
     pip install -r requirements.txt
 )
-python -m autogpt %*
+
+%PY_CMD% -m autogpt %*
 pause
