a4ff4ff4f9fc2e5761163938560f91feb9ae1bad
Dylan <dylanlz@gmail.com>
2023-05-01 05:51:54 +0800

    Updated run script to check for Python 3.10 and above, and provide informative error messages for unsupported versions.

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-python scripts/check_requirements.py requirements.txt
-if [ $? -eq 1 ]
-then
-    echo Installing missing packages...
-    pip install -r requirements.txt
+if python3 -c "import sys; sys.exit(sys.version_info < (3, 10))"; then
+    python3 scripts/check_requirements.py requirements.txt
+    if [ $? -eq 1 ]
+    then
+        echo Installing missing packages...
+        pip3 install -r requirements.txt
+    fi
+    python3 -m autogpt $@
+    read -p "Press any key to continue..."
+else
+    echo "Python 3.10 or higher is required to run Auto GPT."
 fi
-python -m autogpt $@
-read -p "Press any key to continue..."
