fca6879a43661044af7d2851b44334d3aa1d5d6e
Dave Stevenson <dayofthedave@gmail.com>
2023-04-30 02:38:41 -0400

    Make MacOS TTS voices configurable.

--- autogpt/speech/macos_tts.py
+++ autogpt/speech/macos_tts.py
@@ -12,10 +12,13 @@ class MacOSTTS(VoiceBase):
 
     def _speech(self, text: str, voice_index: int = 0) -> bool:
         """Play the given text."""
+        voice1 = os.getenv("MAC_OS_VOICE_1") or "Allison"
+        voice2 = os.getenv("MAC_OS_VOICE_2") or "Ava"
+        voice3 = os.getenv("MAC_OS_VOICE_3") or "Samantha"
         if voice_index == 0:
-            os.system(f'say "{text}"')
+            os.system(f'say -v {voice1} "{text}"')
         elif voice_index == 1:
-            os.system(f'say -v "Ava (Premium)" "{text}"')
+            os.system(f'say -v {voice2} "{text}"')
         else:
-            os.system(f'say -v Samantha "{text}"')
+            os.system(f'say -v {voice3} "{text}"')
         return True
