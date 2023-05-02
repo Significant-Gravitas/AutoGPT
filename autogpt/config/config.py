39df2add70e90fb7d92097e9f0e0fdb225b235c1
Luke K <2609441+pr-0f3t@users.noreply.github.com>
2023-05-02 10:29:26 -0400

    Remove empty line
    The empty line seemed to be affecting code coverage

--- autogpt/config/config.py
+++ autogpt/config/config.py
@@ -139,7 +139,6 @@ class Config(metaclass=Singleton):
             self.plugins_allowlist = plugins_allowlist.split(",")
         else:
             self.plugins_allowlist = []
-
         plugins_denylist = os.getenv("DENYLISTED_PLUGINS", [])
         if plugins_denylist:
             self.plugins_denylist = plugins_denylist.split(",")
