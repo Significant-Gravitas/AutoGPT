d9458eed833e93ed820b0d21610627ff05da6b5e
BillSchumacher <34168009+BillSchumacher@users.noreply.github.com>
2023-05-02 20:58:45 -0500

    Delete much of the prompt

--- autogpt/prompts/prompt.py
+++ autogpt/prompts/prompt.py
@@ -22,20 +22,7 @@ def build_default_prompt_generator() -> PromptGenerator:
 
     # Initialize the PromptGenerator object
     prompt_generator = PromptGenerator()
-
-    # Add constraints to the PromptGenerator object
-    prompt_generator.add_constraint(
-        "~4000 word limit for short term memory. Your short term memory is short, so"
-        " immediately save important information to files."
-    )
-    prompt_generator.add_constraint(
-        "If you are unsure how you previously did something or want to recall past"
-        " events, thinking about similar events will help you remember."
-    )
     prompt_generator.add_constraint("No user assistance")
-    prompt_generator.add_constraint(
-        'Exclusively use the commands listed in double quotes e.g. "command name"'
-    )
 
     # Define the command list
     commands = [
@@ -48,28 +35,16 @@ def build_default_prompt_generator() -> PromptGenerator:
 
     # Add resources to the PromptGenerator object
     prompt_generator.add_resource(
-        "Internet access for searches and information gathering."
+        "Internet access for gathering information."
     )
-    prompt_generator.add_resource("Long Term memory management.")
     prompt_generator.add_resource(
-        "GPT-3.5 powered Agents for delegation of simple tasks."
+        "Agents for delegation of simple tasks."
     )
-    prompt_generator.add_resource("File output.")
+    prompt_generator.add_resource("File I/O for reading and writing files.")
 
     # Add performance evaluations to the PromptGenerator object
     prompt_generator.add_performance_evaluation(
-        "Continuously review and analyze your actions to ensure you are performing to"
-        " the best of your abilities."
-    )
-    prompt_generator.add_performance_evaluation(
-        "Constructively self-criticize your big-picture behavior constantly."
-    )
-    prompt_generator.add_performance_evaluation(
         "Reflect on past decisions and strategies to refine your approach."
-    )
-    prompt_generator.add_performance_evaluation(
-        "Every command has a cost, so be smart and efficient. Aim to complete tasks in"
-        " the least number of steps."
     )
     prompt_generator.add_performance_evaluation(
         "If you cannot think of a valid command to perform start or message an agent to determine the next command."
