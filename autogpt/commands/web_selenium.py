95ce3176cd1ec1eb81b1566d1e3bbe9cefdf8c56
Kory Becker <kbecker@primaryobjects.com>
2023-05-03 14:13:12 -0400

    Set Chrome binary and driver path in env config.

--- autogpt/commands/web_selenium.py
+++ autogpt/commands/web_selenium.py
@@ -102,11 +102,13 @@ def scrape_text_with_selenium(url: str) -> tuple[WebDriver, str]:
             options.add_argument("--remote-debugging-port=9222")
 
         options.add_argument("--no-sandbox")
+        if CFG.chromium_binary_location:
+            options.binary_location = CFG.chromium_binary_location
         if CFG.selenium_headless:
             options.add_argument("--headless=new")
             options.add_argument("--disable-gpu")
 
-        chromium_driver_path = Path("/usr/bin/chromedriver")
+        chromium_driver_path = Path(CFG.chromium_driver_path)
 
         driver = webdriver.Chrome(
             executable_path=chromium_driver_path
--- autogpt/config/config.py
+++ autogpt/config/config.py
@@ -114,6 +114,8 @@ class Config(metaclass=Singleton):
         # Selenium browser settings
         self.selenium_web_browser = os.getenv("USE_WEB_BROWSER", "chrome")
         self.selenium_headless = os.getenv("HEADLESS_BROWSER", "True") == "True"
+        self.chromium_binary_location = os.getenv("CHROMIUM_BINARY_LOCATION", None)
+        self.chromium_driver_path = os.getenv("CHROMIUM_DRIVER_PATH", "/usr/bin/chromedriver")
 
         # User agent header to use when making HTTP requests
         # Some websites might just completely deny request with an error code if
