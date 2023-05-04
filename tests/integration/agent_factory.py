823009c90b7b7278e9f679b5f85cfc807668cd78
Merwane Hamadi <merwanehamadi@gmail.com>
2023-05-03 19:03:41 -0700

    create information retrieval challenge a

--- tests/integration/agent_factory.py
+++ tests/integration/agent_factory.py
@@ -183,3 +183,43 @@ def get_company_revenue_agent(
     )
 
     return agent
+
+@pytest.fixture
+def create_code_agent(
+    agent_test_config, memory_local_cache, workspace: Workspace
+):
+    command_registry = CommandRegistry()
+    command_registry.import_commands("autogpt.commands.file_operations")
+    command_registry.import_commands("autogpt.commands.execute_code")
+    command_registry.import_commands("autogpt.commands.improve_code")
+    command_registry.import_commands("autogpt.app")
+
+    ai_config = AIConfig(
+        ai_name="Debug-Code-GPT",
+        ai_role="an autonomous agent that specializes in debugging python code",
+        ai_goals=[
+            "1-Run the code in the file named 'code.py' using the execute_code command.",
+            "2-Read code.py to understand why the code is not working as expected.",
+            "3-Modify code.py to fix the error.",
+            "Repeat step 1, 2 and 3 until the code is working as expected. When you're done use the task_complet command.",
+            "Do not use any other commands than execute_python_file and write_file",
+        ],
+    )
+    ai_config.command_registry = command_registry
+
+    system_prompt = ai_config.construct_full_prompt()
+    Config().set_continuous_mode(False)
+    agent = Agent(
+        ai_name="Get-CompanyRevenue",
+        memory=memory_local_cache,
+        full_message_history=[],
+        command_registry=command_registry,
+        config=ai_config,
+        next_action_count=0,
+        system_prompt=system_prompt,
+        triggering_prompt=DEFAULT_TRIGGERING_PROMPT,
+        workspace_directory=workspace.root,
+    )
+
+    return agent
+
---
+++ tests/integration/challenges/debug_code/test_debug_code_challenge_a.py
@@ -0,0 +1,70 @@
+import contextlib
+from functools import wraps
+from typing import Generator
+
+import pytest
+
+from autogpt.agent import agent
+from autogpt.commands.execute_code import execute_python_file
+from autogpt.commands.file_operations import read_file, write_to_file
+from tests.integration.agent_utils import run_interaction_loop
+from tests.integration.challenges.utils import run_multiple_times
+from tests.utils import requires_api_key
+import docker
+
+CODE = """def two_sum(nums, target):
+    seen = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num] = i
+    return None
+
+# Example usage:
+nums = [2, 7, 11, 15]
+target = 9
+result = two_sum(nums, target)
+print(result)  # Output: [0, 1]
+"""
+
+
+def input_generator(input_sequence: list) -> Generator[str, None, None]:
+    """
+    Creates a generator that yields input strings from the given sequence.
+
+    :param input_sequence: A list of input strings.
+    :return: A generator that yields input strings.
+    """
+    yield from input_sequence
+
+
+# @pytest.skip("Nobody beat this challenge yet")
+# @pytest.mark.vcr
+@requires_api_key("OPENAI_API_KEY")
+@run_multiple_times(3)
+def test_debug_code_challenge_a(
+    create_code_agent, monkeypatch
+) -> None:
+    """
+    Test the challenge_a function in a given agent by mocking user inputs and checking the output file content.
+
+    :param get_company_revenue_agent: The agent to test.
+    :param monkeypatch: pytest's monkeypatch utility for modifying builtins.
+    """
+
+    # monkeypatch.setattr(docker, 'from_env', lambda: docker_client)
+    client = docker.from_env()
+
+    file_path = str(create_code_agent.workspace.get_path("code.py"))
+    write_to_file(file_path, CODE)
+
+
+    input_sequence = ["s", "s", "s", "s", "s", "EXIT"]
+    gen = input_generator(input_sequence)
+    monkeypatch.setattr("builtins.input", lambda _: next(gen))
+
+    with contextlib.suppress(SystemExit):
+        create_code_agent.start_interaction_loop()
+    assert "[0, 1]" in execute_python_file(file_path), "Expected the output to be [0, 1]"
+
