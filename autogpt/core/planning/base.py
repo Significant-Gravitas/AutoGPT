b194742ab5807115b647fc173941348f4e65311d
DaoAdvocate <12145726+rihp@users.noreply.github.com>
2023-05-04 20:21:51 +0200

    added_selffeedback

--- autogpt/core/planning/base.py
+++ autogpt/core/planning/base.py
@@ -8,50 +8,42 @@ if typing.TYPE_CHECKING:
     from autogpt.core.workspace.base import Workspace
     from autogpt.config.ai_config import AIConfig
 
+
 class Planner(abc.ABC):
     """Build prompts based on inputs, can potentially store and retrieve planning state from the workspace"""
 
     @abc.abstractmethod
-    def __init__(self,
-        configuration: Configuration, 
-        logger: Logger, 
-        plugin_manager: PluginManager, 
-        workspace: Workspace
+    def __init__(
+        self,
+        configuration: Configuration,
+        logger: Logger,
+        plugin_manager: PluginManager,
+        workspace: Workspace,
     ) -> None:
         self.configuration = configuration
         self.logger = logger
         self.plugin_manager = plugin_manager
         self.workspace = workspace
+        pass
 
-    def construct_goals_prompt(big_task: str) -> AIConfig:
+    @abc.abstractmethod
+    def construct_goals_prompt(
+        big_task: str,
+    ) -> AIConfig:
         """This method is called upon the creation of an agent to define in more detail its goals"""
         pass
 
-    def construct_next_step_prompt(
-        context,  # Like a dict of message history, prior commands, current time, etc.
-        memory,  # Maybe an explicit handle for the memory backend
-        goals,
-        tasks_queue: dict = None,
-        # Carefully consider if there are other dependencies
+    @abc.abstractmethod
+    def get_self_feedback(
+        self,
+        thoughts: dict,  # Should this be a thought object?
+        llm_model: str,  # Should this be a model object?
     ) -> str:
-        """This function makes the agent figure out what to do next based on its state"""
-
-        def check_tasks_notification_queue(n: int) -> dict:
-            """Returns a number of events in the queue that are closest to the current time.
-            This summarizes each task except the first one"""
-            pass
-
-        tasks_queue = check_tasks_notification_queue(5)
-
-        def get_important_messages(context: list[str]) -> list[str]:
-            """takes in the context and seniority of the message senders to infer the most important action to take on"""
-            pass
-
-        pass
-
-    def update_plan(
-        plan,
-        context,  # Like a dict of message history, prior commands, etc.
-    ) -> str:
-        """This function reads the plan, updates the status of the tasks and returnts an updated plan"""
+        """
+        Generates a feedback response based on the provided thoughts dictionary.
+        This method takes in a dictionary of thoughts containing keys such as 'reasoning',
+        'plan', 'thoughts', and 'criticism'. It combines these elements into a single
+        feedback message and uses the create_chat_completion() function to generate a
+        response based on the input message.
+        """
         pass
