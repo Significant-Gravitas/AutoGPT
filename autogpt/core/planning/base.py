4b472bd4af82900bab06a8a88e3031d16b8c89d6
DaoAdvocate <12145726+rihp@users.noreply.github.com>
2023-05-04 21:52:04 +0200

    improved_data_classes_and_feedback_fn

--- autogpt/core/planning/base.py
+++ autogpt/core/planning/base.py
@@ -1,19 +1,24 @@
 import abc
+import enum
 import typing
 from dataclasses import dataclass
-import enum
 
 if typing.TYPE_CHECKING:
+    from autogpt.config.ai_config import (  # Should this be replaced with Configuration?
+        AIConfig,
+    )
     from autogpt.core.configuration.base import Configuration
     from autogpt.core.logging.base import Logger
     from autogpt.core.plugin.base import PluginManager
     from autogpt.core.workspace.base import Workspace
-    from autogpt.config.ai_config import AIConfig # Should this be replaced with Configuration?
+
 
 class LLMModel(enum.StrEnum):
+    # This will be defined probably in the LLM interface module
     GPT3 = "gpt3"
     GPT4 = "gpt4"
-    
+
+
 class Role(enum.StrEnum):
     USER = "user"
     SYSTEM = "system"
@@ -25,13 +30,23 @@ class Message:
     role: Role
     message: str
 
-@dataclass  
-class Thought:
+
+@dataclass
+class Context:  # This will be defined probably in the Memory interface module
+    progress: str
+    result: str
+    memories: list(str)
+    feedback: str
+
+
+@dataclass
+class Thoughts:
     system: str
     reasoning: str
     plan: list(str)
-    criticism: str
     thoughts: str
+    criticism: str
+
 
 class Planner(abc.ABC):
     """Build prompts based on inputs, can potentially store and retrieve planning state from the workspace"""
@@ -54,10 +69,10 @@ class Planner(abc.ABC):
     def construct_objective_prompt_from_user_input(
         self,
         user_objective: str,
-    ) -> list[Message]:  # 
+    ) -> list[Message]:  #
         """
         This method is called upon the creation of an agent to refine its goals based on user input.
-        
+
         Args:
             user_objective (str): The user-defined objective for the agent.
 
@@ -66,11 +81,11 @@ class Planner(abc.ABC):
         """
         pass
 
-
     @abc.abstractmethod
     def get_self_feedback_prompt(
         self,
-        thoughts: Thought ,  
+        context: Context,
+        thoughts: Thoughts,
         llm_model: str,  # Should this be a model object?
     ) -> str:
         """
