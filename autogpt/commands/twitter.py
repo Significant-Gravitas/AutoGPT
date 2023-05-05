8e79ec2aaa0e39db25cf3cc1662b3a43d3e6a84d
bowjoww <jofederici@gmail.com>
2023-05-05 18:40:32 -0300

    AutoGPT

--- autogpt/commands/twitter.py
+++ autogpt/commands/twitter.py
@@ -5,22 +5,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 def send_tweet(tweet_text):
     consumer_key = os.environ.get("TW_CONSUMER_KEY")
     consumer_secret = os.environ.get("TW_CONSUMER_SECRET")
     access_token = os.environ.get("TW_ACCESS_TOKEN")
     access_token_secret = os.environ.get("TW_ACCESS_TOKEN_SECRET")
+    bearer_token = os.environ.get("TW_BEARER_TOKEN")
     # Authenticate to Twitter
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-
-    # Create API object
-    api = tweepy.API(auth)
-
+    client = tweepy.Client(bearer_token=bearer_token,
+                    consumer_key=consumer_key,
+                    consumer_secret=consumer_secret,
+                    access_token=access_token,
+                    access_token_secret=access_token_secret)
     # Send tweet
     try:
-        api.update_status(tweet_text)
+        client.create_tweet(text=tweet_text)
         print("Tweet sent successfully!")
     except tweepy.TweepyException as e:
         print("Error sending tweet: {}".format(e.reason))
