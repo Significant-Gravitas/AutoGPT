1f88942de404c8c402208dfa556e9d04e603bd86
Media <12145726+rihp@users.noreply.github.com>
2023-05-05 18:00:27 +0200

    initial logging interface

--- autogpt/core/logging/base.py
+++ autogpt/core/logging/base.py
@@ -1,5 +1,46 @@
 import abc
+class LogFormat(Enum):
+  NONE       = 0
+  MARKDOWN   = 1
+  JSON       = 2
+  TEXT       = 3
+  YAML       = 4
 
+@dataclasses.dataclass
+class LogMessage:
+  title: str
+  message: str
+  format: LogFormat
+
+class LogLevels(Enum):
+  NONE  = 0
+  ERROR = 1
+  WARN  = 2
+  INFO  = 3
+  DEBUG = 4
+  SILLY = 5
 
 class Logger(abc.ABC):
-    pass
+    @abc.abstractmethod
+    def __init__(self, *arg, **kwargs):
+        pass
+
+    @abc.abstractmethod
+    def debug(self, message: LogMessage):
+      pass
+
+    @abc.abstractmethod
+    def info(self, message: LogMessage):
+      pass
+
+    @abc.abstractmethod
+    def warn(self, message: LogMessage):
+      pass
+
+    @abc.abstractmethod
+    def error(self, message: LogMessage):
+      pass
+
+    @abc.abstractmethod
+    def set_log_level(self, level: LogLevels):
+      pass
