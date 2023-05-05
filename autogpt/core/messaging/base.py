3055072e788aa999d1d34fc615416e1c47058199
Media <12145726+rihp@users.noreply.github.com>
2023-05-05 13:34:59 +0200

    Initial base.py for message broker

--- autogpt/core/messaging/base.py
+++ autogpt/core/messaging/base.py
@@ -1,9 +1,40 @@
-import abc
+from abc import ABC, abstractmethod
+from enum import Enum
+import dataclasses
+from typing import Any, List, Callable, Union
 
+class MessageCategory(Enum):
+    USER_INPUT = 0
+    USER_PROMPT = 1
 
-class Message(abc.ABC):
-    pass
+@dataclasses.dataclass
+class Message(ABC):  # ABC should be within parentheses
+    sender: Any  # Use Any to allow any type of sender
+    message: str
+    kind_of_message: MessageCategory
 
+class MessageBroker(ABC):
 
-class MessageBroker(abc.ABC):
-    pass
+    @property  # Use property decorator instead of abc.property
+    def sinks(self) -> List[Callable]:  # Specify the return type as a list of callable objects
+        return []
+
+    @abstractmethod
+    def broadcast_message(self, message: Message) -> None:
+        pass
+
+    @abstractmethod
+    def get_messages(self, filters: Any) -> List[Message]:  # Specify filters and return type
+        pass
+
+    @abstractmethod
+    def get_listeners(self) -> List[Any]:  # Specify the return type as a list of listeners
+        pass
+
+    @abstractmethod
+    def register_listener(self) -> Union[str, None]:  # Return ListenerStatus or None if unsuccessful
+        pass
+
+    @abstractmethod
+    def remove_listener(self) -> Union[str, None]:  # Return ListenerStatus or None if unsuccessful
+        pass
