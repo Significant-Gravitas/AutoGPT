3bbbbf0a53cfa017f45b004b8685d73504d74dac
Boostrix <119627414+Boostrix@users.noreply.github.com>
2023-05-06 20:07:48 +0200

    begin integrating some templating support (standalone example for now using jinja2)

--- autogpt/wizard/FileTemplating.py
+++ autogpt/wizard/FileTemplating.py
@@ -1,29 +1,40 @@
-from jinja2 import Template as JinjaTemplate
+from jinja2 import Environment, FileSystemLoader
 # from Cheetah.Template import Template as CheetahTemplate
+import os
 
 class TemplateAdapter:
-    def __init__(self, template_string, template_type='jinja2'):
-        self.template_string = template_string
+    def __init__(self, template_path_or_string, template_type='jinja2'):
         self.template_type = template_type
+        if os.path.isfile(template_path_or_string):
+            self.template_path = template_path_or_string
+            self.template_string = None
+        else:
+            self.template_path = None
+            self.template_string = template_path_or_string
 
     def render(self, context):
         if self.template_type == 'jinja2':
-            template = JinjaTemplate(self.template_string)
+            if self.template_path:
+                env = Environment(loader=FileSystemLoader(os.path.dirname(self.template_path)))
+                template = env.get_template(os.path.basename(self.template_path))
+            else:
+                template = Environment().from_string(self.template_string)
             return template.render(context)
         elif self.template_type == 'cheetah':
-            template = CheetahTemplate(source=self.template_string)
-            return str(template(searchList=[context]))
+            if self.template_path:
+                template = CheetahTemplate(file=self.template_path, searchList=[context])
+            else:
+                template = CheetahTemplate(source=self.template_string)
+            return str(template)
         else:
             raise ValueError('Invalid template type: {}'.format(self.template_type))
 
-template_string = 'Hello, {{ name }}!'
+# Using a file-based Jinja2 template
+template_path = 'myTemplate.j2'
 context = {'name': 'World'}
 
-adapter = TemplateAdapter(template_string, template_type='jinja2')
+adapter = TemplateAdapter(template_path, template_type='jinja2')
 output = adapter.render(context)
 print(output)  # Output: Hello, World!
 
-#adapter = TemplateAdapter(template_string, template_type='cheetah')
-#output = adapter.render(context)
-#print(output)  # Output: Hello, World!
 
--- autogpt/wizard/wizard.py
+++ autogpt/wizard/wizard.py
@@ -1,10 +1,20 @@
 from typing import Any, Dict
 from abc import ABC, abstractmethod
 
+
+##
+# TODO: should support multiple wizard engines here to be able to tinker around
 from questionary import prompt as questionary_prompt
 #from PyInquirer import prompt as pyinquirer_prompt
 
 import json
+
+###
+# NOTE: This is just a proof-of-concept to illustrate the underlying idea
+# Given the ongoing re-arch it's probably a good idea to wait with major coding efforts
+# However, coding up something in standalone mode using a few stubs and helper classes should
+# be useful in and of itself.
+#
 
 class PromptAdapter(ABC):
     @abstractmethod
@@ -21,17 +31,31 @@ class PyInquirerAdapter(PromptAdapter):
 
 adapter = QuestionaryAdapter() # or PyInquirerAdapter()
 
+
+###
+# TODO: the JSON structure is likely to evolve as needed:
+# - optional/required fields
+# - input validation
+# - execution of commands (top-level cmd mgr), e.g. for validating HTML/XML files generated procedurally
+# - probably need to support some state machine stuff for each step ?
+
 filename = 'webAppGenerator.wizard'
 with open(filename) as f:
     data = json.load(f)
 
+##
+# read out some meta data
 version = data['version']
 name = data['name']
 description = data['description']
 questions = data['questions']
 
+##
+# show some meta info
 print(f"filename: {filename}, version:{version}, name:{name}")
 
+# and run the prompt loop
 answers = adapter.prompt(questions)
 
+# finally show all collected info in JSON form
 print(answers)
