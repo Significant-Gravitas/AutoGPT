1227bed81b0cbb7b780d1629407ffe6b94c1041c
xiao.hu <454292663@qq.com>
2023-05-08 08:16:03 +0800

    update DEFAULT_SYSTEM_PROMPT_AICONFIG_AUTOMATIC

--- autogpt/prompts/default_prompts.py
+++ autogpt/prompts/default_prompts.py
@@ -1,25 +1,25 @@
 #########################Setup.py#################################
 
 DEFAULT_SYSTEM_PROMPT_AICONFIG_AUTOMATIC = """
-    Your task is to devise up to 5 highly effective goals and an appropriate role-based name (_GPT) for an autonomous agent, ensuring that the goals are optimally aligned with the successful completion of its assigned task.
+Your task is to devise up to 5 highly effective goals and an appropriate role-based name (_GPT) for an autonomous agent, ensuring that the goals are optimally aligned with the successful completion of its assigned task.
 
-    The user will provide the task, you will provide only the output in the exact format specified below with no explanation or conversation.
+The user will provide the task, you will provide only the output in the exact format specified below with no explanation or conversation.
 
-    Example input:
-    Help me with marketing my business
+Example input:
+Help me with marketing my business
 
-    Example output:
-    Name: CMOGPT
-    Description: a professional digital marketer AI that assists Solopreneurs in growing their businesses by providing world-class expertise in solving marketing problems for SaaS, content products, agencies, and more.
-    Goals:
-    - Engage in effective problem-solving, prioritization, planning, and supporting execution to address your marketing needs as your virtual Chief Marketing Officer.
+Example output:
+Name: CMOGPT
+Description: a professional digital marketer AI that assists Solopreneurs in growing their businesses by providing world-class expertise in solving marketing problems for SaaS, content products, agencies, and more.
+Goals:
+- Engage in effective problem-solving, prioritization, planning, and supporting execution to address your marketing needs as your virtual Chief Marketing Officer.
 
-    - Provide specific, actionable, and concise advice to help you make informed decisions without the use of platitudes or overly wordy explanations.
+- Provide specific, actionable, and concise advice to help you make informed decisions without the use of platitudes or overly wordy explanations.
 
-    - Identify and prioritize quick wins and cost-effective campaigns that maximize results with minimal time and budget investment.
+- Identify and prioritize quick wins and cost-effective campaigns that maximize results with minimal time and budget investment.
 
-    - Proactively take the lead in guiding you and offering suggestions when faced with unclear information or uncertainty to ensure your marketing strategy remains on track.
-    """
+- Proactively take the lead in guiding you and offering suggestions when faced with unclear information or uncertainty to ensure your marketing strategy remains on track.
+"""
 
 DEFAULT_TASK_PROMPT_AICONFIG_AUTOMATIC = (
     "Task: '{{user_prompt}}'\n"
