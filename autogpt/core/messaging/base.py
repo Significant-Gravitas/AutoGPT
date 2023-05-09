b2ffa0ad76d4764c1e6083368e4394f7944ce756
James Collins <collijk@uw.edu>
2023-05-08 20:14:18 -0700

    Revise message broker interface (#3986)
    * Revise message broker interface
    
    * Add emitter
    
    * This is the last change for a bit, I think
    
    * Incorporate feedback

--- autogpt/core/messaging/base.py
+++ autogpt/core/messaging/base.py
@@ -1,73 +1,199 @@
-from abc import ABC, abstractmethod
-from enum import Enum, StrEnum
-import dataclasses
-from typing import Any, List, Callable, Union
-
-
-class MessageCategory(StrEnum):
-    COMMAND = "command"
-    AGENT_INSTANTIATION = "agent_instantiation"
-    USER_INPUT = "user_input"
-    USER_PROMPT = "user_prompt"
-    AGENT_MESSAGE = "agent_message"
-    SELF_FEEDBACK = "self_feedback"
-    PLAN = "plan"
+"""Abstract interface for a message broker system."""
+import abc
+from dataclasses import dataclass
+from datetime import datetime
+from enum import StrEnum
+from typing import Callable, Dict
 
 
 class Role(StrEnum):
+    """Enumeration of all entities capable of sending messages"""
+
     USER = "user"
-    SYSTEM = "system"
-    ASSISTANT = "assistant"
+    CLIENT_APPLICATION = "client_application"
+    AGENT_FACTORY = "agent_factory"
+    AGENT = "agent"
 
 
+@dataclass
 class Sender:
-    id: int  # Who is sending the message, should use interface Sender
+    """Struct for metadata about a sender."""
+
+    # We'll need some kind of uuids at some point.  Names are fine for now.
+    name: str
     role: Role
 
 
-class Receiver:
-    id: int  # Who is receiving the message, should use interface receiver
-    role: Role
+@dataclass
+class MessageMetadata:
+    """Struct for metadata about a message."""
+
+    sender: Sender
+    timestamp: datetime
+    additional_metadata: Dict = None
+    # TODO: what else goes here?
 
 
-@dataclasses.dataclass
-class Message(ABC):  # ABC should be within parentheses
-    sender: Role  # Use Any to allow any type of sender
-    message: str
-    kind_of_message: MessageCategory
+@dataclass
+class Message:
+    """Struct for a message and its metadata."""
+
+    content: dict  # Some json struct we can define with a strict schema
+    metadata: MessageMetadata
 
 
-class MessageBroker(ABC):
-    @property  # Use property decorator instead of abc.property
-    def sinks(
+Listener = Callable[[Message], None]
+MessageFilter = Callable[[Message], bool]
+
+
+class MessageEmitter(abc.ABC):
+    """Interface for an emitter that can send messages on a particular channel.
+
+    Emitters are used to send messages on a particular channel. They preserve the
+    metadata associated with them when they are created so client code can simply
+    pass messages to them without having to worry about the details of the channel
+    or repeatedly passing metadata.
+
+    """
+
+    @abc.abstractmethod
+    def __init__(
         self,
-    ) -> List[Callable]:  # Specify the return type as a list of callable objects
-        return []
+        message_channel: "MessageChannel",
+        sender_name: str,
+        sender_role: Role,
+        **additional_metadata,
+    ):
+        ...
 
-    @abstractmethod
-    def broadcast_message(self, message: Message) -> None:
-        pass
+    @abc.abstractmethod
+    def send_message(self, content: dict, **extra_metadata) -> bool:
+        """Send a message on this channel .
 
-    @abstractmethod
-    def get_messages(
-        self, filters: Any
-    ) -> List[Message]:  # Specify filters and return type
-        pass
+        Args:
+            content: A json-serializable object containing the message content.
+            **extra_metadata: Any additional metadata to be included when emitting messages.
 
-    @abstractmethod
-    def get_listeners(
+        Returns:
+            True if the message was sent successfully, False otherwise.
+
+        """
+        ...
+
+    @abc.abstractmethod
+    def __repr__(self):
+        ...
+
+
+class MessageChannel(abc.ABC):
+    """Interface for a channel that can propagate messages to a group of listeners."""
+
+    @abc.abstractmethod
+    def __init__(
         self,
-    ) -> List[Any]:  # Specify the return type as a list of listeners
-        pass
+        channel_name: str,
+        message_broker: "MessageBroker",
+    ):
+        ...
 
-    @abstractmethod
+    @abc.abstractmethod
+    def add_listener(
+        self,
+        listener: Listener,
+        message_filter: MessageFilter,
+    ) -> None:
+        """Add a listener to this channel.
+
+        Args:
+            listener: A callback function that responds to messages
+                        sent on this channel.
+            message_filter: A function that filters messages before they are sent to
+                              the listener.  If the filter returns False, the message
+                              is not sent to the listener.
+
+        """
+        ...
+
+    @abc.abstractmethod
+    def send_message(self, message: Message) -> bool:
+        """Send a message on this channel.
+
+        Args:
+            message: The message to send.
+
+        Returns:
+            True if the message was sent successfully, False otherwise.
+
+        """
+        ...
+
+    @abc.abstractmethod
+    def __repr__(self) -> str:
+        ...
+
+
+class MessageBroker(abc.ABC):
+    """Interface for a broker that can create message channels and propagate messages."""
+
+    @abc.abstractmethod
+    def __init__(self, *args, **kwargs):
+        ...
+
+    @abc.abstractmethod
+    def create_message_channel(
+        self,
+        channel_name: str,
+    ) -> None:
+        """Construct a message channel with a given name.
+
+        Args:
+            channel_name: The name of the channel to create.
+
+        """
+        ...
+
+    @abc.abstractmethod
+    def get_emitter(
+        self,
+        channel_name: str,
+        sender_name: str,
+        sender_role: Role,
+        **extra_metadata,
+    ) -> MessageEmitter:
+        """Get an emitter for this channel.
+
+        Args:
+            channel_name: The name of the channel the emitter will send messages on.
+            sender_name: The name of the sender.
+            sender_role: The role of the sender.
+            extra_metadata: Any additional metadata to be included when emitting messages.
+
+        Returns:
+            A MessageEmitter object that can be used to send messages on this channel
+            preserving all metadata.
+
+        """
+        ...
+
+    @abc.abstractmethod
     def register_listener(
         self,
-    ) -> Union[str, None]:  # Return ListenerStatus or None if unsuccessful
-        pass
+        message_channel: str,
+        listener: Listener,
+        message_filter: MessageFilter,
+    ) -> None:
+        """Register a listener for a given message channel.
 
-    @abstractmethod
-    def remove_listener(
-        self,
-    ) -> Union[str, None]:  # Return ListenerStatus or None if unsuccessful
-        pass
+        Args:
+            message_channel: The name of the message channel to listen to.
+            listener: A callback function that responds to messages
+                        sent on this channel.
+            message_filter: A function that filters messages before they are sent to
+                              the listener.  If the filter returns False, the message
+
+        """
+        ...
+
+    @abc.abstractmethod
+    def __repr__(self):
+        ...
