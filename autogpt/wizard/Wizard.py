e59967fbf95b0ec2aa76a2586aa4d4304a9c2c80
Boostrix <119627414+Boostrix@users.noreply.github.com>
2023-05-10 17:04:28 +0200

    add another adapter for PyQt

--- autogpt/wizard/Wizard.py
+++ autogpt/wizard/Wizard.py
@@ -7,6 +7,10 @@ from abc import ABC, abstractmethod
 from questionary import prompt as questionary_prompt
 #from PyInquirer import prompt as pyinquirer_prompt
 
+from typing import Dict, Any
+from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout, QLabel, QLineEdit, QDialogButtonBox
+
+
 ###
 # NOTE: This is just a proof-of-concept to illustrate the underlying idea
 # Given the ongoing re-arch it's probably a good idea to wait with major coding efforts
@@ -27,9 +31,62 @@ class PyInquirerAdapter(PromptAdapter):
     def prompt(self, questions: Dict[str, Any]) -> Dict[str, Any]:
         return pyinquirer_prompt(questions)
 
+class PromptDialog(QDialog):
+    def __init__(self, questions: Dict[str, Any]):
+        super().__init__()
+
+        # Determine the title of the dialog
+        if isinstance(questions, dict):
+            title = questions.get('name', 'Prompt Dialog')
+            questions_list = questions.get('questions', [])
+        else:
+            title = 'Prompt Dialog'
+            questions_list = questions
+
+        self.setWindowTitle(title)
+
+        # Create a layout for the dialog
+        layout = QVBoxLayout()
+
+        # Add labels and line edits for each question
+        self.answers = {}
+        for question in questions_list:
+            label = QLabel(question.get('message', ''))
+            layout.addWidget(label)
+            line_edit = QLineEdit()
+            self.answers[question.get('name', '')] = line_edit
+            layout.addWidget(line_edit)
+
+        # Add OK and Cancel buttons
+        button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        button_box.accepted.connect(self.accept)
+        button_box.rejected.connect(self.reject)
+        layout.addWidget(button_box)
+
+        self.setLayout(layout)
+
+    def get_answers(self) -> Dict[str, Any]:
+        # Return a dictionary of answers, keyed by question name
+        return {name: line_edit.text() for name, line_edit in self.answers.items()}
+
+
+class QtAdapter(PromptAdapter):
+    def prompt(self, questions: Dict[str, Any]) -> Dict[str, Any]:
+        app = QApplication([])
+
+        # Create a dialog to prompt the user for input
+        dialog = PromptDialog(questions)
+
+        # Display the dialog and wait for the user to close it
+        if dialog.exec_() == QDialog.Accepted:
+            # Return the user's answers
+            return dialog.get_answers()
+        else:
+            # User canceled the dialog, so return an empty dictionary
+            return {}
 ##
 # TODO new adapter for Agents (inter-agent messaging)
 
 
-ResponseAdapter = QuestionaryAdapter() # or PyInquirerAdapter()
+ResponseAdapter = QtAdapter() # or PyInquirerAdapter()
 
