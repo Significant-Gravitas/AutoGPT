9d8235352b530a46c69806d54f27c8aa3c780c43
Boostrix <119627414+Boostrix@users.noreply.github.com>
2023-05-10 17:42:24 +0200

    refactor a little

--- autogpt/wizard/Wizard.py
+++ autogpt/wizard/Wizard.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Dict, List
 from abc import ABC, abstractmethod
 
 import sys
@@ -49,66 +49,60 @@ class PromptDialog(QDialog):
         # Create a layout for the dialog
         layout = QVBoxLayout()
 
-        # Add labels, line edits, and tooltips for each question
+        # Add labels and line edits for each question
         self.answers = {}
-        self.tooltips = {}
-        self.validators = {}
-        for question in questions_list:
-            label = QLabel(question.get('message', ''))
-            layout.addWidget(label)
-            line_edit = QLineEdit()
-            self.answers[question.get('name', '')] = line_edit
+        line_edits = create_line_edits(questions_list)
+        for line_edit in line_edits:
+            name = line_edit.objectName()
+            message = line_edit.property('message')
+            layout.addWidget(QLabel(message))
             layout.addWidget(line_edit)
-
-            tooltip = question.get('tooltip', None)
-            if tooltip:
-                self.tooltips[question.get('name', '')] = tooltip
-
-            validation = question.get('validation', None)
-            if validation:
-                regex = validation.get('regex', '')
-                error_message = validation.get('error_message', '')
-                self.validators[question.get('name', '')] = (regex, error_message)
+            self.answers[name] = line_edit
 
         # Add OK and Cancel buttons
         button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
-        button_box.accepted.connect(self.validate_and_accept)
+        button_box.accepted.connect(self.accept)
         button_box.rejected.connect(self.reject)
         layout.addWidget(button_box)
 
         self.setLayout(layout)
 
-    def validate_and_accept(self):
-        # Check for any validation errors before accepting the dialog
-        for name, (regex, error_message) in self.validators.items():
-            line_edit = self.answers[name]
-            text = line_edit.text()
-            if not re.match(regex, text):
-                QMessageBox.warning(self, 'Validation Error', error_message)
-                return
-
-        # If there are no validation errors, accept the dialog
-        self.accept()
-
     def get_answers(self) -> Dict[str, Any]:
         # Return a dictionary of answers, keyed by question name
         return {name: line_edit.text() for name, line_edit in self.answers.items()}
 
-    def showEvent(self, event):
-        # Add tooltips to line edits after the dialog is shown
-        super().showEvent(event)
-        for name, tooltip in self.tooltips.items():
-            line_edit = self.answers[name]
-            line_edit.setToolTip(tooltip)
+
+def create_line_edits(questions_list: List[Dict[str, Any]]) -> List[QLineEdit]:
+    line_edits = []
+    for question in questions_list:
+        widget = question.get('widget', 'text').lower()
+        if widget == 'text':
+            line_edit = QLineEdit()
+            line_edit.setObjectName(question.get('name', ''))
+            line_edit.setProperty('message', question.get('message', ''))
+            line_edits.append(line_edit)
+
+    return line_edits
 
 
 class QtAdapter:
     def prompt(self, questions: Dict[str, Any]) -> Dict[str, Any]:
         app = QApplication(sys.argv)
         dialog = PromptDialog(questions)
+
+        # Set the focus traversal order for the line edits
+        line_edits = dialog.findChildren(QLineEdit)
+        for i in range(len(line_edits) - 1):
+            dialog.setTabOrder(line_edits[i], line_edits[i + 1])
+
         result = dialog.exec_()
         answers = dialog.get_answers()
         return answers if result == QDialog.Accepted else {}
+
+
+def pyqt_prompt(questions: Dict[str, Any]) -> Dict[str, Any]:
+    adapter = QtAdapter()
+    return adapter.prompt(questions)
 
 ##
 # TODO new adapter for Agents (inter-agent messaging)
