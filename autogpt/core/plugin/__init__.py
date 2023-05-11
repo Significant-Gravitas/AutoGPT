b4190fbe0b0e154cb49c438318e7a4c70389ca32
James Collins <collijk@uw.edu>
2023-05-11 14:02:17 -0700

    Status note update for plugins after discussion with Nick

--- autogpt/core/plugin/__init__.py
+++ autogpt/core/plugin/__init__.py
@@ -10,5 +10,8 @@ status = Status(
         "5/6-5/7: First draft of plugin interface complete.\n"
         "5/8: Plugin interface has been adjusted for use with the agent factory as we integrate with hello world. Still working on it.\n"
         "5/10: Interface is pretty solid at this point and limited version of initial implementation is done. Interface revisions submitted in PR and merged.\n"
+        "5/11: Talked with Nick about plans for the plugin service including which plugins we'll support out of the box and which loading formats\n"
+        "      we'll support.  Both are documented in the code. Also discussed how to handle listener plugins (punt, app server will deal with it).\n"
+        "      and plugin metrics (punt, app server will deal with it).\n"
     ),
 )
