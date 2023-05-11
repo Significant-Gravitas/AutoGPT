e7dea8167e7bb89945bdb393f2b410c6d0177314
James Collins <collijk@uw.edu>
2023-05-11 11:27:05 -0700

    Update language model init and status

--- autogpt/core/model/__init__.py
+++ autogpt/core/model/__init__.py
@@ -1,11 +1,14 @@
 """The language model acts as the core intelligence of the Agent."""
-from autogpt.core.llm.base import LanguageModel
+from autogpt.core.model.base import Model, ModelInfo, ModelResponse
+from autogpt.core.model.embedding.base import EmbeddingModel, EmbeddingModelInfo, EmbeddingModelResponse
+from autogpt.core.model.language.base import LanguageModel, LanguageModelInfo, LanguageModelResponse
 from autogpt.core.status import ShortStatus, Status
 
 status = Status(
     module_name=__name__,
     short_status=ShortStatus.INTERFACE_DONE,
     handoff_notes=(
-        "Before times: Interface has been created. Next up is creating a basic implementation."
+        "Before times: Interface has been created. Next up is creating a basic implementation.\n"
+        "5/11: Refactored interface to split out embedding and language models.\n"
     ),
 )
