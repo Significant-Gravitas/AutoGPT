d52b88a283fd356c484c4c8ddb4284974a6de8fe
James Collins <collijk@uw.edu>
2023-05-11 16:30:09 -0700

    Add credentials manager

---
+++ autogpt/core/credentials/__init__.py
@@ -0,0 +1,11 @@
+from autogpt.core.credentials.base import CredentialsManager
+from autogpt.core.status import ShortStatus, Status
+
+status = Status(
+    module_name=__name__,
+    short_status=ShortStatus.IN_PROGRESS,
+    handoff_notes=(
+        "5/11: Realized we need something to manage credentials now or commit ourselves to leaving a pretty big mess.\n"
+        "      Created a credentials manager and a credentials provider for openai.\n"
+    ),
+)
---
+++ autogpt/core/credentials/base.py
@@ -0,0 +1,33 @@
+from typing import Dict
+
+from autogpt.core.configuration import Configuration
+
+
+class CredentialsManager:
+    configuration_defaults = {
+        "credentials": {
+            "openai": {
+                "api_key": "YOUR_API_KEY",
+                "azure_configuration": {
+                    "api_type": "azure",
+                    "api_base": "YOUR_AZURE_API_BASE",
+                    "api_version": "YOUR_AZURE_API_VERSION",
+                    "deployment_ids": {
+                        "fast_model": "YOUR_FAST_LLM_MODEL_DEPLOYMENT_ID",
+                        "smart_model": "YOUR_SMART_LLM_MODEL_DEPLOYMENT_ID",
+                        "embedding_model": "YOUR_EMBEDDING_MODEL_DEPLOYMENT_ID",
+                    },
+                },
+            },
+        },
+    }
+
+    def __init__(self, configuration: Configuration):
+        self._configuration = configuration.credentials
+
+    def add_credentials(self, service_name: str, credentials: Dict):
+        self._configuration[service_name] = credentials
+        # TODO: Save to file.
+
+    def get_credentials(self, service_name: str) -> Dict[str, Dict]:
+        return self._configuration[service_name].copy()
