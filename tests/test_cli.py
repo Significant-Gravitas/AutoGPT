f7d7755c30b80e032fc538776710b0aa57dc162c
Luke lc0rp (pr-0f3t) <2609441+lc0rp@users.noreply.github.com>
2023-05-12 05:41:46 +0000

    Fixing tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,20 +1,36 @@
+import pytest
 from click.testing import CliRunner
+
 from autogpt import cli
 from autogpt.config.config import Config
 from autogpt.configurator import GPT_3_MODEL, GPT_4_MODEL
 
-def test_gpt4only_cli_arg():
+
+@pytest.fixture(scope="function")
+def mocked_config():
+    global Config
+    oldConfigClass = Config
+
+    class MockConfig(oldConfigClass):
+        pass
+
+    yield
+    Config = oldConfigClass
+
+
+def test_gpt4only_cli_arg(mocked_config):
     runner = CliRunner()
-    runner.invoke(cli.main, ['--gpt4only'])
-    
+    runner.invoke(cli.main, ["--gpt4only"])
+
     config = Config()
     assert config.smart_llm_model == GPT_4_MODEL
     assert config.fast_llm_model == GPT_4_MODEL
 
-def test_gpt3only_cli_arg():
+
+def test_gpt3only_cli_arg(mocked_config):
     runner = CliRunner()
-    runner.invoke(cli.main, ['--gpt3only'])
-    
+    runner.invoke(cli.main, ["--gpt3only"])
+
     config = Config()
     assert config.smart_llm_model == GPT_3_MODEL
     assert config.fast_llm_model == GPT_3_MODEL
