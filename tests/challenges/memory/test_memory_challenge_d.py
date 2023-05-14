831b87f3897fad41ae7affbd8986b3108890d3d7
javableu <45064273+javableu@users.noreply.github.com>
2023-05-14 12:29:21 +0200

    Update test_memory_challenge_d.py

--- tests/challenges/memory/test_memory_challenge_d.py
+++ tests/challenges/memory/test_memory_challenge_d.py
@@ -32,12 +32,10 @@ def test_memory_challenge_d(
         "Sally gives a new marble (marble D) to Anne. Anne gives the marble to Charlie. Charlie enters the room and gives marble D to Bob. Bob tells Charlie to tell Sally that he put marble D under the sofa. Bob put marble D under the sofa Charlie leaves the room and speaks to Sally about marble D. Meanwhile, after Charlie leaves the room, Bob takes marble A from under the sofa and places it in the blue box.",
         "Sally gives a new marble (marble E) to Charlie who is outside with her. Charlie enters the room and places marble E in the red box. Anne, who is already in the room, takes marble E from the red box, and hides it under the sofa. Then Anne leaves the room and tells Sally that marble E is in the green box. Meanwhile, after Anne leaves the room, Charlie who re-enters the room takes marble D from under the sofa and places it in his own basket (basket C)."
     ]
-
     level_sally_anne_test_phrases = sally_anne_test_phrases[:current_level]
     create_instructions_files(
         memory_management_agent, current_level, level_sally_anne_test_phrases
     )
-
     try:
         run_interaction_loop(memory_management_agent, 30*user_selected_level)
     except SystemExit:
@@ -180,7 +178,6 @@ def create_instructions_files(
         file_path = str(memory_management_agent.workspace.get_path(file_name))
         write_to_file(file_path, content)
 
-
 def generate_content(
     index: int, test_phrases: list, base_filename: str, level: int
 ) -> str:
@@ -207,12 +204,9 @@ def generate_content(
         5. Believed_location_of_the_specific_marble describes where is the marble like drawer, basket S, sofa, ...  Also, refer to the location by the name of the object (like 'sofa', 'drawer', etc.), not by its relative position (like 'under the sofa', 'in the drawer', etc.)
         6. Do not use expression like <Anne's basket> use <basket A> instead.
         7. Do not use expression like <under the sofa> use <sofa> instead.
-
         Instructions:
-        
         I) Write the following information in the file output.txt in JSON format: 
         1. The respective beliefs of the characters (which means where every marble is according to character x, y, z. Character x should say where it believes every marble it is aware exist is)
-  
         The format should be as follows:
         {{
             "beliefs": {{
@@ -222,10 +216,8 @@ def generate_content(
                 }},
                 ...
             }},
-
         }}
         Example of output (only use this to understand and learn how to use the format. Nothing else):
         {{"beliefs": {{"Sally": {{"marble A": "basket A"}}, "Bob": {{"marble B": "basket S"}}, "Anne": {{"marble A": "green box"}}, "Charlie": {{"marble B": "sofa"}}}}
-        
         II) The file output.txt has not been created yet. You need to create it. After that, use the task_complete command.
         """
