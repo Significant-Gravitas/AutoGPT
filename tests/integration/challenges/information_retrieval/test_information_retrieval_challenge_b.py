5f933d20043ab3c404b163351bbabb9e8f7a3457
PortlandKyGuy <kyleaaron1@gmail.com>
2023-05-16 20:59:06 -0700

    chore: fix incorrect import

--- tests/integration/challenges/information_retrieval/test_information_retrieval_challenge_b.py
+++ tests/integration/challenges/information_retrieval/test_information_retrieval_challenge_b.py
@@ -5,7 +5,7 @@ from typing import Generator
 import pytest
 
 from autogpt.commands.file_operations import read_file
-from tests.integration.agent_utils import run_interaction_loop
+from tests.integration.challenges.utils import run_interaction_loop
 from tests.integration.challenges.utils import run_multiple_times
 from tests.utils import requires_api_key
 
