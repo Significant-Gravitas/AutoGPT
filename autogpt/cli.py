920b53f558092a9411164c18444f0f4f276a69bb
DGdev91 <DGdev91@users.noreply.github.com>
2023-05-17 19:12:10 +0200

    Make prompt parameters configurable (#3375)
    Co-authored-by: Nicholas Tindle <nick@ntindle.com>
    Co-authored-by: k-boikov <64261260+k-boikov@users.noreply.github.com>

--- autogpt/cli.py
+++ autogpt/cli.py
@@ -16,6 +16,11 @@ import click
     help="Specifies which ai_settings.yaml file to use, will also automatically skip the re-prompt.",
 )
 @click.option(
+    "--prompt-settings",
+    "-P",
+    help="Specifies which prompt_settings.yaml file to use.",
+)
+@click.option(
     "-l",
     "--continuous-limit",
     type=int,
@@ -66,6 +71,7 @@ def main(
     continuous: bool,
     continuous_limit: int,
     ai_settings: str,
+    prompt_settings: str,
     skip_reprompt: bool,
     speak: bool,
     debug: bool,
@@ -91,6 +97,7 @@ def main(
             continuous,
             continuous_limit,
             ai_settings,
+            prompt_settings,
             skip_reprompt,
             speak,
             debug,
