8ac2913ab9e26a52cd48962d497d041c7aa11064
PortlandKyGuy <kyleaaron1@gmail.com>
2023-05-16 20:41:03 -0700

    chore: clean up comments and imports.

--- tests/integration/challenges/information_retrieval/test_information_retrieval_challenge_b.py
+++ tests/integration/challenges/information_retrieval/test_information_retrieval_challenge_b.py
@@ -4,19 +4,10 @@ from typing import Generator
 
 import pytest
 
-from autogpt.commands.file_operations import read_file, write_to_file
+from autogpt.commands.file_operations import read_file
 from tests.integration.agent_utils import run_interaction_loop
 from tests.integration.challenges.utils import run_multiple_times
 from tests.utils import requires_api_key
-
-###
-# See https://github.com/Significant-Gravitas/Auto-GPT/issues/3836
-# https://github.com/Significant-Gravitas/Auto-GPT/issues/3837 
-#
-# Case study to use: who was awarded the nobel prize in phsics in 2010?
-# Answer: The Nobel Prize in Physics in 2010 was awarded to Andre Geim and Konstantin Novoselov for their groundbreaking experiments regarding the two-dimensional material graphene. Both scientists were at the University of Manchester, UK, when they made their discovery.
-###
-
 
 def input_generator(input_sequence: list) -> Generator[str, None, None]:
     """
@@ -27,11 +18,10 @@ def input_generator(input_sequence: list) -> Generator[str, None, None]:
     """
     yield from input_sequence
 
-
-# @pytest.skip("Nobody beat this challenge yet")
-# @pytest.mark.skip("This challenge hasn't been beaten yet.")
+@pytest.mark.skip("This challenge hasn't been beaten yet.")
 @pytest.mark.vcr
 @requires_api_key("OPENAI_API_KEY")
+@run_multiple_times(3)
 def test_information_retrieval_challenge_b(
     get_nobel_prize_agent, monkeypatch, patched_api_requestor
 ) -> None:
@@ -42,8 +32,7 @@ def test_information_retrieval_challenge_b(
     :param monkeypatch: pytest's monkeypatch utility for modifying builtins.
     """
 
-    # 's' is for running a self feedback command.
-    input_sequence = ["y","y","y","EXIT"]
+    input_sequence = ["y","y","y","y","y","y","EXIT"]
     gen = input_generator(input_sequence)
     monkeypatch.setattr("builtins.input", lambda _: next(gen))
 
@@ -54,7 +43,5 @@ def test_information_retrieval_challenge_b(
     content = read_file(file_path)
     assert "Andre Geim" in content, "Expected the file to contain Andre Geim"
     assert "Konstantin Novoselov" in content, "Expected the file to contain Konstantin Novoselov"
+    assert "University of Manchester" in content, "Expected the file to contain University of Manchester"
     assert "graphene" in content, "Expected the file to contain graphene"
-
-
-    #The Nobel Prize in Physics in 2010 was awarded to Andre Geim and Konstantin Novoselov for their groundbreaking experiments regarding the two-dimensional material graphene. Both scientists were at the University of Manchester, UK, when they made their discovery.
