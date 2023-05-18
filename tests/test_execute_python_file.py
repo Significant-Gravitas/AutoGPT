b002d123042ce7ebbe7e6942f2ad9c2c245e8ad3
Mauro Druwel <mauro.druwel@gmail.com>
2023-05-18 19:14:10 +0200

    Edit pytest to meet the requirements

--- tests/test_execute_python_file.py
+++ tests/test_execute_python_file.py
@@ -1,8 +1,10 @@
 import os
 from io import StringIO
 from unittest.mock import patch
+
 import pytest
-from autogpt.commands.execute_code import execute_python_file
+
+from autogpt.commands.execute import execute_python_file
 
 @pytest.fixture
 def temp_python_file(request):
