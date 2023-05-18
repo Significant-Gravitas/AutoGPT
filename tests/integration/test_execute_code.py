fd0732bdf2a632568a4d6eb1a511704ac8b17969
Mauro Druwel <mauro.druwel@gmail.com>
2023-05-18 20:41:37 +0200

    Add args to original pytest

--- tests/integration/test_execute_code.py
+++ tests/integration/test_execute_code.py
@@ -23,6 +23,15 @@ def python_test_file(config: Config, random_string):
     yield temp_file.name
     temp_file.close()
 
+@pytest.fixture
+def python_test_file_args(config: Config):
+    temp_file = tempfile.NamedTemporaryFile(dir=config.workspace_path, suffix=".py")
+    temp_file.write(str.encode(f"import sys/nprint(sys.argv[1], sys.argv[2])"))
+    temp_file.flush()
+
+    yield temp_file.name
+    temp_file.close()
+
 
 @pytest.fixture
 def random_string():
@@ -32,6 +41,11 @@ def random_string():
 def test_execute_python_file(python_test_file: str, random_string: str):
     result = sut.execute_python_file(python_test_file)
     assert result == f"Hello {random_string}!\n"
+
+def test_execute_python_file_args(python_test_file_args: str, random_string: str):
+    random_args = random_string + " " + random_string
+    result = sut.execute_python_file(python_test_file_args, random_args)
+    assert result == f"{random_args}!\n"
 
 
 def test_execute_python_file_invalid():
--- tests/scripts/exampleargs.py
+++
@@ -1,3 +0,0 @@
-import sys
-
-print(sys.argv[1], sys.argv[2])
