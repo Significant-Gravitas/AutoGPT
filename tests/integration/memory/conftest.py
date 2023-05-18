6c821e7d9950b285d53b6363f08a50a21ce74be3
Reinier van der Leer <github@pwuts.nl>
2023-05-18 22:14:17 +0200

    Fix `test_json_memory_get_relevant` in CI

--- tests/integration/memory/conftest.py
+++ tests/integration/memory/conftest.py
@@ -2,7 +2,8 @@ import numpy
 import pytest
 from pytest_mock import MockerFixture
 
-import autogpt.memory.vector.utils as memory_utils
+import autogpt.memory.vector.memory_item as memory_item
+import autogpt.memory.vector.providers.base as memory_provider_base
 from autogpt.config.config import Config
 from autogpt.llm.providers.openai import OPEN_AI_EMBEDDING_MODELS
 from autogpt.memory.vector.memory_item import MemoryItem
@@ -10,22 +11,27 @@ from autogpt.memory.vector.utils import Embedding
 
 
 @pytest.fixture
-def embeding_dimension(config: Config):
+def embedding_dimension(config: Config):
     return OPEN_AI_EMBEDDING_MODELS[config.embedding_model].embedding_dimensions
 
 
 @pytest.fixture
-def mock_get_embedding(mocker: MockerFixture, embeding_dimension: int):
+def mock_get_embedding(mocker: MockerFixture, embedding_dimension: int):
     mocker.patch.object(
-        memory_utils,
+        memory_item,
         "get_embedding",
-        return_value=[0.0255] * embeding_dimension,
+        return_value=[0.0255] * embedding_dimension,
+    )
+    mocker.patch.object(
+        memory_provider_base,
+        "get_embedding",
+        return_value=[0.0255] * embedding_dimension,
     )
 
 
 @pytest.fixture
-def mock_embedding(embeding_dimension: int) -> Embedding:
-    return numpy.full((1, embeding_dimension), 0.0255, numpy.float32)[0]
+def mock_embedding(embedding_dimension: int) -> Embedding:
+    return numpy.full((1, embedding_dimension), 0.0255, numpy.float32)[0]
 
 
 @pytest.fixture
