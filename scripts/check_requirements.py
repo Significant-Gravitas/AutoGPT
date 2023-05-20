a29c5b9a08ac169dcccee32806e755e4bcc2d13b
EpicMo <1982742309@qq.com>
2023-05-20 10:06:13 +0800

    lint

--- scripts/check_requirements.py
+++ scripts/check_requirements.py
@@ -1,7 +1,8 @@
-import re
-import sys
 import hashlib
 import os
+import re
+import sys
+
 import pkg_resources
 
 
@@ -9,17 +10,17 @@ def main():
     requirements_file = sys.argv[1]
     # check hash
     file_hash: str
-    with open(requirements_file, 'rb') as f:
+    with open(requirements_file, "rb") as f:
         sha1obj = hashlib.sha1()
         sha1obj.update(f.read())
         hash = sha1obj.hexdigest()
-        if os.path.exists('hash.calc'):
-            last_hash = open('hash.calc').read()
+        if os.path.exists("hash.calc"):
+            last_hash = open("hash.calc").read()
             if last_hash == hash:
                 print("No requirements changed")
                 sys.exit(0)
             else:
-                os.remove('hash.calc')
+                os.remove("hash.calc")
         file_hash = hash
     with open(requirements_file, "r") as f:
         required_packages = [
@@ -34,9 +35,9 @@ def main():
             continue
         pkg = pkg_resources.Requirement.parse(required_package)
         if (
-                pkg.key not in installed_packages
-                or pkg_resources.parse_version(installed_packages[pkg.key])
-                not in pkg.specifier
+            pkg.key not in installed_packages
+            or pkg_resources.parse_version(installed_packages[pkg.key])
+            not in pkg.specifier
         ):
             missing_packages.append(str(pkg))
 
@@ -45,7 +46,7 @@ def main():
         print(", ".join(missing_packages))
         sys.exit(1)
     else:
-        with open('hash.calc', 'a') as f:
+        with open("hash.calc", "a") as f:
             f.write(file_hash)
         print("All packages are installed.")
 
