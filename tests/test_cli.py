09105a373d90d796b6b7ea4055e613d6615ba071
Luke lc0rp (pr-0f3t) <2609441+lc0rp@users.noreply.github.com>
2023-05-22 19:31:06 +0000

    Fixing linting and formating

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,14 +1,17 @@
+""" Tests for the CLI."""
 from unittest import mock
-import pytest
+
 from click.testing import CliRunner
 
 from autogpt import cli
 from autogpt.config.config import Config
 from autogpt.configurator import GPT_3_MODEL, GPT_4_MODEL
-from autogpt.singleton import Singleton
 
 
-def test_gpt4only_cli_arg():
+def test_gpt4only_cli_arg() -> None:
+    """
+    Test that the --gpt4only CLI argument sets the smart and fast LLM models to GPT-4.
+    """
     with mock.patch.object(Config, "set_smart_llm_model") as mocked_set_smart_llm_model:
         with mock.patch.object(
             Config, "set_fast_llm_model"
@@ -20,7 +23,10 @@ def test_gpt4only_cli_arg():
             mocked_set_fast_llm_model.assert_called_once_with(GPT_4_MODEL)
 
 
-def test_gpt3only_cli_arg():
+def test_gpt3only_cli_arg() -> None:
+    """
+    Test that the --gpt3only CLI argument sets the smart and fast LLM models to GPT-3.5.
+    """
     with mock.patch.object(Config, "set_smart_llm_model") as mocked_set_smart_llm_model:
         with mock.patch.object(
             Config, "set_fast_llm_model"
