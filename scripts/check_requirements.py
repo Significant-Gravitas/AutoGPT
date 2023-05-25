1f0125c64a5c8dedb805be683c0764563108fa6e
W3Warp <2129189+W3Warp@users.noreply.github.com>
2023-05-25 03:41:19 +0200

    123

--- scripts/check_requirements.py
+++ scripts/check_requirements.py
@@ -5,7 +5,7 @@ The code reads the file and extracts package names from each line, ignoring comm
 Then it compares these package names with the list of installed packages on the system.
 If required package is missing or is incompatible, it adds it to a list of missing packages.
 Prints either "Missing packages:" "All packages are installed."
-"""
+"""  # noqa: E501
 import sys
 import pkg_resources
 
@@ -14,7 +14,7 @@ def main():
     # Get the path to the requirements file from command line arguments
     requirements_file = sys.argv[1]
 
-    # Read the requirements file and extract package names from each line, ignoring comments
+    # Read the requirements file and extract package names from each line, ignoring comments  # noqa: E501
     with open(requirements_file, "r", encoding="utf-8") as f:
         required_packages = []
         for line in f:
@@ -26,16 +26,16 @@ def main():
                 required_packages.append(package_name)
 
     # Get a dictionary of installed packages on the system
-    installed_packages = {pkg.key: str(pkg.version) for pkg in pkg_resources.working_set}
+    installed_packages = {pkg.key: str(pkg.version) for pkg in pkg_resources.working_set}  # noqa: E501
 
     # Check if each required package is installed and has a compatible version
     missing_packages = []
-    for required_package in required_packages:  # Parse requirement string into a Requirement object
+    for required_package in required_packages:  # Parse requirement string into a Requirement object  # noqa: E501
         pkg = pkg_resources.Requirement.parse(required_package)
         # Check if this requirement is satisfied by any installed package
         if (
             pkg.key not in installed_packages  # Package not found
-            or not pkg.specifier.contains(installed_packages[pkg.key])  # Incompatible version
+            or not pkg.specifier.contains(installed_packages[pkg.key])  # Incompatible version  # noqa: E501
         ):
             missing_packages.append(pkg)
 
