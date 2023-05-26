b7f3489ff8462a4c26cfbe896786552912bcc448
kinance <kinance@gmail.com>
2023-05-26 23:48:16 +0900

    Updated the test according to new agent and chat_with_ai code

--- tests/test_chat.py
+++ tests/test_chat.py
@@ -4,6 +4,7 @@ import pytest
 
 from autogpt.agent import Agent
 from autogpt.config.ai_config import AIConfig
+from autogpt.llm.base import Message
 from autogpt.llm.chat import chat_with_ai
 
 
@@ -11,7 +12,6 @@ from autogpt.llm.chat import chat_with_ai
 def agent():
     ai_name = "Test AI"
     memory = MagicMock()
-    full_message_history = []
     next_action_count = 0
     command_registry = MagicMock()
     config = AIConfig()
@@ -22,7 +22,6 @@ def agent():
     agent = Agent(
         ai_name,
         memory,
-        full_message_history,
         next_action_count,
         command_registry,
         config,
@@ -36,49 +35,16 @@ def agent():
 def test_chat_with_ai_basic_response(mocker, agent):
     prompt = "Welcome to the autogpt!"
     user_input = "Hello, how are you?"
-    full_message_history = []
-    permanent_memory = agent.memory
     token_limit = 4000
     mocker.patch(
         "autogpt.llm.chat.create_chat_completion",
         return_value="I'm doing well, thank you for asking.",
     )
 
-    response = chat_with_ai(
-        agent, prompt, user_input, full_message_history, permanent_memory, token_limit
-    )
+    response = chat_with_ai(agent, prompt, user_input, token_limit)
 
     assert response == "I'm doing well, thank you for asking."
-    assert full_message_history == [
-        {"role": "user", "content": "Hello, how are you?"},
-        {"role": "assistant", "content": "I'm doing well, thank you for asking."},
+    assert agent.history.messages == [
+        Message("user", "Hello, how are you?", None),
+        Message("assistant", "I'm doing well, thank you for asking.", "ai_response"),
     ]
-
-
-def test_chat_with_ai_summary_memory(mocker, agent):
-    prompt = "Welcome to the autogpt!"
-    user_input = "Hello, how are you?"
-    full_message_history = [
-        {"role": "user", "content": "Hello, how are you?"},
-        {"role": "assistant", "content": "I'm doing well, thank you for asking."},
-    ]
-    permanent_memory = agent.memory
-    token_limit = 4000
-    mocker.patch(
-        "autogpt.llm.chat.create_chat_completion",
-        return_value="I'm doing well, thank you for asking.",
-    )
-    mocker.patch(
-        "autogpt.memory_management.summary_memory.create_chat_completion",
-        return_value="I was created and nothing new has happened.",
-    )
-
-    response = chat_with_ai(
-        agent, prompt, user_input, full_message_history, permanent_memory, token_limit
-    )
-
-    assert response == "I'm doing well, thank you for asking."
-    assert agent.summary_memory == {
-        "role": "system",
-        "content": "This reminds you of these events from your past: \nI was created and nothing new has happened.",
-    }
