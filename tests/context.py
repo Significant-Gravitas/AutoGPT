8a80cf5a29ce0d304ba0f8ba7961fb50b81d2df0
World Wide Web <2129189+W3Warp@users.noreply.github.com>
2023-05-28 00:52:31 +0200

    The changes made in this commit include: - Modified tests/context.py to remove unnecessary line breaks and improve readability. - Deleted the line break between the import statements and the comment.

--- tests/context.py
+++ tests/context.py
@@ -2,6 +2,4 @@ import os
 import sys
 
 # Add the scripts directory to the path so that we can import the browse module.
-sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../scripts"))
-)
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../scripts")))
