46f1463ca105d26df56afa6a2679d89ea2663694
World Wide Web <2129189+W3Warp@users.noreply.github.com>
2023-05-28 12:51:13 +0200

    The changes made in this commit include: - Modified autogpt/config/ai_config.py to remove unused imports and change the type annotations for prompt_generator and command_registry from Union[Type1, Type2] | None to just None.

--- autogpt/config/ai_config.py
+++ autogpt/config/ai_config.py
@@ -12,7 +12,6 @@ from typing import Optional
 import distro
 import yaml
 
-from autogpt.commands.command import CommandRegistry
 from autogpt.prompts.generator import PromptGenerator
 
 # Soon this will go in a folder where it remembers more stuff about the run(s)
@@ -54,8 +53,8 @@ class AIConfig:
         self.ai_role = ai_role
         self.ai_goals = ai_goals
         self.api_budget = api_budget
-        self.prompt_generator: PromptGenerator | None = None
-        self.command_registry: CommandRegistry | None = None
+        self.prompt_generator = None
+        self.command_registry = None
 
     @staticmethod
     def load(config_file: str = SAVE_FILE) -> "AIConfig":
