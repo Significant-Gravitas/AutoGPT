fbd15d9ad55fccf11fa21c5c8182777ab0e53e3f
Luke lc0rp (pr-0f3t) <2609441+lc0rp@users.noreply.github.com>
2023-05-29 03:13:03 +0000

    Fixing tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,12 +2,16 @@
 from unittest import mock
 
 from click.testing import CliRunner
+import pytest
 
 from autogpt import cli
 from autogpt.config.config import Config
 from autogpt.configurator import GPT_3_MODEL, GPT_4_MODEL
+from tests.utils import requires_api_key
 
 
+@pytest.mark.vcr
+@requires_api_key("OPENAI_API_KEY")
 def test_gpt4only_cli_arg() -> None:
     """
     Test that the --gpt4only CLI argument sets the smart and fast LLM models to GPT-4.
@@ -16,12 +20,17 @@ def test_gpt4only_cli_arg() -> None:
         with mock.patch.object(
             Config, "set_fast_llm_model"
         ) as mocked_set_fast_llm_model:
-            runner = CliRunner()
-            runner.invoke(cli.main, ["--gpt4only"])
-            mocked_set_smart_llm_model.assert_called_with(GPT_4_MODEL)
-            mocked_set_fast_llm_model.assert_called_with(GPT_4_MODEL)
+            with mock.patch("openai.Model.list") as mock_list_models:
+                # Test when correct model is returned
+                mock_list_models.return_value = {"data": [{"id": "gpt-4"}]}
+                runner = CliRunner()
+                runner.invoke(cli.main, ["--gpt4only"])
+                mocked_set_smart_llm_model.assert_called_with(GPT_4_MODEL)
+                mocked_set_fast_llm_model.assert_called_with(GPT_4_MODEL)
 
 
+@pytest.mark.vcr
+@requires_api_key("OPENAI_API_KEY")
 def test_gpt3only_cli_arg() -> None:
     """
     Test that the --gpt3only CLI argument sets the smart and fast LLM models to GPT-3.5.
