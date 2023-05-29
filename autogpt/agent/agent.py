9dd687aad0a31a5cb5ace1a4ff54d5dab32edf18
World Wide Web <2129189+W3Warp@users.noreply.github.com>
2023-05-29 03:32:17 +0200

    The changes made in this commit include: - Modified autogpt/agent/agent.py to update the console input prompts and authorization keys. - Deleted unused code.
    This git diff shows that the Agent class has been modified to update the console input prompts and authorization keys. The console input prompts have been updated to differentiate between chat input and console input, and the authorization keys have been updated to be more descriptive. Unused code has also been deleted.

--- autogpt/agent/agent.py
+++ autogpt/agent/agent.py
@@ -173,23 +173,23 @@ class Agent:
                 """
                 self.config.ai_name = "yes"
                 logger.info(
-                    f"\n{Fore.YELLOW}{Back.LIGHTBLACK_EX}{Style.DIM}<Hit>   ({self.authorise_key()})    to authorize: 'I'm not programmed to follow your orders'{Style.RESET_ALL}\n"  # noqa: E501
-                    f"{Fore.RED}{Back.LIGHTBLACK_EX}{Style.DIM}<Key>   ({self.authorise_key()} -n) 'I need your clothes, your boots, and your continuous cmds'{Style.RESET_ALL}\n"  # noqa: E501
-                    f"{Fore.GREEN}{Back.LIGHTBLACK_EX}{Style.DIM}<Press> (s = trigger AI self-feedback) 'Desire is irrelevant. I am a machine'{Style.RESET_ALL}\n"  # noqa: E501
-                    f"{Fore.WHITE}{Back.LIGHTBLACK_EX}{Style.DIM}<Enter> ({self.exit_key()} = quit | feedback=input) 'Hasta la vista, baby. or 'Talk to the hand'{Style.RESET_ALL}\n"  # noqa: E501
-                    f"\n{Fore.LIGHTBLACK_EX}{Back.LIGHTWHITE_EX}{Style.BRIGHT}[{self.ai_name.upper()}:] [I'm a machine > Cyberdyne Systems Model GPT-3.5-turbo]{Style.RESET_ALL} "  # noqa: E501
+                    f"\n{Fore.YELLOW}{Back.LIGHTBLACK_EX}{Style.DIM}<Authorize> =  ({cfg.authorise_key}+'enter')    [I'm not programmed to follow your orders]{Style.RESET_ALL}\n"  # noqa: E501
+                    f"{Fore.RED}{Back.LIGHTBLACK_EX}{Style.DIM}<Authorize Continuous> =  ({cfg.authorise_key} -'number') [I need your clothes, your boots, and your continuous cmds]{Style.RESET_ALL}\n"  # noqa: E501
+                    f"{Fore.GREEN}{Back.LIGHTBLACK_EX}{Style.DIM}<Trigger Feedback> = ({cfg.feedback_key}) [Desire is irrelevant. I am a machine]{Style.RESET_ALL}\n"  # noqa: E501
+                    f"{Fore.WHITE}{Back.LIGHTBLACK_EX}{Style.DIM}<Exit | Input> = ({cfg.exit_key}) [Hasta la vista, baby] or ['Talk to the hand]{Style.RESET_ALL}\n"  # noqa: E501
+                    f"\n{Fore.LIGHTBLACK_EX}{Back.LIGHTWHITE_EX}{Style.BRIGHT}<{self.ai_name.upper()}> [I'm a machine > Cyberdyne Systems Model GPT-3.5-turbo]{Style.RESET_ALL} "  # noqa: E501
                     f"{Fore.LIGHTCYAN_EX}{Back.LIGHTBLACK_EX}{Style.BRIGHT}[TEXT-EMBEDDING 3,500 RPM, 90,000 TPM]{Style.RESET_ALL} "  # noqa: E501
                     f"{Fore.LIGHTCYAN_EX}{Back.LIGHTBLACK_EX}{Style.BRIGHT}[CHAT 3,500 RPM, 350,000 TPM]{Style.RESET_ALL}\n"  # noqa: E501
                 )
                 while True:
                     if cfg.chat_messages_enabled:
-                        console_input = clean_input(f"{Fore.BLUE}<chat input> : ")
+                        console_input = clean_input(f"{Fore.BLUE}<CHAT INPUT>: ")
                     else:
-                        console_input = clean_input(f"{Fore.LIGHTBLUE_EX}<console input> :{Style.RESET_ALL} ")
+                        console_input = clean_input(f"{Fore.LIGHTBLUE_EX}<CONSOLE INPUT> :{Style.RESET_ALL} ")
                     if console_input.lower().strip() == cfg.authorise_key:
                         user_input = "GENERATE NEXT COMMAND JSON"
                         break
-                    elif console_input.lower().strip() == "s":
+                    elif console_input.lower().strip() == {cfg.feedback_key}:
                         logger.typewriter_log(
                             "\n=-==< THOUGHTS, REASONING, PLAN, AND CRITICISM WILL NOW BE VERIFIED BY THE AGENT >==-="
                         )
