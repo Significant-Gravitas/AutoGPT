8089bebb6d530a4d1b81dc50d2bc46049f70c0ab
Mauro Druwel <mauro.druwel@gmail.com>
2023-05-29 09:07:31 +0200

    Edit Test With List Syntax

--- tests/integration/test_execute_code.py
+++ tests/integration/test_execute_code.py
@@ -45,9 +45,9 @@ def test_execute_python_file(python_test_file: str, random_string: str):
 
 
 def test_execute_python_file_args(python_test_file_args: str, random_string: str):
-    random_args = random_string + " " + random_string
+    random_args = [random_string, random_string]
     result = sut.execute_python_file(python_test_file_args, random_args)
-    assert result == f"{random_args}\n"
+    assert result == " ".join(random_args) + "\n"
 
 
 def test_execute_python_file_invalid():
