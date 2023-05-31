aac026b07a517e4667cb954a8b4d47fce3f2b362
World Wide Web <2129189+W3Warp@users.noreply.github.com>
2023-05-31 21:49:17 +0200

    The changes made in this commit include: - Modified prompts_welcome_msg.txt to reduce the number of characters and improve readability. - Modified run.bat to add support for virtual environments, autorun scripts, and improved error handling. The script now checks if required packages are installed and installs them if necessary. It also installs plugin dependencies and runs the autogpt command.

--- run.bat
+++ run.bat
@@ -5,8 +5,7 @@ setlocal enabledelayedexpansion
 for %%A in (python python3) do (
     where /Q %%A
     if !errorlevel! EQU 0 (
-        set "PYTHON_CMD=%%A"
-        goto :Found
+        set "PYTHON_CMD=%%A" & goto :FoundPython
     )
 )
 
@@ -14,15 +13,68 @@ echo Python not found. Please install Python.
 pause
 exit /B 1
 
-:Found
-%PYTHON_CMD% scripts/check_requirements.py requirements.txt
+:FoundPython
+set "VENV_DIR=%~dp0.venv"
+if not exist "%VENV_DIR%" (
+    echo Virtual environment not found. Please create a virtual environment at %VENV_DIR%.
+    pause
+    exit /B 1
+)
+
+set "ACTIVATE="
+for %%A in (%VENV_DIR%\Scripts\activate.bat %VENV_DIR%\Scripts\Activate.ps1) do (
+    if exist "%%A" (
+        set "ACTIVATE=%%A" & goto :ActivateFound
+    )
+)
+
+echo activate.bat or Activate.ps1 not found. Please check the virtual environment directory.
+pause
+exit /B 1
+
+:ActivateFound
+call "%ACTIVATE%"
+
+set "AUTORUN="
+for %%A in (%VENV_DIR%\Scripts\autorun.bat %VENV_DIR%\Scripts\autorun.ps1) do (
+    if exist "%%A" (
+        set "AUTORUN=%%A"
+        set /p "USE_AUTORUN=Do you want to use the autorun script at %AUTORUN%? [Y/n] "
+        if /i "!USE_AUTORUN!"=="n" (
+            set "AUTORUN="
+        ) else (
+            goto :AutotunFound
+        )
+    )
+)
+
+:AutotunFound
+
+:CheckRequirements
+rem Check if the required packages are installed
+%PYTHON_CMD% scripts/check_requirements.py %~dp0requirements.txt
 
 if errorlevel 1 (
     echo Installing missing packages ...
-    %PYTHON_CMD% -m pip install -r requirements.txt
-    echo Installing missing packages "(plugins dependencies)" ...
-    %PYTHON_CMD% scripts/install_plugin_deps.py
+    rem Install missing packages using pip
+    %PYTHON_CMD% -m pip install -r %~dp0requirements.txt
 )
 
+if defined AUTORUN (
+    rem Call the autorun script if it exists
+    if /i "!ACTIVATE:~-3!"=="ps1" (
+        powershell.exe -ExecutionPolicy RemoteSigned -File "%AUTORUN%"
+    ) else (
+        call "%AUTORUN%"
+    )
+)
+
+rem Run the autogpt command
 %PYTHON_CMD% -m autogpt %*
+
+:InstallPluginDeps
+rem Install the plugin dependencies
+echo Installing plugin dependencies ...
+%PYTHON_CMD% scripts/install_plugin_deps.py
+
 pause
