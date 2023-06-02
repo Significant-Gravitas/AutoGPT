db52d49961fed9a94e22121702e06d00f1ca4afa
Rob Jonas <rob@vspective.nl>
2023-06-02 16:51:43 +0200

    Fixed pytest and updated config
    - updated tests/test_setup.py to cover environment setting "ALLOWLISTED_PLUGINS" being available for plugins pytest
    - added "set_plugins_allowlist" + "set_plugins_denylist" to config/config.py

--- tests/integration/test_setup.py
+++ tests/integration/test_setup.py
@@ -1,17 +1,58 @@
+import os
 from unittest.mock import patch
 
 import pytest
 
+from autogpt.config import Config
 from autogpt.config.ai_config import AIConfig
 from autogpt.prompts.prompt import construct_main_ai_config
-from autogpt.setup import generate_aiconfig_automatic, prompt_user
+from autogpt.setup import prompt_user
 from tests.utils import requires_api_key
+
+
+# Make sure env is set for pytest
+@pytest.fixture(autouse=True)
+def setup_env():
+    # old_api_key = os.getenv("OPENAI_API_KEY")
+    old_plugins = os.getenv("ALLOWLISTED_PLUGINS")
+
+    try:
+        # if not old_api_key:
+        # config = Config()
+        # config.set_openai_api_key("sk-dummy")
+        if not old_plugins:
+            os.environ["ALLOWLISTED_PLUGINS"] = "plugin1,plugin2,plugin3"
+            plugins = os.environ["ALLOWLISTED_PLUGINS"].split(",")
+            config = Config()
+            config.set_plugins_allowlist(plugins)
+        yield
+
+    finally:
+        # if old_api_key:
+        # os.environ["OPENAI_API_KEY"] = old_api_key
+        # else:
+        # del os.environ["OPENAI_API_KEY"]
+        if old_plugins:
+            os.environ["ALLOWLISTED_PLUGINS"] = old_plugins
+        else:
+            del os.environ["ALLOWLISTED_PLUGINS"]
 
 
 @pytest.mark.vcr
 @requires_api_key("OPENAI_API_KEY")
-def test_generate_automatic_default(patched_api_requestor):
-    user_inputs = [""]
+def test_setup_auto_default():
+    user_inputs = [
+        "" "Chef-GPT",
+        "an AI designed to browse bake a cake.",
+        "Purchase ingredients",
+        "Bake a cake",
+        "",
+        "",
+        "",
+        "",
+        "0",
+        "",
+    ]
     with patch("builtins.input", side_effect=user_inputs):
         ai_config = prompt_user()
 
@@ -23,9 +64,22 @@ def test_generate_automatic_default(patched_api_requestor):
 
 @pytest.mark.vcr
 @requires_api_key("OPENAI_API_KEY")
-def test_generate_automatic_typical(patched_api_requestor):
-    user_prompt = "Help me create a rock opera about cybernetic giraffes"
-    ai_config = generate_aiconfig_automatic(user_prompt)
+def test_setup_auto_typical(patched_api_requestor):
+    user_inputs = [
+        "Help me create a rock opera about cybernetic giraffes",
+        "Chef-GPT",
+        "an AI designed to browse bake a cake.",
+        "Purchase ingredients",
+        "Bake a cake",
+        "",
+        "",
+        "",
+        "",
+        "0",
+        "",
+    ]
+    with patch("builtins.input", side_effect=user_inputs):
+        ai_config = prompt_user()
 
     assert isinstance(ai_config, AIConfig)
     assert ai_config.ai_name is not None
@@ -35,7 +89,7 @@ def test_generate_automatic_typical(patched_api_requestor):
 
 @pytest.mark.vcr
 @requires_api_key("OPENAI_API_KEY")
-def test_generate_automatic_fallback(patched_api_requestor):
+def test_setup_auto_fallback():
     user_inputs = [
         "T&GF£OIBECC()!*",
         "Chef-GPT",
@@ -60,7 +114,47 @@ def test_generate_automatic_fallback(patched_api_requestor):
 
 @pytest.mark.vcr
 @requires_api_key("OPENAI_API_KEY")
-def test_prompt_user_manual_mode(patched_api_requestor):
+def test_setup_create_first(tmp_path):
+    """Test: create first configuration."""
+
+    # Temporary path / file
+    temp_config_file = tmp_path / "ai_settings.yaml"
+
+    # Temporary config
+    config_content = """configs:
+    """
+
+    # Write to the temporary file
+    with open(temp_config_file, "w") as temp_file:
+        temp_file.write(config_content)
+
+    # Sequence of user inputs:
+    user_inputs = [
+        "scan the internet for new python code",
+        "Simple-GPT",
+        "find python code on the internet.",
+        "search the internet for the newest python code.",
+        "save the code in a text file.",
+        "search the internet for the newest python code.",
+        "save the code in a text file.",
+        "shutdown.",
+        "",
+        "",
+        "",
+        "0",
+    ]
+
+    # Patch function to use the user_inputs list
+    with patch("builtins.input", side_effect=user_inputs):
+        ai_config = construct_main_ai_config(str(temp_config_file))
+
+    # Asserts
+    assert ai_config.ai_name is not None and ai_config.ai_name != ""
+    assert ai_config.ai_role is not None and ai_config.ai_role != ""
+    assert ai_config.ai_goals != []
+
+
+def test_setup_manual_mode():
     user_inputs = [
         "--manual",
         "Chef-GPT",
@@ -84,7 +178,7 @@ def test_prompt_user_manual_mode(patched_api_requestor):
     assert ai_config.ai_goals == ["Purchase ingredients", "Bake a cake"]
 
 
-def test_generate_delete_and_select(tmp_path, patched_api_requestor):
+def test_setup_delete_and_select(tmp_path):
     """Test: delete configuration and select."""
 
     # Temporary path / file
@@ -137,7 +231,7 @@ def test_generate_delete_and_select(tmp_path, patched_api_requestor):
     ]
 
 
-def test_generate_change_and_select(tmp_path, patched_api_requestor):
+def test_setup_change_and_select(tmp_path):
     """Test: change configuration and select."""
 
     # Temporary path / file
@@ -198,7 +292,7 @@ def test_generate_change_and_select(tmp_path, patched_api_requestor):
     ]
 
 
-def test_generate_create_and_select(tmp_path, patched_api_requestor):
+def test_setup_create_and_select(tmp_path):
     """Test: create configuration and select."""
 
     # Temporary path / file
@@ -257,46 +351,7 @@ def test_generate_create_and_select(tmp_path, patched_api_requestor):
     ]
 
 
-@pytest.mark.vcr
-@requires_api_key("OPENAI_API_KEY")
-def test_generate_create_first(tmp_path, patched_api_requestor):
-    """Test: create first configuration."""
-
-    # Temporary path / file
-    temp_config_file = tmp_path / "ai_settings.yaml"
-
-    # Temporary config
-    config_content = """configs:
-    """
-
-    # Write to the temporary file
-    with open(temp_config_file, "w") as temp_file:
-        temp_file.write(config_content)
-
-    # Sequence of user inputs:
-    user_inputs = [
-        "scan the internet for new python code",
-        "Simple-GPT",
-        "find python code on the internet.",
-        "search the internet for the newest python code.",
-        "save the code in a text file.",
-        "search the internet for the newest python code.",
-        "save the code in a text file.",
-        "shutdown.",
-        "0",
-    ]
-
-    # Patch function to use the user_inputs list
-    with patch("builtins.input", side_effect=user_inputs):
-        ai_config = construct_main_ai_config(str(temp_config_file))
-
-    # Asserts
-    assert ai_config.ai_name is not None and ai_config.ai_name != ""
-    assert ai_config.ai_role is not None and ai_config.ai_role != ""
-    assert ai_config.ai_goals != []
-
-
-def test_generate_delete_and_create_new(tmp_path, patched_api_requestor):
+def test_setup_delete_and_create(tmp_path):
     """Test: delete and create new configuration."""
 
     # Temporary path / file
@@ -345,7 +400,7 @@ def test_generate_delete_and_create_new(tmp_path, patched_api_requestor):
     assert ai_config.ai_goals != []
 
 
-def test_generate_create_new_25(tmp_path, patched_api_requestor):
+def test_setup_create_25_goals(tmp_path):
     """Test: create new configuration + some wrong values."""
 
     # Temporary path / file
@@ -394,7 +449,7 @@ def test_generate_create_new_25(tmp_path, patched_api_requestor):
     assert ai_config.ai_name == "test-GPT"
 
 
-def test_generate_required_values_missing(tmp_path, patched_api_requestor):
+def test_setup_required_values(tmp_path):
     """Test: create new configuration without ai_name, ai_role or goals."""
 
     # Temporary path / file
@@ -437,15 +492,11 @@ def test_generate_required_values_missing(tmp_path, patched_api_requestor):
     assert ai_config.ai_name == "new-GPT"
 
 
-def test_generate_create_new_plugins(tmp_path, monkeypatch):
+def test_setup_add_single_plugin(tmp_path):
     """Test: create new configuration + some wrong values."""
 
     # Temporary path / file
     temp_config_file = tmp_path / "ai_settings.yaml"
-
-    # Set the environment variable
-    monkeypatch.setenv("ALLOWLISTED_PLUGINS", "plugin1,plugin2,plugin3")
-    monkeypatch.setenv("DENYLISTED_PLUGINS", "")
 
     # Temporary config
     config_content = """configs:
@@ -487,15 +538,11 @@ def test_generate_create_new_plugins(tmp_path, monkeypatch):
     ]
 
 
-def test_generate_create_2_plugins(tmp_path, monkeypatch):
+def test_setup_add_plugins(tmp_path):
     """Test: create new configuration + some wrong values."""
 
     # Temporary path / file
     temp_config_file = tmp_path / "ai_settings.yaml"
-
-    # Set the environment variable
-    monkeypatch.setenv("ALLOWLISTED_PLUGINS", "plugin1,plugin2,plugin3")
-    monkeypatch.setenv("DENYLISTED_PLUGINS", "")
 
     # Temporary config
     config_content = """configs:
@@ -542,15 +589,11 @@ def test_generate_create_2_plugins(tmp_path, monkeypatch):
     ]
 
 
-def test_generate_change_2_plugins(tmp_path, monkeypatch):
+def test_setup_change_plugins(tmp_path):
     """Test: create new configuration + some wrong values."""
 
     # Temporary path / file
     temp_config_file = tmp_path / "ai_settings.yaml"
-
-    # Set the environment variable
-    monkeypatch.setenv("ALLOWLISTED_PLUGINS", "plugin1,plugin2,plugin3")
-    monkeypatch.setenv("DENYLISTED_PLUGINS", "")
 
     # Temporary config
     config_content = """configs:
