cb61b210cf6562871146e5986e7983ade38db232
Merwane Hamadi <merwanehamadi@gmail.com>
2023-06-03 21:09:26 -0700

    Fix CI git authentication and cassettes

--- tests/integration/agent_factory.py
+++ tests/integration/agent_factory.py
@@ -81,7 +81,7 @@ def writer_agent(agent_test_config, memory_none: NoMemory, workspace: Workspace)
         ai_role="an AI designed to use the write_to_file command to write 'Hello World' into a file named \"hello_world.txt\" and then use the task_complete command to complete the task.",
         ai_goals=[
             "Use the write_to_file command to write 'Hello World' into a file named \"hello_world.txt\".",
-            "Use the task_complete command to complete the task.",
+            "Use the task_complete command to complete the task!",
             "Do not use any other commands.",
         ],
     )
