53fc25c2fc5ba8fd77937e29d763171d2dcba1e0
ccdanpian <130242490+ccdanpian@users.noreply.github.com>
2023-06-09 19:17:53 +0800

    Update modelsinfo.py

--- autogpt/llm/modelsinfo.py
+++ autogpt/llm/modelsinfo.py
@@ -1,6 +1,7 @@
 COSTS = {
     "gpt-3.5-turbo": {"prompt": 0.002, "completion": 0.002},
     "gpt-35-turbo": {"prompt": 0.002, "completion": 0.002}, # add azure gpt3.5 turbo model'name,(gpt-35-trubo is the name of azure gpt3.5 trubo)
+    "gpt-35-turbo-0301": {"prompt": 0.002, "completion": 0.002},
     "gpt-3.5-turbo-0301": {"prompt": 0.002, "completion": 0.002},
     "gpt-4-0314": {"prompt": 0.03, "completion": 0.06},
     "gpt-4": {"prompt": 0.03, "completion": 0.06},
