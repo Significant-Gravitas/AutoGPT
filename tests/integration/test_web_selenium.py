a2a02f757a05f2e94fa2d5666385d58088f64bf9
Erik Peterson <e@eriklp.com>
2023-06-10 13:16:00 -0700

    Implement directory-based plugin system (#4548)
    * Implement directory-based plugin system
    
    * Fix Selenium test
    
    ---------
    
    Co-authored-by: Nicholas Tindle <nick@ntindle.com>
    Co-authored-by: Merwane Hamadi <merwanehamadi@gmail.com>

--- tests/integration/test_web_selenium.py
+++ tests/integration/test_web_selenium.py
@@ -1,9 +1,13 @@
+import pytest
 from pytest_mock import MockerFixture
 
 from autogpt.commands.web_selenium import browse_website
 from autogpt.config import Config
+from tests.utils import requires_api_key
 
 
+@pytest.mark.vcr
+@requires_api_key("OPENAI_API_KEY")
 def test_browse_website(config: Config, patched_api_requestor: MockerFixture):
     url = "https://barrel-roll.com"
     question = "How to execute a barrel roll"
