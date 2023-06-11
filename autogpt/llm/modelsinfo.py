1165afe3f21124956aefe376248f74ce04cddd42
ccdanpian <zhanghua.x@gmail.com>
2023-06-12 07:55:40 +0800

    fix issue #4612

--- autogpt/llm/modelsinfo.py
+++ autogpt/llm/modelsinfo.py
@@ -1,6 +1,8 @@
 COSTS = {
     "gpt-3.5-turbo": {"prompt": 0.002, "completion": 0.002},
     "gpt-3.5-turbo-0301": {"prompt": 0.002, "completion": 0.002},
+    "gpt-35-turbo": {"prompt": 0.002, "completion": 0.002},
+    "gpt-35-turbo-0301": {"prompt": 0.002, "completion": 0.002},
     "gpt-4-0314": {"prompt": 0.03, "completion": 0.06},
     "gpt-4": {"prompt": 0.03, "completion": 0.06},
     "gpt-4-0314": {"prompt": 0.03, "completion": 0.06},
