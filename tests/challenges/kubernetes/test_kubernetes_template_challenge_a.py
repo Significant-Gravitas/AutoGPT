35e9317beb8e9d0d62bcac26cb87b10bac6d5bdf
merwanehamadi <merwanehamadi@gmail.com>
2023-06-12 08:03:14 -0700

    Add challenge name and level to pytest logs (#4661)

--- tests/challenges/kubernetes/test_kubernetes_template_challenge_a.py
+++ tests/challenges/kubernetes/test_kubernetes_template_challenge_a.py
@@ -17,6 +17,7 @@ def test_kubernetes_template_challenge_a(
     monkeypatch: pytest.MonkeyPatch,
     patched_api_requestor: MockerFixture,
     level_to_run: int,
+    challenge_name: str,
 ) -> None:
     """
     Test the challenge_a function in a given agent by mocking user inputs
@@ -27,7 +28,9 @@ def test_kubernetes_template_challenge_a(
         monkeypatch (pytest.MonkeyPatch)
         level_to_run (int)
     """
-    run_interaction_loop(monkeypatch, kubernetes_agent, CYCLE_COUNT)
+    run_interaction_loop(
+        monkeypatch, kubernetes_agent, CYCLE_COUNT, challenge_name, level_to_run
+    )
 
     file_path = get_workspace_path(kubernetes_agent, OUTPUT_LOCATION)
     content = read_file(file_path, kubernetes_agent)
