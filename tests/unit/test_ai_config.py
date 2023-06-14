cff0ccb495247e30d44ca6d7851f29dd07bb9596
Reinier van der Leer <github@pwuts.nl>
2023-06-14 21:42:18 +0200

    fix test file structure

--- tests/unit/test_ai_config.py
+++ tests/unit/test_ai_config.py
@@ -72,3 +72,45 @@ def test_ai_config_file_is_empty(workspace):
     assert ai_config.api_budget == 0.0
     assert ai_config.prompt_generator is None
     assert ai_config.command_registry is None
+
+
+def test_ai_config_command_line_overrides():
+    """
+    Test command line overrides for AI parameters are set correctly in the AI config.
+    """
+    ai_config = AIConfig.load(
+        ai_name="testGPT", ai_role="testRole", ai_goals=["testGoal"]
+    )
+
+    assert ai_config.ai_name == "testGPT"
+    assert ai_config.ai_role == "testRole"
+    assert ai_config.ai_goals == ["testGoal"]
+
+
+def test_ai_config_command_line_overrides_with_config_file():
+    """
+    Test command line overrides for AI parameters are set correctly in the AI config.
+    """
+    ai_config = AIConfig.load(
+        ai_name="testGPTOverride",
+        ai_role="testRoleOverride",
+        ai_goals=["testGoalOverride"],
+        config_file="tests/unit/data/test_ai_config.yaml",
+    )
+
+    # Should have loaded from overrides and not from config
+    assert ai_config.ai_name == "testGPTOverride"
+    assert ai_config.ai_role == "testRoleOverride"
+    assert ai_config.ai_goals == ["testGoalOverride"]
+
+
+def test_ai_config_command_line_override_singular():
+    """
+    Test we can supply one override and prompt for the rest
+    """
+
+    ai_config = AIConfig.load(ai_name="testGPTOverride")
+
+    assert ai_config.ai_name == "testGPTOverride"
+    assert ai_config.ai_role == None
+    assert ai_config.ai_goals == []
