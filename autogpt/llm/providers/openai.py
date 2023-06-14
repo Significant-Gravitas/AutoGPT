b6bf6e70a7016d91d16388729d86fb6ace1249b5
Erik Peterson <e@eriklp.com>
2023-06-13 19:06:00 -0700

    Add 16k gpt model

--- autogpt/llm/providers/openai.py
+++ autogpt/llm/providers/openai.py
@@ -29,6 +29,12 @@ OPEN_AI_CHAT_MODELS = {
             max_tokens=4096,
         ),
         ChatModelInfo(
+            name="gpt-3.5-turbo-16k-061",
+            prompt_token_cost=0.004,
+            completion_token_cost=0.003,
+            max_tokens=4096,
+        ),
+        ChatModelInfo(
             name="gpt-3.5-turbo-0301",
             prompt_token_cost=0.002,
             completion_token_cost=0.002,
