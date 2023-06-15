1213e9ecbe13cc41fd1c104259364e5cfee09b1e
Reinier van der Leer <github@pwuts.nl>
2023-06-15 23:59:15 +0200

    Fix test_make_agent

--- tests/unit/test_make_agent.py
+++ tests/unit/test_make_agent.py
@@ -11,10 +11,11 @@ def test_make_agent(agent: Agent, mocker: MockerFixture) -> None:
     mock = mocker.patch("openai.ChatCompletion.create")
 
     response = MagicMock()
-    # del response.error
-    response.choices[0].messages[0].content = "Test message"
+    response.choices[0].message.content = "Test message"
     response.usage.prompt_tokens = 1
     response.usage.completion_tokens = 1
+    del response.error
+
     mock.return_value = response
     start_agent("Test Agent", "chat", "Hello, how are you?", agent, "gpt-3.5-turbo")
     agents = list_agents(agent)
