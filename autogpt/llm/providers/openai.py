9f3a6623335aa8f61e14e384f699634c877d2c0a
Reinier van der Leer <github@pwuts.nl>
2023-06-15 18:19:03 +0200

    Unpin OpenAI model versions in model info mapping

--- autogpt/llm/providers/openai.py
+++ autogpt/llm/providers/openai.py
@@ -48,10 +48,16 @@ OPEN_AI_CHAT_MODELS = {
     ]
 }
 # Set aliases for rolling model IDs
-OPEN_AI_CHAT_MODELS["gpt-3.5-turbo"] = OPEN_AI_CHAT_MODELS["gpt-3.5-turbo-0301"]
-OPEN_AI_CHAT_MODELS["gpt-3.5-turbo-16k"] = OPEN_AI_CHAT_MODELS["gpt-3.5-turbo-16k-0613"]
-OPEN_AI_CHAT_MODELS["gpt-4"] = OPEN_AI_CHAT_MODELS["gpt-4-0314"]
-OPEN_AI_CHAT_MODELS["gpt-4-32k"] = OPEN_AI_CHAT_MODELS["gpt-4-32k-0314"]
+chat_model_mapping = {
+    "gpt-3.5-turbo":     "gpt-3.5-turbo-0301",
+    "gpt-3.5-turbo-16k": "gpt-3.5-turbo-16k-0613",
+    "gpt-4":             "gpt-4-0314",
+    "gpt-4-32k":         "gpt-4-32k-0314",
+}
+for alias, target in chat_model_mapping.items():
+    alias_info = ChatModelInfo(**OPEN_AI_CHAT_MODELS[target].__dict__)
+    alias_info.name = alias
+    OPEN_AI_CHAT_MODELS[alias] = alias_info
 
 OPEN_AI_TEXT_MODELS = {
     info.name: info
