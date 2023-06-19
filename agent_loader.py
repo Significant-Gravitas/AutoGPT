6d93f85380b78cb042cd75899f47ebc76a4148a4
thedreamakeem <137110217+thedreamakeem@users.noreply.github.com>
2023-06-19 13:30:42 -0700

    Update agent_loader.py

--- agent_loader.py
+++ agent_loader.py
@@ -1,1 +1,10 @@
+import pickle
 
+class AgentPersist:
+
+    def __init__(self):
+        pass
+
+    def save(self, agent):
+        with open("agent.pkl", "wb") as f:
+            pickle.dump(agent, f)
