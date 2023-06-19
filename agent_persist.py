fb8d61d107133d52702ae0639ec4691404aa4243
thedreamakeem <137110217+thedreamakeem@users.noreply.github.com>
2023-06-19 13:50:02 -0700

    Update agent_persist.py

--- agent_persist.py
+++ agent_persist.py
@@ -8,3 +8,5 @@ class AgentPersist:
     def save(self, agent):
         with open("agent.pkl", "wb") as f:
             pickle.dump(agent, f)
+if agent.is_saved:
+    raise ValueError("Agent is already saved.")
