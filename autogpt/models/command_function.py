fb6c9671e1a5aeada12086d08c4bc09afff415e8
merwanehamadi <merwanehamadi@gmail.com>
2023-06-21 09:19:35 -0700

    Update autogpt/models/command_function.py
    Co-authored-by: Reinier van der Leer <github@pwuts.nl>

--- autogpt/models/command_function.py
+++ autogpt/models/command_function.py
@@ -1,18 +1,10 @@
 from typing import Any
 
 
+@dataclass
 class CommandFunction:
     """Represents a "function" in OpenAI, which is mapped to a Command in Auto-GPT"""
 
-    def __init__(self, name: str, description: str, parameters: dict[str, Any]):
-        self.name = name
-        self.description = description
-        self.parameters = parameters
-
-    @property
-    def __dict__(self) -> dict:
-        return {
-            "name": self.name,
-            "description": self.description,
-            "parameters": self.parameters,
-        }
+    name: str
+    description: str
+    parameters: dict[str, Any]
