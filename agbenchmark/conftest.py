15c5469bb1aabf291864b5ba11981948b7b64fb2
Silen Naihin <silen.naihin@gmail.com>
2023-06-22 08:18:22 -0400

    Add automatic regression markers (#38)

--- agbenchmark/conftest.py
+++ agbenchmark/conftest.py
@@ -3,6 +3,7 @@ import os
 import pytest
 import shutil
 from agbenchmark.mocks.tests.retrieval_manual import mock_retrieval
+from agbenchmark.tests.regression.RegressionManager import RegressionManager
 import requests
 
 
@@ -41,3 +42,35 @@ def server_response(request, config):
     #     response.status_code == 200
     # ), f"Request failed with status code {response.status_code}"
     mock_retrieval(task, config["workspace"])
+
+
+regression_txt = "agbenchmark/tests/regression/regression_tests.txt"
+
+regression_manager = RegressionManager(regression_txt)
+
+
+def pytest_runtest_makereport(item, call):
+    """Called for each test report. Generated for each stage
+    of a test run (setup, call, teardown)."""
+    if call.when == "call":
+        if (
+            call.excinfo is None
+        ):  # if no error in the call stage, add it as a regression test
+            regression_manager.add_test(item.nodeid)
+        else:  # otherwise, :(
+            regression_manager.remove_test(item.nodeid)
+
+
+def pytest_collection_modifyitems(items):
+    """Called once all test items are collected. Used
+    to add regression marker to collected test items."""
+    for item in items:
+        print("pytest_collection_modifyitems", item.nodeid)
+        if item.nodeid + "\n" in regression_manager.tests:
+            print(regression_manager.tests)
+            item.add_marker(pytest.mark.regression)
+
+
+def pytest_sessionfinish():
+    """Called at the end of the session to save regression tests"""
+    regression_manager.save()
