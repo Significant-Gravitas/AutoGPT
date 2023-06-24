a5073ab57790a84d146877e1b6512eecbfc12b09
Silen Naihin <silen.naihin@gmail.com>
2023-06-24 09:42:36 -0400

    basic challenges, more ChallengeData structure

---
+++ agbenchmark/mocks/tests/basic_mocks.py
@@ -0,0 +1,28 @@
+from agbenchmark.Challenge import Challenge
+from ..basic_gpt_agent import basic_gpt_agent
+
+
+def basic_read_file_mock(task: str, workspace: str):
+    """
+    This mock reads a file and returns its content.
+    """
+
+    Challenge.write_to_file(workspace, "file_to_check.txt", "this is how we're doing")
+
+    file_contents = Challenge.open_file(workspace, "file_to_check.txt")
+
+    Challenge.write_to_file(
+        workspace, "file_to_check.txt", f"random string: {file_contents}"
+    )
+
+
+def basic_write_file_mock(task: str, workspace: str):
+    """
+    This mock writes to a file (creates one if it doesn't exist)
+    """
+
+    # Call the basic_gpt_agent to get a response.
+    response = basic_gpt_agent(task)
+
+    # Open the file in write mode.
+    Challenge.write_to_file(workspace, "file_to_check.txt", response)
--- agbenchmark/tests/basic_abilities/write_file/write_file_test.py
+++ agbenchmark/tests/basic_abilities/write_file/write_file_test.py
@@ -1,22 +1,22 @@
 import pytest
-from agbenchmark.challenges.retrieval.Retrieval import RetrievalChallenge
-from agbenchmark.challenges.define_task_types import ChallengeData, Ground
+from agbenchmark.challenges.define_task_types import ChallengeData
+from agbenchmark.Challenge import Challenge
 import os
 
 data = ChallengeData.deserialize(
-    os.path.join(os.path.dirname(__file__), "r1_data.json")
+    os.path.join(os.path.dirname(__file__), "w_file_data.json")
 )
 
 
-class TestRetrieval1(RetrievalChallenge):
-    """The first information-retrieval challenge"""
+class TestWriteFile(Challenge):
+    """Testing if LLM can write to a file"""
 
     @pytest.mark.parametrize(
         "server_response",
         [(data.task, data.mock_func)],
         indirect=True,
     )
-    @pytest.mark.retrieval
+    @pytest.mark.basic
     def test_retrieval(self, workspace):
         file = self.open_file(workspace, data.ground.files[0])
 
