adc6b225a6063bc2b0981f1156f25bde9279040e
Silen Naihin <silen.naihin@gmail.com>
2023-06-25 11:12:33 -0400

    update regression tests info

--- agbenchmark/tests/basic_abilities/write_file/write_file_test.py
+++ agbenchmark/tests/basic_abilities/write_file/write_file_test.py
@@ -16,6 +16,11 @@ class TestWriteFile(BasicChallenge):
         [(data.task, data.mock_func)],
         indirect=True,
     )
+    @pytest.mark.parametrize(
+        "regression_data",
+        [data],
+        indirect=True,
+    )
     @pytest.mark.depends(name="test_write_file")
     def test_write_file(self, workspace):
         file = self.open_file(workspace, data.ground.files[0])
