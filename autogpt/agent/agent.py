Significant-Gravitas/AutoGPT

commit 230956e871f0da87b7bf1a9d79453b25de62fe1e

Use Configuration of the rearch branch (#4803)

autogpt/core/configuration/schema.py
@@ -1,0 +1,98 @@
+import abc
+import copy
+import typing
+from typing import Any
+
+from pydantic import BaseModel
+
+
+class SystemConfiguration(BaseModel):
+    def get_user_config(self) -> dict[str, Any]:
+        return _get_user_config_fields(self)
+
+    class Config:
+        extra = "forbid"
+        use_enum_values = True
+
+
+class SystemSettings(BaseModel, abc.ABC):
+    """A base class for all system settings."""
+
+    name: str
+    description: typing.Optional[str]
+
+    class Config:
+        extra = "forbid"
+        use_enum_values = True
+
+
+class Configurable(abc.ABC):
+    """A base class for all configurable objects."""
+
+    prefix: str = ""
+    defaults_settings: typing.ClassVar[SystemSettings]
+
+    @classmethod
+    def get_user_config(cls) -> dict[str, Any]:
+        return _get_user_config_fields(cls.defaults_settings)
+
+    @classmethod
+    def build_agent_configuration(cls, configuration: dict = {}) -> SystemSettings:
+        """Process the configuration for this object."""
+
+        defaults_settings = cls.defaults_settings.dict()
+        final_configuration = deep_update(defaults_settings, configuration)
+
+        return cls.defaults_settings.__class__.parse_obj(final_configuration)
+
+
+def _get_user_config_fields(instance: BaseModel) -> dict[str, Any]:
+    """
+    Get the user config fields of a Pydantic model instance.
+    Args:
+        instance: The Pydantic model instance.
+    Returns:
+        The user config fields of the instance.
+    """
+    user_config_fields = {}
+
+    for name, value in instance.__dict__.items():
+        field_info = instance.__fields__[name]
+        if "user_configurable" in field_info.field_info.extra:
+            user_config_fields[name] = value
+        elif isinstance(value, SystemConfiguration):
+            user_config_fields[name] = value.get_user_config()
+        elif isinstance(value, list) and all(
+            isinstance(i, SystemConfiguration) for i in value
+        ):
+            user_config_fields[name] = [i.get_user_config() for i in value]
+        elif isinstance(value, dict) and all(
+            isinstance(i, SystemConfiguration) for i in value.values()
+        ):
+            user_config_fields[name] = {
+                k: v.get_user_config() for k, v in value.items()
+            }
+
+    return user_config_fields
+
+
+def deep_update(original_dict: dict, update_dict: dict) -> dict:
+    """
+    Recursively update a dictionary.
+    Args:
+        original_dict (dict): The dictionary to be updated.
+        update_dict (dict): The dictionary to update with.
+    Returns:
+        dict: The updated dictionary.
+    """
+    original_dict = copy.deepcopy(original_dict)
+    for key, value in update_dict.items():
+        if (
+            key in original_dict
+            and isinstance(original_dict[key], dict)
+            and isinstance(value, dict)
+        ):
+            original_dict[key] = deep_update(original_dict[key], value)
+        else:
+            original_dict[key] = value
+    return original_dict

autogpt/main.py
@@ -5,7 +5,7 @@ import sys
 from colorama import Fore, Style
 
 from autogpt.agent import Agent
-from autogpt.config import Config, check_openai_api_key
+from autogpt.config.config import Config, check_openai_api_key
 from autogpt.configurator import create_config
 from autogpt.logs import logger
 from autogpt.memory.vector import get_memory
@@ -52,7 +52,8 @@ def run_auto_gpt(
     logger.set_level(logging.DEBUG if debug else logging.INFO)
     logger.speak_mode = speak
 
-    config = Config()
+    config = Config.build_config_from_env()
+
     # TODO: fill in llm values here
     check_openai_api_key(config)
 
@@ -120,7 +121,7 @@ def run_auto_gpt(
     # HACK: doing this here to collect some globals that depend on the workspace.
     Workspace.build_file_logger_path(config, workspace_directory)
 
-    config.set_plugins(scan_plugins(config, config.debug_mode))
+    config.plugins = scan_plugins(config, config.debug_mode)
     # Create a CommandRegistry instance and scan default folder
     command_registry = CommandRegistry()
 

benchmarks.py
@@ -13,9 +13,9 @@ def run_task(task) -> None:
 
 
 def bootstrap_agent(task):
-    config = Config()
-    config.set_continuous_mode(False)
-    config.set_temperature(0)
+    config = Config.build_config_from_env()
+    config.continuous_mode = False
+    config.temperature = 0
     config.plain_output = True
     command_registry = get_command_registry(config)
     config.memory_backend = "no_memory"

data_ingestion.py
@@ -5,7 +5,7 @@ from autogpt.commands.file_operations import ingest_file, list_files
 from autogpt.config import Config
 from autogpt.memory.vector import VectorMemory, get_memory
 
-config = Config()
+config = Config.build_config_from_env()
 
 
 def configure_logging():

tests/conftest.py
@@ -49,13 +49,17 @@ def temp_plugins_config_file():
 def config(
     temp_plugins_config_file: str, mocker: MockerFixture, workspace: Workspace
 ) -> Config:
-    config = Config()
+    config = Config.build_config_from_env()
     if not os.environ.get("OPENAI_API_KEY"):
         os.environ["OPENAI_API_KEY"] = "sk-dummy"
 
     config.plugins_dir = "tests/unit/data/test_plugins"
     config.plugins_config_file = temp_plugins_config_file
-    config.load_plugins_config()
+
+    # avoid circular dependency
+    from autogpt.plugins.plugins_config import PluginsConfig
+
+    config.plugins_config = PluginsConfig.load_config(global_config=config)
 
     # Do a little setup and teardown since the config object is a singleton
     mocker.patch.multiple(
@@ -95,8 +99,7 @@ def agent(config: Config, workspace: Workspace) -> Agent:
 
     command_registry = CommandRegistry()
     ai_config.command_registry = command_registry
-
-    config.set_memory_backend("json_file")
+    config.memory_backend = "json_file"
     memory_json_file = get_memory(config)
     memory_json_file.clear()
 

autogpt/plugins/__init__.py
@@ -219,8 +219,8 @@ def scan_plugins(config: Config, debug: bool = False) -> List[AutoGPTPluginTempl
     loaded_plugins = []
     # Generic plugins
     plugins_path_path = Path(config.plugins_dir)
-    plugins_config = config.plugins_config
 
+    plugins_config = config.plugins_config
     # Directory-based plugins
     for plugin_path in [f.path for f in os.scandir(config.plugins_dir) if f.is_dir()]:
         # Avoid going into __pycache__ or other hidden directories

autogpt/configurator.py
@@ -51,13 +51,13 @@ def create_config(
         allow_downloads (bool): Whether to allow Auto-GPT to download files natively
         skips_news (bool): Whether to suppress the output of latest news on startup
     """
-    config.set_debug_mode(False)
-    config.set_continuous_mode(False)
-    config.set_speak_mode(False)
+    config.debug_mode = False
+    config.continuous_mode = False
+    config.speak_mode = False
 
     if debug:
         logger.typewriter_log("Debug Mode: ", Fore.GREEN, "ENABLED")
-        config.set_debug_mode(True)
+        config.debug_mode = True
 
     if continuous:
         logger.typewriter_log("Continuous Mode: ", Fore.RED, "ENABLED")
@@ -68,13 +68,13 @@ def create_config(
             " cause your AI to run forever or carry out actions you would not usually"
             " authorise. Use at your own risk.",
         )
-        config.set_continuous_mode(True)
+        config.continuous_mode = True
 
         if continuous_limit:
             logger.typewriter_log(
                 "Continuous Limit: ", Fore.GREEN, f"{continuous_limit}"
             )
-            config.set_continuous_limit(continuous_limit)
+            config.continuous_limit = continuous_limit
 
     # Check if continuous limit is used without continuous mode
     if continuous_limit and not continuous:
@@ -82,14 +82,14 @@ def create_config(
 
     if speak:
         logger.typewriter_log("Speak Mode: ", Fore.GREEN, "ENABLED")
-        config.set_speak_mode(True)
+        config.speak_mode = True
 
     # Set the default LLM models
     if gpt3only:
         logger.typewriter_log("GPT3.5 Only Mode: ", Fore.GREEN, "ENABLED")
         # --gpt3only should always use gpt-3.5-turbo, despite user's FAST_LLM_MODEL config
-        config.set_fast_llm_model(GPT_3_MODEL)
-        config.set_smart_llm_model(GPT_3_MODEL)
+        config.fast_llm_model = GPT_3_MODEL
+        config.smart_llm_model = GPT_3_MODEL
 
     elif (
         gpt4only
@@ -97,13 +97,11 @@ def create_config(
     ):
         logger.typewriter_log("GPT4 Only Mode: ", Fore.GREEN, "ENABLED")
         # --gpt4only should always use gpt-4, despite user's SMART_LLM_MODEL config
-        config.set_fast_llm_model(GPT_4_MODEL)
-        config.set_smart_llm_model(GPT_4_MODEL)
+        config.fast_llm_model = GPT_4_MODEL
+        config.smart_llm_model = GPT_4_MODEL
     else:
-        config.set_fast_llm_model(check_model(config.fast_llm_model, "fast_llm_model"))
-        config.set_smart_llm_model(
-            check_model(config.smart_llm_model, "smart_llm_model")
-        )
+        config.fast_llm_model = check_model(config.fast_llm_model, "fast_llm_model")
+        config.smart_llm_model = check_model(config.smart_llm_model, "smart_llm_model")
 
     if memory_type:
         supported_memory = get_supported_memory_backends()

commit 98fede59471f7f84ab3ffb3be67a517bba7158b6

Fix Config type hint problems caused by #4803

autogpt/agent/agent.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 import signal
 import sys
 from datetime import datetime
@@ -64,7 +65,7 @@ class Agent:
         ai_config: AIConfig,
         system_prompt: str,
         triggering_prompt: str,
-        workspace_directory: str,
+        workspace_directory: str | Path,
         config: Config,
     ):
         self.ai_name = ai_name

autogpt/config/__init__.py
@@ -1,11 +1,12 @@
 """
 This module contains the configuration classes for AutoGPT.
 """
-from autogpt.config.ai_config import AIConfig
-from autogpt.config.config import Config, check_openai_api_key
+from .ai_config import AIConfig
+from .config import Config, ConfigBuilder, check_openai_api_key
 
 __all__ = [
     "check_openai_api_key",
     "AIConfig",
     "Config",
+    "ConfigBuilder",
 ]

autogpt/config/config.py
@@ -1,4 +1,6 @@
 """Configuration class to store the state of bools for different scripts access."""
+from __future__ import annotations
+
 import contextlib
 import os
 import re
@@ -8,21 +10,22 @@ import yaml
 from colorama import Fore
 
 from autogpt.core.configuration.schema import Configurable, SystemSettings
+from autogpt.plugins.plugins_config import PluginsConfig
 
 AZURE_CONFIG_FILE = os.path.join(os.path.dirname(__file__), "../..", "azure.yaml")
 from typing import Optional
 
 
-class ConfigSettings(SystemSettings):
+class Config(SystemSettings):
     fast_llm_model: str
     smart_llm_model: str
     continuous_mode: bool
     skip_news: bool
-    workspace_path: Optional[str]
-    file_logger_path: Optional[str]
+    workspace_path: Optional[str] = None
+    file_logger_path: Optional[str] = None
     debug_mode: bool
     plugins_dir: str
-    plugins_config: dict[str, str]
+    plugins_config: PluginsConfig
     continuous_limit: int
     speak_mode: bool
     skip_reprompt: bool
@@ -37,31 +40,31 @@ class ConfigSettings(SystemSettings):
     prompt_settings_file: str
     embedding_model: str
     browse_spacy_language_model: str
-    openai_api_key: Optional[str]
-    openai_organization: Optional[str]
+    openai_api_key: Optional[str] = None
+    openai_organization: Optional[str] = None
     temperature: float
     use_azure: bool
     execute_local_commands: bool
     restrict_to_workspace: bool
-    openai_api_type: Optional[str]
-    openai_api_base: Optional[str]
-    openai_api_version: Optional[str]
+    openai_api_type: Optional[str] = None
+    openai_api_base: Optional[str] = None
+    openai_api_version: Optional[str] = None
     openai_functions: bool
-    elevenlabs_api_key: Optional[str]
+    elevenlabs_api_key: Optional[str] = None
     streamelements_voice: str
     text_to_speech_provider: str
-    github_api_key: Optional[str]
-    github_username: Optional[str]
-    google_api_key: Optional[str]
-    google_custom_search_engine_id: Optional[str]
-    image_provider: Optional[str]
+    github_api_key: Optional[str] = None
+    github_username: Optional[str] = None
+    google_api_key: Optional[str] = None
+    google_custom_search_engine_id: Optional[str] = None
+    image_provider: Optional[str] = None
     image_size: int
-    huggingface_api_token: Optional[str]
+    huggingface_api_token: Optional[str] = None
     huggingface_image_model: str
     audio_to_text_provider: str
-    huggingface_audio_to_text_model: Optional[str]
-    sd_webui_url: Optional[str]
-    sd_webui_auth: Optional[str]
+    huggingface_audio_to_text_model: Optional[str] = None
+    sd_webui_url: Optional[str] = None
+    sd_webui_auth: Optional[str] = None
     selenium_web_browser: str
     selenium_headless: bool
     user_agent: str
@@ -76,12 +79,17 @@ class ConfigSettings(SystemSettings):
     plugins_openai: list[str]
     plugins_config_file: str
     chat_messages_enabled: bool
-    elevenlabs_voice_id: Optional[str]
+    elevenlabs_voice_id: Optional[str] = None
     plugins: list[str]
     authorise_key: str
 
+    # Executed immediately after init by Pydantic
+    def model_post_init(self, **kwargs) -> None:
+        if not self.plugins_config.plugins:
+            self.plugins_config = PluginsConfig.load_config(self)
 
-class Config(Configurable):
+
+class ConfigBuilder(Configurable[Config]):
     default_plugins_config_file = os.path.join(
         os.path.dirname(os.path.abspath(__file__)), "..", "..", "plugins_config.yaml"
     )
@@ -96,7 +104,7 @@ class Config(Configurable):
     else:
         default_tts_provider = "gtts"
 
-    defaults_settings = ConfigSettings(
+    defaults_settings = Config(
         name="Default Server Config",
         description="This is a default server configuration",
         smart_llm_model="gpt-3.5-turbo",
@@ -106,7 +114,7 @@ class Config(Configurable):
         skip_news=False,
         debug_mode=False,
         plugins_dir="plugins",
-        plugins_config={},
+        plugins_config=PluginsConfig({}),
         speak_mode=False,
         skip_reprompt=False,
         allow_downloads=False,

autogpt/core/configuration/schema.py
@@ -1,7 +1,7 @@
 import abc
 import copy
 import typing
-from typing import Any
+from typing import Any, Generic, TypeVar
 
 from pydantic import BaseModel
 
@@ -26,18 +26,20 @@ class SystemSettings(BaseModel, abc.ABC):
         use_enum_values = True
 
 
-class Configurable(abc.ABC):
+S = TypeVar("S", bound=SystemSettings)
+
+class Configurable(abc.ABC, Generic[S]):
     """A base class for all configurable objects."""
 
     prefix: str = ""
-    defaults_settings: typing.ClassVar[SystemSettings]
+    defaults_settings: typing.ClassVar[S]
 
     @classmethod
     def get_user_config(cls) -> dict[str, Any]:
         return _get_user_config_fields(cls.defaults_settings)
 
     @classmethod
-    def build_agent_configuration(cls, configuration: dict = {}) -> SystemSettings:
+    def build_agent_configuration(cls, configuration: dict = {}) -> S:
         """Process the configuration for this object."""
 
         defaults_settings = cls.defaults_settings.dict()

autogpt/main.py
@@ -1,11 +1,12 @@
 """The application entry point.  Can be invoked by a CLI or any other front end application."""
 import logging
+from pathlib import Path
 import sys
 
 from colorama import Fore, Style
 
 from autogpt.agent import Agent
-from autogpt.config.config import Config, check_openai_api_key
+from autogpt.config.config import ConfigBuilder, check_openai_api_key
 from autogpt.configurator import create_config
 from autogpt.logs import logger
 from autogpt.memory.vector import get_memory
@@ -45,14 +46,14 @@ def run_auto_gpt(
     browser_name: str,
     allow_downloads: bool,
     skip_news: bool,
-    workspace_directory: str,
+    workspace_directory: str | Path,
     install_plugin_deps: bool,
 ):
     # Configure logging before we do anything else.
     logger.set_level(logging.DEBUG if debug else logging.INFO)
     logger.speak_mode = speak
 
-    config = Config.build_config_from_env()
+    config = ConfigBuilder.build_config_from_env()
 
     # TODO: fill in llm values here
     check_openai_api_key(config)

autogpt/plugins/plugins_config.py
@@ -1,15 +1,19 @@
+from __future__ import annotations
+
 import os
-from typing import Any, Union
+from typing import Any, Union, TYPE_CHECKING
 
 import yaml
 
-from autogpt.config.config import Config
+if TYPE_CHECKING:
+    from autogpt.config import Config
 from autogpt.logs import logger
 from autogpt.plugins.plugin_config import PluginConfig
 
 
 class PluginsConfig:
     """Class for holding configuration of all plugins"""
+    plugins: dict[str, PluginConfig]
 
     def __init__(self, plugins_config: dict[str, Any]):
         self.plugins = {}
@@ -33,7 +37,7 @@ class PluginsConfig:
 
     def is_enabled(self, name) -> bool:
         plugin_config = self.plugins.get(name)
-        return plugin_config and plugin_config.enabled
+        return plugin_config is not None and plugin_config.enabled
 
     @classmethod
     def load_config(cls, global_config: Config) -> "PluginsConfig":

autogpt/workspace/workspace.py
@@ -10,6 +10,7 @@ agent.
 from __future__ import annotations
 
 from pathlib import Path
+from typing import Optional
 
 from autogpt.config import Config
 from autogpt.logs import logger
@@ -77,7 +78,7 @@ class Workspace:
     @staticmethod
     def _sanitize_path(
         relative_path: str | Path,
-        root: str | Path = None,
+        root: Optional[str | Path] = None,
         restrict_to_root: bool = True,
     ) -> Path:
         """Resolve the relative path within the given root if possible.
@@ -139,7 +140,7 @@ class Workspace:
         return full_path
 
     @staticmethod
-    def build_file_logger_path(config, workspace_directory):
+    def build_file_logger_path(config: Config, workspace_directory: Path):
         file_logger_path = workspace_directory / "file_logger.txt"
         if not file_logger_path.exists():
             with file_logger_path.open(mode="w", encoding="utf-8") as f:
@@ -147,10 +148,10 @@ class Workspace:
         config.file_logger_path = str(file_logger_path)
 
     @staticmethod
-    def get_workspace_directory(config: Config, workspace_directory: str = None):
+    def get_workspace_directory(config: Config, workspace_directory: Optional[str | Path] = None):
         if workspace_directory is None:
             workspace_directory = Path(__file__).parent / "auto_gpt_workspace"
-        else:
+        elif type(workspace_directory) == str:
             workspace_directory = Path(workspace_directory)
         # TODO: pass in the ai_settings file and the env file and have them cloned into
         #   the workspace directory so we can bind them to the agent.

benchmarks.py
@@ -1,5 +1,5 @@
 from autogpt.agent import Agent
-from autogpt.config import AIConfig, Config
+from autogpt.config import AIConfig, Config, ConfigBuilder
 from autogpt.main import COMMAND_CATEGORIES
 from autogpt.memory.vector import get_memory
 from autogpt.models.command_registry import CommandRegistry
@@ -13,7 +13,7 @@ def run_task(task) -> None:
 
 
 def bootstrap_agent(task):
-    config = Config.build_config_from_env()
+    config = ConfigBuilder.build_config_from_env()
     config.continuous_mode = False
     config.temperature = 0
     config.plain_output = True
@@ -42,7 +42,7 @@ def bootstrap_agent(task):
     )
 
 
-def get_command_registry(config):
+def get_command_registry(config: Config):
     command_registry = CommandRegistry()
     enabled_command_categories = [
         x for x in COMMAND_CATEGORIES if x not in config.disabled_command_categories

data_ingestion.py
@@ -2,10 +2,10 @@ import argparse
 import logging
 
 from autogpt.commands.file_operations import ingest_file, list_files
-from autogpt.config import Config
+from autogpt.config import ConfigBuilder
 from autogpt.memory.vector import VectorMemory, get_memory
 
-config = Config.build_config_from_env()
+config = ConfigBuilder.build_config_from_env()
 
 
 def configure_logging():

tests/conftest.py
@@ -8,7 +8,7 @@ from pytest_mock import MockerFixture
 
 from autogpt.agent.agent import Agent
 from autogpt.config.ai_config import AIConfig
-from autogpt.config.config import Config
+from autogpt.config import AIConfig, Config, ConfigBuilder
 from autogpt.llm.api_manager import ApiManager
 from autogpt.logs import TypingConsoleHandler
 from autogpt.memory.vector import get_memory
@@ -49,7 +49,7 @@ def temp_plugins_config_file():
 def config(
     temp_plugins_config_file: str, mocker: MockerFixture, workspace: Workspace
 ) -> Config:
-    config = Config.build_config_from_env()
+    config = ConfigBuilder.build_config_from_env()
     if not os.environ.get("OPENAI_API_KEY"):
         os.environ["OPENAI_API_KEY"] = "sk-dummy"
 