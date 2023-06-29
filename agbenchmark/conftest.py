ac5af736963dac95969f0cb3d0f99480a0a4f401
Silen Naihin <silen.naihin@gmail.com>
2023-06-28 21:28:46 -0400

    trying to get kill process

--- agbenchmark/conftest.py
+++ agbenchmark/conftest.py
@@ -2,11 +2,10 @@ import json
 import os
 import pytest
 import shutil
-from agbenchmark.tests.regression.RegressionManager import RegressionManager
-import requests
-from agbenchmark.mocks.MockManager import MockManager
 import subprocess
-from agbenchmark.Challenge import Challenge
+import sys
+from agbenchmark.tests.regression.RegressionManager import RegressionManager
+from agbenchmark.mocks.MockManager import MockManager
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -44,8 +43,16 @@ def pytest_addoption(parser):
     parser.addoption("--mock", action="store_true", default=False)
 
 
+def check_cycle_count(cycle_count: int, cutoff: int, proc):
+    """Increment, print, and check cycle count."""
+    cycle_count += 1
+    print(f"Cycle count: {cycle_count}")
+    if cycle_count >= cutoff:
+        proc.terminate(force=True)
+    return cycle_count
+
+
 AGENT_NAME = os.getenv("AGENT_NAME")
-AGENT_TIMEOUT = os.getenv("AGENT_TIMEOUT")
 
 
 @pytest.fixture(autouse=True)
@@ -70,19 +77,48 @@ def run_agent(request, config):
     else:
         path = os.path.join(os.getcwd(), f"agent\\{AGENT_NAME}")
 
-        try:
-            timeout = int(AGENT_TIMEOUT) if AGENT_TIMEOUT is not None else 60
+        timeout = sys.maxsize
 
-            subprocess.run(
-                ["python", "miniagi.py", task],
-                check=True,
-                cwd=path,
-                timeout=timeout
-                # text=True,
-                # capture_output=True
-            )
-        except subprocess.TimeoutExpired:
-            print("The subprocess has exceeded the time limit and was terminated.")
+        if config["cutoff"]["type"] == "time":
+            timeout = config["cutoff"]["count"] or 60
+            
+        from pexpect.popen_spawn import PopenSpawn
+
+        print(f"Running {task} with timeout {timeout}")
+
+        # Starting the subprocess using pexpect
+        proc = PopenSpawn("python", ["miniagi.py", task], timeout=timeout, cwd=path)
+
+        print("proc", proc)
+
+        cycle_count = 0
+
+        while True:
+            try:
+                # If we get the prompt for user input, we send "\n"
+                if config["cutoff"]["type"] == "user_input":
+                    proc.expect([config["cutoff"]["user_prompt"]])
+                    proc.sendline(config["cutoff"]["user_input"])
+                    cycle_count = check_cycle_count(
+                        cycle_count, config["cutoff"]["count"], proc
+                    )
+                elif config["cutoff"]["type"] == "cycle_count":
+                    match = proc.expect([r"Cycle count: (\d+)"])
+                    if match is not None:
+                        cycle_count = int(match.group(1))  # type: ignore
+                        cycle_count = check_cycle_count(
+                            cycle_count, config["cutoff"]["count"], proc
+                        )
+
+                # for cutoff type "time", just let it run until timeout
+            except expect.TIMEOUT:
+                print("The subprocess has exceeded the time limit and was terminated.")
+                break
+            except expect.EOF:
+                print("The subprocess has finished running.")
+                break
+
+        proc.close()
 
 
 regression_json = "agbenchmark/tests/regression/regression_tests.json"
