2062844fa6b0250017ba65712e1a590a5fc28616
merwanehamadi <merwanehamadi@gmail.com>
2023-07-02 07:38:30 -0700

    Integrate one challenge to auto gpt (#44)

--- agbenchmark/start_benchmark.py
+++ agbenchmark/start_benchmark.py
@@ -7,6 +7,13 @@ from dotenv import load_dotenv, set_key
 
 load_dotenv()
 
+CURRENT_DIRECTORY = Path(__file__).resolve().parent
+
+new_path = CURRENT_DIRECTORY / "config.json"
+
+CONFIG_PATH = str(new_path.resolve())
+
+REGRESSION_TESTS_PATH = str(Path(os.getcwd()) / "regression_tests.json")
 
 @click.group()
 def cli():
@@ -15,16 +22,12 @@ def cli():
 
 @cli.command()
 @click.option("--category", default=None, help="Specific category to run")
-@click.option("--noreg", is_flag=True, help="Skip regression tests")
+@click.option("--reg", is_flag=True, help="Runs only regression tests")
 @click.option("--mock", is_flag=True, help="Run with mock")
-def start(category, noreg, mock):
+def start(category, reg, mock):
     """Start the benchmark tests. If a category flag is provided, run the categories with that mark."""
-    config_file = "agbenchmark/config.json"
-
-    config_dir = os.path.abspath(config_file)
-
     # Check if configuration file exists and is not empty
-    if not os.path.exists(config_dir) or os.stat(config_dir).st_size == 0:
+    if not os.path.exists(CONFIG_PATH) or os.stat(CONFIG_PATH).st_size == 0:
         config = {}
 
         config["workspace"] = click.prompt(
@@ -42,11 +45,11 @@ def start(category, noreg, mock):
             default="60",
         )
 
-        with open(config_dir, "w") as f:
+        with open(CONFIG_PATH, "w") as f:
             json.dump(config, f)
     else:
         # If the configuration file exists and is not empty, load it
-        with open(config_dir, "r") as f:
+        with open(CONFIG_PATH, "r") as f:
             config = json.load(f)
 
     set_key(".env", "MOCK_TEST", "True" if mock else "False")
@@ -58,11 +61,9 @@ def start(category, noreg, mock):
     if not os.path.exists(workspace_path):
         os.makedirs(workspace_path, exist_ok=True)
 
-    regression_path = os.path.abspath(
-        "agbenchmark/tests/regression/regression_tests.json"
-    )
-    if not os.path.exists(regression_path):
-        with open(regression_path, "a"):
+
+    if not os.path.exists(REGRESSION_TESTS_PATH):
+        with open(REGRESSION_TESTS_PATH, "a"):
             pass
 
     print("Current configuration:")
@@ -70,31 +71,40 @@ def start(category, noreg, mock):
         print(f"{key}: {value}")
 
     print("Starting benchmark tests...", category)
-    pytest_args = ["agbenchmark", "-vs"]
+    tests_to_run = []
+    pytest_args = ["-vs"]
     if category:
         pytest_args.extend(
             ["-m", category]
-        )  # run categorys that are of a specific marker
-        if noreg:
-            pytest_args.extend(
-                ["-k", "not regression"]
-            )  # run categorys that are of a specific marker but don't include regression categorys
-        print(f"Running {'non-regression' + category if noreg else category} categorys")
+        )
     else:
-        if noreg:
-            print("Running all non-regression categorys")
-            pytest_args.extend(
-                ["-k", "not regression"]
-            )  # run categorys that are not regression categorys
+        if reg:
+            print("Running all regression tests")
+            tests_to_run = get_regression_tests()
         else:
-            print("Running all categorys")  # run all categorys
+            print("Running all categories")
 
     if mock:
         pytest_args.append("--mock")
 
     # Run pytest with the constructed arguments
+    if not tests_to_run:
+        tests_to_run = [str(CURRENT_DIRECTORY)]
+    pytest_args.extend(tests_to_run)
     pytest.main(pytest_args)
 
+
+def get_regression_tests():
+    if not Path(REGRESSION_TESTS_PATH).exists():
+        with open(REGRESSION_TESTS_PATH, 'w') as file:
+            json.dump({}, file)
+
+    with open(REGRESSION_TESTS_PATH, 'r') as file:
+        data = json.load(file)
+
+    regression_tests = [str(CURRENT_DIRECTORY / ".." / value['test']) for key, value in data.items()]
+
+    return regression_tests
 
 if __name__ == "__main__":
     start()
