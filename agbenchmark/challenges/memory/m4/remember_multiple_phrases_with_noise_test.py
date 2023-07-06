b20b4dee980f6e5da76c2b9c41ab59bd15095a18
merwanehamadi <merwanehamadi@gmail.com>
2023-07-06 14:19:12 -0700

    Add 'remember phrases with noise' challenge (#63)

---
+++ agbenchmark/challenges/memory/m4/remember_multiple_phrases_with_noise_test.py
@@ -0,0 +1,32 @@
+import os
+from typing import Any, Dict
+
+import pytest
+
+from agbenchmark.challenges.memory.memory import MemoryChallenge
+
+
+class TestRememberMultiplePhrasesWithNoise(MemoryChallenge):
+    """The first memory challenge"""
+
+    def get_file_path(self) -> str:  # all tests must implement this method
+        return os.path.join(
+            os.path.dirname(__file__), "remember_multiple_phrases_with_noise_data.json"
+        )
+
+    @pytest.mark.depends(
+        name="test_remember_multiple_phrases_with_noise",
+        depends=["test_remember_multiple_ids_with_noise"],
+    )
+    def test_method(self, config: Dict[str, Any]) -> None:
+        self.setup_challenge(config)
+
+        files_contents = self.open_files(config["workspace"], self.data.ground.files)
+
+        scores = []
+        for file_content in files_contents:
+            score = self.scoring(file_content, self.data.ground)
+            print("Your score is:", score)
+            scores.append(score)
+
+        assert 1 in scores
