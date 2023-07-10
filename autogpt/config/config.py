cd5b67e582adfaf462e3f6a9b633d44089cd412f
James Collins <collijk@uw.edu>
2023-07-09 19:32:04 -0700

    Fix regression: restore api_base and organization configurability (#4933)

--- autogpt/config/config.py
+++ autogpt/config/config.py
@@ -86,7 +86,18 @@ class Config(SystemSettings):
     plugins: list[str]
     authorise_key: str
 
-    def get_azure_kwargs(self, model: str) -> dict[str, str]:
+    def get_openai_credentials(self, model: str) -> dict[str, str]:
+        credentials = {
+            "api_key": self.openai_api_key,
+            "api_base": self.openai_api_base,
+            "organization": self.openai_organization,
+        }
+        if self.use_azure:
+            azure_credentials = self.get_azure_credentials(model)
+            credentials.update(azure_credentials)
+        return credentials
+
+    def get_azure_credentials(self, model: str) -> dict[str, str]:
         """Get the kwargs for the Azure API."""
 
         # Fix --gpt3only and --gpt4only in combination with Azure
