e75b9d56db0ad7f485d240ce60455c89e679d40b
Merwane Hamadi <merwanehamadi@gmail.com>
2023-07-22 13:13:59 -0700

    Install agbenchmark and make continuous mode dynamic
    Signed-off-by: Merwane Hamadi <merwanehamadi@gmail.com>

--- agbenchmark/benchmarks.py
+++ agbenchmark/benchmarks.py
@@ -1,62 +1,28 @@
-<<<<<<< HEAD
+import sys
 from pathlib import Path
+from typing import Tuple
 
 from autogpt.agents import Agent
 from autogpt.app.main import run_interaction_loop
 from autogpt.commands import COMMAND_CATEGORIES
-=======
-import sys
-from typing import Tuple
-
-from autogpt.agent import Agent
->>>>>>> c29ec925 (WIP)
 from autogpt.config import AIConfig, Config, ConfigBuilder
 from autogpt.memory.vector import get_memory
 from autogpt.models.command_registry import CommandRegistry
 from autogpt.prompts.prompt import DEFAULT_TRIGGERING_PROMPT
 from autogpt.workspace import Workspace
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 PROJECT_DIR = Path().resolve()
 
-=======
-def run_specific_agent(task) -> Tuple[str, int]:
-=======
->>>>>>> 8f6e811a (Fix Auto-GPT/benchmark integration)
 
-def run_specific_agent(task) -> Tuple[str, int]:
-    cycle_count = 0
->>>>>>> c29ec925 (WIP)
-
-    agent = bootstrap_agent(task)
-<<<<<<< HEAD
-<<<<<<< HEAD
+def run_specific_agent(task, continuous_mode=False) -> Tuple[str, int]:
+    agent = bootstrap_agent(task, continuous_mode)
     run_interaction_loop(agent)
 
 
-def bootstrap_agent(task):
+def bootstrap_agent(task, continuous_mode) -> Agent:
     config = ConfigBuilder.build_config_from_env(workdir=PROJECT_DIR)
-    config.continuous_mode = False
-=======
-    response = agent.start_interaction_loop()
-
-    if response:
-        cycle_count += 1
-
-
-
-    return response, 1
-=======
-    agent.start_interaction_loop()
->>>>>>> 8f6e811a (Fix Auto-GPT/benchmark integration)
-
-
-def bootstrap_agent(task):
-    config = ConfigBuilder.build_config_from_env()
     config.debug_mode = True
-    config.continuous_mode = True
->>>>>>> c29ec925 (WIP)
+    config.continuous_mode = continuous_mode
     config.temperature = 0
     config.plain_output = True
     command_registry = get_command_registry(config)
@@ -95,4 +61,4 @@ if __name__ == "__main__":
         print("Usage: python script.py <task>")
         sys.exit(1)
     task = sys.argv[1]
-    run_specific_agent(task)
+    run_specific_agent(task, continuous_mode=True)
