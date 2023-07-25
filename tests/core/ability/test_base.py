2836c575310916daf2a5126f0325368d16087b9e
Cyrus <alireza2467@gmail.com>
2023-07-25 11:53:53 +0300

    provide pytests for AbilityRegistry class in autogpt/core/ability/base.py

--- tests/core/ability/test_base.py
+++ tests/core/ability/test_base.py
@@ -1,12 +1,19 @@
 import abc
+from importlib import import_module
 from pprint import pformat
-from typing import Any
+from typing import Any, Dict, List
 
 import pytest
 from inflection import underscore
 
-from autogpt.core.ability import Ability, AbilityResult
+from autogpt.core.ability import Ability, AbilityRegistry, AbilityResult
+from autogpt.core.ability.base import AbilityConfiguration
 from autogpt.core.ability.schema import Knowledge
+from autogpt.core.ability.simple import (
+    AbilityRegistryConfiguration,
+    AbilityRegistrySettings,
+)
+from autogpt.core.plugin.base import PluginLocation, PluginStorageFormat
 
 
 class MockDerivedAbility(Ability):
@@ -14,6 +21,14 @@ class MockDerivedAbility(Ability):
     This is a mock class simulating implementation of Ability class. It is
     used inside the various test methods of TestAbility class in the following.
     """
+
+    default_configuration = AbilityConfiguration(
+        location=PluginLocation(
+            storage_format=PluginStorageFormat.INSTALLED_PACKAGE,
+            # storage_route="tests.core.ability.test_base.MockDerivedAbility",
+            storage_route="test_base.MockDerivedAbility",
+        ),
+    )
 
     @classmethod
     def description(cls) -> str:
@@ -31,6 +46,121 @@ class MockDerivedAbility(Ability):
             ability_args={"arg1": str(arg1)},
             message="Test msg",
         )
+
+
+class MockDerivedAbilityRegistry(AbilityRegistry):
+    def __init__(
+        self,
+        settings: AbilityRegistrySettings,
+    ):
+        self.abilities: List[Ability] = []
+
+        for (
+            ability_name,
+            ability_configuration,
+        ) in settings.configuration.abilities.items():
+            self.register_ability(ability_name, ability_configuration)
+
+    def register_ability(
+        self, ability_name: str, ability_configuration: AbilityConfiguration
+    ) -> None:
+        # storage_route = test_base.MockDerivedAbility
+        (
+            module_path,
+            _,
+            class_name,
+        ) = ability_configuration.location.storage_route.rpartition(".")
+        ability_class = getattr(import_module(module_path), class_name)
+        ability_args: Dict[str, Any] = {}
+        ability = ability_class(**ability_args)
+        self.abilities.append(ability)
+
+    def list_abilities(self) -> list[str]:
+        return [
+            f"{ability.name()}: {ability.description()}" for ability in self.abilities
+        ]
+
+    def dump_abilities(self) -> list[dict]:
+        return [ability.dump() for ability in self.abilities]
+
+    def get_ability(self, ability_name: str) -> Ability:
+        for ability in self.abilities:
+            if ability.name() == ability_name:
+                return ability
+        raise ValueError(f"Ability '{ability_name}' not found.")
+
+    async def perform(self, ability_name: str, **kwargs: Any) -> AbilityResult:
+        ability = self.get_ability(ability_name)
+        return await ability(**kwargs)
+
+
+class TestAbilityRegistry:
+    """
+    Provides various tests for AbilityRegistry class
+    """
+
+    MOCK_ABILITIES = {MockDerivedAbility.name(): MockDerivedAbility}
+
+    @staticmethod
+    def test_base_methods_exist() -> None:
+        assert issubclass(AbilityRegistry, abc.ABC)
+        assert hasattr(AbilityRegistry, "register_ability")
+        assert hasattr(AbilityRegistry, "list_abilities")
+        assert hasattr(AbilityRegistry, "dump_abilities")
+        assert hasattr(AbilityRegistry, "get_ability")
+        assert hasattr(AbilityRegistry, "perform")
+        assert callable(AbilityRegistry.dump_abilities)
+        assert callable(AbilityRegistry.get_ability)
+        assert callable(AbilityRegistry.perform)
+        assert callable(AbilityRegistry.list_abilities)
+        assert callable(AbilityRegistry.register_ability)
+
+    def setup_method(self) -> None:
+        ability_registry_settings = AbilityRegistrySettings(
+            name="mock_derived_ability_registry",
+            description="A mock ability registry for testing.",
+            configuration=AbilityRegistryConfiguration(
+                abilities={
+                    ability_name: ability.default_configuration
+                    for ability_name, ability in self.MOCK_ABILITIES.items()
+                },
+            ),
+        )
+        self.ability_registry = MockDerivedAbilityRegistry(ability_registry_settings)
+
+    def test_get_ability(self) -> None:
+        # Test if the ability can be retrieved from the registry
+        ability = self.ability_registry.get_ability("mock_derived_ability")
+        assert isinstance(ability, MockDerivedAbility)
+
+        with pytest.raises(ValueError):
+            self.ability_registry.get_ability("DOESN't EXIST")
+
+    def test_list_abilities(self) -> None:
+        # Test if the list of abilities returns all registered abilities
+        abilities = self.ability_registry.list_abilities()
+        assert len(abilities) == len(self.MOCK_ABILITIES)
+        assert abilities[0] == "mock_derived_ability: Test Ability Description"
+
+    @pytest.mark.asyncio
+    async def test_perform_ability(self) -> None:
+        # Test if the ability can be performed and returns a valid AbilityResult
+        result = await self.ability_registry.perform("mock_derived_ability", arg1=42)
+        assert isinstance(result, AbilityResult)
+        assert result.success
+        assert result.ability_name == "Test-name"
+        assert result.ability_args == {"arg1": "42"}
+        assert result.message == "Test msg"
+
+    def test_dump_abilities(self) -> None:
+        # Test all the abilities are dumped appropirately
+        dumped_abilities = self.ability_registry.dump_abilities()
+        assert len(dumped_abilities) == len(self.MOCK_ABILITIES)
+
+        # check the returned type
+        assert isinstance(dumped_abilities, list)
+        for dumped in dumped_abilities:
+            assert isinstance(dumped, dict)
 
 
 class TestAbility:
@@ -116,3 +246,4 @@ class TestAbility:
 
 if __name__ == "__main__":
     pytest.main()
+    # pass
