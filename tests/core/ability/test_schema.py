a7e02aed33fa14f2ae8e52e8dee2adcaee841c95
Cyrus <alireza2467@gmail.com>
2023-07-26 14:55:19 +0300

    Provide tests for Knowledge class defined in autogpt/core/ability/schema.py

--- tests/core/ability/test_schema.py
+++ tests/core/ability/test_schema.py
@@ -4,7 +4,9 @@ autogpt/core/ability/schema.py
 """
 import enum
 
-from autogpt.core.ability.schema import ContentType
+import pytest
+
+from autogpt.core.ability.schema import ContentType, Knowledge
 
 
 class TestContentType:
@@ -44,3 +46,51 @@ class TestContentType:
         assert len(members) == 2
         for field_name in members.mapping.keys():
             assert field_name in ("TEXT", "CODE")
+
+
+class TestKnowledgeModel:
+    """
+    Provides necessary tests for the Knowledge class.
+    """
+
+    @staticmethod
+    def test_knowledge_model_attributes() -> None:
+        # Test if the Knowledge class has the expected attributes
+        knowledge = Knowledge(
+            content="Sample content", content_type=ContentType.TEXT, content_metadata={}
+        )
+        assert knowledge.content == "Sample content"
+        assert knowledge.content_type == ContentType.TEXT
+        assert knowledge.content_metadata == {}
+
+    @staticmethod
+    def test_knowledge_model_types() -> None:
+        # Test if the attribute types of Knowledge class match the type hints
+        knowledge = Knowledge(
+            content="Sample content",
+            content_type=ContentType.CODE,
+            content_metadata={"key": "value"},
+        )
+
+        assert isinstance(knowledge.content, str)
+        assert isinstance(knowledge.content_type, ContentType)
+        assert isinstance(knowledge.content_metadata, dict)
+
+    @staticmethod
+    def test_knowledge_model_invalid_content_type() -> None:
+        # Test if the Knowledge model raises a ValueError for an invalid content_type
+        with pytest.raises(ValueError):
+            Knowledge(
+                content="Invalid content",
+                content_type="invalid_type",
+                content_metadata={},
+            )
+
+    @staticmethod
+    def test_member_counts() -> None:
+        # Test there are exactly three memebers in Knowledge
+        members = Knowledge.__fields__
+        assert len(members) == 3
+        # check they have the right name too
+        for field_name in members.keys():
+            assert field_name in ("content", "content_type", "content_metadata")
