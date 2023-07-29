0eafe788562169fda1a21501d94fb7c19a237138
Reinier van der Leer <github@pwuts.nl>
2023-07-29 18:41:55 +0200

    Verify model compatibility if OPENAI_FUNCTIONS is set

--- autogpt/config/config.py
+++ autogpt/config/config.py
@@ -13,6 +13,7 @@ from colorama import Fore
 from pydantic import Field, validator
 
 from autogpt.core.configuration.schema import Configurable, SystemSettings
+from autogpt.llm.providers.openai import OPEN_AI_CHAT_MODELS
 from autogpt.plugins.plugins_config import PluginsConfig
 
 AI_SETTINGS_FILE = "ai_settings.yaml"
@@ -146,6 +147,15 @@ class Config(SystemSettings, arbitrary_types_allowed=True):
             p.__class__.__name__ != "AutoGPTPluginTemplate"
         ), f"Plugins must subclass AutoGPTPluginTemplate; {p} is a template instance"
         return p
+
+    @validator("openai_functions")
+    def validate_openai_functions(cls, v: bool, values: dict[str, Any]):
+        if v:
+            smart_llm = values["smart_llm"]
+            assert OPEN_AI_CHAT_MODELS[smart_llm].supports_functions, (
+                f"Model {smart_llm} does not support OpenAI Functions. "
+                "Please disable OPENAI_FUNCTIONS or choose a suitable model."
+            )
 
     def get_openai_credentials(self, model: str) -> dict[str, str]:
         credentials = {
