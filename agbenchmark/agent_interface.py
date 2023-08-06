530eb61f258c37490fc07457c7adeb5ed69b8253
merwanehamadi <merwanehamadi@gmail.com>
2023-08-05 18:00:05 -0700

    Add agent protocol interface test (#259)
    Signed-off-by: Merwane Hamadi <merwanehamadi@gmail.com>

--- agbenchmark/agent_interface.py
+++ agbenchmark/agent_interface.py
@@ -20,56 +20,57 @@ def run_agent(
     task: str, config: Dict[str, Any], artifacts_location: str, cutoff: int
 ) -> None:
     """Calling to get a response"""
-
+    if task == "":
+        return
     if MOCK_FLAG:
         print("Running mock agent")
         copy_artifacts_into_workspace(
             config["workspace"], "artifacts_out", artifacts_location
         )
+        return
+    entry_path = "agbenchmark.benchmarks"
+
+    timeout = cutoff
+    if "--nc" in sys.argv:
+        timeout = 100000
+
+    print(f"Running '{entry_path}' with timeout {timeout}")
+
+    command = [sys.executable, "-m", entry_path, str(task)]
+    process = subprocess.Popen(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        universal_newlines=True,
+        cwd=HOME_DIRECTORY,
+        bufsize=1,
+    )
+
+    start_time = time.time()
+
+    while True:
+        try:
+            # This checks if there's data to be read from stdout without blocking.
+            if process.stdout and select.select([process.stdout], [], [], 0)[0]:
+                output = process.stdout.readline()
+                print(output.strip())
+        except Exception as e:
+            continue
+
+        # Check if process has ended, has no more output, or exceeded timeout
+        if process.poll() is not None or (time.time() - start_time > timeout):
+            break
+
+    if time.time() - start_time > timeout:
+        print("The Python function has exceeded the time limit and was terminated.")
+        process.kill()
     else:
-        entry_path = "agbenchmark.benchmarks"
+        print("The Python function has finished running.")
 
-        timeout = cutoff
-        if "--nc" in sys.argv:
-            timeout = 100000
+    process.wait()
 
-        print(f"Running '{entry_path}' with timeout {timeout}")
-
-        command = [sys.executable, "-m", entry_path, str(task)]
-        process = subprocess.Popen(
-            command,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            universal_newlines=True,
-            cwd=HOME_DIRECTORY,
-            bufsize=1,
-        )
-
-        start_time = time.time()
-
-        while True:
-            try:
-                # This checks if there's data to be read from stdout without blocking.
-                if process.stdout and select.select([process.stdout], [], [], 0)[0]:
-                    output = process.stdout.readline()
-                    print(output.strip())
-            except Exception as e:
-                continue
-
-            # Check if process has ended, has no more output, or exceeded timeout
-            if process.poll() is not None or (time.time() - start_time > timeout):
-                break
-
-        if time.time() - start_time > timeout:
-            print("The Python function has exceeded the time limit and was terminated.")
-            process.kill()
-        else:
-            print("The Python function has finished running.")
-
-        process.wait()
-
-        if process.returncode != 0:
-            print(f"The agent timed out")
+    if process.returncode != 0:
+        print(f"The agent timed out")
 
 
 def copy_artifacts_into_workspace(
