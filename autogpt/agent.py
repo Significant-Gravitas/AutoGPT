28f7a017978bf00dd39809cb523ffcd06fb72896
SwiftyOS <craigswift13@gmail.com>
2023-08-14 14:03:22 +0200

    Agent protocol now available

--- autogpt/agent.py
+++ autogpt/agent.py
@@ -1,5 +1,5 @@
 from agent_protocol import Agent, Step, Task
-
+import time 
 import autogpt.utils
 
 
@@ -11,12 +11,15 @@ class AutoGPT:
 
     async def task_handler(self, task: Task) -> None:
         print(f"task: {task.input}")
+        await Agent.db.create_step(task.task_id, task.input, is_last=True)
+        time.sleep(2)
         autogpt.utils.run(task.input)
-        await Agent.db.create_step(task.task_id, task.input)
+        
 
     async def step_handler(self, step: Step) -> Step:
         print(f"step: {step.input}")
-        step = Agent.db.get_step(step.step_id)
-        updated_step: Step = Agent.db.update_step(step.step_id, status="completed")
-        updated_step.output = step.input
+        agent_step = await Agent.db.get_step(step.task_id, step.step_id)
+        updated_step: Step = await Agent.db.update_step(agent_step.task_id, agent_step.step_id, status="completed")
+        updated_step.output = agent_step.input
+        print(f"Step completed: {updated_step}")
         return updated_step
