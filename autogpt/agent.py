28714d37f01ba32fe03389f0a62d55b037cd1161
SwiftyOS <craigswift13@gmail.com>
2023-08-14 18:20:10 +0200

    formatting

--- autogpt/agent.py
+++ autogpt/agent.py
@@ -1,11 +1,11 @@
+import time
+
 from agent_protocol import Agent, Step, Task
-import time 
+
 import autogpt.utils
 
 
 class AutoGPT:
-
-
     def __init__(self) -> None:
         pass
 
@@ -16,12 +16,13 @@ class AutoGPT:
         autogpt.utils.run(task.input)
         # print(f"Created Task id: {task.task_id}")
         return task
-        
 
     async def step_handler(self, step: Step) -> Step:
         # print(f"step: {step}")
         agent_step = await Agent.db.get_step(step.task_id, step.step_id)
-        updated_step: Step = await Agent.db.update_step(agent_step.task_id, agent_step.step_id, status="completed")
+        updated_step: Step = await Agent.db.update_step(
+            agent_step.task_id, agent_step.step_id, status="completed"
+        )
         updated_step.output = agent_step.input
         print(f"Step completed: {updated_step}")
         return updated_step
