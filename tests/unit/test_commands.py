63bb6d7b1ef42ed3af39bbdf290bdffcc42ba523
Reinier van der Leer <github@pwuts.nl>
2023-08-23 01:04:39 +0200

    Adjust command formatting in prompt

--- tests/unit/test_commands.py
+++ tests/unit/test_commands.py
@@ -33,7 +33,7 @@ def test_command_creation():
     assert cmd.description == "Example command"
     assert cmd.method == example_command_method
     assert (
-        str(cmd) == "example: Example command, params: (arg1: int, arg2: Optional[str])"
+        str(cmd) == "example: Example command. Params: (arg1: int, arg2: Optional[str])"
     )
 
 
