11c82880a89ac3be20a07e9bb237c367dd190b86
Reinier van der Leer <github@pwuts.nl>
2023-08-24 17:35:50 +0200

    Fix some more broken tests

--- tests/integration/test_web_selenium.py
+++ tests/integration/test_web_selenium.py
@@ -7,10 +7,10 @@ from autogpt.commands.web_selenium import BrowsingError, browse_website
 @pytest.mark.vcr
 @pytest.mark.requires_openai_api_key
 def test_browse_website_nonexistent_url(agent: Agent, patched_api_requestor: None):
-    url = "https://barrel-roll.com"
+    url = "https://auto-gpt-thinks-this-website-does-not-exist.com"
     question = "How to execute a barrel roll"
 
-    with pytest.raises(BrowsingError, match=r"CONNECTION_CLOSED") as raised:
+    with pytest.raises(BrowsingError, match=r"NAME_NOT_RESOLVED") as raised:
         browse_website(url, question, agent)
 
         # Sanity check that the response is not too long
