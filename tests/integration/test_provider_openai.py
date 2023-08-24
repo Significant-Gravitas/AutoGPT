efe92786935af8ed2ebf5db5a491aa69458f2dcb
Reinier van der Leer <github@pwuts.nl>
2023-08-24 17:35:50 +0200

    Fix some more broken tests

--- tests/integration/test_provider_openai.py
+++ tests/integration/test_provider_openai.py
@@ -14,41 +14,20 @@ def reset_api_manager():
     yield
 
 
-class TestProviderOpenAI:
-    @staticmethod
-    def test_create_chat_completion_debug_mode(caplog):
-        """Test if debug mode logs response."""
-        messages = [
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "Who won the world series in 2020?"},
-        ]
-        model = "gpt-3.5-turbo"
-        with patch("openai.ChatCompletion.create") as mock_create:
-            mock_response = MagicMock()
-            del mock_response.error
-            mock_response.usage.prompt_tokens = 10
-            mock_response.usage.completion_tokens = 20
-            mock_create.return_value = mock_response
+def test_create_chat_completion_empty_messages():
+    """Test if empty messages result in zero tokens and cost."""
+    messages = []
+    model = "gpt-3.5-turbo"
 
-            openai.create_chat_completion(messages, model=model)
+    with patch("openai.ChatCompletion.create") as mock_create:
+        mock_response = MagicMock()
+        del mock_response.error
+        mock_response.usage.prompt_tokens = 0
+        mock_response.usage.completion_tokens = 0
+        mock_create.return_value = mock_response
 
-            assert "Response" in caplog.text
+        openai.create_chat_completion(messages, model=model)
 
-    @staticmethod
-    def test_create_chat_completion_empty_messages():
-        """Test if empty messages result in zero tokens and cost."""
-        messages = []
-        model = "gpt-3.5-turbo"
-
-        with patch("openai.ChatCompletion.create") as mock_create:
-            mock_response = MagicMock()
-            del mock_response.error
-            mock_response.usage.prompt_tokens = 0
-            mock_response.usage.completion_tokens = 0
-            mock_create.return_value = mock_response
-
-            openai.create_chat_completion(messages, model=model)
-
-            assert api_manager.get_total_prompt_tokens() == 0
-            assert api_manager.get_total_completion_tokens() == 0
-            assert api_manager.get_total_cost() == 0
+        assert api_manager.get_total_prompt_tokens() == 0
+        assert api_manager.get_total_completion_tokens() == 0
+        assert api_manager.get_total_cost() == 0
