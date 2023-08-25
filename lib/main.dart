9fe3082da8851a831212039d5f925466389eb6da
hunteraraujo <hunter_araujo@msn.com>
2023-08-24 21:43:02 -0700

    Update main_layout.dart + main.dart to integrate ChatView

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'views/main_layout.dart';
 import 'package:provider/provider.dart';
 import 'package:auto_gpt_flutter_client/viewmodels/task_viewmodel.dart';
+import 'package:auto_gpt_flutter_client/viewmodels/chat_viewmodel.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key); // Corrected the constructor
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -17,8 +18,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ChangeNotifierProvider(
-        create: (context) => TaskViewModel(),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => TaskViewModel()),
+          ChangeNotifierProvider(create: (context) => ChatViewModel()),
+        ],
         child: const MainLayout(),
       ), // Set MainLayout as the home screen of the app
     );
