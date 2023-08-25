4bac51206e52247e4ab967599a1b03879af4eae9
Reinier van der Leer <github@pwuts.nl>
2023-08-25 02:09:38 +0200

    Integrate ActionHistory into Agent + BaseAgent

---
+++ autogpt/prompts/utils.py
@@ -0,0 +1,5 @@
+from typing import Any
+
+
+def format_numbered_list(items: list[Any], start_at: int = 1) -> str:
+    return "\n".join(f"{i}. {str(item)}" for i, item in enumerate(items, start_at))
