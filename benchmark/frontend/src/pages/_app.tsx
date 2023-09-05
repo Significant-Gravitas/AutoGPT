9fc0e2d4d84fb3c9accb4ebf0c559a885029d672
Merwane Hamadi <merwanehamadi@gmail.com>
2023-09-05 10:12:15 -0700

    remove benchmark

--- benchmark/frontend/src/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import { type AppType } from "next/dist/shared/lib/utils";
-import "~/styles/globals.css";
-
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
-
-export default MyApp;
