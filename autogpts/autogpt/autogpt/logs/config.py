4b1049e0a3e2b4b9e69fe5ea6164c74d6d3373cf
James Collins <collijk@uw.edu>
2023-09-08 14:28:36 -0700

    Refactor/remove abstract singleton as voice base parent (#4931)
    Co-authored-by: Reinier van der Leer <reinier.vanderleer@agpt.co>

--- autogpts/autogpt/autogpt/logs/config.py
+++ autogpts/autogpt/autogpt/logs/config.py
@@ -27,6 +27,7 @@ DEBUG_LOG_FORMAT = (
     "  %(title)s%(message)s"
 )
 
+SPEECH_OUTPUT_LOGGER = "VOICE"
 USER_FRIENDLY_OUTPUT_LOGGER = "USER_FRIENDLY_OUTPUT"
 
 _chat_plugins: list[AutoGPTPluginTemplate] = []
@@ -95,6 +96,11 @@ def configure_logging(config: Config, log_dir: Path = LOG_DIR) -> None:
     user_friendly_output_logger.addHandler(error_log_handler)
     user_friendly_output_logger.addHandler(stderr)
     user_friendly_output_logger.propagate = False
+
+    speech_output_logger = logging.getLogger(SPEECH_OUTPUT_LOGGER)
+    speech_output_logger.setLevel(logging.INFO)
+    speech_output_logger.addHandler(TTSHandler(config))
+    speech_output_logger.propagate = False
 
     # JSON logger with better formatting
     json_logger = logging.getLogger("JSON_LOGGER")
