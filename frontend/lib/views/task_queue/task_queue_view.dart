a9386d76f95c5de4260590194c1f275a3c30b033
hunteraraujo <hunter_araujo@msn.com>
2023-09-12 13:34:48 -0700

    Implement UI enhancements for TaskQueueView
    This commit introduces several UI improvements to the TaskQueueView:
    - Tiles are padded 20 units from both the leading and trailing edges.
    - Tiles now have a white background.
    - Added a thin black border to the tiles.
    - Incorporated a slight corner radius for the tiles.
    - Centered the title and subtitle horizontally within the tiles.
    - Added a checkmark button with a tooltip at the bottom-right corner for running a suite of tests.
    
    These changes aim to improve the user experience and visual appeal of the TaskQueueView.

--- frontend/lib/views/task_queue/task_queue_view.dart
+++ frontend/lib/views/task_queue/task_queue_view.dart
@@ -1,40 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:auto_gpt_flutter_client/viewmodels/skill_tree_viewmodel.dart';
+import 'package:provider/provider.dart';
 
-class TestQueueView extends StatefulWidget {
-  @override
-  _TestQueueViewState createState() => _TestQueueViewState();
-}
-
-class _TestQueueViewState extends State<TestQueueView> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+// TODO: Add view model for task queue instead of skill tree view model
+class TaskQueueView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16.0),
-      color: Colors.blueGrey[200], // Background color for visibility
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+    final viewModel = Provider.of<SkillTreeViewModel>(context);
+
+    // Reverse the node hierarchy
+    final reversedHierarchy =
+        viewModel.selectedNodeHierarchy?.reversed.toList() ?? [];
+
+    return Material(
+      color: Colors.white,
+      child: Stack(
         children: [
-          Text(
-            'This is the TestQueueView',
-            style: TextStyle(fontSize: 24),
+          // The list of tasks (tiles)
+          ListView.builder(
+            itemCount: reversedHierarchy.length,
+            itemBuilder: (context, index) {
+              final node = reversedHierarchy[index];
+              return Container(
+                margin: EdgeInsets.fromLTRB(20, 5, 20, 5),
+                decoration: BoxDecoration(
+                  color: Colors.white, // white background
+                  border: Border.all(
+                      color: Colors.black, width: 1), // thin black border
+                  borderRadius: BorderRadius.circular(4), // small corner radius
+                ),
+                child: ListTile(
+                  title: Center(child: Text('Node ID: ${node.id}')),
+                  subtitle: Center(child: Text('Color: ${node.color}')),
+                ),
+              );
+            },
           ),
-          SizedBox(height: 16),
-          Text(
-            'Counter Value: $_counter',
-            style: TextStyle(fontSize: 20),
-          ),
-          ElevatedButton(
-            onPressed: _incrementCounter,
-            child: Text('Increment Counter'),
+
+          // Checkmark button at the bottom right
+          Positioned(
+            bottom: 50,
+            right: 50,
+            child: Tooltip(
+              message: 'Run suite of tests',
+              child: ElevatedButton(
+                onPressed: () {
+                  // Add your logic here to run the suite of tests
+                },
+                child: Icon(Icons.check, color: Colors.green),
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                  side: MaterialStateProperty.all(
+                      BorderSide(color: Colors.green, width: 3)),
+                  minimumSize:
+                      MaterialStateProperty.all(Size(50, 50)), // Square size
+                  padding: MaterialStateProperty.all(EdgeInsets.all(0)),
+                ),
+              ),
+            ),
           ),
         ],
       ),
