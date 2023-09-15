00822a9e1544c68a66ec51f1a2bd97c347fa33b2
hunteraraujo <hunter_araujo@msn.com>
2023-09-15 14:15:00 -0700

    Add firebase libraries + google/github logo assets

--- frontend/windows/flutter/generated_plugin_registrant.cc
+++ frontend/windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <firebase_core/firebase_core_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FirebaseCorePluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
 }
