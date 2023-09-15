7bc7cd46712708486f09e422c28620753effaf4e
SwiftyOS <craigswift13@gmail.com>
2023-09-15 21:26:18 +0200

    added swifty agent

---
+++ autogpts/swifty/run_benchmark.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+kill $(lsof -t -i :8000)
+poetry install
+poetry run pip3 uninstall agbenchmark --yes 
+poetry run pip3 install -e ../../benchmark
+poetry run python3 -m forge &
+export PYTHONPATH=$PYTHONPATH:../../benchmark/agbenchmark
+poetry run python3 -m agbenchmark start "$@"
