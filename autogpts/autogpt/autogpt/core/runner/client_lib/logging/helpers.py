b6fd54f5997d57e0058193e8f4f41e04045e2aac
Reinier van der Leer <reinier.vanderleer@agpt.co>
2023-09-17 22:39:26 +0200

    Unbreak AutoGPT

--- autogpts/autogpt/autogpt/core/runner/client_lib/logging/helpers.py
+++ autogpts/autogpt/autogpt/core/runner/client_lib/logging/helpers.py
@@ -1,11 +1,13 @@
 from math import ceil, floor
+from typing import TYPE_CHECKING
 
-from autogpt.core.planning import LanguageModelPrompt
+if TYPE_CHECKING:
+    from autogpt.core.planning import LanguageModelPrompt
 
 SEPARATOR_LENGTH = 42
 
 
-def dump_prompt(prompt: LanguageModelPrompt) -> str:
+def dump_prompt(prompt: "LanguageModelPrompt") -> str:
     def separator(text: str):
         half_sep_len = (SEPARATOR_LENGTH - 2 - len(text)) / 2
         return f"{floor(half_sep_len)*'-'} {text.upper()} {ceil(half_sep_len)*'-'}"
