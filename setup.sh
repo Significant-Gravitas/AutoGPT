a63a421459191c9175ad16e53d8a892e4f714d71
SwiftyOS <craigswift13@gmail.com>
2023-09-21 08:06:12 +0200

    Removed flutter and chrome from setup as not required

--- setup.sh
+++ setup.sh
@@ -28,35 +28,3 @@ else
         curl -sSL https://install.python-poetry.org | python3 -
     fi
 fi
-
-if ! command -v flutter &> /dev/null
-then
-    echo "flutter could not be found"
-    echo "Please install flutter following the instrutions found at https://docs.flutter.dev/get-started/install"
-    exit 1
-fi
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    if ! command -v google-chrome-stable &> /dev/null
-    then
-        echo "Google Chrome could not be found"
-        echo "Please install Google Chrome manually from https://www.google.com/chrome/"
-        exit 1
-    fi
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    if ! command -v /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome &> /dev/null
-    then
-        echo "Google Chrome could not be found"
-        echo "Please install Google Chrome manually from https://www.google.com/chrome/"
-        exit 1
-    fi
-elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
-    if ! command -v /c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe &> /dev/null
-    then
-        echo "Google Chrome could not be found"
-        echo "Please install Google Chrome manually from https://www.google.com/chrome/"
-        exit 1
-    fi
-else
-    echo "Unsupported OS. Please install Google Chrome manually from https://www.google.com/chrome/"
-    exit 1
-fi
