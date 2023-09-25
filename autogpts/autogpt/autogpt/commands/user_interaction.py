4c9f5b00300a0e97c5e9608b6a4219d81983426c
mike dupont <mike.dupont@introspector.local>
2023-09-25 18:38:03 -0400

    update

--- autogpts/autogpt/autogpt/commands/user_interaction.py
+++ autogpts/autogpt/autogpt/commands/user_interaction.py
@@ -8,7 +8,8 @@ COMMAND_CATEGORY_TITLE = "User Interaction"
 from autogpt.agents.agent import Agent
 from autogpt.app.utils import clean_input
 from autogpt.command_decorator import command
-
+#import  ai_ticket.events.inference
+import ai_ticket.backends.pygithub
 
 @command(
     "ask_user",
@@ -49,5 +50,57 @@ async def ask_user(question: str, agent: Agent) -> str:
 )
 def request_assistence(ticket_url: str, next_action: str, agent: Agent) -> str:
     #raise Exception ("testo")
-    resp = clean_input(agent.config, f"{agent.ai_config.ai_name} reviews ticket: '{ticket_url}': ")
+    #config.github_api_key
+    issue_last_comment = ""
+    # try:
+    #     response = requests.get(ticket_url)
+    #     response.raise_for_status()
+    url_components = urllib.parse.urlparse(ticket_url)
+    if url_components.netloc != "github.com":
+        raise ValueError("Invalid ticket system")
+    # except requests.exceptions.RequestException as e:
+    #     return f"Sorry, I could not access the ticket url. Please check the url and try again. Error: {e}"
+    # except ValueError as e:
+    #     return f"Sorry, I only support tickets from GitHub. Please provide a valid GitHub ticket url. Error: {e}"
+
+    # Use the github library to interact with the GitHub API
+    try:
+        git_hub_repo = url_components.path.split("/")[1]
+        (github,repo) = load_repo(git_hub_repo,agent.config.github_api_key)
+
+        issue = repo.get_issue(url_components.path.split("/")[3])
+        issue_title = issue.title
+        issue_body = issue.body
+        issue_comments = issue.get_comments()
+        issue_last_comment = issue_comments[-1].body if issue_comments else None
+
+        
+    except github.GithubException as e:
+        return f"Sorry, I could not access the ticket data from GitHub. Please check the api token and try again. Error: {e}"
+
+    # # Use the next_action parameter to determine what kind of assistance the user needs
+    # if next_action == "write_code":
+    #     # Use the evaluate_code or improve_code commands to provide code suggestions
+    #     code = clean_input(agent.config, f"{agent.ai_config.ai_name} writes code for ticket: '{issue_title}': ")
+    #     suggestions = agent.execute_command("evaluate_code", code)
+    #     if suggestions:
+    #         improved_code = agent.execute_command("improve_code", suggestions, code)
+    #         return f"Here is the improved code for your ticket:\n\n{improved_code}\n\nI made the following changes:\n\n{suggestions}"
+    #     else:
+    #         return f"Here is the code for your ticket:\n\n{code}\n\nI did not find any suggestions for improvement."
+    # elif next_action == "understand_ticket":
+    #     # Use the summarize_text or answer_question commands to provide a summary or an answer
+    #     question = clean_input(agent.config, f"{agent.ai_config.ai_name} asks a question about ticket: '{issue_title}': ")
+    #     if question:
+    #         answer = agent.execute_command("answer_question", question, issue_body + "\n\n" + issue_last_comment if issue_last_comment else issue_body)
+    #         return f"Here is the answer to your question:\n\n{answer}"
+    #     else:
+    #         summary = agent.execute_command("summarize_text", issue_body + "\n\n" + issue_last_comment if issue_last_comment else issue_body)
+    #         return f"Here is a summary of your ticket:\n\n{summary}"
+    # else:
+    #     # Handle any invalid or unsupported values for the next_action parameter
+    #     return f"Sorry, I do not understand what kind of assistance you need. Please provide a valid value for the next_action parameter. The possible values are: write_code, understand_ticket." 
+    
+
+    resp = clean_input(agent.config, f"{agent.ai_config.ai_name} reviews ticket: '{ticket_url}': {issue_last_comment}")
     return f"The user's answer: '{resp}'"
