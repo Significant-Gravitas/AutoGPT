f4a490931c5106bac7ac1ec8afa7dd5c63af0c9c
Reinier van der Leer <pwuts@agpt.co>
2023-09-27 19:02:03 -0400

    Remove generic type annotations from PromptStrategy

--- autogpts/autogpt/autogpt/core/prompting/base.py
+++ autogpts/autogpt/autogpt/core/prompting/base.py
@@ -1,16 +1,12 @@
 import abc
-from typing import Generic, TypedDict, TypeVar, Unpack
 
 from autogpt.core.configuration import SystemConfiguration
 from autogpt.core.resource.model_providers import AssistantChatMessageDict
 
 from .schema import ChatPrompt, LanguageModelClassification
 
-IN = TypeVar("IN", bound=TypedDict)
-OUT = TypeVar("OUT")
 
-
-class PromptStrategy(abc.ABC, Generic[IN, OUT]):
+class PromptStrategy(abc.ABC):
     default_configuration: SystemConfiguration
 
     @property
@@ -19,9 +15,9 @@ class PromptStrategy(abc.ABC, Generic[IN, OUT]):
         ...
 
     @abc.abstractmethod
-    def build_prompt(self, *_, **kwargs: Unpack[IN]) -> ChatPrompt:
+    def build_prompt(self, *_, **kwargs) -> ChatPrompt:
         ...
 
     @abc.abstractmethod
-    def parse_response_content(self, response_content: AssistantChatMessageDict) -> OUT:
+    def parse_response_content(self, response_content: AssistantChatMessageDict):
         ...
