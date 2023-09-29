969188cede6a170de7b9dd9517f530fd522cd128
Zack <zack@zackbradshaw.com>
2023-09-28 23:42:51 -0500

    wip tut1

--- autogpts/swarms/forge/agent.py
+++ autogpts/swarms/forge/agent.py
@@ -16,61 +16,47 @@ from forge.sdk import (
 
 LOG = ForgeLogger(__name__)
 
+    class ForgeAgent(Agent):
 
-class ForgeAgent(Agent):
-    """
-    The goal of the Forge is to take care of the boilerplate code so you can focus on
-    agent design.
+    prompt_engine = PromptEngine("gpt-3.5-turbo")
+    system_prompt = prompt_engine.load_prompt("system-format")
 
-    There is a great paper surveying the agent landscape: https://arxiv.org/abs/2308.11432
-    Which I would highly recommend reading as it will help you understand the possabilities.
 
-    Here is a summary of the key components of an agent:
+    task_kwargs = {
+        "task": task.input,
+        "abilities": self.abilities.list_abilities_for_prompt(),
+    }
 
-    Anatomy of an agent:
-         - Profile
-         - Memory
-         - Planning
-         - Action
+    task_prompt = prompt_engine.load_prompt("task-step", **task_kwargs)
 
-    Profile:
+    messages list:
+    messages = [
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": task_prompt}
+            ]
 
-    Agents typically perform a task by assuming specific roles. For example, a teacher,
-    a coder, a planner etc. In using the profile in the llm prompt it has been shown to
-    improve the quality of the output. https://arxiv.org/abs/2305.14688
+    try:
+    # Define the parameters for the chat completion request
+    chat_completion_kwargs = {
+        "messages": messages,
+        "model": "gpt-3.5-turbo",
+    }
+    # Make the chat completion request and parse the response
+    chat_response = await chat_completion_request(**chat_completion_kwargs)
+    answer = json.loads(chat_response["choices"][0]["message"]["content"])
 
-    Additionally baed on the profile selected, the agent could be configured to use a
-    different llm. The possabilities are endless and the profile can be selected selected
-    dynamically based on the task at hand.
+    # Log the answer for debugging purposes
+    LOG.info(pprint.pformat(answer))
 
-    Memory:
+    except json.JSONDecodeError as e:
+        # Handle JSON decoding errors
+        LOG.error(f"Unable to decode chat response: {chat_response}")
+    except Exception as e:
+        # Handle other exceptions
+        LOG.error(f"Unable to generate chat response: {e}")
 
-    Memory is critical for the agent to acculmulate experiences, self-evolve, and behave
-    in a more consistent, reasonable, and effective manner. There are many approaches to
-    memory. However, some thoughts: there is long term and short term or working memory.
-    You may want different approaches for each. There has also been work exploring the
-    idea of memory reflection, which is the ability to assess its memories and re-evaluate
-    them. For example, condensting short term memories into long term memories.
 
-    Planning:
-
-    When humans face a complex task, they first break it down into simple subtasks and then
-    solve each subtask one by one. The planning module empowers LLM-based agents with the ability
-    to think and plan for solving complex tasks, which makes the agent more comprehensive,
-    powerful, and reliable. The two key methods to consider are: Planning with feedback and planning
-    without feedback.
-
-    Action:
-
-    Actions translate the agents decisions into specific outcomes. For example, if the agent
-    decides to write a file, the action would be to write the file. There are many approaches you
-    could implement actions.
-
-    The Forge has a basic module for each of these areas. However, you are free to implement your own.
-    This is just a starting point.
-    """
-
-    def __init__(self, database: AgentDB, workspace: Workspace):
+__init__(self, database: AgentDB, workspace: Workspace):
         """
         The database is used to store tasks, steps and artifact metadata. The workspace is used to
         store artifacts. The workspace is a directory on the file system.
