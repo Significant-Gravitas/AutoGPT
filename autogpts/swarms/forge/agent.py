3426002f845d1c3e247c5f588867f0cb8aa41026
Zack <zack@zackbradshaw.com>
2023-09-29 15:10:57 -0500

    Add write file functionality

--- autogpts/swarms/forge/agent.py
+++ autogpts/swarms/forge/agent.py
@@ -16,47 +16,61 @@ from forge.sdk import (
 
 LOG = ForgeLogger(__name__)
 
-    class ForgeAgent(Agent):
 
-    prompt_engine = PromptEngine("gpt-3.5-turbo")
-    system_prompt = prompt_engine.load_prompt("system-format")
+class ForgeAgent(Agent):
+    """
+    The goal of the Forge is to take care of the boilerplate code so you can focus on
+    agent design.
 
+    There is a great paper surveying the agent landscape: https://arxiv.org/abs/2308.11432
+    Which I would highly recommend reading as it will help you understand the possabilities.
 
-    task_kwargs = {
-        "task": task.input,
-        "abilities": self.abilities.list_abilities_for_prompt(),
-    }
+    Here is a summary of the key components of an agent:
 
-    task_prompt = prompt_engine.load_prompt("task-step", **task_kwargs)
+    Anatomy of an agent:
+         - Profile
+         - Memory
+         - Planning
+         - Action
 
-    messages list:
-    messages = [
-                {"role": "system", "content": system_prompt},
-                {"role": "user", "content": task_prompt}
-            ]
+    Profile:
 
-    try:
-    # Define the parameters for the chat completion request
-    chat_completion_kwargs = {
-        "messages": messages,
-        "model": "gpt-3.5-turbo",
-    }
-    # Make the chat completion request and parse the response
-    chat_response = await chat_completion_request(**chat_completion_kwargs)
-    answer = json.loads(chat_response["choices"][0]["message"]["content"])
+    Agents typically perform a task by assuming specific roles. For example, a teacher,
+    a coder, a planner etc. In using the profile in the llm prompt it has been shown to
+    improve the quality of the output. https://arxiv.org/abs/2305.14688
 
-    # Log the answer for debugging purposes
-    LOG.info(pprint.pformat(answer))
+    Additionally baed on the profile selected, the agent could be configured to use a
+    different llm. The possabilities are endless and the profile can be selected selected
+    dynamically based on the task at hand.
 
-    except json.JSONDecodeError as e:
-        # Handle JSON decoding errors
-        LOG.error(f"Unable to decode chat response: {chat_response}")
-    except Exception as e:
-        # Handle other exceptions
-        LOG.error(f"Unable to generate chat response: {e}")
+    Memory:
 
+    Memory is critical for the agent to acculmulate experiences, self-evolve, and behave
+    in a more consistent, reasonable, and effective manner. There are many approaches to
+    memory. However, some thoughts: there is long term and short term or working memory.
+    You may want different approaches for each. There has also been work exploring the
+    idea of memory reflection, which is the ability to assess its memories and re-evaluate
+    them. For example, condensting short term memories into long term memories.
 
-__init__(self, database: AgentDB, workspace: Workspace):
+    Planning:
+
+    When humans face a complex task, they first break it down into simple subtasks and then
+    solve each subtask one by one. The planning module empowers LLM-based agents with the ability
+    to think and plan for solving complex tasks, which makes the agent more comprehensive,
+    powerful, and reliable. The two key methods to consider are: Planning with feedback and planning
+    without feedback.
+
+    Action:
+
+    Actions translate the agents decisions into specific outcomes. For example, if the agent
+    decides to write a file, the action would be to write the file. There are many approaches you
+    could implement actions.
+
+    The Forge has a basic module for each of these areas. However, you are free to implement your own.
+    This is just a starting point.
+    """
+
+    def __init__(self, database: AgentDB, workspace: Workspace):
         """
         The database is used to store tasks, steps and artifact metadata. The workspace is used to
         store artifacts. The workspace is a directory on the file system.
@@ -81,48 +95,58 @@ __init__(self, database: AgentDB, workspace: Workspace):
         return task
 
     async def execute_step(self, task_id: str, step_request: StepRequestBody) -> Step:
-        """
-        The agent protocol, which is the core of the Forge, works by creating a task and then
-        executing steps for that task. This method is called when the agent is asked to execute
-        a step.
-
-        The task that is created contains an input string, for the bechmarks this is the task
-        the agent has been asked to solve and additional input, which is a dictionary and
-        could contain anything.
-
-        If you want to get the task use:
-
-        ```
+        # Firstly we get the task this step is for so we can access the task input
         task = await self.db.get_task(task_id)
-        ```
 
-        The step request body is essentailly the same as the task request and contains an input
-        string, for the bechmarks this is the task the agent has been asked to solve and
-        additional input, which is a dictionary and could contain anything.
-
-        You need to implement logic that will take in this step input and output the completed step
-        as a step object. You can do everything in a single step or you can break it down into
-        multiple steps. Returning a request to continue in the step output, the user can then decide
-        if they want the agent to continue or not.
-        """
-        # An example that
+        # Create a new step in the database
         step = await self.db.create_step(
             task_id=task_id, input=step_request, is_last=True
         )
 
-        self.workspace.write(task_id=task_id, path="output.txt", data=b"Washington D.C")
+        # Initialize the PromptEngine
+        prompt_engine = PromptEngine("gpt-3.5-turbo")
+        system_prompt = prompt_engine.load_prompt("system-format")
+        task_kwargs = {
+            "task": task.input,
+            "abilities": self.abilities.list_abilities_for_prompt(),
+        }
+        task_prompt = prompt_engine.load_prompt("task-step", **task_kwargs)
+        messages = [
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": task_prompt}
+        ]
 
+        try:
+            # Define the parameters for the chat completion request
+            chat_completion_kwargs = {
+                "messages": messages,
+                "model": "gpt-3.5-turbo",
+            }
+            # Make the chat completion request and parse the response
+            chat_response = await chat_completion_request(**chat_completion_kwargs)
+            answer = json.loads(chat_response["choices"][0]["message"]["content"])
 
-        await self.db.create_artifact(
-            task_id=task_id,
-            step_id=step.step_id,
-            file_name="output.txt",
-            relative_path="",
-            agent_created=True,
-        )
-        
-        step.output = "Washington D.C"
+            # Log the answer for debugging purposes
+            LOG.info(pprint.pformat(answer))
 
+            # Extract the ability and parameters from the answer
+            ability = answer["ability"]
+            params = answer["parameters"]
+
+            # Execute the ability
+            ability_result = await self.abilities.run_ability(ability, task_id, **params)
+
+        except json.JSONDecodeError as e:
+            # Handle JSON decoding errors
+            LOG.error(f"Unable to decode chat response: {chat_response}")
+        except Exception as e:
+            # Handle other exceptions
+            LOG.error(f"Unable to generate chat response: {e}")
+
+        # Optionally, you could update the step output with the result of the ability execution
+        step.output = ability_result
+
+        # Log the completion of the step
         LOG.info(f"\t✅ Final Step completed: {step.step_id}")
 
         return step
