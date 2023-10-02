e2e74a6a5b9ce1b09afed815598a37c487fc2fbc
merwanehamadi <merwanehamadi@gmail.com>
2023-10-02 11:50:04 -0700

    Revert hot reload (#5476)
    * Revert "Hot reload mode (#5475)"
    
    This reverts commit 846cfc005d22ccd5eb92d6be9987e8e8730dfa44.
    
    * Revert changes hot reload

--- autogpts/forge/forge/__main__.py
+++ autogpts/forge/forge/__main__.py
@@ -1,10 +1,14 @@
 import os
 
-import uvicorn
+from dotenv import load_dotenv
+
+load_dotenv()
 import forge.sdk.forge_log
 
-LOG = forge.sdk.forge_log.ForgeLogger(__name__)
+forge.sdk.forge_log.setup_logger()
 
+
+LOG = forge.sdk.forge_log.ForgeLogger(__name__)
 
 logo = """\n\n
        d8888          888             .d8888b.  8888888b. 88888888888 
@@ -32,8 +36,19 @@ d88P     888  "Y88888  "Y888 "Y88P"   "Y8888P88 888           888
 \n"""
 
 if __name__ == "__main__":
-    print(logo)
-    port = os.getenv("PORT", 8000)
-    LOG.info(f"Agent server starting on http://localhost:{port}")
+    """Runs the agent server"""
 
-    uvicorn.run("forge.app:app", host="localhost", port=port, log_level="info", reload=True)
+    # modules are imported here so that logging is setup first
+    import forge.agent
+    import forge.sdk.db
+    from forge.sdk.workspace import LocalWorkspace
+
+    print(logo)
+    database_name = os.getenv("DATABASE_STRING")
+    workspace = LocalWorkspace(os.getenv("AGENT_WORKSPACE"))
+    port = os.getenv("PORT", 8000)
+
+    database = forge.sdk.db.AgentDB(database_name, debug_enabled=False)
+    agent = forge.agent.ForgeAgent(database=database, workspace=workspace)
+
+    agent.start(port=port)
