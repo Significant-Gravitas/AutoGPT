d1274a7decd9c3182966cd114f94841772a4917c
Zack <zack@zackbradshaw.com>
2023-10-04 11:07:18 -0500

    fix: Submodule

--- autogpts/Ares/autogpt/commands/__init__.py
+++
@@ -1,7 +0,0 @@
-COMMAND_CATEGORIES = [
-    "autogpt.commands.execute_code",
-    "autogpt.commands.file_operations",
-    "autogpt.commands.web_search",
-    "autogpt.commands.web_selenium",
-    "autogpt.commands.system",
-]
--- autogpts/Synthia/autogpt/core/runner/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-This module contains the runner for the v2 agent server and client.
-"""
