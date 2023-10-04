2ac814bcefd13683dddbfaaa2e9eb1f35c8bf025
Zack <zack@zackbradshaw.com>
2023-10-04 10:49:43 -0500

    Remove the submodules

--- autogpts/Cyber/autogpt/__main__.py
+++
@@ -1,5 +0,0 @@
-"""Auto-GPT: A GPT powered AI Assistant"""
-import autogpt.app.cli
-
-if __name__ == "__main__":
-    autogpt.app.cli.main()
--- autogpts/Cyber/autogpt/speech/__init__.py
+++
@@ -1,4 +0,0 @@
-"""This module contains the speech recognition and speech synthesis functions."""
-from autogpt.speech.say import say_text
-
-__all__ = ["say_text"]
