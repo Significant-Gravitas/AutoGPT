5043ab2a1a1e2a064efb0c7d23b4202e0b5e672b
dk-optimism-io <55523489+dk-optimism-io@users.noreply.github.com>
2023-10-05 21:39:31 +0300

    Refactor and Improve Clarity of the LLM-based Agent Script (#1)
    * chore: update server.py
    
    * chore: using uuid

--- benchmark/server.py
+++ benchmark/server.py
@@ -1,25 +1,52 @@
 import io
-import json
 import logging
-import shutil
 from pathlib import Path
 from random import randint
-from typing import Annotated, Any, Dict, List
+from typing import Any, Dict, List
+import uuid
 
 from fastapi import FastAPI, File, Form, HTTPException, UploadFile
-from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
+
+app = FastAPI()
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-app = FastAPI()
+# Global artifacts store
 artifacts: List[Dict[str, Any]] = []
 
 
 class Task(BaseModel):
     input: str
+
+
+class Artifact(BaseModel):
+    binary: bytes
+    relative_path: str
+    file_name: str
+    artifact_id: str
+
+
+def generate_artifact_id() -> str:
+    """
+    Generate a unique artifact ID.
+    """
+    artifact_id = str(randint(0, 100000))
+    while artifact_id in [artifact["artifact_id"] for artifact in artifacts]:
+        artifact_id = str(randint(0, 100000))
+    return artifact_id
+
+
+def find_artifact_by_id(artifact_id: str) -> Dict[str, Any]:
+    """
+    Retrieve an artifact by its ID.
+    """
+    for artifact in artifacts:
+        if artifact["artifact_id"] == artifact_id:
+            return artifact
+    return {}
 
 
 @app.post("/agent/tasks/{task_id}/artifacts")
@@ -36,9 +63,8 @@ async def upload_file(
         / "projects/my-new-project/workspace"
     )
 
-    random_string = str(randint(0, 100000))
-    while random_string in artifacts:
-        random_string = str(randint(0, 100000))
+    # Generate a unique artifact_id using UUID
+    artifact_id = str(uuid.uuid4())
 
     artifact_data = await file.read()
     artifacts.append(
@@ -46,17 +72,16 @@ async def upload_file(
             "binary": artifact_data,
             "relative_path": relative_path,
             "file_name": file.filename,
-            "artifact_id": random_string,
+            "artifact_id": artifact_id,
         }
     )
 
     print(artifacts)
     return {
-        "artifact_id": random_string,
+        "artifact_id": artifact_id,
         "file_name": "file_name",
         "relative_path": "relative_path",
     }
-
 
 @app.get("/agent/tasks/{task_id}/artifacts")
 async def get_files() -> List[Dict[str, Any]]:
@@ -66,30 +91,22 @@ async def get_files() -> List[Dict[str, Any]]:
 
 @app.get("/agent/tasks/{task_id}/artifacts/{artifact_id}")
 async def get_file(artifact_id: str):
-    for artifact in artifacts:
-        if artifact["artifact_id"] == artifact_id:
-            break
-    else:
-        logger.error("Attempt to access nonexistent artifact with ID: %s", artifact_id)
+    artifact = find_artifact_by_id(artifact_id)
+    if not artifact:
+        logger.error(f"Attempt to access nonexistent artifact with ID: {artifact_id}")
         raise HTTPException(status_code=404, detail="Artifact not found")
 
-    logger.info("Fetching artifact with ID: %s", artifact_id)
-    # find aritifact where artifact_id = artifact_id
-
-    for artifact in artifacts:
-        if artifact["artifact_id"] == artifact_id:
-            return StreamingResponse(
-                io.BytesIO(artifact["binary"]),
-                media_type="application/octet-stream",
-                headers={"Content-Disposition": f"attachment; filename=test.txt"},
-            )
-    # return 404
-    return HTTPException(status_code=404, detail="Artifact not found")
+    logger.info(f"Fetching artifact with ID: {artifact_id}")
+    return StreamingResponse(
+        io.BytesIO(artifact["binary"]),
+        media_type="application/octet-stream",
+        headers={"Content-Disposition": f"attachment; filename={artifact['file_name']}"},
+    )
 
 
 @app.post("/agent/tasks/{task_id}/steps")
 async def create_steps(task_id: str):
-    logger.info("Creating step for task_id: %s", task_id)
+    logger.info(f"Creating step for task_id: {task_id}")
     return {
         "input": "random",
         "additional_input": {},
