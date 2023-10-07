e2d2006f9482be1930e3afecfcde8c342551fc7a
HMDCrew <andrei523070@gmail.com>
2023-10-08 00:16:02 +0200

    Hello World!

---
+++ autogpts/hmd/forge/sdk/agent.py
@@ -0,0 +1,364 @@
+import os
+import pathlib
+from io import BytesIO
+from uuid import uuid4
+
+import uvicorn
+from fastapi import APIRouter, FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse, StreamingResponse
+from fastapi.staticfiles import StaticFiles
+
+from .abilities.registry import AbilityRegister
+from .db import AgentDB
+from .errors import NotFoundError
+from .forge_log import ForgeLogger
+from .middlewares import AgentMiddleware
+from .routes.agent_protocol import base_router
+from .schema import *
+from .workspace import Workspace
+
+from .prompting import PromptEngine
+
+LOG = ForgeLogger(__name__)
+
+
+class Agent:
+    def __init__(self, database: AgentDB, workspace: Workspace):
+        self.db = database
+        self.workspace = workspace
+        self.abilities = AbilityRegister(self)
+
+    def get_agent_app(self, router: APIRouter = base_router):
+        """
+        Start the agent server.
+        """
+
+        app = FastAPI(
+            title="AutoGPT Forge",
+            description="Modified version of The Agent Protocol.",
+            version="v0.4",
+        )
+
+        # Add CORS middleware
+        origins = [
+            "http://localhost:5000",
+            "http://127.0.0.1:5000",
+            "http://localhost:8000",
+            "http://127.0.0.1:8000",
+            "http://localhost:8080",
+            "http://127.0.0.1:8080",
+            # Add any other origins you want to whitelist
+        ]
+
+        app.add_middleware(
+            CORSMiddleware,
+            allow_origins=origins,
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+
+        app.include_router(router, prefix="/ap/v1")
+        script_dir = os.path.dirname(os.path.realpath(__file__))
+        frontend_path = pathlib.Path(
+            os.path.join(script_dir, "../../../../frontend/build/web")
+        ).resolve()
+
+        if os.path.exists(frontend_path):
+            app.mount("/app", StaticFiles(directory=frontend_path), name="app")
+
+            @app.get("/", include_in_schema=False)
+            async def root():
+                return RedirectResponse(url="/app/index.html", status_code=307)
+
+        else:
+            LOG.warning(
+                f"Frontend not found. {frontend_path} does not exist. The frontend will not be served"
+            )
+        app.add_middleware(AgentMiddleware, agent=self)
+
+        return app
+
+    def start(self, port):
+        uvicorn.run(
+            "forge.app:app", host="localhost", port=port, log_level="error", reload=True
+        )
+
+    # async def create_task(self, task_request: TaskRequestBody) -> Task:
+    #     """
+    #     The agent protocol, which is the core of the Forge, works by creating a task and then
+    #     executing steps for that task. This method is called when the agent is asked to create
+    #     a task.
+
+    #     We are hooking into function to add a custom log message. Though you can do anything you
+    #     want here.
+    #     """
+    #     task = await super().create_task(task_request)
+    #     LOG.info(
+    #         f"📦 Task created: {task.task_id} input: {task.input[:40]}{'...' if len(task.input) > 40 else ''}"
+    #     )
+    #     return task
+
+    async def create_task(self, task_request: TaskRequestBody) -> Task:
+        """
+        Create a task for the agent.
+        """
+        try:
+            task = await self.db.create_task(
+                input=task_request.input,
+                additional_input=task_request.additional_input,
+            )
+
+            LOG.info(
+                f"📦 Task created: {task.task_id} input: {task.input[:40]}{'...' if len(task.input) > 40 else ''}"
+            )
+
+            return task
+        except Exception as e:
+            raise
+
+    async def list_tasks(self, page: int = 1, pageSize: int = 10) -> TaskListResponse:
+        """
+        List all tasks that the agent has created.
+        """
+        try:
+            tasks, pagination = await self.db.list_tasks(page, pageSize)
+            response = TaskListResponse(tasks=tasks, pagination=pagination)
+            return response
+        except Exception as e:
+            raise
+
+    async def get_task(self, task_id: str) -> Task:
+        """
+        Get a task by ID.
+        """
+        try:
+            task = await self.db.get_task(task_id)
+        except Exception as e:
+            raise
+        return task
+
+    async def list_steps(
+        self, task_id: str, page: int = 1, pageSize: int = 10
+    ) -> TaskStepsListResponse:
+        """
+        List the IDs of all steps that the task has created.
+        """
+        try:
+            steps, pagination = await self.db.list_steps(task_id, page, pageSize)
+            response = TaskStepsListResponse(steps=steps, pagination=pagination)
+            return response
+        except Exception as e:
+            raise
+
+    async def execute_step(self, task_id: str, step_request: StepRequestBody) -> Step:
+        """
+        The agent protocol, which is the core of the Forge, works by creating a task and then
+        executing steps for that task. This method is called when the agent is asked to execute
+        a step.
+
+        The task that is created contains an input string, for the bechmarks this is the task
+        the agent has been asked to solve and additional input, which is a dictionary and
+        could contain anything.
+
+        If you want to get the task use:
+
+        ```
+        task = await self.db.get_task(task_id)
+        ```
+
+        The step request body is essentailly the same as the task request and contains an input
+        string, for the bechmarks this is the task the agent has been asked to solve and
+        additional input, which is a dictionary and could contain anything.
+
+        You need to implement logic that will take in this step input and output the completed step
+        as a step object. You can do everything in a single step or you can break it down into
+        multiple steps. Returning a request to continue in the step output, the user can then decide
+        if they want the agent to continue or not.
+        """
+
+        # An example that
+        # self.workspace.write(task_id=task_id, path="output.txt", data=b"Washington D.C")
+
+        # step = await self.db.create_step(
+        #     task_id=task_id, input=step_request, is_last=True
+        # )
+
+        # step_input = "None"
+        # if step.input:
+        #     step_input = step.input[:19]
+        # message=f"\t🔄 Step executed: {step.step_id} input: {step_input}"
+
+        # if step.is_last:
+        #     message = (
+        #         f"\t✅ Final Step completed: {step.step_id} input: {step_input}"
+        #     )
+
+        # LOG.info (message)
+        
+        # artifact = await self.db.create_artifact(
+        #     task_id=task_id,
+        #     step_id=step.step_id,
+        #     file_name="output.txt",
+        #     relative_path="",
+        #     agent_created=True,
+        # )
+        # step.output = "Washington D.C"
+
+        # return step
+
+        task = await self.db.get_task(task_id)
+
+        step = await self.db.create_step(
+            task_id=task_id, input=step_request, is_last=True
+        )
+
+        # Log the message
+        LOG.info(f"\t✅ Final Step completed: {step.step_id} input: {step.input[:19]}")
+
+        # Initialize the PromptEngine with the "gpt-3.5-turbo" model
+        prompt_engine = PromptEngine("gpt-3.5-turbo")
+
+        # Load the system and task prompts
+        system_prompt = prompt_engine.load_prompt("system-format")
+
+        # Initialize the messages list with the system prompt
+        messages = [
+            {"role": "system", "content": system_prompt},
+        ]
+        
+        # Define the task parameters
+        task_kwargs = {
+            "task": task.input,
+            "abilities": self.abilities.list_abilities_for_prompt(),
+        }
+
+        # Load the task prompt with the defined task parameters
+        task_prompt = prompt_engine.load_prompt("task-step", **task_kwargs)
+
+        # Append the task prompt to the messages list
+        messages.append({"role": "user", "content": task_prompt})
+
+        # print(messages)
+        LOG.info(system_prompt)
+
+        try:
+            # Define the parameters for the chat completion request
+            chat_completion_kwargs = {
+                "messages": messages,
+                "model": "gpt-3.5-turbo",
+            }
+            # Make the chat completion request and parse the response
+            chat_response = await chat_completion_request(**chat_completion_kwargs)
+            answer = json.loads(chat_response["choices"][0]["message"]["content"])
+
+            # Log the answer for debugging purposes
+            LOG.info(pprint.pformat(answer))
+
+        except json.JSONDecodeError as e:
+            # Handle JSON decoding errors
+            LOG.error(f"Unable to decode chat response: {chat_response}")
+        except Exception as e:
+            # Handle other exceptions
+            LOG.error(f"Unable to generate chat response: {e}") #Step 7: Executing the Derived Ability
+
+        # self.abilities = AbilityRegister(self)
+
+        # Extract the ability from the answer
+        ability = answer["ability"]
+
+        # Run the ability and get the output
+        # We don't actually use the output in this example
+        output = await self.abilities.run_ability(
+            task_id, ability["name"], **ability["args"]
+        )
+
+        # Set the step output to the "speak" part of the answer
+        step.output = answer["thoughts"]["speak"]
+
+        #step.output = "Washington D.C"
+        return step
+
+        raise NotImplementedError
+
+    async def get_step(self, task_id: str, step_id: str) -> Step:
+        """
+        Get a step by ID.
+        """
+        try:
+            step = await self.db.get_step(task_id, step_id)
+            return step
+        except Exception as e:
+            raise
+
+    async def list_artifacts(
+        self, task_id: str, page: int = 1, pageSize: int = 10
+    ) -> TaskArtifactsListResponse:
+        """
+        List the artifacts that the task has created.
+        """
+        try:
+            artifacts, pagination = await self.db.list_artifacts(
+                task_id, page, pageSize
+            )
+            return TaskArtifactsListResponse(artifacts=artifacts, pagination=pagination)
+
+        except Exception as e:
+            raise
+
+    async def create_artifact(
+        self, task_id: str, file: UploadFile, relative_path: str
+    ) -> Artifact:
+        """
+        Create an artifact for the task.
+        """
+        data = None
+        file_name = file.filename or str(uuid4())
+        try:
+            data = b""
+            while contents := file.file.read(1024 * 1024):
+                data += contents
+            # Check if relative path ends with filename
+            if relative_path.endswith(file_name):
+                file_path = relative_path
+            else:
+                file_path = os.path.join(relative_path, file_name)
+
+            self.workspace.write(task_id, file_path, data)
+
+            artifact = await self.db.create_artifact(
+                task_id=task_id,
+                file_name=file_name,
+                relative_path=relative_path,
+                agent_created=False,
+            )
+        except Exception as e:
+            raise
+        return artifact
+
+    async def get_artifact(self, task_id: str, artifact_id: str) -> Artifact:
+        """
+        Get an artifact by ID.
+        """
+        try:
+            artifact = await self.db.get_artifact(artifact_id)
+            if artifact.file_name not in artifact.relative_path:
+                file_path = os.path.join(artifact.relative_path, artifact.file_name)
+            else:
+                file_path = artifact.relative_path
+            retrieved_artifact = self.workspace.read(task_id=task_id, path=file_path)
+        except NotFoundError as e:
+            raise
+        except FileNotFoundError as e:
+            raise
+        except Exception as e:
+            raise
+
+        return StreamingResponse(
+            BytesIO(retrieved_artifact),
+            media_type="application/octet-stream",
+            headers={
+                "Content-Disposition": f"attachment; filename={artifact.file_name}"
+            },
+        )
---
+++ autogpts/hmd/tutorials/wip_006_planning_loop.md
@@ -0,0 +1,80 @@
+# Mastering the Agent Planning Loop: Strategies for Effective Development
+
+## Introduction
+- Understanding the Agent Planning Loop
+- Significance of Effective Planning in Agent Development
+
+## Section 1: Concepts of Agent Planning Loop
+- The Structure of an Agent Planning Loop
+- Key Components and Functions
+
+## Section 2: Developing an Effective Planning Strategy
+- Setting Goals and Objectives
+- Identifying Tasks and Steps within the Planning Loop
+
+## Section 3: Implementing the Planning Loop
+- Coding the Planning Loop in the Forge Environment
+- Utilizing the Agent Protocol APIs
+
+## Section 4: Testing and Optimization
+- Test-Driven Development of the Planning Loop
+- Optimizing the Planning Loop for Better Performance
+
+## Section 5: Best Practices
+- Tips for Effective Planning Loop Implementation
+- Common Pitfalls to Avoid
+
+## Conclusion
+- Recap of the Tutorial
+- Leveraging the Planning Loop for Advanced Agent Development
+
+## Additional Resources
+
+From **The Rise and Potential of Large Language Model Based Agents: A Survey** *Zhiheng Xi (Fudan University) et al. arXiv.* [[paper](https://arxiv.org/abs/2305.14497)] [[code](https://github.com/woooodyy/llm-agent-paper-list)]
+
+### Reasoning
+
+- [2023/05] **Self-Polish: Enhance Reasoning in Large Language Models via Problem Refinement.** *Zhiheng Xi (Fudan University) et al. arXiv.* [[paper](https://arxiv.org/abs/2305.14497)] [[code](https://github.com/woooodyy/self-polish)]
+
+- [2023-03] **Large Language Models are Zero-Shot Reasoners.** *Takeshi Kojima (The University of Tokyo) et al. arXiv.* [[paper](https://arxiv.org/abs/2205.11916)][[code](https://github.com/kojima-takeshi188/zero_shot_cot)]
+
+- [2023/03] **Self-Refine: Iterative Refinement with Self-Feedback.** *Aman Madaan (Carnegie Mellon University) et al. arXiv.* [[paper](https://arxiv.org/abs/2303.17651)] [[code](https://github.com/madaan/self-refine)]
+
+- [2022/05] **Selection-Inference: Exploiting Large Language Models for Interpretable Logical Reasoning.** *Antonia Creswell (DeepMind) et al. arXiv.* [[paper](https://arxiv.org/abs/2205.09712)]
+
+- [2022/03] **Self-Consistency Improves Chain of Thought Reasoning in Language Models.** *Xuezhi Wang(Google Research) et al. arXiv.* [[paper](https://arxiv.org/abs/2203.11171)] [[code](https://github.com/huggingface/transformers/tree/main/src/transformers/models/bart)]
+
+- [2022/01] **Chain-of-Thought Prompting Elicits Reasoning in Large Language Models.** *Jason Wei (Google Research,) et al. arXiv.* [[paper](https://arxiv.org/abs/2201.11903)] 
+
+
+### Planning
+
+#### Plan formulation
+
+- [2023/05] **Tree of Thoughts: Deliberate Problem Solving with Large Language Models.** *Shunyu Yao (Princeton University) et al. arXiv.* [[paper](https://arxiv.org/abs/2305.10601)] [[code](https://github.com/princeton-nlp/tree-of-thought-llm)]
+- [2023/05] **Plan, Eliminate, and Track -- Language Models are Good Teachers for Embodied Agents.** *Yue Wu(Carnegie Mellon University) et al. arXiv.* [[paper](https://arxiv.org/abs/2305.02412)]
+- [2023/05] **Reasoning with Language Model is Planning with World Model.** *Shibo Hao (UC San Diego) et al. arXiv.* [[paper](https://arxiv.org/abs/2305.14992)] [[code](https://github.com/Ber666/RAP)]
+- [2023/05] **SwiftSage: A Generative Agent with Fast and Slow Thinking for Complex Interactive Tasks.** *Bill Yuchen Lin (Allen Institute for Artificial Intelligence) et al. arXiv.* [[paper](https://arxiv.org/abs/2305.17390)] [[code](https://github.com/yuchenlin/swiftsage)]
+- [2023/04] **LLM+P: Empowering Large Language Models with Optimal Planning Proficiency.** *Bo Liu (University of Texas at Austin) et al. arXiv.* [[paper](https://arxiv.org/abs/2304.11477)] [[code](https://github.com/Cranial-XIX/llm-pddl)]
+- [2023/03] **HuggingGPT: Solving AI Tasks with ChatGPT and its Friends in Hugging Face.** *Yongliang Shen (Microsoft Research Asia) et al. arXiv.* [[paper](https://arxiv.org/abs/2303.17580)] [[code](https://github.com/microsoft/JARVIS)]
+- [2023/02] **Describe, Explain, Plan and Select: Interactive Planning with Large Language Models Enables Open-World Multi-Task Agents.** *ZiHao Wang (Peking University) et al. arXiv.* [[paper](https://arxiv.org/abs/2302.01560)] [[code](https://github.com/CraftJarvis/MC-Planner)]
+- [2022/05] **Least-to-Most Prompting Enables Complex Reasoning in Large Language Models.** *Denny Zhou (Google Research) et al. arXiv.* [[paper](https://arxiv.org/abs/2205.10625)] 
+- [2022/05] **MRKL Systems: A modular, neuro-symbolic architecture that combines large language models, external knowledge sources and discrete reasoning.** *Ehud Karpas (AI21 Labs) et al. arXiv.* [[paper](https://arxiv.org/abs/2205.00445)] 
+- [2022/04] **Do As I Can, Not As I Say: Grounding Language in Robotic Affordances.** *Michael Ahn (Robotics at Google) et al. arXiv.* [[paper](https://arxiv.org/abs/2204.01691)]
+- [2023/05] **Agents: An Open-source Framework for Autonomous Language Agents.** Wangchunshu Zhou (AIWaves) et al. arXiv.* [[paper](https://arxiv.org/pdf/2309.07870.pdf)] [[code](https://github.com/aiwaves-cn/agents)]
+
+
+#### Plan reflection
+
+- [2023/08] **SelfCheck: Using LLMs to Zero-Shot Check Their Own Step-by-Step Reasoning.** *Ning Miao (University of Oxford) et al. arXiv.* [[paper](https://arxiv.org/abs/2308.00436)] [[code](https://github.com/NingMiao/SelfCheck)]
+- [2023/05] **ChatCoT: Tool-Augmented Chain-of-Thought Reasoning on Chat-based Large Language Models.** *Zhipeng Chen (Renmin University of China) et al. arXiv.* [[paper](https://arxiv.org/abs/2305.14323)] [[code](https://github.com/RUCAIBOX/ChatCoT)]
+- [2023/05] **Voyager: An Open-Ended Embodied Agent with Large Language Models.** *Guanzhi Wang (NVIDA) et al. arXiv.* [[paper](https://arxiv.org/abs/2305.16291)] [[code](https://voyager.minedojo.org/)]
+- [2023/03] **Chat with the Environment: Interactive Multimodal Perception Using Large Language Models.** *Xufeng Zhao (University Hamburg) et al. arXiv.* [[paper](https://arxiv.org/abs/2303.08268)] [[code](https://matcha-model.github.io/)]
+- [2022/12] **LLM-Planner: Few-Shot Grounded Planning for Embodied Agents with Large Language Models.** *Chan Hee Song (The Ohio State University) et al. arXiv.* [[paper](https://arxiv.org/abs/2212.04088)] [[code](https://dki-lab.github.io/LLM-Planner/)]
+- [2022/10] **ReAct: Synergizing Reasoning and Acting in Language Models.** *Shunyu Yao ( Princeton University) et al. arXiv.* [[paper](https://arxiv.org/abs/2210.03629)] [[code](https://react-lm.github.io/)]
+- [2022/07] **Inner Monologue: Embodied Reasoning through Planning with Language Models.** *Wenlong Huang (Robotics at Google) et al. arXiv.* [[paper](https://arxiv.org/abs/2207.05608)] [[code](https://innermonologue.github.io/)]
+- [2021/10] **AI Chains: Transparent and Controllable Human-AI Interaction by Chaining Large Language Model Prompts.** *Tongshuang Wu (University of Washington) et al. arXiv.* [[paper](https://arxiv.org/abs/2110.01691)]
+
+## Appendix
+- Example Planning Loop Implementations
+- Glossary of Planning Loop Terms
