c77ade5b2f62c5373fc7573e5c45581f003c77a3
hunteraraujo <hunter_araujo@msn.com>
2023-10-09 11:30:58 -0700

    Gate test suites from the task view based off isDeveloperModeEnabled flag

--- frontend/lib/views/chat/chat_view.dart
+++ frontend/lib/views/chat/chat_view.dart
@@ -1,6 +1,5 @@
 import 'package:auto_gpt_flutter_client/models/message_type.dart';
 import 'package:auto_gpt_flutter_client/viewmodels/settings_viewmodel.dart';
-import 'package:auto_gpt_flutter_client/viewmodels/skill_tree_viewmodel.dart';
 import 'package:auto_gpt_flutter_client/viewmodels/task_queue_viewmodel.dart';
 import 'package:auto_gpt_flutter_client/viewmodels/task_viewmodel.dart';
 import 'package:auto_gpt_flutter_client/views/chat/agent_message_tile.dart';
