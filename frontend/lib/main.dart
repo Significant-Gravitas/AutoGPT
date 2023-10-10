6028f12025abd17cd97e9a966e21da44cfb862db
hunteraraujo <hunter_araujo@msn.com>
2023-10-10 15:33:52 -0700

    Refactor TaskViewModel to use SharedPreferencesService

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -102,8 +102,12 @@ class MyApp extends StatelessWidget {
                     create: (context) => ChatViewModel(
                         Provider.of<ChatService>(context, listen: false))),
                 ChangeNotifierProvider(
-                    create: (context) => TaskViewModel(
-                        Provider.of<TaskService>(context, listen: false))),
+                  create: (context) => TaskViewModel(
+                    Provider.of<TaskService>(context, listen: false),
+                    Provider.of<SharedPreferencesService>(context,
+                        listen: false),
+                  ),
+                ),
                 ChangeNotifierProvider(
                     create: (context) => SkillTreeViewModel()),
                 ChangeNotifierProvider(
