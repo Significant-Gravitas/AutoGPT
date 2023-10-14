f3cf6989ba8606a090a71bf391c90fb191be5d85
mike dupont <mike.dupont@introspector.local>
2023-10-14 17:55:00 -0400

    since 2023

--- arena/report_jc.sh
+++ arena/report_jc.sh
@@ -1,2 +1,3 @@
-jc git log --all --stat | jq > all.json
+jc git log --all --stat --since 2023-09-01 | jq > all_30.json
 jq -c -r  ".[]?|[.author_email,.stats.files[]?]" all.json |sort |uniq -c | sort -n > report2.txt
+jq -c -r  ".[]?|.author_email" all.json |sort |uniq -c | sort -n > report4.txt
