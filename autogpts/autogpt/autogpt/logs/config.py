971b78d4d454f2bc43906dce09a83475f6587c11
Reinier van der Leer <pwuts@agpt.co>
2023-10-16 15:50:44 -0700

    Remove double milliseconds field from AutoGPT log format

--- autogpts/autogpt/autogpt/logs/config.py
+++ autogpts/autogpt/autogpt/logs/config.py
@@ -24,7 +24,7 @@ ERROR_LOG_FILE = "error.log"
 
 SIMPLE_LOG_FORMAT = "%(asctime)s %(levelname)s  %(title)s%(message)s"
 DEBUG_LOG_FORMAT = (
-    "%(asctime)s.%(msecs)03d %(levelname)s %(filename)s:%(lineno)d"
+    "%(asctime)s %(levelname)s %(filename)s:%(lineno)d"
     "  %(title)s%(message)s"
 )
 
