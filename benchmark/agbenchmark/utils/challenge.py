825c3adf62879fa9f91a19c11010336de5c98bfc
Silen Naihin <silen.naihin@gmail.com>
2023-10-20 08:26:29 -0700

    case sensitivity, updating challenges

--- benchmark/agbenchmark/utils/challenge.py
+++ benchmark/agbenchmark/utils/challenge.py
@@ -123,6 +123,9 @@ class Challenge(ABC):
         print("\033[1;34mScoring content:\033[0m", content)
         if ground.should_contain:
             for should_contain_word in ground.should_contain:
+                if not getattr(ground, 'case_sensitive', True):
+                    should_contain_word = should_contain_word.lower()
+                    content = content.lower()
                 print_content = (
                     f"\033[1;34mWord that should exist\033[0m - {should_contain_word}:"
                 )
@@ -134,6 +137,9 @@ class Challenge(ABC):
 
         if ground.should_not_contain:
             for should_not_contain_word in ground.should_not_contain:
+                if not getattr(ground, 'case_sensitive', True):
+                    should_not_contain_word = should_not_contain_word.lower()
+                    content = content.lower()
                 print_content = f"\033[1;34mWord that should not exist\033[0m - {should_not_contain_word}:"
                 if should_not_contain_word in content:
                     print(print_content, "False")
