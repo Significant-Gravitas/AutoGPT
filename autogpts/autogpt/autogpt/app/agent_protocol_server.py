0719780c91ba6c1395f43e0c87715ad998812c89
SwiftyOS <craigswift13@gmail.com>
2023-11-15 10:53:11 +0100

    Allowed all for CORS added autogpt to the arena

--- autogpts/autogpt/autogpt/app/agent_protocol_server.py
+++ autogpts/autogpt/autogpt/app/agent_protocol_server.py
@@ -64,12 +64,7 @@ class AgentProtocolServer:
 
         # Add CORS middleware
         origins = [
-            "http://localhost:5000",
-            "http://127.0.0.1:5000",
-            "http://localhost:8000",
-            "http://127.0.0.1:8000",
-            "http://localhost:8080",
-            "http://127.0.0.1:8080",
+            "*",
             # Add any other origins you want to whitelist
         ]
 
