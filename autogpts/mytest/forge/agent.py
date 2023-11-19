cc4aa3a8190856963c23fb0df1e4326ae4bb3107
Yu Peng <1455750388@qq.com>
2023-11-19 08:39:10 +0800

    add my test agent

---
+++ autogpts/mytest/forge/agent.py
@@ -0,0 +1,172 @@
+from forge.sdk import (
+    Agent,
+    AgentDB,
+    ForgeLogger,
+    Step,
+    StepRequestBody,
+    Task,
+    TaskRequestBody,
+    Workspace,    
+    PromptEngine,	
+    chat_completion_request,	
+    ChromaMemStore	
+)
+import json	
+import pprint
+
+LOG = ForgeLogger(__name__)
+
+
+class ForgeAgent(Agent):
+    """
+    The goal of the Forge is to take care of the boilerplate code, so you can focus on
+    agent design.
+
+    There is a great paper surveying the agent landscape: https://arxiv.org/abs/2308.11432
+    Which I would highly recommend reading as it will help you understand the possabilities.
+
+    Here is a summary of the key components of an agent:
+
+    Anatomy of an agent:
+         - Profile
+         - Memory
+         - Planning
+         - Action
+
+    Profile:
+
+    Agents typically perform a task by assuming specific roles. For example, a teacher,
+    a coder, a planner etc. In using the profile in the llm prompt it has been shown to
+    improve the quality of the output. https://arxiv.org/abs/2305.14688
+
+    Additionally, based on the profile selected, the agent could be configured to use a
+    different llm. The possibilities are endless and the profile can be selected
+    dynamically based on the task at hand.
+
+    Memory:
+
+    Memory is critical for the agent to accumulate experiences, self-evolve, and behave
+    in a more consistent, reasonable, and effective manner. There are many approaches to
+    memory. However, some thoughts: there is long term and short term or working memory.
+    You may want different approaches for each. There has also been work exploring the
+    idea of memory reflection, which is the ability to assess its memories and re-evaluate
+    them. For example, condensing short term memories into long term memories.
+
+    Planning:
+
+    When humans face a complex task, they first break it down into simple subtasks and then
+    solve each subtask one by one. The planning module empowers LLM-based agents with the ability
+    to think and plan for solving complex tasks, which makes the agent more comprehensive,
+    powerful, and reliable. The two key methods to consider are: Planning with feedback and planning
+    without feedback.
+
+    Action:
+
+    Actions translate the agent's decisions into specific outcomes. For example, if the agent
+    decides to write a file, the action would be to write the file. There are many approaches you
+    could implement actions.
+
+    The Forge has a basic module for each of these areas. However, you are free to implement your own.
+    This is just a starting point.
+    """
+
+    def __init__(self, database: AgentDB, workspace: Workspace):
+        """
+        The database is used to store tasks, steps and artifact metadata. The workspace is used to
+        store artifacts. The workspace is a directory on the file system.
+
+        Feel free to create subclasses of the database and workspace to implement your own storage
+        """
+        super().__init__(database, workspace)
+
+    async def create_task(self, task_request: TaskRequestBody) -> Task:
+        """
+        The agent protocol, which is the core of the Forge, works by creating a task and then
+        executing steps for that task. This method is called when the agent is asked to create
+        a task.
+
+        We are hooking into function to add a custom log message. Though you can do anything you
+        want here.
+        """
+        task = await super().create_task(task_request)
+        LOG.info(
+            f"📦 Task created: {task.task_id} input: {task.input[:40]}{'...' if len(task.input) > 40 else ''}"
+        )
+        return task
+
+    async def execute_step(self, task_id: str, step_request: StepRequestBody) -> Step:
+        # Firstly we get the task this step is for so we can access the task input
+        task = await self.db.get_task(task_id)
+
+        # Create a new step in the database
+        step = await self.db.create_step(
+            task_id=task_id, input=step_request, is_last=True
+        )
+
+        # Log the message
+        LOG.info(f"\t✅ Final Step completed: {step.step_id} input: {step.input[:19]}")
+
+        # Initialize the PromptEngine with the "gpt-3.5-turbo" model
+        prompt_engine = PromptEngine("gpt-3.5-turbo")
+
+        # Load the system and task prompts
+        system_prompt = prompt_engine.load_prompt("system-format")
+
+        # Initialize the messages list with the system prompt
+        messages = [
+            {"role": "system", "content": system_prompt},
+        ]
+        # Define the task parameters
+        task_kwargs = {
+            "task": task.input,
+            "abilities": self.abilities.list_abilities_for_prompt(),
+        }
+
+        # Load the task prompt with the defined task parameters
+        task_prompt = prompt_engine.load_prompt("task-step", **task_kwargs)
+
+        # Append the task prompt to the messages list
+        messages.append({"role": "user", "content": task_prompt})
+        print("------------------------------------------------------")
+        # [{'role': 'system', 'content': 'Reply only in json with the following format:\n\n{\n    \\"thoughts\\": {\n        \\"text\\":  \\"thoughts\\",\n        \\"reasoning\\": \\"reasoning behind thoughts\\",\n        \\"plan\\": \\"- short bulleted\\\\n- list that conveys\\\\n- long-term plan\\",\n        \\"criticism\\": \\"constructive self-criticism\\",\n        \\"speak\\": \\"thoughts summary to say to user\\",\n    },\n    \\"ability\\": {\n        \\"name\\": \\"ability name\\",\n        \\"args\\": {\n            \\"arg1\\": \\"value1", etc...\n        }\n    }\n}'}, {'role': 'user', 'content': 'Answer as an expert in Planner. \nYour task is:\n\nTell me who you are.\n\nAnswer in the provided format.\n\nYour decisions must always be made independently without seeking user assistance. Play to your strengths as an LLM and\npursue simple strategies with no legal complications.\n\n\n\n\n\n\n## Abilities\nYou have access to the following abilities you can call:\n\n- finish(reason: string) -> None. Usage: Use this to shut down once you have accomplished all of your goals, or when there are insurmountable problems that make it impossible for you to finish your task.,\n\n- list_files(path: string) -> list[str]. Usage: List files in a directory,\n\n- read_file(file_path: string) -> bytes. Usage: Read data from a file,\n\n- write_file(file_path: string, data: bytes) -> None. Usage: Write data to a file,\n\n- read_webpage(url: string, question: string) -> string. Usage: Read a webpage, and extract specific information from it if a question is specified. If you are looking to extract specific information from the webpage, you should specify a question.,\n\n- web_search(query: string) -> list[str]. Usage: Searches the web,\n\n\n\n\n\n\n'}]
+
+        print(messages)
+        print("------------------------------------------------------")
+
+        try:
+            # Define the parameters for the chat completion request
+            chat_completion_kwargs = {
+                "messages": messages,
+                "model": "gpt-3.5-turbo",
+            }
+            # Make the chat completion request and parse the response
+            chat_response = await chat_completion_request(**chat_completion_kwargs)
+            answer = json.loads(chat_response["choices"][0]["message"]["content"])
+
+            # Log the answer for debugging purposes
+            LOG.info(pprint.pformat(answer))
+
+        except json.JSONDecodeError as e:
+            # Handle JSON decoding errors
+            LOG.error(f"Unable to decode chat response: {chat_response}")
+        except Exception as e:
+            # Handle other exceptions
+            LOG.error(f"Unable to generate chat response: {e}")
+
+        # Extract the ability from the answer   
+        ability = answer["ability"]
+        print("------------------------------------------------------")
+        print(ability)
+        print("------------------------------------------------------")
+
+        # Run the ability and get the output
+        # We don't actually use the output in this example
+        output = await self.abilities.run_ability(
+            task_id, ability["name"], **ability["args"]
+        )
+
+        # Set the step output to the "speak" part of the answer
+        step.output = answer["thoughts"]["speak"]
+
+        # Return the completed step
+        return step
