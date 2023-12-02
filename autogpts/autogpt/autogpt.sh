b501e2f48ac3cfc71779b47531f3ccdb180bbaca
Reinier van der Leer <pwuts@agpt.co>
2023-12-02 14:14:35 +0100

    fix: Fix poetry env issues with the agent's entrypoint scripts
    - Update autogpt.bat to use `poetry install` instead of `%PYTHON_CMD% -m poetry install`
    - Update autogpt.sh to use `poetry install` instead of `$PYTHON_CMD -m poetry install`
    - Use `poetry run` to execute the `autogpt` command in both scripts

--- autogpts/autogpt/autogpt.sh
+++ autogpts/autogpt/autogpt.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 function find_python_command() {
-    if command -v python &> /dev/null
-    then
-        echo "python"
-    elif command -v python3 &> /dev/null
+    if command -v python3 &> /dev/null
     then
         echo "python3"
+    elif command -v python &> /dev/null
+    then
+        echo "python"
     else
         echo "Python not found. Please install Python."
         exit 1
@@ -16,16 +16,14 @@ function find_python_command() {
 PYTHON_CMD=$(find_python_command)
 
 if $PYTHON_CMD -c "import sys; sys.exit(sys.version_info < (3, 10))"; then
-    $PYTHON_CMD scripts/check_requirements.py
-    if [ $? -eq 1 ]
-    then
+    if ! $PYTHON_CMD scripts/check_requirements.py; then
         echo
-        $PYTHON_CMD -m poetry install --without dev
+        poetry install --without dev
         echo
         echo "Finished installing packages! Starting AutoGPT..."
         echo
     fi
-    $PYTHON_CMD -m autogpt "$@"
+    poetry run autogpt "$@"
 else
     echo "Python 3.10 or higher is required to run Auto GPT."
 fi
