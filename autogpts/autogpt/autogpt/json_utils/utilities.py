acf4df9f874855ec4b7dda80c5d18856561547b1
Reinier van der Leer <pwuts@agpt.co>
2023-12-13 22:41:55 +0100

    fix: Implement self-correction for invalid LLM responses
    - Fix the parsing of invalid LLM responses by appending an error message to the prompt and allowing the LLM to fix its mistakes.
    - Update the `OpenAIProvider` to handle the self-correction process and limit the number of attempts to fix parsing errors.
    - Update the `BaseAgent` to profit from the new pasing and parse-fixing mechanism.
    
    This change ensures that the system can handle and recover from errors in parsing LLM responses.
    
    Hopefully this fixes #1407 once and for all.

--- autogpts/autogpt/autogpt/json_utils/utilities.py
+++ autogpts/autogpt/autogpt/json_utils/utilities.py
@@ -26,10 +26,10 @@ def extract_dict_from_response(response_content: str) -> dict[str, Any]:
 
     # Response content comes from OpenAI as a Python `str(content_dict)`.
     # `literal_eval` does the reverse of `str(dict)`.
-    try:
-        return ast.literal_eval(response_content)
-    except BaseException as e:
-        logger.info(f"Error parsing JSON response with literal_eval {e}")
-        logger.debug(f"Invalid JSON received in response:\n{response_content}")
-        # TODO: How to raise an error here without causing the program to exit?
-        return {}
+    result = ast.literal_eval(response_content)
+    if not isinstance(result, dict):
+        raise ValueError(
+            f"Response '''{response_content}''' evaluated to "
+            f"non-dict value {repr(result)}"
+        )
+    return result
