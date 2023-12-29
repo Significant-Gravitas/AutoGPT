ce2c8dbfb10b08b4f8d726a1346812dc9fc705f2
ph-ausseil <ph.ausseil@gmail.com>
2023-12-29 22:42:17 +0100

    Create proof of concept tools for Google Calendar, Google Document Search, and YouTube Transcript.
    These changes introduce proof of concept tools for Google Calendar,
    Google Document Search, and YouTube Transcript.

---
+++ AFAAS/sample/tool/google_document_search.py
@@ -0,0 +1,34 @@
+from llama_index import VectorStoreIndex, download_loader
+from AFAAS.core.tools.tool_decorator import tool
+
+# Assuming 'index' is your VectorStoreIndex instance and is accessible here
+
+@tool(
+    name="googledocument_search",
+    description="Search an information within a GoogleDocsdocument.",
+    parameters = {
+        "query": {
+            "type": "string",
+            "description": "The search query string.",
+            "required": True
+        },
+        "document_id": {
+            "type": "string",
+            "description": "The ID of the Google Docs document to be searched.",
+            "required": True
+        },
+        "k": {
+            "type": "integer",
+            "description": "The number of search results to return. Optional.",
+        }
+    }
+)
+def document_search(query: str, document_id: str, k: int = 10):  
+    GoogleDocsReader = download_loader('GoogleDocsReader')
+
+    gdoc_ids = [document_id]
+    loader = GoogleDocsReader()
+    documents = loader.load_data(document_ids=gdoc_ids)
+    index = VectorStoreIndex.from_documents(documents)
+
+    return index.query(query, k=k)
