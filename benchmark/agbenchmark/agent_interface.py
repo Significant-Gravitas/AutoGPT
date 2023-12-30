56d8d835429f326530195becf1810cd4c8627d02
Reinier van der Leer <pwuts@agpt.co>
2023-12-30 15:01:15 +0100

    refactor(benchmark): Remove unused server.py and agent_interface.py::run_agent
    - Remove unused server.py file
    - Remove unused run_agent function from agent_interface.py

--- benchmark/agbenchmark/agent_interface.py
+++ benchmark/agbenchmark/agent_interface.py
@@ -1,11 +1,8 @@
 import os
 import shutil
-import sys
 from pathlib import Path
 
 from dotenv import load_dotenv
-
-from agbenchmark.execute_sub_process import execute_subprocess
 
 load_dotenv()
 
@@ -13,14 +10,6 @@ helicone_graphql_logs = os.getenv("HELICONE_GRAPHQL_LOGS")
 HELICONE_GRAPHQL_LOGS = (
     helicone_graphql_logs.lower() == "true" if helicone_graphql_logs else False
 )
-
-
-def run_agent(task: str, timeout: int) -> None:
-    print(f"Running agbenchmark/benchmarks.py with timeout {timeout}")
-
-    command = [sys.executable, "-m", "agbenchmark_config.benchmarks", str(task)]
-
-    execute_subprocess(command, timeout)
 
 
 def get_list_of_file_paths(
