89b892668e716c7fa00ac94d0ad7903e063974be
ph-ausseil <ph.ausseil@gmail.com>
2024-02-01 18:01:38 +0100

    INIT (#149)

--- app/cli_app/main.py
+++ app/cli_app/main.py
@@ -76,6 +76,9 @@ async def run_cli_demo():
 
                 if selected_agent_index.isdigit():
                     selected_agent_index = int(selected_agent_index)
+                else :
+                    selected_agent_index = 0
+
         else:
             selected_agent_index: int = 1
 
