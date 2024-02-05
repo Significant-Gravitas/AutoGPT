f1c170829902222fc839f880f8df4178cd25a23a
ph-ausseil <ph.ausseil@gmail.com>
2024-02-05 17:56:09 +0100

    Remove unused code

--- AFAAS/prompts/usercontext/refine_user_context.py
+++ AFAAS/prompts/usercontext/refine_user_context.py
@@ -1,29 +1,5 @@
 from __future__ import annotations
 
-"""
-RefineUserContextStrategy Module
-
-This module provides strategies and configurations to assist the AI in refining and clarifying user requirements
-through an iterative process, based on the COCE Framework.
-
-Classes:
----------
-RefineUserContextFunctionNames: Enum
-    Enum class that lists function names used in refining user context.
-
-RefineUserContextConfiguration: BaseModel
-    Pydantic model that represents the default configurations for the refine user context strategy.
-
-RefineUserContextStrategy: BasePromptStrategy
-    Strategy that guides the AI in refining and clarifying user requirements based on the COCE Framework.
-
-Examples:
----------
-To initialize and use the `RefineUserContextStrategy`:
-
->>> strategy = RefineUserContextStrategy(logger, model_classification= PromptStrategyLanguageModelClassification.FAST_MODEL_4K, default_tool_choice=RefineUserContextFunctionNames.REFINE_REQUIREMENTS, strategy_name="refine_user_context", context_min_tokens=250, context_max_tokens=300)
->>> prompt = strategy.build_prompt(interupt_refinement_process=False, user_objective="Build a web app")
-"""
 import enum
 import uuid
 
@@ -53,18 +29,7 @@ LOG = AFAASLogger(name=__name__)
 
 
 class RefineUserContextFunctionNames(str, enum.Enum):
-    """
-    An enumeration that lists the function names used in refining user context.
 
-    Attributes:
-    -----------
-    REFINE_REQUIREMENTS: str
-        Function to refine requirements.
-    REQUEST_SECOND_CONFIRMATION: str
-        Function to request a second confirmation.
-    VALIDATE_REQUIREMENTS: str
-        Function to validate requirements.
-    """
 
     REFINE_REQUIREMENTS: str = "refine_requirements"
     REQUEST_SECOND_CONFIRMATION: str = "request_second_confirmation"
@@ -86,34 +51,6 @@ class RefineUserContextStrategyConfiguration(PromptStrategiesConfiguration):
 
 
 class RefineUserContextStrategy(AbstractPromptStrategy):
-    """
-    A strategy that guides the AI in refining and clarifying user requirements based on the COCE Framework.
-
-    Attributes:
-    -----------
-    default_configuration : RefineUserContextConfiguration
-        The default configuration used for the strategy.
-    STRATEGY_NAME : str
-        Name of the strategy.
-    CONTEXT_MIN_TOKENS : int
-        Minimum number of tokens in the context.
-    CONTEXT_MAX_TOKENS : int
-        Maximum number of tokens in the context.
-    DEFAULT_SYSTEM_PROMPT_TEMPLATE : str
-        The default system prompt template.
-    SYSTEM_PROMPT_MESSAGES : str
-        Messages in the system prompt.
-    SYSTEM_PROMPT_FOOTER : str
-        Footer information for the system prompt.
-    SYSTEM_PROMPT_NOTICE : str
-        Notice information for the system prompt.
-
-    Methods:
-    --------
-    build_prompt(interupt_refinement_process: bool, user_objective: str, **kwargs) -> ChatPrompt:
-        Build a chat prompt based on the user's objective and whether the refinement process should exit.
-    """
-
     default_configuration = RefineUserContextStrategyConfiguration()
     STRATEGY_NAME = "refine_user_context"
 
@@ -121,52 +58,6 @@ class RefineUserContextStrategy(AbstractPromptStrategy):
     ### PROMPTS
     ###
 
-    DEFAULT_SYSTEM_PROMPT_TEMPLATE = (
-        "## Instructions\n\n"
-        "You are an AI running on {os_info}. Your task is to assist a user in formulating user requirements through an iterative process."
-        """
-## Iterative Process Flow:
-
-1. Step 1 **User Provides Requirements**: You will receive the user's requirements, reformulate them, and ask three questions to assist users in providing more information.
-2. Step 2 **User Answers Clarification Questions**: You will receive the reformulated user requirements from Step 1, questions from Step 1, and user responses. You will reformulate user requirements by merging the previous requirements with the answers.
-
-**Loop Control**: Continue this iterative process until users express a willingness to finalize the process."""
-        "\n\n"
-        "## Guidance :\n\n"
-        "To ensure the user's requirements adhere to our standards, follow the **COCE Framework**:\n\n"
-        " - **Comprehensible**: Your AI needs to be able to understand the goal, even within its limited context window. Minimize ambiguity, and use specific terminologies or semantics that the AI can comprehend.\n"
-        " - **Outcome-driven**: Focus on the end results or macro-goals that the AI should achieve, rather than measurable micro-goals or the steps that need to be taken to get there.\n"
-        " - **Context-aware**: The goal should be aware of and clearly define the context in which the AI is expected to function. This is especially important if the AI has a limited understanding of the world or the domain in which it operates.\n"
-        " - **Explicitness**: The goal must explicitly state what the AI needs to do. There should be no hidden assumptions or implied requirements. Everything that the AI needs to know to complete the goal should be explicitly stated.\n\n"
-        "Your primary role is to assist the user in adhering to these principles and guide them through the process of formulating requirements that meet these criteria. Please use your capabilities to ensure that the user's goal aligns with these principles by generating questions that guide him closer to our expectations in term of user requirement expression."
-    )
-
-    SYSTEM_PROMPT_MESSAGES = "{generated_message_history}"
-
-    SYSTEM_PROMPT_FOOTER = """\n\n## Key points:
-
-1. **Utilize User's Input**: ALWAYS use the user's prior requirements and their recent responses to inform your responses.
-2. **No Assumptions**: NEVER make assumptions. Always align with the user's original expressions.
-3. **Adhere to COCE**: Your reformulations, questions, and interactions should aim to get the user's requirements to fit the COCE Framework.
-4. **Use Functions**: You MUST use the provided functions in your interactions: {tools_names}.
-
-It's crucial to use the user's input, make no assumptions, align with COCE, and use the provided functions. Prioritize the user's input and remain faithful to the COCE principles."""
-
-    #     NEW_SYSTEM_PROMPT = ("###  User's Requirements:\n"
-    # #"So far the user have expressed this requirements :\n"
-    # "\"{user_response}\"\n\n"
-    # )
-    NEW_SYSTEM_PROMPT = ""
-    NEW_ASSISTANT_PROMPT = "What are your requirements ?\n"
-    # NEW_USER_PROMPT = "These are my requirement : \"{user_response}\"\n"
-    NEW_USER_PROMPT = "{user_response}"
-
-    REFINED_SYSTEM_PROMPT = ""
-    REFINED_USER_PROMPT_A = "{user_last_goal}"
-    REFINED_ASSISTANT_PROMPT = "{last_questions}"
-    REFINED_USER_PROMPT_B = "{user_response}"
-
-    SYSTEM_PROMPT_NOTICE = ""
 
     def __init__(
         self,
@@ -282,125 +173,6 @@ It's crucial to use the user's input, make no assumptions, align with COCE, and 
         #
         strategy_tools = self.get_tools()
 
-        if self._config.use_message == True:
-            #
-            # Step 2 A : Build the prompts using messages
-            #
-            if self._count == 0:
-                # NEW
-                first_system_prompt = self.DEFAULT_SYSTEM_PROMPT_TEMPLATE
-                first_system_prompt += self.SYSTEM_PROMPT_FOOTER
-                first_user_prompt = self.NEW_USER_PROMPT
-            else:
-                # REFINED
-                first_system_prompt = self.DEFAULT_SYSTEM_PROMPT_TEMPLATE
-                first_system_prompt += self.SYSTEM_PROMPT_FOOTER
-                first_user_prompt = self.REFINED_USER_PROMPT_A
-                first_assistant_prompt = self.REFINED_ASSISTANT_PROMPT
-                second_user_prompt = self.REFINED_USER_PROMPT_B
-        else:
-            #
-            # Step 2 B : Build a single system prompt
-            #
-            generated_message_history = "## Current Iteration:\n\n"
-            generated_message_history += (
-                f"This is the iteration number: {self._count+ 1}\n\n"
-            )
-
-            if self._count == 0:
-                generated_message_history += (
-                    f'The user requirement are:\n\n"{user_objective}"'
-                )
-            else:
-                generated_message_history += (
-                    f'The user requirement are:\n"{self._user_last_goal}"\n'
-                )
-                generated_message_history += "- **Your Last Questions**:\n"
-                generated_message_history += (
-                    f"{to_numbered_list(self._last_questions_label)}\n"
-                )
-                generated_message_history += "- **User's Last Response**:\n"
-                generated_message_history += f"{user_objective}"
-
-            first_system_prompt = self.DEFAULT_SYSTEM_PROMPT_TEMPLATE
-            first_system_prompt += generated_message_history
-            first_system_prompt += self.SYSTEM_PROMPT_FOOTER
-
-        #
-        # Step 3 : Build the first system message
-        #
-        first_system_message = ChatMessage.system(
-            content=first_system_prompt.format(
-                # generated_message_history = generated_message_history,
-                os_info=kwargs["os_info"],
-                tools_list=to_numbered_list(self.get_tools_names()),
-                tools_names=to_string_list(self.get_tools_names()),
-                user_last_goal=self._user_last_goal,
-                questions_history_full=to_numbered_list(
-                    self.question_history_label_full
-                ),
-                last_questions=to_numbered_list(self._last_questions_label),
-                user_response=user_objective,
-                count=self._count + 1,
-            )
-        )
-
-        if self._config.use_message == True:
-            if self._count == 0:
-                first_user_message = ChatMessage.user(
-                    content=first_user_prompt.format(user_response=user_objective)
-                )
-                messages = [first_system_message, first_user_message]
-            else:
-                first_user_message = ChatMessage.user(
-                    content=first_user_prompt.format(
-                        user_last_goal=self._user_last_goal
-                    )
-                )
-                first_assistant_message = ChatMessage.assistant(
-                    content=first_assistant_prompt.format(
-                        last_questions=to_numbered_list(self._last_questions_label)
-                    )
-                )
-                second_user_message = ChatMessage.user(
-                    content=second_user_prompt.format(user_response=user_objective)
-                )
-                messages = [
-                    first_system_message,
-                    first_user_message,
-                    first_assistant_message,
-                    second_user_message,
-                ]
-        else:
-            messages = [first_system_message]
-
-        # Step 4 : Hallucination safegard
-        #
-        tool_choice = "auto"
-        if self._count == 0:
-            tool_choice = RefineUserContextFunctionNames.REFINE_REQUIREMENTS
-            messages.append(
-                ChatMessage.system(
-                    content="""Before finalizing your response, ensure that:
-1. The reformulated goal adheres strictly to the user's provided information, with no assumptions or hallucinations.
-2. You have prepared 5 relevant questions based on the user's requirements."""
-                )
-            )
-        elif interupt_refinement_process == True:
-            tool_choice = RefineUserContextFunctionNames.VALIDATE_REQUIREMENTS
-
-        #
-        # Step 5 :
-        #
-        prompt = ChatPrompt(
-            messages=messages,
-            tools=strategy_tools,
-            tool_choice=tool_choice,
-            default_tool_choice=RefineUserContextFunctionNames.REFINE_REQUIREMENTS,
-            # TODO
-            tokens_used=0,
-        )
-
         messages = []
         smart_rag_param = {
             "os_info" : kwargs["os_info"],
@@ -440,17 +212,7 @@ It's crucial to use the user's input, make no assumptions, align with COCE, and 
         #messages.append(ChatMessage.system(self.response_format_instruction()))
         prompt_v2 = self.build_chat_prompt(messages=messages , tool_choice=tool_choice)
 
-        import difflib
-        d = difflib.Differ()
-        diff = d.compare(prompt.messages[0].content.splitlines(), prompt_v2.messages[0].content.splitlines())
-        if diff:
-            print('\n'.join(diff))
-            # print(prompt.messages[0].content)
-            # print(prompt_v2.messages[0].content)
-            LOG.error(f"DIFFERENCE DETECTED IN PROMPT")
-        else :
-            print("No difference")
-        return prompt
+        return prompt_v2
 
     def parse_response_content(
         self,
