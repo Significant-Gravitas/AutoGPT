489a8821c753b0998e493fc19483e2f030b803b4
Krzysztof Czerwinski <34861343+kcze@users.noreply.github.com>
2024-03-11 22:26:14 +0100

    feat(agent): Fully abstracted file storage access with `FileStorage` (#6931)
    * Rename `FileWorkspace` to `FileStorage`
       - `autogpt.file_workspace` -> `autogpt.file_storage`
       - `LocalFileWorkspace` -> `LocalFileStorage`
       - `S3FileWorkspace` -> `S3FileStorage`
       - `GCSFileWorkspace` -> `GCSFileStorage`
    
    * Rename `WORKSPACE_BACKEND` to `FILE_STORAGE_BACKEND`
    * Rename `WORKSPACE_STORAGE_BUCKET` to `STORAGE_BUCKET`
    
    * Rewrite `AgentManager` to use `FileStorage` rather than direct local file access
    * Rename `AgentManager.retrieve_state(..)` method to `load_agent_state`
    * Add docstrings to `AgentManager`
    
    * Create `AgentFileManagerMixin` to replace `AgentFileManager`, `FileWorkspaceMixin`, `BaseAgent.attach_fs(..)`
    * Replace `BaseAgentSettings.save_to_json_file(..)` method by `AgentFileManagerMixin.save_state()`
    * Replace `BaseAgent.set_id(..)` method by `AgentFileManagerMixin.change_agent_id(..)`
    * Remove `BaseAgentSettings.load_from_json_file(..)`
    * Remove `AgentSettings.agent_data_dir`
    
    * Update `AgentProtocolServer` to work with the new `FileStorage` system and `AgentFileManagerMixin`
    
    * Make `agent_id` and `file_storage` parameters for creating an Agent:
       - `create_agent`, `configure_agent_with_state`, `_configure_agent`, `create_agent_state` in `autogpt.agent_factory.configurators`
       - `generate_agent_for_task` in `autogpt.agent_factory.generators`
       - `Agent.__init__(..)`
       - `BaseAgent.__init__(..)`
       - Initialize and pass in `file_storage` in `autogpt.app.main.run_auto_gpt(..)` and `autogpt.app.main.run_auto_gpt_server(..)`
    
    * Add `clone_with_subroot` to `FileStorage`
    * Add `exists`, `make_dir`, `delete_dir`, `rename`, `list_files`, `list_folders` methods to `FileStorage`
    
    * Update `autogpt.commands.file_operations` to use `FileStorage` and `AgentFileManagerMixin` features
    
    * Update tests for `FileStorage` implementations and usages
    * Rename `workspace` fixture to `storage`
       * Update conftest.py

--- autogpts/autogpt/agbenchmark_config/benchmarks.py
+++ autogpts/autogpt/agbenchmark_config/benchmarks.py
@@ -3,10 +3,12 @@ import logging
 import sys
 from pathlib import Path
 
+from autogpt.agent_manager.agent_manager import AgentManager
 from autogpt.agents.agent import Agent, AgentConfiguration, AgentSettings
 from autogpt.app.main import _configure_openai_provider, run_interaction_loop
 from autogpt.commands import COMMAND_CATEGORIES
 from autogpt.config import AIProfile, ConfigBuilder
+from autogpt.file_storage import FileStorageBackendName, get_storage
 from autogpt.logs.config import configure_logging
 from autogpt.models.command_registry import CommandRegistry
 
@@ -42,6 +44,7 @@ def bootstrap_agent(task: str, continuous_mode: bool) -> Agent:
     agent_prompt_config.use_functions_api = config.openai_functions
     agent_settings = AgentSettings(
         name=Agent.default_settings.name,
+        agent_id=AgentManager.generate_id("AutoGPT-benchmark"),
         description=Agent.default_settings.description,
         ai_profile=ai_profile,
         config=AgentConfiguration(
@@ -55,13 +58,20 @@ def bootstrap_agent(task: str, continuous_mode: bool) -> Agent:
         history=Agent.default_settings.history.copy(deep=True),
     )
 
+    local = config.file_storage_backend == FileStorageBackendName.LOCAL
+    restrict_to_root = not local or config.restrict_to_workspace
+    file_storage = get_storage(
+        config.file_storage_backend, root_path="data", restrict_to_root=restrict_to_root
+    )
+    file_storage.initialize()
+
     agent = Agent(
         settings=agent_settings,
         llm_provider=_configure_openai_provider(config),
         command_registry=command_registry,
+        file_storage=file_storage,
         legacy_config=config,
     )
-    agent.attach_fs(config.app_data_dir / "agents" / "AutoGPT-benchmark")  # HACK
     return agent
 
 
--- autogpts/autogpt/tests/unit/test_file_operations.py
+++ autogpts/autogpt/tests/unit/test_file_operations.py
@@ -1,7 +1,5 @@
-import hashlib
 import os
 import re
-from io import TextIOWrapper
 from pathlib import Path
 
 import pytest
@@ -11,7 +9,7 @@ import autogpt.commands.file_operations as file_ops
 from autogpt.agents.agent import Agent
 from autogpt.agents.utils.exceptions import DuplicateOperationError
 from autogpt.config import Config
-from autogpt.file_workspace import FileWorkspace
+from autogpt.file_storage import FileStorage
 from autogpt.memory.vector.memory_item import MemoryItem
 from autogpt.memory.vector.utils import Embedding
 
@@ -46,40 +44,22 @@ def test_file_name():
 
 
 @pytest.fixture
-def test_file_path(test_file_name: Path, workspace: FileWorkspace):
-    return workspace.get_path(test_file_name)
+def test_file_path(test_file_name: Path, storage: FileStorage):
+    return storage.get_path(test_file_name)
 
 
 @pytest.fixture()
-def test_file(test_file_path: Path):
-    file = open(test_file_path, "w")
-    yield file
-    if not file.closed:
-        file.close()
+def test_directory(storage: FileStorage):
+    return storage.get_path("test_directory")
 
 
 @pytest.fixture()
-def test_file_with_content_path(test_file: TextIOWrapper, file_content, agent: Agent):
-    test_file.write(file_content)
-    test_file.close()
-    file_ops.log_operation(
-        "write", Path(test_file.name), agent, file_ops.text_checksum(file_content)
-    )
-    return Path(test_file.name)
+def test_nested_file(storage: FileStorage):
+    return storage.get_path("nested/test_file.txt")
 
 
-@pytest.fixture()
-def test_directory(workspace: FileWorkspace):
-    return workspace.get_path("test_directory")
-
-
-@pytest.fixture()
-def test_nested_file(workspace: FileWorkspace):
-    return workspace.get_path("nested/test_file.txt")
-
-
-def test_file_operations_log(test_file: TextIOWrapper):
-    log_file_content = (
+def test_file_operations_log():
+    all_logs = (
         "File Operation Logger\n"
         "write: path/to/file1.txt #checksum1\n"
         "write: path/to/file2.txt #checksum2\n"
@@ -87,8 +67,7 @@ def test_file_operations_log(test_file: TextIOWrapper):
         "append: path/to/file2.txt #checksum4\n"
         "delete: path/to/file3.txt\n"
     )
-    test_file.write(log_file_content)
-    test_file.close()
+    logs = all_logs.split("\n")
 
     expected = [
         ("write", "path/to/file1.txt", "checksum1"),
@@ -97,28 +76,7 @@ def test_file_operations_log(test_file: TextIOWrapper):
         ("append", "path/to/file2.txt", "checksum4"),
         ("delete", "path/to/file3.txt", None),
     ]
-    assert list(file_ops.operations_from_log(test_file.name)) == expected
-
-
-def test_file_operations_state(test_file: TextIOWrapper):
-    # Prepare a fake log file
-    log_file_content = (
-        "File Operation Logger\n"
-        "write: path/to/file1.txt #checksum1\n"
-        "write: path/to/file2.txt #checksum2\n"
-        "write: path/to/file3.txt #checksum3\n"
-        "append: path/to/file2.txt #checksum4\n"
-        "delete: path/to/file3.txt\n"
-    )
-    test_file.write(log_file_content)
-    test_file.close()
-
-    # Call the function and check the returned dictionary
-    expected_state = {
-        "path/to/file1.txt": "checksum1",
-        "path/to/file2.txt": "checksum4",
-    }
-    assert file_ops.file_operations_state(test_file.name) == expected_state
+    assert list(file_ops.operations_from_log(logs)) == expected
 
 
 def test_is_duplicate_operation(agent: Agent, mocker: MockerFixture):
@@ -167,11 +125,11 @@ def test_is_duplicate_operation(agent: Agent, mocker: MockerFixture):
 
 
 # Test logging a file operation
-def test_log_operation(agent: Agent):
-    file_ops.log_operation("log_test", Path("path/to/test"), agent=agent)
-    with open(agent.file_manager.file_ops_log_path, "r", encoding="utf-8") as f:
-        content = f.read()
-    assert "log_test: path/to/test\n" in content
+@pytest.mark.asyncio
+async def test_log_operation(agent: Agent):
+    await file_ops.log_operation("log_test", Path("path/to/test"), agent=agent)
+    log_entry = agent.get_file_operation_lines()[-1]
+    assert "log_test: path/to/test" in log_entry
 
 
 def test_text_checksum(file_content: str):
@@ -181,22 +139,27 @@ def test_text_checksum(file_content: str):
     assert checksum != different_checksum
 
 
-def test_log_operation_with_checksum(agent: Agent):
-    file_ops.log_operation(
+@pytest.mark.asyncio
+async def test_log_operation_with_checksum(agent: Agent):
+    await file_ops.log_operation(
         "log_test", Path("path/to/test"), agent=agent, checksum="ABCDEF"
     )
-    with open(agent.file_manager.file_ops_log_path, "r", encoding="utf-8") as f:
-        content = f.read()
-    assert "log_test: path/to/test #ABCDEF\n" in content
+    log_entry = agent.get_file_operation_lines()[-1]
+    assert "log_test: path/to/test #ABCDEF" in log_entry
 
 
-def test_read_file(
+@pytest.mark.asyncio
+async def test_read_file(
     mock_MemoryItem_from_text,
-    test_file_with_content_path: Path,
+    test_file_path: Path,
     file_content,
     agent: Agent,
 ):
-    content = file_ops.read_file(test_file_with_content_path, agent=agent)
+    await agent.workspace.write_file(test_file_path.name, file_content)
+    await file_ops.log_operation(
+        "write", Path(test_file_path.name), agent, file_ops.text_checksum(file_content)
+    )
+    content = file_ops.read_file(test_file_path.name, agent=agent)
     assert content.replace("\r", "") == file_content
 
 
@@ -229,15 +192,14 @@ async def test_write_file_logs_checksum(test_file_name: Path, agent: Agent):
     new_content = "This is new content.\n"
     new_checksum = file_ops.text_checksum(new_content)
     await file_ops.write_to_file(test_file_name, new_content, agent=agent)
-    with open(agent.file_manager.file_ops_log_path, "r", encoding="utf-8") as f:
-        log_entry = f.read()
-    assert log_entry == f"write: {test_file_name} #{new_checksum}\n"
+    log_entry = agent.get_file_operation_lines()[-1]
+    assert log_entry == f"write: {test_file_name} #{new_checksum}"
 
 
 @pytest.mark.asyncio
 async def test_write_file_fails_if_content_exists(test_file_name: Path, agent: Agent):
     new_content = "This is new content.\n"
-    file_ops.log_operation(
+    await file_ops.log_operation(
         "write",
         test_file_name,
         agent=agent,
@@ -249,81 +211,42 @@ async def test_write_file_fails_if_content_exists(test_file_name: Path, agent: A
 
 @pytest.mark.asyncio
 async def test_write_file_succeeds_if_content_different(
-    test_file_with_content_path: Path, agent: Agent
+    test_file_path: Path, file_content: str, agent: Agent
 ):
+    await agent.workspace.write_file(test_file_path.name, file_content)
+    await file_ops.log_operation(
+        "write", Path(test_file_path.name), agent, file_ops.text_checksum(file_content)
+    )
     new_content = "This is different content.\n"
-    await file_ops.write_to_file(test_file_with_content_path, new_content, agent=agent)
+    await file_ops.write_to_file(test_file_path.name, new_content, agent=agent)
 
 
 @pytest.mark.asyncio
-async def test_append_to_file(test_nested_file: Path, agent: Agent):
-    append_text = "This is appended text.\n"
-    await file_ops.write_to_file(test_nested_file, append_text, agent=agent)
+async def test_list_files(agent: Agent):
+    # Create files A and B
+    file_a_name = "file_a.txt"
+    file_b_name = "file_b.txt"
+    test_directory = Path("test_directory")
 
-    file_ops.append_to_file(test_nested_file, append_text, agent=agent)
-
-    with open(test_nested_file, "r") as f:
-        content_after = f.read()
-
-    assert content_after == append_text + append_text
-
-
-def test_append_to_file_uses_checksum_from_appended_file(
-    test_file_name: Path, agent: Agent
-):
-    append_text = "This is appended text.\n"
-    file_ops.append_to_file(
-        agent.workspace.get_path(test_file_name),
-        append_text,
-        agent=agent,
-    )
-    file_ops.append_to_file(
-        agent.workspace.get_path(test_file_name),
-        append_text,
-        agent=agent,
-    )
-    with open(agent.file_manager.file_ops_log_path, "r", encoding="utf-8") as f:
-        log_contents = f.read()
-
-    digest = hashlib.md5()
-    digest.update(append_text.encode("utf-8"))
-    checksum1 = digest.hexdigest()
-    digest.update(append_text.encode("utf-8"))
-    checksum2 = digest.hexdigest()
-    assert log_contents == (
-        f"append: {test_file_name} #{checksum1}\n"
-        f"append: {test_file_name} #{checksum2}\n"
-    )
-
-
-def test_list_files(workspace: FileWorkspace, test_directory: Path, agent: Agent):
-    # Case 1: Create files A and B, search for A, and ensure we don't return A and B
-    file_a = workspace.get_path("file_a.txt")
-    file_b = workspace.get_path("file_b.txt")
-
-    with open(file_a, "w") as f:
-        f.write("This is file A.")
-
-    with open(file_b, "w") as f:
-        f.write("This is file B.")
+    await agent.workspace.write_file(file_a_name, "This is file A.")
+    await agent.workspace.write_file(file_b_name, "This is file B.")
 
     # Create a subdirectory and place a copy of file_a in it
-    if not os.path.exists(test_directory):
-        os.makedirs(test_directory)
+    agent.workspace.make_dir(test_directory)
+    await agent.workspace.write_file(
+        test_directory / file_a_name, "This is file A in the subdirectory."
+    )
 
-    with open(os.path.join(test_directory, file_a.name), "w") as f:
-        f.write("This is file A in the subdirectory.")
-
-    files = file_ops.list_folder(str(workspace.root), agent=agent)
-    assert file_a.name in files
-    assert file_b.name in files
-    assert os.path.join(Path(test_directory).name, file_a.name) in files
+    files = file_ops.list_folder(".", agent=agent)
+    assert file_a_name in files
+    assert file_b_name in files
+    assert os.path.join(test_directory, file_a_name) in files
 
     # Clean up
-    os.remove(file_a)
-    os.remove(file_b)
-    os.remove(os.path.join(test_directory, file_a.name))
-    os.rmdir(test_directory)
+    agent.workspace.delete_file(file_a_name)
+    agent.workspace.delete_file(file_b_name)
+    agent.workspace.delete_file(test_directory / file_a_name)
+    agent.workspace.delete_dir(test_directory)
 
     # Case 2: Search for a file that does not exist and make sure we don't throw
     non_existent_file = "non_existent_file.txt"
