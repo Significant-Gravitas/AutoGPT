02b589d549a7c996a6e3acda8d65804c8980b251
Krzysztof Czerwinski <kpczerwinski@gmail.com>
2024-03-15 18:06:09 +0100

    Update tests

--- autogpts/autogpt/tests/unit/test_json_utils.py
+++ autogpts/autogpt/tests/unit/test_json_utils.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 
 from autogpt.core.json_utils.utils import json_loads
@@ -14,9 +16,8 @@ _JSON_FIXABLE: list[tuple[str, str]] = [
     # Extra newlines
     ('{"name": "John Doe",\n"age": 30}', '{"name": "John Doe", "age": 30}'),
     ("[1, 2,\n3]", "[1, 2, 3]"),
-    # Missing brace
+    # Missing closing brace or bracket
     ('{"name": "John Doe", "age": 30', '{"name": "John Doe", "age": 30}'),
-    # Missing bracket
     ("[1, 2, 3", "[1, 2, 3]"),
     # Different numerals
     ("[+1, ---2, .5, +-4.5, 123.]", "[1, -2, 0.5, -4.5, 123]"),
@@ -26,50 +27,67 @@ _JSON_FIXABLE: list[tuple[str, str]] = [
         '[1, 2 3, "yes" true, false null, 25, {"obj": "var"}',
         '[1, 2, 3, "yes", true, false, null, 25, {"obj": "var"}]',
     ),
+    # Codeblock
+    (
+        '```json\n{"name": "John Doe", "age": 30}\n```',
+        '{"name": "John Doe", "age": 30}',
+    ),
     # Mutliple problems
     (
         '{"name":"John Doe" "age": 30\n "empty": "","address": '
         "// random comment\n"
-        '{"city": "New\nYork", "state": "NY",'
-        '"skills": ["Python" "C++", "Java",""],}',
+        '{"city": "New York", "state": "NY"},'
+        '"skills": ["Python" "C++", "Java",""],',
         '{"name": "John Doe", "age": 30, "empty": "", "address": '
-        '{"city": "New\nYork", "state": "NY", '
-        '"skills": ["Python", "C++", "Java", ""]}}',
+        '{"city": "New York", "state": "NY"}, '
+        '"skills": ["Python", "C++", "Java", ""]}',
     ),
     # All good
     (
         '{"name": "John Doe", "age": 30, "address": '
-        '{"city": "New\nYork", "state": "NY", '
-        '"skills": ["Python", "C++", "Java"]}}',
+        '{"city": "New York", "state": "NY"}, '
+        '"skills": ["Python", "C++", "Java"]}',
         '{"name": "John Doe", "age": 30, "address": '
-        '{"city": "New\nYork", "state": "NY", '
-        '"skills": ["Python", "C++", "Java"]}}',
+        '{"city": "New York", "state": "NY"}, '
+        '"skills": ["Python", "C++", "Java"]}',
     ),
+    ("true", "true"),
+    ("false", "false"),
+    ("null", "null"),
+    ("123.5", "123.5"),
+    ('"Hello, World!"', '"Hello, World!"'),
+    ("{}", "{}"),
+    ("[]", "[]"),
 ]
 
 _JSON_UNFIXABLE: list[tuple[str, str]] = [
-    # Broken booleans
-    ("[TRUE, False]", "[true, false]"),
+    # Broken booleans and null
+    ("[TRUE, False, NULL]", "[true, false, null]"),
     # Missing values in array
     ("[1, , 3]", "[1, 3]"),
-    # Leading zeros (are threated as octal)
+    # Leading zeros (are treated as octal)
     ("[0023, 015]", "[23, 15]"),
+    # Missing quotes
+    ('{"name": John Doe}', '{"name": "John Doe"}'),
+    # Missing opening braces or bracket
+    ('"name": "John Doe"}', '{"name": "John Doe"}'),
+    ("1, 2, 3]", "[1, 2, 3]"),
 ]
 
 
 @pytest.fixture(params=_JSON_FIXABLE)
-def fixable_json(request) -> tuple[str, str]:
+def fixable_json(request: pytest.FixtureRequest) -> tuple[str, str]:
     return request.param
 
 
 @pytest.fixture(params=_JSON_UNFIXABLE)
-def unfixable_json(request) -> tuple[str, str]:
+def unfixable_json(request: pytest.FixtureRequest) -> tuple[str, str]:
     return request.param
 
 
 def test_json_loads_fixable(fixable_json: tuple[str, str]):
-    assert json_loads(fixable_json[0]) == json_loads(fixable_json[1])
+    assert json_loads(fixable_json[0]) == json.loads(fixable_json[1])
 
 
 def test_json_loads_unfixable(unfixable_json: tuple[str, str]):
-    assert json_loads(unfixable_json[0]) != json_loads(unfixable_json[1])
+    assert json_loads(unfixable_json[0]) != json.loads(unfixable_json[1])
