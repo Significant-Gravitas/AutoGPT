276d3ad77a147d928424ddbe28eaba6cc7febafc
Matheus Oliveira <matheudev@gmail.com>
2024-03-22 10:45:07 -0300

    security(agent): Replace unsafe `pyyaml` loader with `SafeLoader` (#7035)
    Co-authored-by: pixeebot[bot] <104101892+pixeebot[bot]@users.noreply.github.com>

--- autogpts/autogpt/autogpt/utils.py
+++ autogpts/autogpt/autogpt/utils.py
@@ -7,7 +7,7 @@ from colorama import Fore
 def validate_yaml_file(file: str | Path):
     try:
         with open(file, encoding="utf-8") as fp:
-            yaml.load(fp.read(), Loader=yaml.FullLoader)
+            yaml.load(fp.read(), Loader=yaml.SafeLoader)
     except FileNotFoundError:
         return (False, f"The file {Fore.CYAN}`{file}`{Fore.RESET} wasn't found")
     except yaml.YAMLError as e:
