7b4a376c1546926a04afce56cb2fbe5366d9a67b
Krzysztof Czerwinski <kpczerwinski@gmail.com>
2024-04-08 12:21:38 +0200

    Move utils

--- autogpts/autogpt/autogpt/agents/components.py
+++ autogpts/autogpt/autogpt/agents/components.py
@@ -28,23 +28,11 @@ class ComponentError(Exception):
 class ProtocolError(ComponentError):
     """Error of an entire pipeline of one component type."""
 
-    def __init__(self, message: str = ""):
-        self.message = message
-        super().__init__(message)
-
 
 class PipelineError(ComponentError):
     """Error of a group of component types;
     multiple protocols."""
 
-    def __init__(self, message: str = ""):
-        self.message = message
-        super().__init__(message)
-
 
 class ComponentSystemError(ComponentError):
     """Error of an entire system."""
-
-    def __init__(self, message: str = ""):
-        self.message = message
-        super().__init__(message)
