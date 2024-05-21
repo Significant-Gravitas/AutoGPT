799fd9889619dc67dfb4c1742be58287a330c6ae
Krzysztof Czerwinski <kpczerwinski@gmail.com>
2024-05-21 20:59:33 +0200

    Update

--- autogpts/forge/forge/logging/config.py
+++ autogpts/forge/forge/logging/config.py
@@ -71,50 +71,6 @@ class LoggingConfig(SystemConfiguration):
     )
 
 
-class FancyConsoleFormatter(logging.Formatter):
-    """
-    A custom logging formatter designed for console output.
-
-    This formatter enhances the standard logging output with color coding. The color
-    coding is based on the level of the log message, making it easier to distinguish
-    between different types of messages in the console output.
-
-    The color for each level is defined in the LEVEL_COLOR_MAP class attribute.
-    """
-
-    # level -> (level & text color, title color)
-    LEVEL_COLOR_MAP = {
-        logging.DEBUG: Fore.LIGHTBLACK_EX,
-        logging.INFO: Fore.BLUE,
-        logging.WARNING: Fore.YELLOW,
-        logging.ERROR: Fore.RED,
-        logging.CRITICAL: Fore.RED + Style.BRIGHT,
-    }
-
-    def format(self, record: logging.LogRecord) -> str:
-        # Make sure `msg` is a string
-        if not hasattr(record, "msg"):
-            record.msg = ""
-        elif not type(record.msg) is str:
-            record.msg = str(record.msg)
-
-        # Determine default color based on error level
-        level_color = ""
-        if record.levelno in self.LEVEL_COLOR_MAP:
-            level_color = self.LEVEL_COLOR_MAP[record.levelno]
-            record.levelname = f"{level_color}{record.levelname}{Style.RESET_ALL}"
-
-        # Determine color for message
-        color = getattr(record, "color", level_color)
-        color_is_specified = hasattr(record, "color")
-
-        # Don't color INFO messages unless the color is explicitly specified.
-        if color and (record.levelno != logging.INFO or color_is_specified):
-            record.msg = f"{color}{record.msg}{Style.RESET_ALL}"
-
-        return super().format(record)
-
-
 def configure_logging(
     debug: bool = False,
     level: Optional[int | str] = None,
