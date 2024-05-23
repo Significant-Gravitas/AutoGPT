a88e607b9c738ac08ec377ba040920f5759c6e23
Zamil Majdy <zamil.majdy@agpt.co>
2024-05-23 08:17:27 +0700

    Add test

--- rnd/autogpt_server/autogpt_server/app.py
+++ rnd/autogpt_server/autogpt_server/app.py
@@ -1,11 +1,11 @@
-from multiprocessing import Process
-
 from autogpt_server.agent_api import start_server
 from autogpt_server.agent_executor import start_executor
 from autogpt_server.data import ExecutionQueue
 
-if __name__ == "__main__":
+def main() -> None:
     queue = ExecutionQueue()
-    executor_process = Process(target=start_executor, args=(5, queue))
-    executor_process.start()
+    start_executor(5, queue)
     start_server(queue)
+
+if __name__ == "__main__":
+    main()
