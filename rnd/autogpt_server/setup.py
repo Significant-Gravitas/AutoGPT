70892608e033cb88ca7770e0cc528d45cfaa4e24
Nicholas Tindle <nicholas.tindle@agpt.co>
2024-06-03 16:05:00 -0500

    feat(agent_server): more build attempts

--- rnd/autogpt_server/setup.py
+++ rnd/autogpt_server/setup.py
@@ -1,25 +1,25 @@
 from shutil import which
 from pkgutil import iter_modules
 
-# from cx_Freeze import setup, Executable
+from cx_Freeze import setup, Executable
 
-# import os
+import os
 
-# packages = [
-#     m.name for m in iter_modules() if m.ispkg and ".venv" in m.module_finder.path
-# ]
+packages = [
+    m.name for m in iter_modules() if m.ispkg and ".venv" in m.module_finder.path
+]
 
-# setup(
-#     executables=[
-#         Executable(("autogpt_server/app.py"), target_name="server", base="service")
-#     ],
-#     options={
-#         "build_exe": {
-#             "packages": packages,
-#             "includes": [
-#                 "autogpt_server",
-#             ],
-#         },
-#         "bdist_mac": {"bundle_name": "AutoGPT", "include_resources": ["IMG_3775.jpeg"]},
-#     },
-# )
+setup(
+    executables=[
+        # Executable(which("autogpt_server"), target_name="server", base="console"),
+        # Executable(which("hypercorn"), target_name="hypercorn"),
+        Executable(which("uvicorn"), target_name="uvicorn"),
+    ],
+    options={
+        "build_exe": {
+            "packages": packages,
+            "includes": ["autogpt_server"],
+        },
+        "bdist_mac": {"bundle_name": "AutoGPT", "include_resources": ["IMG_3775.jpeg"]},
+    },
+)
