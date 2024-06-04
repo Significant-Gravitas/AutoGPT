71635aead6578612d8eb6ed8f0ee81f2f5eb2cdc
SwiftyOS <craigswift13@gmail.com>
2024-06-04 10:42:57 +0200

    simplified dir naming

--- rnd/autogpt_server/autogpt_server/app.py
+++ rnd/autogpt_server/autogpt_server/app.py
@@ -1,5 +1,5 @@
-from autogpt_server.agent_api import start_server
-from autogpt_server.agent_executor import start_executor_manager
+from autogpt_server.server import start_server
+from autogpt_server.executor import start_executor_manager
 from autogpt_server.data import ExecutionQueue
 
 
--- rnd/autogpt_server/test/test_app.py
+++ rnd/autogpt_server/test/test_app.py
@@ -1,8 +1,8 @@
 import pytest
 from fastapi.testclient import TestClient
 
-from autogpt_server.agent_api import start_server
-from autogpt_server.agent_executor import start_executor_manager
+from autogpt_server.server import start_server
+from autogpt_server.executor import start_executor_manager
 from autogpt_server.data import ExecutionQueue
 
 
