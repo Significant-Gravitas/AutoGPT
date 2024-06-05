23dfdad454b5142773e5b9571bde85222e98b67b
Nicholas Tindle <nicholas.tindle@agpt.co>
2024-06-05 00:14:08 -0500

    feat(autogpt,autogpt_server): Build AutoGPT's Constituent Parts for deployment (#7188)

--- rnd/autogpt_server/test/test_app.py
+++ rnd/autogpt_server/test/test_app.py
@@ -1,9 +1,9 @@
 import pytest
 from fastapi.testclient import TestClient
 
-from autogpt_server.server import start_server
-from autogpt_server.executor import start_executor_manager
 from autogpt_server.data import ExecutionQueue
+from autogpt_server.executor import start_executor_manager
+from autogpt_server.server import start_server
 
 
 @pytest.fixture
