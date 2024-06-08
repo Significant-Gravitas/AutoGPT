76708dc053bbfaa627d187b8fbdf458a2c0cdfa9
Reinier van der Leer <pwuts@agpt.co>
2024-06-09 00:09:01 +0200

    fix(frontend): Sync request models up with `forge.agent_protocol` (#7202)
    Frontend broke in #7171 because of changes to the request models in `forge.agent_protocol`. This PR unbreaks it.
    
    Changes:
    - Make `input` required on `TaskRequestBody` and `StepRequestBody`
    - Amend `toJson()` on `TaskRequestBody` and `StepRequestBody` to omit attributes with `null` value

--- frontend/lib/models/step_request_body.dart
+++ frontend/lib/models/step_request_body.dart
@@ -1,13 +1,12 @@
 class StepRequestBody {
-  final String? input;
+  final String input;
   final Map<String, dynamic>? additionalInput;
 
   StepRequestBody({required this.input, this.additionalInput});
 
   Map<String, dynamic> toJson() {
-    if (input == null && additionalInput == null) {
-      return {};
-    }
-    return {'input': input, 'additional_input': additionalInput};
+    Map<String, dynamic> result = {'input': input, 'additional_input': additionalInput};
+    result.removeWhere((_, v) => v == null);
+    return result;
   }
 }
