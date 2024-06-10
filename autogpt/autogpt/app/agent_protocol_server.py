c734867365e31737baf70bdedb1abc2772e4a9f1
Nicholas Tindle <nicholas.tindle@agpt.co>
2024-06-10 15:11:17 -0500

    feat(autogpt): fix the origins being repalced

--- autogpt/autogpt/app/agent_protocol_server.py
+++ autogpt/autogpt/app/agent_protocol_server.py
@@ -98,7 +98,7 @@ class AgentProtocolServer:
 
         app.add_middleware(
             CORSMiddleware,
-            allow_origins=["*"],
+            allow_origins=[origins],
             allow_credentials=True,
             allow_methods=["*"],
             allow_headers=["*"],
