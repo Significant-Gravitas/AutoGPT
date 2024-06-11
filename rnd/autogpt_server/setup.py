be9d6ea849b6356f12d71c747fe11d830e14b9e7
Nicholas Tindle <nicholas.tindle@agpt.co>
2024-06-11 12:29:23 -0500

    ci(server): Agent Server CI (#7193)

--- rnd/autogpt_server/setup.py
+++ rnd/autogpt_server/setup.py
@@ -20,6 +20,7 @@ icon = (
 setup(
     name="AutoGPT Server",
     url="https://agpt.co",
+    # The entry points of the application
     executables=[
         Executable(
             "autogpt_server/app.py",
@@ -28,10 +29,11 @@ setup(
             icon=icon,
         ),
         Executable(
-            "autogpt_server/cli.py", target_name="agpt_cli", base="console", icon=icon
+            "autogpt_server/cli.py", target_name="agpt_server_cli", base="console", icon=icon
         ),
     ],
     options={
+      # Options for building all the executables
         "build_exe": {
             "packages": packages,
             "includes": [
@@ -41,22 +43,36 @@ setup(
                 "uvicorn.protocols.websockets.auto",
                 "uvicorn.lifespan.on",
             ],
+            # Exclude the two module from readability.compat as it causes issues
             "excludes": ["readability.compat.two"],
         },
+        # Mac .app specific options
         "bdist_mac": {
             "bundle_name": "AutoGPT",
             "iconfile": "../../assets/gpt_dark_RGB.icns",
             # "include_resources": ["IMG_3775.jpeg"],
         },
+        # Mac .dmg specific options
         "bdist_dmg": {
             "applications_shortcut": True,
-            "volume_label": "AutoGPT Server",
+            "volume_label": "AutoGPTServer",
         },
+        # Windows .msi specific options
         "bdist_msi": {
             "target_name": "AutoGPTServer",
             "add_to_path": True,
             "install_icon": "../../assets/gpt_dark_RGB.ico",
         },
+        # Linux .appimage specific options
         "bdist_appimage": {},
+        # Linux rpm specific options
+        "bdist_rpm": {
+            "name": "AutoGPTServer",
+            "description": "AutoGPT Server",
+            "version": "0.1",
+            "license": "UNKNOWNORPROPRIETARY",
+            "url": "https://agpt.co",
+            "long_description": "AutoGPT Server",
+        },
     },
 )
