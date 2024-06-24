9f1e5218578f3eae08f2a6211438b484a0aef3ba
Zamil Majdy <zamil.majdy@agpt.co>
2024-06-24 06:41:02 +0400

    feat(rnd): Add AutoGPT server scheduling service (#7226)
    ### Background
    
    Agent execution should be able to be triggered in a recurring manner.
    This PR introduced an ExecutionScheduling service, a process responsible for managing the execution schedule and triggering its execution based on a predefined cron expression.
    
    ### Changes 🏗️
    
    * Added `scheduler.py` / `ExecutionScheduler` implementation.
    * Added scheduler test.
    * Added `AgentExecutionSchedule` table and its logical model & prisma queries.
    * Moved `add_execution` from API server to `execution_manager`

--- rnd/autogpt_server/autogpt_server/server/server.py
+++ rnd/autogpt_server/autogpt_server/server/server.py
@@ -34,66 +34,75 @@ class AgentServer(AppProcess):
         router = APIRouter()
         router.add_api_route(
             path="/blocks",
-            endpoint=AgentServer.get_agent_blocks,
+            endpoint=self.get_agent_blocks,
             methods=["GET"],
         )
         router.add_api_route(
             path="/agents",
-            endpoint=AgentServer.get_agents,
+            endpoint=self.get_agents,
             methods=["GET"],
         )
         router.add_api_route(
             path="/agents/{agent_id}",
-            endpoint=AgentServer.get_agent,
+            endpoint=self.get_agent,
             methods=["GET"],
         )
         router.add_api_route(
             path="/agents",
-            endpoint=AgentServer.create_agent,
+            endpoint=self.create_agent,
             methods=["POST"],
         )
         router.add_api_route(
             path="/agents/{agent_id}/execute",
-            endpoint=AgentServer.execute_agent,
+            endpoint=self.execute_agent,
             methods=["POST"],
         )
         router.add_api_route(
             path="/agents/{agent_id}/executions/{run_id}",
-            endpoint=AgentServer.get_executions,
+            endpoint=self.get_executions,
             methods=["GET"],
         )
         router.add_api_route(
             path="/agents/{agent_id}/schedules",
-            endpoint=AgentServer.schedule_agent,
+            endpoint=self.schedule_agent,
             methods=["POST"],
         )
         router.add_api_route(
             path="/agents/{agent_id}/schedules",
-            endpoint=AgentServer.get_execution_schedules,
+            endpoint=self.get_execution_schedules,
             methods=["GET"],
+        )
+        router.add_api_route(
+            path="/agents/schedules/{schedule_id}",
+            endpoint=self.update_schedule,
+            methods=["PUT"],
         )
 
         app.include_router(router)
         uvicorn.run(app, host="0.0.0.0", port=8000)
 
-    @staticmethod
-    async def get_agent_blocks() -> list[dict]:
+    @property
+    def execution_manager_client(self) -> ExecutionManager:
+        return get_service_client(ExecutionManager)
+
+    @property
+    def execution_scheduler_client(self) -> ExecutionScheduler:
+        return get_service_client(ExecutionScheduler)
+
+    async def get_agent_blocks(self) -> list[dict]:
         return [v.to_dict() for v in await block.get_blocks()]
 
-    @staticmethod
-    async def get_agents() -> list[str]:
+    async def get_agents(self) -> list[str]:
         return await graph.get_graph_ids()
 
-    @staticmethod
-    async def get_agent(agent_id: str) -> graph.Graph:
+    async def get_agent(self, agent_id: str) -> graph.Graph:
         agent = await graph.get_graph(agent_id)
         if not agent:
             raise HTTPException(status_code=404, detail=f"Agent #{agent_id} not found.")
 
         return agent
 
-    @staticmethod
-    async def create_agent(agent: graph.Graph) -> graph.Graph:
+    async def create_agent(self, agent: graph.Graph) -> graph.Graph:
         agent.id = str(uuid.uuid4())
 
         id_map = {node.id: str(uuid.uuid4()) for node in agent.nodes}
@@ -104,62 +113,36 @@ class AgentServer(AppProcess):
 
         return await graph.create_graph(agent)
 
-    @staticmethod
-    async def execute_agent(agent_id: str, node_input: dict) -> dict:
-        agent = await graph.get_graph(agent_id)
-        if not agent:
-            raise HTTPException(status_code=404, detail=f"Agent #{agent_id} not found.")
+    async def execute_agent(self, agent_id: str, node_input: dict) -> dict:
+        try:
+            return self.execution_manager_client.add_execution(agent_id, node_input)
+        except Exception as e:
+            msg = e.__str__().encode().decode('unicode_escape')
+            raise HTTPException(status_code=400, detail=msg)
 
-        run_id = str(uuid.uuid4())
-        executions = []
-        execution_manager = get_service_client(ExecutionManager)
-
-        # Currently, there is no constraint on the number of root nodes in the graph.
-        for node in agent.starting_nodes:
-            node_block = await block.get_block(node.block_id)
-            if not node_block:
-                raise HTTPException(
-                    status_code=404,
-                    detail=f"Block #{node.block_id} not found.",
-                )
-            if error := node_block.input_schema.validate_data(node_input):
-                raise HTTPException(
-                    status_code=400,
-                    detail=f"Input data doesn't match {node_block.name} input: {error}",
-                )
-
-            exec_id = execution_manager.add_execution(
-                run_id=run_id, node_id=node.id, data=node_input
-            )
-            executions.append({
-                "exec_id": exec_id,
-                "node_id": node.id,
-            })
-
-        return {
-            "run_id": run_id,
-            "executions": executions,
-        }
-
-    @staticmethod
     async def get_executions(
-            agent_id: str,
-            run_id: str
-    ) -> list[execution.ExecutionResult]:
+            self, agent_id: str, run_id: str) -> list[execution.ExecutionResult]:
         agent = await graph.get_graph(agent_id)
         if not agent:
             raise HTTPException(status_code=404, detail=f"Agent #{agent_id} not found.")
 
         return await execution.get_executions(run_id)
 
-    @staticmethod
-    def schedule_agent(agent_id: str, cron: str, input_data: dict) -> dict:
-        execution_scheduler = get_service_client(ExecutionScheduler)
+    async def schedule_agent(self, agent_id: str, cron: str, input_data: dict) -> dict:
+        agent = await graph.get_graph(agent_id)
+        if not agent:
+            raise HTTPException(status_code=404, detail=f"Agent #{agent_id} not found.")
+        execution_scheduler = self.execution_scheduler_client
         return {
             "id": execution_scheduler.add_execution_schedule(agent_id, cron, input_data)
         }
 
-    @staticmethod
-    def get_execution_schedules(agent_id: str) -> list[dict]:
-        execution_scheduler = get_service_client(ExecutionScheduler)
+    def update_schedule(self, schedule_id: str, input_data: dict) -> dict:
+        execution_scheduler = self.execution_scheduler_client
+        is_enabled = input_data.get("is_enabled", False)
+        execution_scheduler.update_schedule(schedule_id, is_enabled)
+        return {"id": schedule_id}
+
+    def get_execution_schedules(self, agent_id: str) -> dict[str, str]:
+        execution_scheduler = self.execution_scheduler_client
         return execution_scheduler.get_execution_schedules(agent_id)
