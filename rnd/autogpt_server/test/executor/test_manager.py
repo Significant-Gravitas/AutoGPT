26bcb26bb7121f071b7aa3af0509063b74fa90fa
Zamil Majdy <zamil.majdy@agpt.co>
2024-06-26 14:41:55 +0400

    feat(rnd): Refactor AgentServer Node Input/Output Relation & Block output interface (#7231)
    ### Background
    
    The current implementation of AgentServer doesn't allow for a single pin to be connected to multiple nodes, this will be problematic when you have a single output node that needs to be propagated into many nodes. Or multiple nodes that possibly feed the data into a single pin (first come first serve).
    
    This infra change is also part of the preparation for changing the `block` interface to return a stream of output instead of a single output.  Treating blocks as streams requires this capability.
    
    ### Changes 🏗️
    
    * Update block run interface from returning `(output_name, output_data)` to `Generator[(output_name, output_data)]`
    * Removed `agent` term in the API, replace it with `graph` for consistency.
    * Reintroduced `AgentNodeExecutionInputOutput`. `AgentNodeExecution` input & output will be a list of `AgentNodeExecutionInputOutput` which describes the input & output data of its execution. Making an execution has 1-many relation to its input output data.
    * Propagating the relation and block interface change into the execution engine.

--- rnd/autogpt_server/test/executor/test_manager.py
+++ rnd/autogpt_server/test/executor/test_manager.py
@@ -45,18 +45,18 @@ async def create_test_graph() -> graph.Graph:
     return test_graph
 
 
-async def execute_agent(test_manager: ExecutionManager, test_graph: graph.Graph):
+async def execute_graph(test_manager: ExecutionManager, test_graph: graph.Graph):
     # --- Test adding new executions --- #
     text = "Hello, World!"
     input_data = {"input": text}
     agent_server = AgentServer()
-    response = await agent_server.execute_agent(test_graph.id, input_data)
+    response = await agent_server.execute_graph(test_graph.id, input_data)
     executions = response["executions"]
-    run_id = response["run_id"]
+    graph_exec_id = response["id"]
     assert len(executions) == 2
 
     async def is_execution_completed():
-        execs = await agent_server.get_executions(test_graph.id, run_id)
+        execs = await agent_server.get_executions(test_graph.id, graph_exec_id)
         return test_manager.queue.empty() and len(execs) == 4
 
     # Wait for the executions to complete
@@ -67,34 +67,30 @@ async def execute_agent(test_manager: ExecutionManager, test_graph: graph.Graph)
 
     # Execution queue should be empty
     assert await is_execution_completed()
-    executions = await agent_server.get_executions(test_graph.id, run_id)
+    executions = await agent_server.get_executions(test_graph.id, graph_exec_id)
 
     # Executing ParrotBlock1
     exec = executions[0]
     assert exec.status == execution.ExecutionStatus.COMPLETED
-    assert exec.run_id == run_id
-    assert exec.output_name == "output"
-    assert exec.output_data == "Hello, World!"
-    assert exec.input_data == input_data
+    assert exec.graph_exec_id == graph_exec_id
+    assert exec.output_data == {"output": ["Hello, World!"]}
+    assert exec.input_data == {"input": text}
     assert exec.node_id == test_graph.nodes[0].id
 
     # Executing ParrotBlock2
     exec = executions[1]
     assert exec.status == execution.ExecutionStatus.COMPLETED
-    assert exec.run_id == run_id
-    assert exec.output_name == "output"
-    assert exec.output_data == "Hello, World!"
-    assert exec.input_data == input_data
+    assert exec.graph_exec_id == graph_exec_id
+    assert exec.output_data == {"output": ["Hello, World!"]}
+    assert exec.input_data == {"input": text}
     assert exec.node_id == test_graph.nodes[1].id
 
     # Executing TextCombinerBlock
     exec = executions[2]
     assert exec.status == execution.ExecutionStatus.COMPLETED
-    assert exec.run_id == run_id
-    assert exec.output_name == "combined_text"
-    assert exec.output_data == "Hello, World!,Hello, World!"
+    assert exec.graph_exec_id == graph_exec_id
+    assert exec.output_data == {"combined_text": ["Hello, World!,Hello, World!"]}
     assert exec.input_data == {
-        "format": "{text1},{text2}",
         "text1": "Hello, World!",
         "text2": "Hello, World!",
     }
@@ -103,9 +99,8 @@ async def execute_agent(test_manager: ExecutionManager, test_graph: graph.Graph)
     # Executing PrintingBlock
     exec = executions[3]
     assert exec.status == execution.ExecutionStatus.COMPLETED
-    assert exec.run_id == run_id
-    assert exec.output_name == "status"
-    assert exec.output_data == "printed"
+    assert exec.graph_exec_id == graph_exec_id
+    assert exec.output_data == {"status": ["printed"]}
     assert exec.input_data == {"text": "Hello, World!,Hello, World!"}
     assert exec.node_id == test_graph.nodes[3].id
 
@@ -116,4 +111,4 @@ async def test_agent_execution():
         with ExecutionManager(1) as test_manager:
             await db.connect()
             test_graph = await create_test_graph()
-            await execute_agent(test_manager, test_graph)
+            await execute_graph(test_manager, test_graph)
