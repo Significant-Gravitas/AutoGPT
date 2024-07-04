833944e2284e11c01fe18a809b260edf6f4ad2cb
Zamil Majdy <zamil.majdy@agpt.co>
2024-07-04 14:37:28 +0400

    feat(rnd): Add strong pydantic type & composite data extraction for Block input/output schema + add reddit agent-blocks (#7288)
    * feat(rnd): Add type hint and strong pydantic type validation for block input/output + add reddit agent-blocks.
    
    * feat(rnd): Add type hint and strong pydantic type validation for block input/output + add reddit agent-blocks.
    
    * Fix reddit block
    
    * Fix serialization
    
    * Eliminate deprecated class property
    
    * Remove RedditCredentialsBlock
    
    * Cache jsonschema computation, add dictionary construction
    
    * Add dict_split and list_split to output, add more blocks
    
    * Add objc_split for completeness, int both input and output
    
    * Update reddit block
    
    * Add reddit test (untested)
    
    * Resolved json issue on pydantic
    
    * Add creds check on client
    
    * Add dict <--> pydantic object flexibility
    
    * Fix error retry
    
    * Skip reddit test
    
    * Code cleanup
    
    * Chang prompt
    
    * Make this work
    
    * Fix linting
    
    * Hide input_links and output_links from Node
    
    * Add docs
    
    ---------
    
    Co-authored-by: Aarushi <50577581+aarushik93@users.noreply.github.com>

--- rnd/autogpt_server/autogpt_server/executor/manager.py
+++ rnd/autogpt_server/autogpt_server/executor/manager.py
@@ -9,6 +9,7 @@ from autogpt_server.data.execution import (
     create_graph_execution,
     get_node_execution_input,
     merge_execution_input,
+    parse_execution_output,
     update_execution_status as execution_update,
     upsert_execution_output,
     upsert_execution_input,
@@ -16,7 +17,7 @@ from autogpt_server.data.execution import (
     ExecutionStatus,
     ExecutionQueue,
 )
-from autogpt_server.data.graph import Node, get_node, get_graph
+from autogpt_server.data.graph import Link, Node, get_node, get_graph
 from autogpt_server.util.service import AppService, expose
 
 logger = logging.getLogger(__name__)
@@ -57,7 +58,7 @@ def execute_node(loop: asyncio.AbstractEventLoop, data: Execution) -> ExecutionS
         logger.error(f"Node {node_id} not found.")
         return
 
-    node_block = wait(get_block(node.block_id))
+    node_block = get_block(node.block_id)
     if not node_block:
         logger.error(f"Block {node.block_id} not found.")
         return
@@ -74,7 +75,11 @@ def execute_node(loop: asyncio.AbstractEventLoop, data: Execution) -> ExecutionS
             wait(upsert_execution_output(node_exec_id, output_name, output_data))
 
             for execution in enqueue_next_nodes(
-                    loop, node, output_name, output_data, graph_exec_id
+                loop=loop,
+                node=node,
+                output=(output_name, output_data),
+                graph_exec_id=graph_exec_id,
+                prefix=prefix,
             ):
                 yield execution
     except Exception as e:
@@ -88,45 +93,43 @@ def execute_node(loop: asyncio.AbstractEventLoop, data: Execution) -> ExecutionS
 def enqueue_next_nodes(
         loop: asyncio.AbstractEventLoop,
         node: Node,
-        output_name: str,
-        output_data: Any,
+        output: tuple[str, Any],
         graph_exec_id: str,
+        prefix: str,
 ) -> list[Execution]:
     def wait(f: Coroutine[T, Any, T]) -> T:
         return loop.run_until_complete(f)
 
-    prefix = get_log_prefix(graph_exec_id, node.id)
-    node_id = node.id
+    def get_next_node_execution(node_link: Link) -> Execution | None:
+        next_output_name = node_link.source_name
+        next_input_name = node_link.sink_name
+        next_node_id = node_link.sink_id
 
-    # Try to enqueue next eligible nodes
-    next_node_ids = [nid for name, nid in node.output_nodes if name == output_name]
-    if not next_node_ids:
-        logger.error(f"{prefix} Output [{output_name}] has no subsequent node.")
-        return []
+        next_data = parse_execution_output(output, next_output_name)
+        if next_data is None:
+            return
 
-    def validate_node_execution(next_node_id: str):
         next_node = wait(get_node(next_node_id))
         if not next_node:
             logger.error(f"{prefix} Error, next node {next_node_id} not found.")
             return
 
-        next_node_input_name = next(
-            name for name, nid in next_node.input_nodes if nid == node_id
-        )
         next_node_exec_id = wait(upsert_execution_input(
             node_id=next_node_id,
             graph_exec_id=graph_exec_id,
-            input_name=next_node_input_name,
-            data=output_data
+            input_name=next_input_name,
+            data=next_data
         ))
-
         next_node_input = wait(get_node_execution_input(next_node_exec_id))
-        is_valid, validation_resp = wait(validate_exec(next_node, next_node_input))
+
+        is_valid, validation_msg = validate_exec(next_node, next_node_input)
+        suffix = f"{next_output_name}~{next_input_name}#{next_node_id}:{validation_msg}"
+
         if not is_valid:
-            logger.warning(f"{prefix} Skipped {next_node_id}: {validation_resp}")
+            logger.warning(f"{prefix} Skipped queueing {suffix}")
             return
 
-        logger.warning(f"{prefix} Enqueue next node {next_node_id}-{validation_resp}")
+        logger.warning(f"{prefix} Enqueued {suffix}")
         return Execution(
             graph_exec_id=graph_exec_id,
             node_exec_id=next_node_exec_id,
@@ -134,14 +137,11 @@ def enqueue_next_nodes(
             data=next_node_input
         )
 
-    executions = []
-    for nid in next_node_ids:
-        if execution := validate_node_execution(nid):
-            executions.append(execution)
-    return executions
+    executions = [get_next_node_execution(link) for link in node.output_links]
+    return [v for v in executions if v]
 
 
-async def validate_exec(node: Node, data: dict[str, Any]) -> tuple[bool, str]:
+def validate_exec(node: Node, data: dict[str, Any]) -> tuple[bool, str]:
     """
     Validate the input data for a node execution.
 
@@ -153,21 +153,24 @@ async def validate_exec(node: Node, data: dict[str, Any]) -> tuple[bool, str]:
         A tuple of a boolean indicating if the data is valid, and a message if not.
         Return the executed block name if the data is valid.
     """
-    node_block: Block | None = await get_block(node.block_id)
+    node_block: Block | None = get_block(node.block_id)
     if not node_block:
         return False, f"Block for {node.block_id} not found."
 
+    error_message = f"Input data missing for {node_block.name}:"
+
     input_fields_from_schema = node_block.input_schema.get_required_fields()
     if not input_fields_from_schema.issubset(data):
-        return False, f"Input data missing: {input_fields_from_schema - set(data)}"
+        return False, f"{error_message} {input_fields_from_schema - set(data)}"
 
-    input_fields_from_nodes = {name for name, _ in node.input_nodes}
+    input_fields_from_nodes = {link.sink_name for link in node.input_links}
     if not input_fields_from_nodes.issubset(data):
-        return False, f"Input data missing: {input_fields_from_nodes - set(data)}"
+        return False, f"{error_message} {input_fields_from_nodes - set(data)}"
 
     if error := node_block.input_schema.validate_data(data):
-        logger.error("Input value doesn't match schema: %s", error)
-        return False, f"Input data doesn't match {node_block.name}: {error}"
+        error_message = f"Input data doesn't match {node_block.name}: {error}"
+        logger.error(error_message)
+        return False, error_message
 
     return True, node_block.name
 
@@ -221,7 +224,7 @@ class ExecutionManager(AppService):
         # Currently, there is no constraint on the number of root nodes in the graph.
         for node in graph.starting_nodes:
             input_data = merge_execution_input({**node.input_default, **data})
-            valid, error = self.run_and_wait(validate_exec(node, input_data))
+            valid, error = validate_exec(node, input_data)
             if not valid:
                 raise Exception(error)
 
