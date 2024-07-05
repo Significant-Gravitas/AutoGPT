f359ed0983e3d8667e20ec1b33ea888497562699
Reinier van der Leer <pwuts@agpt.co>
2024-07-05 08:14:45 +0200

    feat(agent_builder): Add shad/cn UI library (#7316)
    - Add shad/cn + Radix Icons + Tailwind
    - move favicon.ico to static folder
    - delete empty custominput.css
    
    ---------
    
    Co-authored-by: Bentlybro <tomnoon9@gmail.com>

--- rnd/autogpt_builder/src/app/layout.tsx
+++ rnd/autogpt_builder/src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { ThemeProvider as NextThemeProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -9,6 +11,10 @@ export const metadata: Metadata = {
   description: "Your one stop shop to creating AI Agents",
 };
 
+function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemeProvider {...props}>{children}</NextThemeProvider>
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -16,7 +22,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
