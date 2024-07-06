db6e15e64de268c5411b6cda10260f32dfb1f180
Reinier van der Leer <pwuts@agpt.co>
2024-07-06 00:57:59 -0600

    monitor/page.tsx: Switch from mock data to API & reorder file

--- rnd/autogpt_builder/src/app/monitor/page.tsx
+++ rnd/autogpt_builder/src/app/monitor/page.tsx
@@ -1,42 +1,161 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
 import moment from 'moment';
 import { ComposedChart, Legend, Line, ResponsiveContainer, Scatter, Tooltip, XAxis, YAxis } from 'recharts';
 import { Pencil2Icon } from '@radix-ui/react-icons';
+import AutoGPTServerAPI, { Flow, NodeExecutionResult } from '@/lib/autogpt_server_api';
+import { hashString } from '@/lib/utils';
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { cn } from "@/lib/utils";
 
-type Flow = {
-  id: number
-  name: string
-  status: 'active' | 'disabled' | 'failing'
-  lastRun: {
-    timestamp: string
-    status: 'running' | 'waiting' | 'success' | 'failed'
+const Monitor = () => {
+  const [flows, setFlows] = useState<Flow[]>([]);
+  const [flowRuns, setFlowRuns] = useState<FlowRun[]>([]);
+  const [selectedFlow, setSelectedFlow] = useState<Flow | null>(null);
+
+  const api = new AutoGPTServerAPI();
+
+  useEffect(() => fetchFlowsAndRuns(), []);
+
+  function fetchFlowsAndRuns() {
+    // Fetch flow IDs
+    api.listFlowIDs()
+    .then(flowIDs => {
+      Promise.all(flowIDs.map(flowID => {
+        // Fetch flow run IDs
+        api.listFlowRunIDs(flowID)
+        .then(runIDs => {
+          runIDs.map(runID => {
+            // Fetch flow run
+            api.getFlowExecutionInfo(flowID, runID)
+            .then(execInfo => setFlowRuns(flowRuns => {
+              const flowRunIndex = flowRuns.findIndex(fr => fr.id == runID);
+              const flowRun = flowRunFromNodeExecutionResults(flowID, runID, execInfo)
+              if (flowRunIndex > -1) {
+                flowRuns.splice(flowRunIndex, 1, flowRun)
+              }
+              else {
+                flowRuns.push(flowRun)
+              }
+              return flowRuns
+            }));
+          });
+        });
+
+        // Fetch flow
+        return api.getFlow(flowID);
+      }))
+      .then(flows => setFlows(flows));
+    });
   }
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-4 xl:grid-cols-10 gap-4">
+      <div className="lg:col-span-2 xl:col-span-2">
+        <AgentFlowList
+          flows={flows}
+          flowRuns={flowRuns}
+          selectedFlow={selectedFlow}
+          onSelectFlow={f => setSelectedFlow(f.id == selectedFlow?.id ? null : f)}
+        />
+      </div>
+      <div className="lg:col-span-2 xl:col-span-2 space-y-4">
+        <FlowRunsList
+          flows={flows}
+          runs={
+            (
+              selectedFlow
+                ? flowRuns.filter(v => v.flowID == selectedFlow.id)
+                : flowRuns
+            )
+            .toSorted((a, b) => Number(a.startTime) - Number(b.startTime))
+          }
+        />
+      </div>
+      <div className="col-span-1 lg:col-span-4 xl:col-span-6">
+        {selectedFlow && (
+          <Card>
+            <CardHeader className="flex-row items-center justify-between space-y-0 space-x-3">
+              <div>
+                <CardTitle>{selectedFlow.name}</CardTitle>
+                <p className="mt-2"><code>{selectedFlow.id}</code></p>
+              </div>
+              <Link className={buttonVariants({ variant: "outline" })} href={`/build?flowID=${selectedFlow.id}`}>
+                <Pencil2Icon className="mr-2" /> Edit Flow
+              </Link>
+            </CardHeader>
+            <CardContent>
+              <FlowRunsStats
+                flows={flows}
+                flowRuns={flowRuns.filter(v => v.flowID == selectedFlow.id)}
+              />
+            </CardContent>
+          </Card>
+        ) || (
+          <FlowRunsStats flows={flows} flowRuns={flowRuns} />
+        )}
+      </div>
+    </div>
+  );
 };
 
-const FlowStatusBadge = ({ status }: { status: Flow['status'] }) => (
-  <Badge
-    variant="default"
-    className={
-      status === 'active' ? 'bg-green-500 dark:bg-green-600' :
-      status === 'failing' ? 'bg-red-500 dark:bg-red-700' :
-      'bg-gray-500 dark:bg-gray-600'
+type FlowRun = {
+  id: string
+  flowID: string
+  status: 'running' | 'waiting' | 'success' | 'failed'
+  startTime: number // unix timestamp (ms)
+  duration: number // seconds
+
+  nodeExecutionResults: NodeExecutionResult[]
+};
+
+function flowRunFromNodeExecutionResults(
+  flowID: string, runID: string, nodeExecutionResults: NodeExecutionResult[]
+): FlowRun {
+  // Determine overall status
+  let status: 'running' | 'waiting' | 'success' | 'failed' = 'success';
+  for (const execution of nodeExecutionResults) {
+    if (execution.status === 'FAILED') {
+      status = 'failed';
+      break;
+    } else if (['QUEUED', 'RUNNING'].includes(execution.status)) {
+      status = 'running';
+      break;
+    } else if (execution.status === 'INCOMPLETE') {
+      status = 'waiting';
     }
-  >
-    {status}
-  </Badge>
-)
+  }
+
+  // Determine aggregate startTime and duration
+  const startTime = Math.min(
+    ...nodeExecutionResults.map(ner => ner.start_time?.getTime() || Date.now())
+  );
+  const endTime = (
+    ['success', 'failed'].includes(status)
+      ? Math.max(...nodeExecutionResults.map(ner => ner.end_time?.getTime() || 0))
+      : Date.now()
+  );
+  const duration = (endTime - startTime) / 1000; // Convert to seconds
+
+  return {
+    id: runID,
+    flowID: flowID,
+    status,
+    startTime,
+    duration,
+    nodeExecutionResults: nodeExecutionResults
+  };
+}
 
 const AgentFlowList = (
-  { flows, selectedFlow, onSelectFlow }: {
-    flows: Array<Flow>,
+  { flows, flowRuns, selectedFlow, onSelectFlow }: {
+    flows: Flow[],
+    flowRuns?: FlowRun[],
     selectedFlow: Flow | null,
     onSelectFlow: (f: Flow) => void,
   }
@@ -50,34 +169,83 @@ const AgentFlowList = (
         <TableHeader>
           <TableRow>
             <TableHead>Name</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Most recent run</TableHead>
+            {/* <TableHead>Status</TableHead> */}
+            {/* <TableHead>Last updated</TableHead> */}
+            {flowRuns && <TableHead># of runs</TableHead>}
+            {flowRuns && <TableHead>Last run</TableHead>}
           </TableRow>
         </TableHeader>
         <TableBody>
-          {flows.map((flow) => (
-            <TableRow
-              key={flow.id}
-              className="cursor-pointer"
-              onClick={() => onSelectFlow(flow)}
-              data-state={selectedFlow?.id == flow.id ? "selected" : null}
-            >
-              <TableCell>{flow.name}</TableCell>
-              <TableCell><FlowStatusBadge status={flow.status} /></TableCell>
-              <TableCell>
-                {flow.lastRun.timestamp}
-                <Badge
-                  variant="default"
-                  className={`ml-3 ${
-                    flow.lastRun.status === 'running' ? 'bg-blue-500 dark:bg-blue-700' :
-                    flow.lastRun.status === 'waiting' ? 'bg-yellow-500 dark:bg-yellow-600' :
-                    flow.lastRun.status === 'success' ? 'bg-green-500 dark:bg-green-600' :
-                    'bg-red-500 dark:bg-red-700'
-                  }`}
-                >
-                  {flow.lastRun.status}
-                </Badge>
-              </TableCell>
+          {flows.map((flow) => {
+            let runCount, lastRun: FlowRun | null;
+            if (flowRuns) {
+              const _flowRuns = flowRuns.filter(r => r.flowID == flow.id);
+              runCount = _flowRuns.length;
+              lastRun = runCount == 0 ? null : _flowRuns.reduce(
+                (a, c) => a.startTime < c.startTime ? a : c
+              );
+            }
+            return (
+              <TableRow
+                key={flow.id}
+                className="cursor-pointer"
+                onClick={() => onSelectFlow(flow)}
+                data-state={selectedFlow?.id == flow.id ? "selected" : null}
+              >
+                <TableCell>{flow.name}</TableCell>
+                {/* <TableCell><FlowStatusBadge status={flow.status ?? "active"} /></TableCell> */}
+                {/* <TableCell>
+                  {flow.updatedAt ?? "???"}
+                </TableCell> */}
+                {flowRuns && <TableCell>{runCount}</TableCell>}
+                {flowRuns && (!lastRun ? <TableCell /> :
+                <TableCell title={moment(lastRun.startTime).toString()}>
+                  {moment(lastRun.startTime).fromNow()}
+                </TableCell>)}
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
+    </CardContent>
+  </Card>
+);
+
+const FlowStatusBadge = ({ status }: { status: "active" | "disabled" | "failing" }) => (
+  <Badge
+    variant="default"
+    className={
+      status === 'active' ? 'bg-green-500 dark:bg-green-600' :
+      status === 'failing' ? 'bg-red-500 dark:bg-red-700' :
+      'bg-gray-500 dark:bg-gray-600'
+    }
+  >
+    {status}
+  </Badge>
+);
+
+const FlowRunsList = ({ flows, runs }: { flows: Flow[], runs: FlowRun[] }) => (
+  <Card>
+    <CardHeader>
+      <CardTitle>Flow Runs</CardTitle>
+    </CardHeader>
+    <CardContent>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Flow</TableHead>
+            <TableHead>Started</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Duration</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {runs.map((run) => (
+            <TableRow key={run.id}>
+              <TableCell>{flows.find(f => f.id == run.flowID)!.name}</TableCell>
+              <TableCell>{moment(run.startTime).format("HH:mm")}</TableCell>
+              <TableCell><FlowRunStatusBadge status={run.status} /></TableCell>
+              <TableCell>{formatDuration(run.duration)}</TableCell>
             </TableRow>
           ))}
         </TableBody>
@@ -85,14 +253,6 @@ const AgentFlowList = (
     </CardContent>
   </Card>
 );
-
-type FlowRun = {
-  id: number
-  flowId: number
-  status: 'running' | 'waiting' | 'success' | 'failed'
-  startTime: number // unix timestamp (ms)
-  duration: number // seconds
-};
 
 const FlowRunStatusBadge = ({ status }: { status: FlowRun['status'] }) => (
   <Badge
@@ -106,38 +266,6 @@ const FlowRunStatusBadge = ({ status }: { status: FlowRun['status'] }) => (
   >
     {status}
   </Badge>
-)
-
-const FlowRunsList = ({ flows, runs }: { flows: Flow[], runs: FlowRun[] }) => (
-  <Card>
-    <CardHeader>
-      <CardTitle>Flow Runs</CardTitle>
-    </CardHeader>
-    <CardContent>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Flow</TableHead>
-            <TableHead>Start time</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Duration</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {runs.map((run) => (
-            <TableRow key={run.id}>
-              <TableCell>{flows.find(f => f.id == run.flowId)!.name}</TableCell>
-              <TableCell>{moment(run.startTime).format()}</TableCell>
-              <TableCell>
-                <FlowRunStatusBadge status={run.status} />
-              </TableCell>
-              <TableCell>{run.duration}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </CardContent>
-  </Card>
 );
 
 
@@ -187,7 +315,7 @@ const FlowRunsStats = (
         </div>
       </CardHeader>
       <CardContent>
-        <FlowRunsTimeline flows={flows} flowRuns={flowRuns} dataMin={statsSince} />
+        <FlowRunsTimeline flows={flows} flowRuns={flowRuns} dataMin={statsSince} className={"mb-6"} />
         <Card className="p-3">
           <p><strong>Total runs:</strong> {filteredFlowRuns.length}</p>
           <p>
@@ -195,7 +323,7 @@ const FlowRunsStats = (
               filteredFlowRuns.reduce((total, run) => total + run.duration, 0)
             } seconds
           </p>
-          <p><strong>Total cost:</strong> €1,23</p>{/* TODO: implement */}
+          {/* <p><strong>Total cost:</strong> €1,23</p> */}
         </Card>
       </CardContent>
     </Card>
@@ -203,13 +331,15 @@ const FlowRunsStats = (
 }
 
 const FlowRunsTimeline = (
-  { flows, flowRuns, dataMin }: {
+  { flows, flowRuns, dataMin, className }: {
     flows: Flow[],
     flowRuns: FlowRun[],
     dataMin: "dataMin" | number,
+    className?: string,
   }
 ) => (
-  <ResponsiveContainer width="100%" height={120}>
+  /* TODO: make logarithmic? */
+  <ResponsiveContainer width="100%" height={120} className={className}>
     <ComposedChart>
       <XAxis
         dataKey="time"
@@ -231,24 +361,24 @@ const FlowRunsTimeline = (
             : time.format('YYYY-MM-DD HH:mm');
         }}
         name="Time"
+        scale="time"
       />
-      <YAxis dataKey="_duration" name="Duration (s)" tickFormatter={s => s > 90 ? `${Math.round(s / 60)}m` : `${s}s`} />
+      <YAxis
+        dataKey="_duration"
+        name="Duration (s)"
+        tickFormatter={s => s > 90 ? `${Math.round(s / 60)}m` : `${s}s`}
+      />
       <Tooltip
         content={({ payload, label }) => {
           if (payload && payload.length) {
-            const data = payload[0].payload;
-            const flow = flows.find(f => f.id === data.flowId);
+            const data: FlowRun & { time: number, _duration: number } = payload[0].payload;
+            const flow = flows.find(f => f.id === data.flowID);
             return (
               <Card className="p-3">
                 <p><strong>Flow:</strong> {flow ? flow.name : 'Unknown'}</p>
                 <p><strong>Start Time:</strong> {moment(data.startTime).format('YYYY-MM-DD HH:mm:ss')}</p>
                 <p>
-                  <strong>Duration:</strong>
-                  {
-                    data.duration > 90
-                      ? ` ${Math.round(data.duration/60)}m`
-                      : ` ${data.duration}s`
-                    }
+                  <strong>Duration:</strong> {formatDuration(data.duration)}
                 </p>
                 <p><strong>Status:</strong> <FlowRunStatusBadge status={data.status} /></p>
               </Card>
@@ -259,13 +389,13 @@ const FlowRunsTimeline = (
       />
       {flows.map((flow, index) => (
         <Scatter
-          data={flowRuns.filter(fr => fr.flowId == flow.id).map(fr => ({
+          data={flowRuns.filter(fr => fr.flowID == flow.id).map(fr => ({
             ...fr,
             time: fr.startTime + (fr.duration * 1000),
             _duration: fr.duration,
           }))}
           name={flow.name}
-          fill={`hsl(${flow.id * 137.5 % 360}, 70%, 50%)`}
+          fill={`hsl(${hashString(flow.id) * 137.5 % 360}, 70%, 50%)`}
         />
       ))}
       {flowRuns.map((run, index) => (
@@ -276,83 +406,23 @@ const FlowRunsTimeline = (
             { ...run, time: run.startTime, _duration: 0 },
             { ...run, time: run.startTime + (run.duration * 1000), _duration: run.duration }
           ]}
-          stroke={`hsl(${run.flowId * 137.5 % 360}, 70%, 50%)`}
+          stroke={`hsl(${hashString(run.flowID) * 137.5 % 360}, 70%, 50%)`}
           strokeWidth={2}
           dot={false}
           legendType="none"
         />
       ))}
-      <Legend />
+      <Legend wrapperStyle={{ fontSize: '0.75em' }} iconSize={8} />
     </ComposedChart>
   </ResponsiveContainer>
 );
 
-const Monitor = () => {
-  const [selectedFlow, setSelectedFlow] = useState<Flow | null>(null);
-
-  // Mock data
-  const flows: Array<Flow> = [
-    { id: 1, name: 'JARVIS', status: 'active', lastRun: { timestamp: '5 minutes ago', status: 'waiting' } },
-    { id: 2, name: 'Time machine', status: 'failing', lastRun: { timestamp: '10 minutes ago', status: 'failed' } },
-    { id: 3, name: 'BlueSky digest', status: 'active', lastRun: { timestamp: '2 minutes ago', status: 'success' } },
-  ];
-
-  const flowRuns: Array<FlowRun> = [
-    { flowId: 1, id: 1, startTime: Date.now() - (4 * 3.6e6 + 55 * 60e3), status: 'success', duration: 59 },
-    { flowId: 1, id: 3, startTime: Date.now() - (3 * 3.6e6 + 23 * 60e3), status: 'success', duration: 102 },
-    { flowId: 1, id: 6, startTime: Date.now() - (2 * 3.6e6 + 8 * 60e3), status: 'waiting', duration: 133 },
-    { flowId: 2, id: 2, startTime: Date.now() - (4 * 3.6e6 + 11 * 60e3), status: 'failed', duration: 151 },
-    { flowId: 2, id: 5, startTime: Date.now() - (2 * 3.6e6 + 57 * 60e3), status: 'failed', duration: 77 },
-    { flowId: 2, id: 8, startTime: Date.now() - (1 * 3.6e6 + 42 * 60e3), status: 'failed', duration: 185 },
-    { flowId: 3, id: 4, startTime: Date.now() - (3 * 3.6e6 + 15 * 60e3), status: 'success', duration: 67 },
-    { flowId: 3, id: 7, startTime: Date.now() - (1 * 3.6e6 + 49 * 60e3), status: 'success', duration: 83 },
-    { flowId: 3, id: 9, startTime: Date.now() - (31 * 60e3), status: 'success', duration: 118 },
-  ];
-
+function formatDuration(seconds: number): string {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-4 xl:grid-cols-10 gap-4">
-      <div className="lg:col-span-2 xl:col-span-2">
-        <AgentFlowList
-          flows={flows}
-          selectedFlow={selectedFlow}
-          onSelectFlow={(f: Flow) => setSelectedFlow(f.id == selectedFlow?.id ? null : f)}
-        />
-      </div>
-      <div className="lg:col-span-2 xl:col-span-2 space-y-4">
-        <FlowRunsList
-          flows={flows}
-          runs={
-            (
-              selectedFlow
-                ? flowRuns.filter(v => v.flowId == selectedFlow.id)
-                : flowRuns
-            )
-            .toSorted((a, b) => Number(a.startTime) - Number(b.startTime))
-          }
-        />
-      </div>
-      <div className="col-span-1 lg:col-span-4 xl:col-span-6">
-        {selectedFlow && (
-            <Card>
-              <CardHeader className="flex-row items-center justify-between space-y-0 space-x-3">
-                <CardTitle>{selectedFlow.name}</CardTitle>
-                <Button variant="outline">
-                  <Pencil2Icon className="mr-2" /> Edit Flow
-                </Button>
-              </CardHeader>
-              <CardContent>
-                <FlowRunsStats
-                  flows={flows}
-                  flowRuns={flowRuns.filter(v => v.flowId == selectedFlow.id)}
-                />
-              </CardContent>
-            </Card>
-        ) || (
-          <FlowRunsStats flows={flows} flowRuns={flowRuns} />
-        )}
-      </div>
-    </div>
-  );
-};
+    seconds < 100
+      ? seconds.toPrecision(2)
+      : Math.round(seconds)
+  ).toString() + "s";
+}
 
 export default Monitor;
--- rnd/autogpt_builder/src/lib/utils.ts
+++ rnd/autogpt_builder/src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+/** Derived from https://stackoverflow.com/a/7616484 */
+export function hashString(str: string): number {
+  let hash = 0, chr: number;
+  if (str.length === 0) return hash;
+  for (let i = 0; i < str.length; i++) {
+    chr = str.charCodeAt(i);
+    hash = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+}
