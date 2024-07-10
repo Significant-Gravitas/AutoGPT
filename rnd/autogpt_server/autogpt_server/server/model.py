5bcd8ed73462f43c1243767d5a86b010311d24ab
Swifty <craigswift13@gmail.com>
2024-07-10 11:54:18 +0200

    feat(autogpt_server): Expose rest api via websocket (#7350)
    * Add in websocket event types
    
    * adding in api endpoints
    
    * Updated ws messages

--- rnd/autogpt_server/autogpt_server/server/model.py
+++ rnd/autogpt_server/autogpt_server/server/model.py
@@ -6,13 +6,24 @@ import pydantic
 class Methods(enum.Enum):
     SUBSCRIBE = "subscribe"
     UNSUBSCRIBE = "unsubscribe"
-    UPDATE = "update"
+    EXECUTION_EVENT = "execution_event"
+    GET_BLOCKS = "get_blocks"
+    EXECUTE_BLOCK = "execute_block"
+    GET_GRAPHS = "get_graphs"
+    GET_GRAPH = "get_graph"
+    CREATE_GRAPH = "create_graph"
+    RUN_GRAPH = "run_graph"
+    GET_GRAPH_RUNS = "get_graph_runs"
+    CREATE_SCHEDULED_RUN = "create_scheduled_run"
+    GET_SCHEDULED_RUNS = "get_scheduled_runs"
+    UPDATE_SCHEDULED_RUN = "update_scheduled_run"
+    UPDATE_CONFIG = "update_config"
     ERROR = "error"
 
 
 class WsMessage(pydantic.BaseModel):
     method: Methods
-    data: typing.Dict[str, typing.Any] | None = None
+    data: typing.Dict[str, typing.Any] | list[typing.Any] | None = None
     success: bool | None = None
     channel: str | None = None
     error: str | None = None
