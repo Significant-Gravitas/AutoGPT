14ddb915bfda33be8b432a0c3f751081d4cb9a1a
Bently <tomnoon9@gmail.com>
2024-07-10 17:01:07 +0100

    feat(autogpt_builder): swapping to white theme for the layout (#7358)
    * reverts dark theme for now
    
    * change "Show/Hide nodes" button to be "Icon"
    
    * swap over to light mode + fix sizing
    
    * fix color for agent name + description text
    
    * update icon

--- rnd/autogpt_builder/src/app/build/page.tsx
+++ rnd/autogpt_builder/src/app/build/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
       <div className="flex flex-col items-center px-12">
           <div className="z-10 w-full items-center justify-between font-mono text-sm lg:flex">
-              <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+              <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-600 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-900 dark:bg-zinc-900 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
                   Get started by adding a&nbsp;
                   <code className="font-mono font-bold">node</code>
               </p>
@@ -22,7 +22,7 @@ export default function Home() {
                   >
                       By{" "}
                       <Image
-                          src="/autogpt_logo_light.png"
+                          src="/autogpt_logo_dark.svg"
                           alt="AutoGPT Logo"
                           width={100}
                           height={24}
