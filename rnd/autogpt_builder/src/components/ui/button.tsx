e4f03b0ee8c8cab3b2259bafba46261006584059
Swifty <craigswift13@gmail.com>
2024-07-10 18:02:06 +0200

    tweak(ui): Tweaked light mode color (#7366)
    * reverts dark theme for now
    
    * change "Show/Hide nodes" button to be "Icon"
    
    * swap over to light mode + fix sizing
    
    * fix color for agent name + description text
    
    * Change navbar to white
    
    * Added darkmode tag for the navbar
    
    * Added dark mode text color
    
    ---------
    
    Co-authored-by: Bentlybro <tomnoon9@gmail.com>

--- rnd/autogpt_builder/src/components/ui/button.tsx
+++ rnd/autogpt_builder/src/components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
           "border border-gray-200 bg-white shadow-sm hover:bg-gray-100 hover:text-gray-900 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50",
         secondary:
           "bg-gray-100 text-gray-900 shadow-sm hover:bg-gray-100/80 dark:bg-gray-800 dark:text-gray-50 dark:hover:bg-gray-800/80",
-        ghost: "hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-50",
+        ghost: "hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:text-white",
         link: "text-gray-900 underline-offset-4 hover:underline dark:text-gray-50",
       },
       size: {
