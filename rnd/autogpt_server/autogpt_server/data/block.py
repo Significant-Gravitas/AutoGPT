f6c34a726836177a912236668dd05d9d77da84b9
Zamil Majdy <zamil.majdy@agpt.co>
2024-07-11 18:49:18 +0700

    Fix jsonscherma

--- rnd/autogpt_server/autogpt_server/data/block.py
+++ rnd/autogpt_server/autogpt_server/data/block.py
@@ -79,10 +79,17 @@ class BlockSchema(BaseModel):
 
         def ref_to_dict(obj):
             if isinstance(obj, dict):
+                # OpenAPI <3.1 does not support sibling fields that has a $ref key
+                # So sometimes, the schema has an "allOf"/"anyOf"/"oneOf" with 1 item.
+                keys = {"allOf", "anyOf", "oneOf"}
+                one_key = next((k for k in keys if k in obj and len(obj[k]) == 1), None)
+                if one_key:
+                    obj.update(obj[one_key][0])
+
                 return {
                     key: ref_to_dict(value)
                     for key, value in obj.items()
-                    if not key.startswith("$")
+                    if not key.startswith("$") and key != one_key
                 }
             elif isinstance(obj, list):
                 return [ref_to_dict(item) for item in obj]
