555e1137061af0bf442bc7e5fbe67a1e8782a7d0
Krzysztof Czerwinski <34861343+kcze@users.noreply.github.com>
2024-07-16 20:08:43 +0100

    feat(agent_builder, agent_server): Add customizable placeholders for input fields (#7451)
    - Add `SchemaField` that works like Pydantic `Field` but allows to add extra json schema values. This PR adds `placeholder` entry but it could be extended with other data.
    - Render `placeholder` inside input fields if available.
    - Restyle placeholders so they are visually distinct from user-entered values

---
+++ rnd/autogpt_server/autogpt_server/data/model.py
@@ -0,0 +1,32 @@
+from typing import Any, Callable, Optional, TypeVar
+
+from pydantic import Field
+from pydantic_core import PydanticUndefined, PydanticUndefinedType
+
+
+T = TypeVar("T")
+
+def SchemaField(
+    default: T | PydanticUndefinedType = PydanticUndefined,
+    *args,
+    default_factory: Optional[Callable[[], T]] = None,
+    title: str = "",
+    description: str = "",
+    placeholder: str = "",
+    exclude: bool = False,
+    **kwargs,
+) -> T:
+    json_extra: dict[str, Any] = {}
+    if placeholder:
+        json_extra["placeholder"] = placeholder
+
+    return Field(
+        default,
+        *args,
+        default_factory=default_factory,
+        title=title,
+        description=description,
+        exclude=exclude,
+        json_schema_extra=json_extra,
+        **kwargs,
+    )
