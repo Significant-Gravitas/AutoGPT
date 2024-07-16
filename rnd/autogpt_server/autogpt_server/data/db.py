1fa7e49e76b89425c10f44de4b4cdb846d352d93
Zamil Majdy <zamil.majdy@agpt.co>
2024-07-16 07:51:15 +0700

    Revert

--- rnd/autogpt_server/autogpt_server/data/db.py
+++ rnd/autogpt_server/autogpt_server/data/db.py
@@ -19,6 +19,6 @@ class BaseDbModel(BaseModel):
     id: str = Field(default_factory=lambda: str(uuid4()))
 
     @field_validator("id", mode="before")
-    def set_model_id(self, id: str) -> str:
+    def set_model_id(cls, id: str) -> str:
         # In case an empty ID is submitted
         return id or str(uuid4())
