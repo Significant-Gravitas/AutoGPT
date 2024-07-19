ea1a376c46d2a31e6bdcf088d7f7240f98636df0
Zamil Majdy <zamil.majdy@agpt.co>
2024-07-19 09:56:27 +0700

    Add mapping for objectlookup

--- rnd/autogpt_server/autogpt_server/blocks/basic.py
+++ rnd/autogpt_server/autogpt_server/blocks/basic.py
@@ -126,6 +126,13 @@ class ObjectLookupBlock(Block):
             yield "output", obj[key]
         elif isinstance(obj, list) and isinstance(key, int) and 0 <= key < len(obj):
             yield "output", obj[key]
+        elif isinstance(obj, list) and isinstance(key, str):
+            if len(obj) == 0:
+                yield "output", []
+            elif isinstance(obj[0], dict) and key in obj[0]:
+                yield "output", [item[key] for item in obj if key in item]
+            else:
+                yield "output", [getattr(val, key) for val in obj if hasattr(val, key)]
         elif isinstance(obj, object) and isinstance(key, str) and hasattr(obj, key):
             yield "output", getattr(obj, key)
         else:
