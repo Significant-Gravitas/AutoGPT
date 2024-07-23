dff532710f97963197c546f4bfc02fdbb3abdda9
Aarushi <aarushik93@gmail.com>
2024-07-23 16:17:28 +0100

    linting

--- rnd/infra/terraform/main.tf
+++ rnd/infra/terraform/main.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = ">= 1.9.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
--- rnd/infra/terraform/variables.tf
+++ rnd/infra/terraform/variables.tf
@@ -1,54 +1,65 @@
 variable "project_id" {
   description = "The project ID to host the cluster in"
+  type        = string
 }
 
 variable "region" {
   description = "Project region"
+  type        = string
   default     = "us-central1"
 }
 
 variable "zone" {
   description = "The zone to host the cluster in"
+  type        = string
   default     = "us-central1-a"
 }
 
 variable "network_name" {
   description = "The name of the VPC network"
+  type        = string
   default     = "gke-network"
 }
 
 variable "subnet_name" {
   description = "The name of the subnet"
+  type        = string
   default     = "gke-subnet"
 }
 
 variable "subnet_cidr" {
   description = "The CIDR range for the subnet"
+  type        = string
   default     = "10.0.0.0/24"
 }
 
 variable "cluster_name" {
   description = "The name for the GKE cluster"
+  type        = string
   default     = "gke-cluster"
 }
 
 variable "node_count" {
   description = "Number of nodes in the cluster"
+  type        = number
   default     = 3
 }
 
 variable "node_pool_name" {
   description = "The name for the node pool"
+  type        = string
   default     = "default-pool"
 }
 
 variable "machine_type" {
   description = "Type of machine to use for nodes"
+  type        = string
   default     = "e2-medium"
 }
 
 variable "disk_size_gb" {
   description = "Size of the disk attached to each node, specified in GB"
+  type        = number
   default     = 100
 }
 
