157c363a6f0b1eedc113ee151d3bb84d88dc7601
SwiftyOS <craigswift13@gmail.com>
2024-07-25 17:27:25 +0200

    Added sentry.io integration

--- rnd/market/market/app.py
+++ rnd/market/market/app.py
@@ -1,15 +1,37 @@
-import logging
-from contextlib import asynccontextmanager
 
+from contextlib import asynccontextmanager
+import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.gzip import GZipMiddleware
 from prisma import Prisma
+import sentry_sdk
+from sentry_sdk.integrations.asyncio import AsyncioIntegration
+from sentry_sdk.integrations.fastapi import FastApiIntegration
+from sentry_sdk.integrations.starlette import StarletteIntegration
 
 from market.routes import agents
 
 load_dotenv()
-logger = logging.getLogger(__name__)
+
+if os.environ.get("SENTRY_DSN"):
+    sentry_sdk.init(
+        dsn=os.environ.get("SENTRY_DSN"), 
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        traces_sample_rate=1.0,
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # We recommend adjusting this value in production.
+        profiles_sample_rate=1.0,
+        enable_tracing=True,
+        environment=os.environ.get("RUN_ENV", default="CLOUD").lower(),
+        integrations=[
+            StarletteIntegration(transaction_style="url"),
+            FastApiIntegration(transaction_style="url"),
+            AsyncioIntegration(),
+        ],
+    )
 
 db_client = Prisma(auto_register=True)
 
