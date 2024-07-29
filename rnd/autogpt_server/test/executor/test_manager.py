597086bf80a9e462ce172320c85dc1e588125865
Zamil Majdy <zamil.majdy@agpt.co>
2024-07-29 14:11:58 +0700

    feat(rnd): Add static input link feature for retaining input data to be re-used in multiple node executions

--- rnd/autogpt_server/test/executor/test_manager.py
+++ rnd/autogpt_server/test/executor/test_manager.py
@@ -1,6 +1,8 @@
 import pytest
 
 from autogpt_server.blocks.basic import ObjectLookupBlock, ValueBlock
+from autogpt_server.blocks.if_block import ComparisonOperator, ConditionBlock
+from autogpt_server.blocks.maths import MathsBlock, Operation
 from autogpt_server.data import execution, graph
 from autogpt_server.executor import ExecutionManager
 from autogpt_server.server import AgentServer
@@ -9,13 +11,13 @@ from autogpt_server.util.test import wait_execution
 
 
 async def execute_graph(
+    agent_server: AgentServer,
     test_manager: ExecutionManager,
     test_graph: graph.Graph,
-    input_data: dict[str, str],
+    input_data: dict,
     num_execs: int = 4,
 ) -> str:
     # --- Test adding new executions --- #
-    agent_server = AgentServer()
     response = await agent_server.execute_graph(test_graph.id, input_data)
     graph_exec_id = response["id"]
 
@@ -24,9 +26,10 @@ async def execute_graph(
     return graph_exec_id
 
 
-async def assert_sample_graph_executions(test_graph: graph.Graph, graph_exec_id: str):
+async def assert_sample_graph_executions(
+    agent_server: AgentServer, test_graph: graph.Graph, graph_exec_id: str
+):
     text = "Hello, World!"
-    agent_server = AgentServer()
     executions = await agent_server.get_run_execution_results(
         test_graph.id, graph_exec_id
     )
@@ -53,8 +56,11 @@ async def assert_sample_graph_executions(test_graph: graph.Graph, graph_exec_id:
     assert exec.graph_exec_id == graph_exec_id
     assert exec.output_data == {"output": ["Hello, World!,Hello, World!,!!!"]}
     assert exec.input_data == {
+        "format": "{texts[0]},{texts[1]},{texts[2]}",
+        "texts": ["Hello, World!", "Hello, World!", "!!!"],
         "texts_$_1": "Hello, World!",
         "texts_$_2": "Hello, World!",
+        "texts_$_3": "!!!",
     }
     assert exec.node_id == test_graph.nodes[2].id
 
@@ -72,8 +78,10 @@ async def test_agent_execution(server):
     test_graph = create_test_graph()
     await graph.create_graph(test_graph)
     data = {"input": "Hello, World!"}
-    graph_exec_id = await execute_graph(server.exec_manager, test_graph, data, 4)
-    await assert_sample_graph_executions(test_graph, graph_exec_id)
+    graph_exec_id = await execute_graph(
+        server.agent_server, server.exec_manager, test_graph, data, 4
+    )
+    await assert_sample_graph_executions(server.agent_server, test_graph, graph_exec_id)
 
 
 @pytest.mark.asyncio(scope="session")
@@ -125,10 +133,11 @@ async def test_input_pin_always_waited(server):
     )
 
     test_graph = await graph.create_graph(test_graph)
-    graph_exec_id = await execute_graph(server.exec_manager, test_graph, {}, 3)
+    graph_exec_id = await execute_graph(
+        server.agent_server, server.exec_manager, test_graph, {}, 3
+    )
 
-    agent_server = AgentServer()
-    executions = await agent_server.get_run_execution_results(
+    executions = await server.agent_server.get_run_execution_results(
         test_graph.id, graph_exec_id
     )
     assert len(executions) == 3
@@ -136,3 +145,79 @@ async def test_input_pin_always_waited(server):
     # Hence executing extraction of "key" from {"key1": "value1", "key2": "value2"}
     assert executions[2].status == execution.ExecutionStatus.COMPLETED
     assert executions[2].output_data == {"output": ["value2"]}
+
+
+@pytest.mark.asyncio(scope="session")
+async def test_static_input_link_on_loop(server):
+    """
+    This test is asserting the behaviour of static input link, e.g: reusable input link.
+    In this test, the input link is reused on the loop, and the input value is retained.
+
+    Test scenario:
+    ValueBlock --C MathBlock -- IfBlock --
+                      |_________________|
+
+    ValueBlock will be connected to both the input of MathBlock (`a` and `b`).
+    Link to `a` will be static, IfBlock `no_output` will be also connected to `b`.
+    This will create a loop to add keep adding `ValueBlock` until IF condition is met.
+    """
+    nodes = [
+        graph.Node(block_id=ValueBlock().id),
+        graph.Node(
+            block_id=MathsBlock().id,
+            input_default={"operation": Operation.ADD.value},
+        ),
+        graph.Node(
+            block_id=ConditionBlock().id,
+            input_default={
+                "operator": ComparisonOperator.GREATER_THAN.value,
+                "value2": 10,
+            },
+        ),
+    ]
+    links = [
+        graph.Link(
+            source_id=nodes[0].id,
+            sink_id=nodes[1].id,
+            source_name="output",
+            sink_name="a",
+            is_static=True,  # This is the static link to test.
+        ),
+        graph.Link(
+            source_id=nodes[0].id,
+            sink_id=nodes[1].id,
+            source_name="output",
+            sink_name="b",
+        ),
+        graph.Link(
+            source_id=nodes[1].id,
+            sink_id=nodes[2].id,
+            source_name="result",
+            sink_name="value1",
+        ),
+        graph.Link(
+            source_id=nodes[2].id,
+            sink_id=nodes[1].id,
+            source_name="no_output",
+            sink_name="b",
+        ),
+    ]
+    test_graph = graph.Graph(
+        name="TestGraph",
+        description="Test graph",
+        nodes=nodes,
+        links=links,
+    )
+
+    test_graph = await graph.create_graph(test_graph)
+    input_data = {"input": 3}
+    graph_exec_id = await execute_graph(
+        server.agent_server, server.exec_manager, test_graph, input_data, 7
+    )
+    executions = await server.agent_server.get_run_execution_results(
+        test_graph.id, graph_exec_id
+    )
+    # Loop: 3+3=6, 6+3=9, 9+3=12
+    assert len(executions) == 7
+    assert executions[6].status == execution.ExecutionStatus.COMPLETED
+    assert executions[6].output_data == {"result": [True], "yes_output": [12]}
