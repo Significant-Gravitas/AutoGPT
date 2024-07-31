9ff2c2063702b4583659c919aad45edf9cf05f8b
Andy Hooker <58448663+andrewhooker2@users.noreply.github.com>
2024-07-31 03:43:13 -0500

    feat(builder): UI / UX upgrade and simplification of tailwind styles (#7647)
    * fix(builder): Implementing a basic shadCn theme until color palette is decided upon
    
    * feat(builder): Separating NavBar into its own component and providing a standard UI/UX Approach
    
    * feat(builder): Removing old implementation of logo, removing excessive css implementation, updating styles to better support standard desktop views.
    
    * feature(builder): Addition of UI component Sheet from ShadCn for update

--- rnd/autogpt_builder/src/app/layout.tsx
+++ rnd/autogpt_builder/src/app/layout.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Link from "next/link";
-import { Pencil1Icon, TimerIcon } from "@radix-ui/react-icons";
 
 import "./globals.css";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button, buttonVariants } from "@/components/ui/button";
-import {
-  DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger
-} from "@/components/ui/dropdown-menu";
 import { Providers } from "@/app/providers";
+import {NavBar} from "@/components/NavBar";
+import {cn} from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,58 +14,34 @@ export const metadata: Metadata = {
   title: "NextGen AutoGPT",
   description: "Your one stop shop to creating AI Agents",
 };
-
-const NavBar = () => (
-    <nav className="bg-white dark:bg-slate-800 p-4 flex justify-between items-center shadow">
-        <div className="flex space-x-4">
-            <Link href="/monitor" className={buttonVariants({ variant: "ghost" })}>
-                <TimerIcon className="mr-1" /> Monitor
-            </Link>
-            <Link href="/build" className={buttonVariants({ variant: "ghost" })}>
-                <Pencil1Icon className="mr-1" /> Build
-            </Link>
-        </div>
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="h-8 w-8 rounded-full">
-                    <Avatar>
-                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                        <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-                <DropdownMenuItem>Settings</DropdownMenuItem>
-                <DropdownMenuItem>Switch Workspace</DropdownMenuItem>
-                <DropdownMenuItem>Log out</DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
-    </nav>
-);
 export default function RootLayout({
- children,
+   children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-      <html lang="en">
-      <body className={inter.className}>
-      <Providers
-          attribute="class"
-          defaultTheme="light"
-          // Feel free to remove this line if you want to use the system theme by default
-          // enableSystem
-          disableTransitionOnChange
-      >
-        <div className="min-h-screen bg-gray-200 text-gray-900">
-          <NavBar />
-          <main className="mx-auto p-4">
-            {children}
-          </main>
-        </div>
-      </Providers>
-      </body>
-      </html>
-  );
+    return (
+        <html lang="en">
+        <body className={
+            cn(
+                'antialiased transition-colors',
+                inter.className
+            )
+        }>
+        <Providers
+            attribute="class"
+            defaultTheme="light"
+            // Feel free to remove this line if you want to use the system theme by default
+            // enableSystem
+            disableTransitionOnChange
+        >
+            <div className="flex flex-col min-h-screen ">
+                <NavBar/>
+                <main className="flex-1 p-4 overflow-hidden">
+                    {children}
+                </main>
+            </div>
+        </Providers>
+        </body>
+        </html>
+    );
 }
