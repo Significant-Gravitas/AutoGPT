ca9c52f76aa36b0c66ed508ef39a42ace203b777
Krzysztof Czerwinski <34861343+kcze@users.noreply.github.com>
2024-08-02 12:58:28 +0100

    feat(builder): Add Supabase Auth, session and sign in UI (#7655)
    This PR adds Supabase Auth (cloud) integration, login and profile UI, configures password login and three OAuth providers: Google, GitHub and Discord.
    
    For `Account` button to show up and ability to login two env vars need to be set in `.env.local`: `NEXT_PUBLIC_SUPABASE_URL` and `NEXT_PUBLIC_SUPABASE_ANON_KEY`. OAuth providers are by the Supabase and don't require env vars.
    
    Email confirmation (for email/password signup) is disabled because there's limit of 3 emails per hour without custom SMTP server configuration. [Link](https://supabase.com/dashboard/project/adfjtextkuilwuhzdjpf/auth/templates) to configure custom SMTP server and email template.
    
    ### Added dependencies:
    - "@supabase/ssr": "^0.4.0"
    - "@supabase/supabase-js": "^2.45.0"
    - "react-icons": "^5.2.1"
    
    ### Added pages/routes:
    - `app/auth/auth-code-error/page.tsx`: displayed when login using OAuth provider fails
    - `app/auth/callback/route.ts`: route accessed when logging in using OAuth provider; it passes session code to Supabase
    - `app/auth/confirm/route.ts`: accessed when confirming email, users will be directed here from email they get after signing in.
    - `app/error/page.tsx`: Generic error page without explanation (any errors should be visible in the console)
    - `app/login/page.tsx` and `app/login/actions.ts`: Login page and related login/signup server actions
    - `app/profile/page.tsx`: Profile page, displays email address of the user and button to logout
    
    ### Changes
    - Update `layout.tsx`: add `Log In` button and make icons consistent. The log in button shows up if user is logged out, avatar is shown when logged in, and if supabase is unavailable nothing shows up.
    - Login form is verified using `zod` on the frontend (recommended by shadcn) and in case login fails feedback is displayed. On successful login users are redirected to `/profile`
    - Add `PasswordInput` component, [source](https://gist.github.com/mjbalcueva/b21f39a8787e558d4c536bf68e267398)
    - Add `SupabaseProvider` with context for Supabase accessed via hook `useSupabase(): { supabase: SupabaseClient | null, isLoading: boolean }`
    - Add `useUser` hook to get `{ user, session, isLoading, error }` on the client 
    - Add `getServerUser`: async function to get `{ user: User | null, error: string | null }` on the server side
    - Add `src/middleware.ts` and `client.ts`, `server.ts`, `middleware.ts` in `src/lib/supabase` which are utility functions and middleware to refresh auth token

---
+++ rnd/autogpt_builder/src/app/login/page.tsx
@@ -0,0 +1,168 @@
+"use client";
+import useUser from '@/hooks/useUser';
+import { login, signup } from './actions'
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { useForm } from 'react-hook-form';
+import { Input } from '@/components/ui/input';
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { PasswordInput } from '@/components/PasswordInput';
+import { FaGoogle, FaGithub, FaDiscord, FaSpinner } from "react-icons/fa";
+import { useState } from 'react';
+import { useSupabase } from '@/components/SupabaseProvider';
+import { useRouter } from 'next/navigation';
+
+const loginFormSchema = z.object({
+  email: z.string().email().min(2).max(64),
+  password: z.string().min(6).max(64),
+})
+
+export default function LoginPage() {
+  const { supabase, isLoading: isSupabaseLoading } = useSupabase();
+  const { user, isLoading: isUserLoading } = useUser();
+  const [feedback, setFeedback] = useState<string | null>(null);
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+
+  if (user) {
+    console.log('User exists, redirecting to profile')
+    router.push('/profile')
+  }
+
+  if (isUserLoading || isSupabaseLoading || user) {
+    return (
+      <div className="flex justify-center items-center h-[80vh]">
+        <FaSpinner className="mr-2 h-16 w-16 animate-spin"/>
+      </div>
+    );
+  }
+
+  if (!supabase) {
+    return <div>User accounts are disabled because Supabase client is unavailable</div>
+  }
+
+  async function handleSignInWithProvider(provider: 'google' | 'github' | 'discord') {
+    const { data, error } = await supabase!.auth.signInWithOAuth({
+      provider: provider,
+      options: {
+        redirectTo: process.env.AUTH_CALLBACK_URL ?? `http://localhost:3000/auth/callback`,
+        // Get Google provider_refresh_token
+        // queryParams: {
+        //   access_type: 'offline',
+        //   prompt: 'consent',
+        // },
+      },
+    })
+
+    if (!error) {
+      setFeedback(null)
+      return
+    }
+    setFeedback(error.message)
+  }
+
+  const onLogin = async (data: z.infer<typeof loginFormSchema>) => {
+    setIsLoading(true)
+    const error = await login(data)
+    setIsLoading(false)
+    if (error) {
+      setFeedback(error)
+      return
+    }
+    setFeedback(null)
+  }
+
+  const onSignup = async (data: z.infer<typeof loginFormSchema>) => {
+    if (await form.trigger()) {
+      setIsLoading(true)
+      const error = await signup(data)
+      setIsLoading(false)
+      if (error) {
+        setFeedback(error)
+        return
+      }
+      setFeedback(null)
+    }
+  }
+
+  return (
+    <div className="flex items-center justify-center h-[80vh]">
+      <div className="w-full max-w-md p-8 rounded-lg shadow-md space-y-6">
+        <div className='mb-6 space-y-2'>
+          <Button className="w-full" onClick={() => handleSignInWithProvider('google')} variant="outline" type="button" disabled={isLoading}>
+            <FaGoogle className="mr-2 h-4 w-4" />
+            Sign in with Google
+          </Button>
+          <Button className="w-full" onClick={() => handleSignInWithProvider('github')} variant="outline" type="button" disabled={isLoading}>
+            <FaGithub className="mr-2 h-4 w-4" />
+            Sign in with GitHub
+          </Button>
+          <Button className="w-full" onClick={() => handleSignInWithProvider('discord')} variant="outline" type="button" disabled={isLoading}>
+            <FaDiscord className="mr-2 h-4 w-4" />
+            Sign in with Discord
+          </Button>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onLogin)}>
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className='mb-4'>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="user@email.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <PasswordInput placeholder="password" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Password needs to be at least 6 characters long
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className='flex w-full space-x-4 mt-6 mb-6'>
+              <Button className='w-1/2 flex justify-center' type="submit" disabled={isLoading}>
+                Log in
+              </Button>
+              <Button
+                className='w-1/2 flex justify-center'
+                variant='outline'
+                type="button"
+                onClick={form.handleSubmit(onSignup)}
+                disabled={isLoading}
+              >
+                Sign up
+              </Button>
+            </div>
+          </form>
+          <p className='text-red-500 text-sm'>{feedback}</p>
+          <p className='text-primary text-center text-sm'>
+            By continuing you agree to everything
+          </p>
+        </Form>
+      </div>
+    </div>
+  )
+}
---
+++ rnd/autogpt_builder/src/components/ProfileDropdown.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { useSupabase } from "./SupabaseProvider";
+import { useRouter } from "next/navigation";
+import useUser from "@/hooks/useUser";
+
+const ProfileDropdown = () => {
+  const { supabase } = useSupabase();
+  const router = useRouter();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 rounded-full">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => router.push('profile')}>Profile</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => supabase?.auth.signOut()}>Log out</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export default ProfileDropdown;
