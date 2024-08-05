49b8e752bc521b79264cf7d2da08ea13d5a947b3
SwiftyOS <craigswift13@gmail.com>
2024-08-05 15:18:58 +0200

    Allow only next.js dev server

--- rnd/market/market/app.py
+++ rnd/market/market/app.py
@@ -54,10 +54,13 @@ app = fastapi.FastAPI(
 )
 
 app.add_middleware(fastapi.middleware.gzip.GZipMiddleware, minimum_size=1000)
-# ! This is a security risk, do not use in production
 app.add_middleware(
     middleware_class=fastapi.middleware.cors.CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        # Currently, we allow only next.js dev server
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
