10726cefe8ec9e2d7d328f4e9ed423f2b4db6cb2
SwiftyOS <craigswift13@gmail.com>
2024-08-07 11:53:02 +0200

    removed database changes

--- rnd/market/market/db.py
+++ rnd/market/market/db.py
@@ -1,7 +1,6 @@
 import typing
 
 import fuzzywuzzy.fuzz
-import prisma.enums
 import prisma.errors
 import prisma.models
 import prisma.types
@@ -9,7 +8,6 @@ import pydantic
 
 import market.model
 import market.utils.extension_types
-
 
 
 class AgentQueryError(Exception):
@@ -440,36 +438,3 @@ async def get_featured_agents(
     except Exception as e:
         # Catch any other unexpected exceptions
         raise AgentQueryError(f"Unexpected error occurred: {str(e)}") from e
-
-
-async def get_or_create_user(user_id: str) -> prisma.models.User:
-    user = await prisma.models.User.prisma().find_unique(where={"id": user_id})
-    if not user:
-        user = await prisma.models.User.prisma().create(
-            data={
-                "id": user_id,
-                "role": prisma.enums.UserRole.USER,
-            }
-        )
-    return prisma.models.User.model_validate(user)
-
-
-async def get_user_by_id(user_id: str) -> typing.Optional[prisma.models.User]:
-    user = await prisma.models.User.prisma().find_unique(where={"id": user_id})
-    return prisma.models.User.model_validate(user) if user else None
-
-
-async def create_default_user(enable_auth: str) -> typing.Optional[prisma.models.User]:
-    if not enable_auth.lower() == "true":
-        user = await prisma.models.User.prisma().find_unique(
-            where={"id": "3e53486c-cf57-477e-ba2a-cb02dc828e1a"}
-        )
-        if not user:
-            user = await prisma.models.User.prisma().create(
-                data={
-                    "id": "3e53486c-cf57-477e-ba2a-cb02dc828e1a",
-                    "role": prisma.enums.UserRole.ADMIN,
-                }
-            )
-        return prisma.models.User.model_validate(user)
-    return None
