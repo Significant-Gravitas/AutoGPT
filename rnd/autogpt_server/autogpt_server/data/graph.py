9d4978f726033862f51f2bf3d328ca05b1ca55a6
Zamil Majdy <zamil.majdy@agpt.co>
2024-08-16 16:06:42 +0200

    feat(rnd): Add staticOutput field on block API

--- rnd/autogpt_server/autogpt_server/data/graph.py
+++ rnd/autogpt_server/autogpt_server/data/graph.py
@@ -7,7 +7,7 @@ import prisma.types
 from prisma.models import AgentGraph, AgentNode, AgentNodeLink
 from pydantic import PrivateAttr
 
-from autogpt_server.blocks.basic import InputBlock, OutputBlock, ValueBlock
+from autogpt_server.blocks.basic import InputBlock, OutputBlock
 from autogpt_server.data.block import BlockInput, get_block, get_blocks
 from autogpt_server.data.db import BaseDbModel, transaction
 from autogpt_server.data.user import DEFAULT_USER_ID
@@ -175,10 +175,10 @@ class Graph(GraphMeta):
                     )
         node_map = {v.id: v for v in self.nodes}
 
-        def is_value_block(nid: str) -> bool:
+        def is_static_output_block(nid: str) -> bool:
             bid = node_map[nid].block_id
             b = get_block(bid)
-            return isinstance(b, ValueBlock)
+            return b.static_output if b else False
 
         def is_input_output_block(nid: str) -> bool:
             bid = node_map[nid].block_id
@@ -227,7 +227,7 @@ class Graph(GraphMeta):
             ):
                 raise ValueError(f"{suffix}, Connecting nodes from different subgraph.")
 
-            if is_value_block(link.source_id):
+            if is_static_output_block(link.source_id):
                 link.is_static = True  # Each value block output should be static.
 
             # TODO: Add type compatibility check here.
