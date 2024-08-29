4b023ba9a1e99bcc3a4f9cbfa7cc73e7861a4119
Zamil Majdy <zamil.majdy@agpt.co>
2024-08-28 22:36:53 -0500

    fix(rnd): Fix broken test and Input/Output block field renaming

--- rnd/autogpt_server/test/executor/test_manager.py
+++ rnd/autogpt_server/test/executor/test_manager.py
@@ -35,7 +35,6 @@ async def assert_sample_graph_executions(
     test_user: User,
     graph_exec_id: str,
 ):
-    input = {"input_1": "Hello", "input_2": "World"}
     executions = await agent_server.get_run_execution_results(
         test_graph.id, graph_exec_id, test_user.id
     )
@@ -44,16 +43,16 @@ async def assert_sample_graph_executions(
     exec = executions[0]
     assert exec.status == execution.ExecutionStatus.COMPLETED
     assert exec.graph_exec_id == graph_exec_id
-    assert exec.output_data == {"output": ["Hello"]}
-    assert exec.input_data == {"input": input, "key": "input_1"}
+    assert exec.output_data == {"value": ["Hello"]}
+    assert exec.input_data == {"value": "Hello", "name": "input_1"}
     assert exec.node_id in [test_graph.nodes[0].id, test_graph.nodes[1].id]
 
     # Executing ValueBlock
     exec = executions[1]
     assert exec.status == execution.ExecutionStatus.COMPLETED
     assert exec.graph_exec_id == graph_exec_id
-    assert exec.output_data == {"output": ["World"]}
-    assert exec.input_data == {"input": input, "key": "input_2"}
+    assert exec.output_data == {"value": ["World"]}
+    assert exec.input_data == {"value": "World", "name": "input_2"}
     assert exec.node_id in [test_graph.nodes[0].id, test_graph.nodes[1].id]
 
     # Executing TextFormatterBlock
@@ -151,7 +150,7 @@ async def test_input_pin_always_waited(server):
         server.agent_server, server.exec_manager, test_graph, test_user, {}, 3
     )
 
-    executions = await server.agent_server.get_run_execution_results(
+    executions = await server.agent_server.get_graph_run_node_execution_results(
         test_graph.id, graph_exec_id, test_user.id
     )
     assert len(executions) == 3
@@ -231,7 +230,7 @@ async def test_static_input_link_on_graph(server):
     graph_exec_id = await execute_graph(
         server.agent_server, server.exec_manager, test_graph, test_user, {}, 8
     )
-    executions = await server.agent_server.get_run_execution_results(
+    executions = await server.agent_server.get_graph_run_node_execution_results(
         test_graph.id, graph_exec_id, test_user.id
     )
     assert len(executions) == 8
