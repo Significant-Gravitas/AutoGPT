a4cfc2bdbbadf2a7b5849e96155c5289b939525c
Reinier van der Leer <pwuts@agpt.co>
2024-09-02 16:13:56 +0200

    fix(server): Fix type checking and propagation issues (#7941)
    - fix type propagation by `AppService.run_and_wait(..)`
    - fix type propagation by `@expose` and add note
    - fix type propagation by `wait(..)` in `.executor.manager.execute_node(..)`
    - fix type propagation by `wait(..)` in `.executor.manager._enqueue_next_nodes(..)`
    - remove unnecessary null checks for `.data.graph.get_node(..)`
    - fix type issue in `ExecutionScheduler`
    - reduce use of `# type: ignore` in `.data.execution`
    - reduce usage of `# type: ignore` in `.executor.manager`
    - reduce usage of `# type: ignore` in `.server`
    - reduce usage of `# type: ignore` in cli.py
    - update `pyright` to v1.1.378

--- rnd/autogpt_server/autogpt_server/util/service.py
+++ rnd/autogpt_server/autogpt_server/util/service.py
@@ -19,11 +19,21 @@ conn_retry = retry(
     stop=stop_after_attempt(30), wait=wait_exponential(multiplier=1, min=1, max=30)
 )
 T = TypeVar("T")
+C = TypeVar("C", bound=Callable)
 
 pyro_host = Config().pyro_host
 
 
-def expose(func: Callable) -> Callable:
+def expose(func: C) -> C:
+    """
+    Decorator to mark a method or class to be exposed for remote calls.
+
+    ## ⚠️ Gotcha
+    The types on the exposed function signature are respected **as long as they are
+    fully picklable**. This is not the case for Pydantic models, so if you really need
+    to pass a model, try dumping the model and passing the resulting dict instead.
+    """
+
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
@@ -32,7 +42,7 @@ def expose(func: Callable) -> Callable:
             logger.exception(msg)
             raise Exception(msg, e)
 
-    return pyro.expose(wrapper)
+    return pyro.expose(wrapper)  # type: ignore
 
 
 class PyroNameServer(AppProcess):
@@ -60,10 +70,10 @@ class AppService(AppProcess):
         while True:
             time.sleep(10)
 
-    def __run_async(self, coro: Coroutine[T, Any, T]):
+    def __run_async(self, coro: Coroutine[Any, Any, T]):
         return asyncio.run_coroutine_threadsafe(coro, self.shared_event_loop)
 
-    def run_and_wait(self, coro: Coroutine[T, Any, T]) -> T:
+    def run_and_wait(self, coro: Coroutine[Any, Any, T]) -> T:
         future = self.__run_async(coro)
         return future.result()
 
@@ -107,7 +117,6 @@ def get_service_client(service_type: Type[AS]) -> AS:
     service_name = service_type.service_name
 
     class DynamicClient:
-
         @conn_retry
         def __init__(self):
             ns = pyro.locate_ns()
