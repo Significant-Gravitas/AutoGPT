d3161227d83c450e81c230733a973e7975ee53d9
Nicholas Tindle <nicholas.tindle@agpt.co>
2024-09-03 12:09:12 -0500

    fix: formatting

--- rnd/market/market/app.py
+++ rnd/market/market/app.py
@@ -65,7 +65,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:3000",
-        "https://dev-builder.agpt.co"
+        "https://dev-builder.agpt.co",
     ],
     allow_credentials=True,
     allow_methods=["*"],
