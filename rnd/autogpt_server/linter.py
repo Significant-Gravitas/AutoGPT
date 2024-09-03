39b3b33ee1c1b9b53e0fc01c8bc3e968f9ed2472
Aarushi <aarushik93@gmail.com>
2024-09-03 19:46:19 +0100

    remove schema linting

--- rnd/autogpt_server/linter.py
+++ rnd/autogpt_server/linter.py
@@ -20,29 +20,10 @@ def lint():
         print("Lint failed, try running `poetry run format` to fix the issues: ", e)
         raise e
 
-    try:
-        run("schema_lint")
-    except subprocess.CalledProcessError as e:
-        print("Lint failed, try running `poetry run schema` to fix the issues: ", e)
-        raise e
-
 
 def format():
     run("ruff", "check", "--fix", ".")
     run("isort", "--profile", "black", ".")
     run("black", ".")
     run("pyright", ".")
-
-
-def schema():
-    file = os.path.join(directory, "schema.prisma")
-    text = open(file, "r").read()
-    text = re.sub(r'provider\s+=\s+".*"', 'provider = "postgresql"', text, 1)
-    text = re.sub(r'url\s+=\s+".*"', 'url = env("DATABASE_URL")', text, 1)
-    text = "// THIS FILE IS AUTO-GENERATED, RUN `poetry run schema` TO UPDATE\n" + text
-    with open(os.path.join(directory, "schema.prisma"), "w") as f:
-        f.write(text)
-
-    run("prisma", "format", "--schema", "schema.prisma")
-    run("prisma", "migrate", "dev", "--schema", "schema.prisma")
 
