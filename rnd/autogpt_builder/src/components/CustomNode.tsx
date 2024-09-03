09951fed4b1a3ceb9cd3c7eb631ff0d427cef15e
Krzysztof Czerwinski <34861343+kcze@users.noreply.github.com>
2024-09-03 12:01:42 +0100

    feat(builder, server): Add advanced block inputs (#7934)
    - Add `advanced` to `SchemaField` and pass it to `json_extra`
    - Add `advanced` to `BlockIOSubSchemaMeta` type
    - Update `CustomNode`, so that:
      - non-required advanced inputs are hidden
      - non-advanced and required inputs are always shown

--- rnd/autogpt_builder/src/components/CustomNode.tsx
+++ rnd/autogpt_builder/src/components/CustomNode.tsx
@@ -118,12 +118,6 @@ export function CustomNode({ data, id, width, height }: NodeProps<CustomNode>) {
     setIsAdvancedOpen(checked);
   };
 
-  const hasOptionalFields =
-    data.inputSchema &&
-    Object.keys(data.inputSchema.properties).some((key) => {
-      return !data.inputSchema.required?.includes(key);
-    });
-
   const generateOutputHandles = (schema: BlockIORootSchema) => {
     if (!schema?.properties) return null;
     const keys = Object.keys(schema.properties);
@@ -368,10 +362,19 @@ export function CustomNode({ data, id, width, height }: NodeProps<CustomNode>) {
 
   const errorClass =
     hasConfigErrors || hasOutputError ? "border-red-500 border-2" : "";
+
   const statusClass =
     hasConfigErrors || hasOutputError
       ? "failed"
       : (data.status?.toLowerCase() ?? "");
+
+  const hasAdvancedFields =
+    data.inputSchema &&
+    Object.entries(data.inputSchema.properties).some(([key, value]) => {
+      return (
+        value.advanced === true && !data.inputSchema.required?.includes(key)
+      );
+    });
 
   return (
     <div
@@ -421,8 +424,12 @@ export function CustomNode({ data, id, width, height }: NodeProps<CustomNode>) {
               ([propKey, propSchema]) => {
                 const isRequired = data.inputSchema.required?.includes(propKey);
                 const isConnected = isHandleConnected(propKey);
+                const isAdvanced = propSchema.advanced;
                 return (
-                  (isRequired || isAdvancedOpen || isConnected) && (
+                  (isRequired ||
+                    isAdvancedOpen ||
+                    isConnected ||
+                    !isAdvanced) && (
                     <div key={propKey} onMouseOver={() => {}}>
                       <NodeHandle
                         keyName={propKey}
@@ -479,7 +486,7 @@ export function CustomNode({ data, id, width, height }: NodeProps<CustomNode>) {
       <div className="mt-2.5 flex items-center pb-4 pl-4">
         <Switch checked={isOutputOpen} onCheckedChange={toggleOutput} />
         <span className="m-1 mr-4">Output</span>
-        {hasOptionalFields && (
+        {hasAdvancedFields && (
           <>
             <Switch onCheckedChange={toggleAdvancedSettings} />
             <span className="m-1">Advanced</span>
