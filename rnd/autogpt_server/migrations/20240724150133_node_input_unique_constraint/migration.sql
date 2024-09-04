5000aa7ee01b9b207eed843a94866fac3d3e087c
Aarushi <50577581+aarushik93@users.noreply.github.com>
2024-09-04 10:18:57 +0100

    tweak(rnd,docker) Remove SQLite (#7966)
    * move migrations, update networking and dockignore
    
    * update docs
    
    * remove sqlite from ci
    
    * remove schema linting checks
    
    * fix formatting
    
    * remove schema linting
    
    * add test script
    
    * formatting and linting
    
    * stop pg not down
    
    * seperate test db
    
    * diff port
    
    * remove duplicate

--- rnd/autogpt_server/migrations/20240724150133_node_input_unique_constraint/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[referencedByInputExecId,referencedByOutputExecId,name]` on the table `AgentNodeExecutionInputOutput` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "AgentNodeExecutionInputOutput_referencedByInputExecId_referencedByOutputExecId_name_key" ON "AgentNodeExecutionInputOutput"("referencedByInputExecId", "referencedByOutputExecId", "name");
