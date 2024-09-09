faa58218249ac310266bb824fc32050e30ac923d
Aarushi <aarushik93@gmail.com>
2024-09-09 19:19:49 +0100

    remove execution from rest

--- rnd/autogpt_server/autogpt_server/rest.py
+++ rnd/autogpt_server/autogpt_server/rest.py
@@ -8,7 +8,6 @@ def main():
     Run all the processes required for the AutoGPT-server REST API.
     """
     run_processes(
-        ExecutionManager(),
         ExecutionScheduler(),
         AgentServer(),
     )
