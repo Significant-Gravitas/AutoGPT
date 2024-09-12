6fde030c3723a5aa3b471b90bb875c7b5abfd96e
Swifty <craigswift13@gmail.com>
2024-09-12 13:03:37 +0200

    feat(rnd): Reduce container size remove dep with forge and autogpt (#8040)
    * Remove forge and autogpt
    
    * update lock files
    
    * Update build process to reduce image size
    
    * Reduced built image size
    
    * fixed docker compose watch
    
    * Updated logging
    
    * updated env.example
    
    * formatting
    
    * linting issue
    
    * linting not working in github actions..
    
    * trying to get around github action linting issue
    
    * updated version
    
    * sleep for prisma issues
    
    * add exp backoff on connection issues
    
    * updated config based on review comments
    
    * Sorting alphabetical
    
    * updated default config
    
    * updated depends checks
    
    * fixed missing prisma binaries
    
    * remove dead layer
    
    * remove try
    
    * remove dead layer

--- rnd/autogpt_server/autogpt_server/util/logging.py
+++ rnd/autogpt_server/autogpt_server/util/logging.py
@@ -1,17 +1,15 @@
 import os
 
-from forge.logging.config import LogFormatName
-
 
 def configure_logging():
     import logging
 
-    from forge.logging import configure_logging
+    import autogpt_libs.logging.config
 
     if os.getenv("APP_ENV") != "cloud":
-        configure_logging()
+        autogpt_libs.logging.config.configure_logging(force_cloud_logging=False)
     else:
-        configure_logging(log_format=LogFormatName.STRUCTURED)
+        autogpt_libs.logging.config.configure_logging(force_cloud_logging=True)
 
     # Silence httpx logger
     logging.getLogger("httpx").setLevel(logging.WARNING)
