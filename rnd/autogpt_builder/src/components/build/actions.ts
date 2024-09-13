4ff1520f01ae1dae66ab0d3d1d8acf8b01d73390
Nicholas Tindle <nicholas.tindle@agpt.co>
2024-09-13 07:37:33 -0500

    Update actions.ts

--- rnd/autogpt_builder/src/components/build/actions.ts
+++ rnd/autogpt_builder/src/components/build/actions.ts
@@ -4,7 +4,7 @@ import { TutorialStepData } from "@/lib/autogpt-server-api/types";
 import AutoGPTServerAPI from "@/lib/autogpt-server-api/client";
 
 export const sendTutorialStep = async (data: TutorialStepData) => {
-  console.log("sendTutorialStep", data);
+  console.debug("sendTutorialStep", data);
   const api = new AutoGPTServerAPI();
 
   await api.logTutorialStep(data);
