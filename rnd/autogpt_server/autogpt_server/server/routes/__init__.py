55e206fcd7b20f2f38596ed2fc388f6750fb4044
SwiftyOS <craigswift13@gmail.com>
2024-09-17 15:30:31 +0200

    updated routes init

--- rnd/autogpt_server/autogpt_server/server/routes/__init__.py
+++ rnd/autogpt_server/autogpt_server/server/routes/__init__.py
@@ -1,3 +1,4 @@
 from .agents import router as agents_router
 from .root import router as root_router
 from .blocks import router as blocks_router
+from .integrations import integrations_api_router as integrations_router
