3af4acf532ddd65fa199d3ee68072711d00afbfe
Aarushi <50577581+aarushik93@users.noreply.github.com>
2024-09-19 00:06:04 +0100

    fix(frontend): Update REST API port (#8096)
    update server port to 8006
    
    Co-authored-by: Zamil Majdy <zamil.majdy@agpt.co>

--- rnd/autogpt_builder/src/lib/autogpt-server-api/baseClient.ts
+++ rnd/autogpt_builder/src/lib/autogpt-server-api/baseClient.ts
@@ -22,7 +22,7 @@ export default class BaseAutoGPTServerAPI {
 
   constructor(
     baseUrl: string = process.env.NEXT_PUBLIC_AGPT_SERVER_URL ||
-      "http://localhost:8000/api",
+      "http://localhost:8006/api",
     wsUrl: string = process.env.NEXT_PUBLIC_AGPT_WS_SERVER_URL ||
       "ws://localhost:8001/ws",
     supabaseClient: SupabaseClient | null = null,
