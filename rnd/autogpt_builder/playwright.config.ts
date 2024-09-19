39c784f569311dd9a50314e1243eb0fe011dec34
Nicholas Tindle <nicholas.tindle@agpt.co>
2024-09-19 17:20:41 -0500

    Update playwright.config.ts

--- rnd/autogpt_builder/playwright.config.ts
+++ rnd/autogpt_builder/playwright.config.ts
@@ -71,10 +71,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "npm run prepTests",
-    url: "http://localhost:3000/",
-    reuseExistingServer: !process.env.CI,
-    timeout: 120000,
-  },
+  // webServer: {
+  //   command: "npm run prepTests",
+  //   url: "http://localhost:3000/",
+  //   reuseExistingServer: !process.env.CI,
+  //   timeout: 120000,
+  // },
 });
