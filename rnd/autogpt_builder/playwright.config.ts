7ebe4f2b236eccf5eff20b25ff40765fff2e55d8
Nicholas Tindle <nicholas.tindle@agpt.co>
2024-09-19 16:46:23 -0500

    feat(ci): try to start with npm

--- rnd/autogpt_builder/playwright.config.ts
+++ rnd/autogpt_builder/playwright.config.ts
@@ -71,9 +71,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "npm run prepTests",
-    url: "http://localhost:3000",
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: "npm run prepTests",
+  //   url: "http://localhost:3000",
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
