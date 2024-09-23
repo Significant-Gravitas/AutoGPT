9f1617c3fed143ecbab1849b123defca4a30fb27
Nicholas Tindle <nicholas.tindle@agpt.co>
2024-09-23 18:13:07 +0000

    dx(frontend): remove sentry popup

--- autogpt_platform/frontend/sentry.client.config.ts
+++ autogpt_platform/frontend/sentry.client.config.ts
@@ -31,14 +31,6 @@ Sentry.init({
     /^https:\/\/dev\-builder\.agpt\.co\/api/,
   ],
 
-  // beforeSend(event, hint) {
-  //   // Check if it is an exception, and if so, show the report dialog
-  //   if (event.exception && event.event_id) {
-  //     Sentry.showReportDialog({ eventId: event.event_id });
-  //   }
-  //   return event;
-  // },
-
   // Define how likely Replay events are sampled.
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
