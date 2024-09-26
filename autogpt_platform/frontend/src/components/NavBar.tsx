e2d1a3007f9d8b21ed7a81d1412ea44a154d8c77
Swifty <craigswift13@gmail.com>
2024-09-26 09:27:26 +0200

    tweak(platform): Update Nav Menu styling  (#8148)
    * easy nav ui changes
    
    * teaked mobile nav menu

--- autogpt_platform/frontend/src/components/NavBar.tsx
+++ autogpt_platform/frontend/src/components/NavBar.tsx
@@ -15,6 +15,9 @@ import {
 } from "@/components/ui/icons";
 import AutoGPTServerAPI from "@/lib/autogpt-server-api";
 import CreditButton from "@/components/CreditButton";
+import { BsBoxes } from "react-icons/bs";
+import { LuLaptop } from "react-icons/lu";
+import { LuShoppingCart } from "react-icons/lu";
 
 export async function NavBar() {
   const isAvailable = Boolean(
@@ -24,7 +27,7 @@ export async function NavBar() {
   const { user } = await getServerUser();
 
   return (
-    <header className="sticky top-0 z-50 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+    <header className="sticky top-0 z-50 flex h-16 items-center gap-4 border-b bg-background px-4 md:rounded-b-3xl md:px-6 md:shadow-md">
       <div className="flex flex-1 items-center gap-4">
         <Sheet>
           <SheetTrigger asChild>
@@ -40,63 +43,57 @@ export async function NavBar() {
           <SheetContent side="left">
             <nav className="grid gap-6 text-lg font-medium">
               <Link
-                href="/"
-                className="flex flex-row gap-2 text-muted-foreground hover:text-foreground"
+                href="/marketplace"
+                className="mt-4 flex flex-row items-center gap-2 text-muted-foreground hover:text-foreground"
               >
-                <IconSquareActivity /> Monitor
+                <LuShoppingCart /> Marketplace
+              </Link>
+              <Link
+                href="/"
+                className="flex flex-row items-center gap-2 text-muted-foreground hover:text-foreground"
+              >
+                <LuLaptop /> Monitor
               </Link>
               <Link
                 href="/build"
-                className="flex flex-row gap-2 text-muted-foreground hover:text-foreground"
+                className="flex flex-row items-center gap-2 text-muted-foreground hover:text-foreground"
               >
-                <IconWorkFlow /> Build
-              </Link>
-              <Link
-                href="/marketplace"
-                className="flex flex-row gap-2 text-muted-foreground hover:text-foreground"
-              >
-                <IconPackage2 /> Marketplace
+                <BsBoxes /> Build
               </Link>
             </nav>
           </SheetContent>
         </Sheet>
-        <nav className="hidden md:flex md:flex-row md:items-center md:gap-5 lg:gap-6">
+        <nav className="hidden md:flex md:flex-row md:items-center md:gap-7 lg:gap-8">
+          <div className="flex h-10 w-20 flex-1 flex-row items-center justify-center gap-2">
+            <a href="https://agpt.co/">
+              <Image
+                src="/AUTOgpt_Logo_dark.png"
+                alt="AutoGPT Logo"
+                width={100}
+                height={40}
+                priority
+              />
+            </a>
+          </div>
+          <Link
+            href="/marketplace"
+            className="text-basehover:text-foreground flex flex-row items-center gap-2 font-semibold text-foreground"
+          >
+            <LuShoppingCart /> Marketplace
+          </Link>
           <Link
             href="/"
-            className="flex flex-row items-center gap-2 text-muted-foreground hover:text-foreground"
+            className="text-basehover:text-foreground flex flex-row items-center gap-2 font-semibold text-foreground"
           >
-            <IconSquareActivity /> Monitor
+            <LuLaptop className="mr-1" /> Monitor
           </Link>
           <Link
             href="/build"
-            className="flex flex-row items-center gap-2 text-muted-foreground hover:text-foreground"
+            className="flex flex-row items-center gap-2 text-base font-semibold text-foreground hover:text-foreground"
           >
-            <IconWorkFlow /> Build
-          </Link>
-          <Link
-            href="/marketplace"
-            className="flex flex-row items-center gap-2 text-muted-foreground hover:text-foreground"
-          >
-            <IconPackage2 /> Marketplace
+            <BsBoxes className="mr-1" /> Build
           </Link>
         </nav>
-      </div>
-      <div className="relative flex flex-1 justify-center">
-        <a
-          className="pointer-events-auto flex place-items-center gap-2"
-          href="https://news.agpt.co/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          By{" "}
-          <Image
-            src="/AUTOgpt_Logo_dark.png"
-            alt="AutoGPT Logo"
-            width={100}
-            height={20}
-            priority
-          />
-        </a>
       </div>
       <div className="flex flex-1 items-center justify-end gap-4">
         {isAvailable && user && <CreditButton />}
