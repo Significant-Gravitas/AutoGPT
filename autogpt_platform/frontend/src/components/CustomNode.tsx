2155199e6f52eaa3b58c1d8d725aeea7588dd252
SwiftyOS <craigswift13@gmail.com>
2024-09-27 15:30:19 +0200

    Changed status badge color

--- autogpt_platform/frontend/src/components/CustomNode.tsx
+++ autogpt_platform/frontend/src/components/CustomNode.tsx
@@ -733,15 +733,15 @@ export function CustomNode({ data, id, width, height }: NodeProps<CustomNode>) {
                 className={cn(
                   "mr-4 flex min-w-[114px] items-center justify-center rounded-3xl text-center text-xs font-semibold",
                   data.status === "COMPLETED" &&
-                    "border-green-500 bg-green-500 text-white",
+                    "border-green-600 bg-green-600 text-white",
                   data.status === "RUNNING" &&
-                    "border-yellow-500 bg-yellow-500 text-white",
+                    "border-yellow-600 bg-yellow-600 text-white",
                   data.status === "FAILED" &&
-                    "border-red-500 bg-red-500 text-white",
+                    "border-red-600 bg-red-600 text-white",
                   data.status === "QUEUED" &&
-                    "border-blue-500 bg-blue-500 text-white",
+                    "border-blue-600 bg-blue-600 text-white",
                   data.status === "INCOMPLETE" &&
-                    "border-gray-500 bg-gray-500 font-black",
+                    "border-gray-600 bg-gray-600 font-black",
                 )}
               >
                 {data.status ? beautifyString(data.status) : "Not Run"}
