96534f05e99bf602921941d252a6d13636fddebc
SwiftyOS <craigswift13@gmail.com>
2024-10-07 17:11:27 +0200

    fix linting issues

--- autogpt_platform/frontend/src/components/ui/dialog.stories.tsx
+++ autogpt_platform/frontend/src/components/ui/dialog.stories.tsx
@@ -54,7 +54,7 @@ export const WithForm: Story = {
         <DialogHeader>
           <DialogTitle>Edit profile</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Make changes to your profile here. Click save when you&apos;re done.
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
--- autogpt_platform/frontend/src/stories/Page.tsx
+++ autogpt_platform/frontend/src/stories/Page.tsx
@@ -40,7 +40,7 @@ export const Page: React.FC = () => {
         <ul>
           <li>
             Use a higher-level connected component. Storybook helps you compose
-            such data from the "args" of child component stories
+            such data from the &quot;args&quot; of child component stories
           </li>
           <li>
             Assemble data in the page component from your services. You can mock
