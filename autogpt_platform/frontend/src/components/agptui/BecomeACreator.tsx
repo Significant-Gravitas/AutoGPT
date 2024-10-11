6b18951cdd8cac3288ce5290e5a579e2d3068c16
SwiftyOS <craigswift13@gmail.com>
2024-10-11 12:01:48 +0200

    Added become a creator component

---
+++ autogpt_platform/frontend/src/components/agptui/BecomeACreator.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { Button } from "./Button";
+
+interface BecomeACreatorProps {
+  title: string;
+  heading: string;
+  description: string;
+  buttonText: string;
+  onButtonClick: () => void;
+}
+
+export const BecomeACreator: React.FC<BecomeACreatorProps> = ({
+  title = "Want to contribute?",
+  heading = "We're always looking for more Creators!",
+  description = "Join our ever-growing community of hackers and tinkerers",
+  buttonText = "Become a Creator",
+  onButtonClick = () => {},
+}) => {
+  return (
+    <div className="w-full flex flex-col items-center justify-between space-y-8 py-8">
+      <div className="font-['PP Neue Montreal TT'] mb-8 self-start text-[23px] font-bold leading-9 tracking-tight text-[#282828]">
+        {title}
+      </div>
+      <div className="font-['PP Neue Montreal TT'] text-center text-5xl font-medium leading-9 tracking-wide text-[#272727] max-w-full">
+        {heading}
+      </div>
+      <div className="font-['PP Neue Montreal TT'] text-center text-[26px] font-medium leading-9 tracking-tight text-[#878787] max-w-full">
+        {description}
+      </div>
+      <Button onClick={onButtonClick} className="mt-8">
+        {buttonText}
+      </Button>
+    </div>
+  );
+};
