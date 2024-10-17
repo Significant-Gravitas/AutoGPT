3a0a8e53ac45f05637daceffe5d3c5cecf0e4b83
SwiftyOS <craigswift13@gmail.com>
2024-10-17 12:25:12 +0200

    Mobile Menu and accessability

--- autogpt_platform/frontend/.storybook/main.ts
+++ autogpt_platform/frontend/.storybook/main.ts
@@ -3,6 +3,7 @@ import type { StorybookConfig } from "@storybook/nextjs";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
+    "@storybook/addon-a11y",
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
