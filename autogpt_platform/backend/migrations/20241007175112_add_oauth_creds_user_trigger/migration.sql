70cbe29abd2807cacfe65da8eb75b5da58df52a8
Zamil Majdy <zamil.majdy@agpt.co>
2024-10-22 08:27:03 +0700

    Make the migration safe

--- autogpt_platform/backend/migrations/20241007175112_add_oauth_creds_user_trigger/migration.sql
+++ autogpt_platform/backend/migrations/20241007175112_add_oauth_creds_user_trigger/migration.sql
@@ -7,8 +7,14 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
+-- This CREATE commands is for dev-only, where full-fledged Supabase can be missing.
+-- Create the 'auth' schema if it doesn't exist
+CREATE SCHEMA IF NOT EXISTS auth;
+CREATE TABLE IF NOT EXISTS auth.users (id UUID PRIMARY KEY, email TEXT);
+
+-- DropTigger
+DROP TRIGGER IF EXISTS user_added_to_platform ON auth.users;
+
 --CreateTrigger
-CREATE TRIGGER user_added_to_platform
-AFTER INSERT ON auth.users
-FOR EACH ROW
-EXECUTE FUNCTION add_user_to_platform();
+CREATE TRIGGER user_added_to_platform AFTER INSERT ON auth.users
+FOR EACH ROW EXECUTE FUNCTION add_user_to_platform();
