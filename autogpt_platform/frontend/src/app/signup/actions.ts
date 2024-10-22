5b9bdfb558c1514fc1f14a64a8c73255304adc3d
Avhimanyu <2023ebcs396@online.bits-pilani.ac.in>
2024-10-22 10:41:34 +0530

    Combine login and signup routes with redirect for new users

--- autogpt_platform/frontend/src/app/signup/actions.ts
+++ autogpt_platform/frontend/src/app/signup/actions.ts
@@ -1,45 +1,16 @@
 "use server";
+import { createServerClient } from "@/lib/supabase/server";
+import * as Sentry from "@sentry/nextjs";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-import { createServerClient } from "@/lib/supabase/server";
 import { z } from "zod";
-import * as Sentry from "@sentry/nextjs";
 
-const loginFormSchema = z.object({
+const SignupFormSchema = z.object({
   email: z.string().email().min(2).max(64),
   password: z.string().min(6).max(64),
 });
 
-export async function login(values: z.infer<typeof loginFormSchema>) {
-  return await Sentry.withServerActionInstrumentation("login", {}, async () => {
-    const supabase = createServerClient();
-
-    if (!supabase) {
-      redirect("/error");
-    }
-
-    // We are sure that the values are of the correct type because zod validates the form
-    const { data, error } = await supabase.auth.signInWithPassword(values);
-
-    if (error) {
-      if (error.status == 400) {
-        // Hence User is not present
-        redirect("/signup");
-      }
-
-      return error.message;
-    }
-
-    if (data.session) {
-      await supabase.auth.setSession(data.session);
-    }
-
-    revalidatePath("/", "layout");
-    redirect("/");
-  });
-}
-
-export async function signup(values: z.infer<typeof loginFormSchema>) {
+export async function signup(values: z.infer<typeof SignupFormSchema>) {
   "use server";
   return await Sentry.withServerActionInstrumentation(
     "signup",
@@ -57,6 +28,9 @@ export async function signup(values: z.infer<typeof loginFormSchema>) {
       if (error) {
         if (error.message.includes("P0001")) {
           return "Please join our waitlist for your turn: https://agpt.co/waitlist";
+        }
+        if (error.code?.includes("user_already_exists")) {
+          redirect("/login");
         }
         return error.message;
       }
