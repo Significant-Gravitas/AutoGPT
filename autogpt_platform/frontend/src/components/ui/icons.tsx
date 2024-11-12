3839e012bf940ad019cdcd00b450280ebcc5af05
SwiftyOS <craigswift13@gmail.com>
2024-11-12 09:49:36 +0100

    remove use client

--- autogpt_platform/frontend/src/components/ui/icons.tsx
+++ autogpt_platform/frontend/src/components/ui/icons.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
@@ -1518,6 +1516,7 @@ export enum IconType {
   Settings,
   LogOut,
 }
+
 export function getIconForSocial(
   url: string,
   props: IconProps,
