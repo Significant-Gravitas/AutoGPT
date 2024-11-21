3d15e17b4fcb1897d89e620df0821da450ac8ce8
SwiftyOS <craigswift13@gmail.com>
2024-11-21 13:27:26 +0100

    dark mode tweaks

--- autogpt_platform/frontend/src/components/agptui/CreditsCard.tsx
+++ autogpt_platform/frontend/src/components/agptui/CreditsCard.tsx
@@ -18,12 +18,12 @@ const CreditsCard = ({ credits }: CreditsCardProps) => {
   };
 
   return (
-    <div className="inline-flex h-[60px] items-center gap-2.5 rounded-2xl bg-neutral-200 p-4">
+    <div className="inline-flex h-[60px] items-center gap-2.5 rounded-2xl bg-neutral-200 dark:bg-neutral-800 p-4">
       <div className="flex items-center gap-0.5">
-        <span className="font-['Geist'] text-base font-semibold leading-7 text-neutral-900">
+        <span className="font-['Poppins'] text-base font-semibold leading-7 text-neutral-900 dark:text-neutral-50">
           {currentCredits.toLocaleString()}
         </span>
-        <span className="font-['Geist'] text-base font-normal leading-7 text-neutral-900">
+        <span className="font-['Poppins'] pl-1 text-base font-semibold leading-7 text-neutral-900 dark:text-neutral-50">
           credits
         </span>
       </div>
