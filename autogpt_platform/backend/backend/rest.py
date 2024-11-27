ae9bd87161f7689d062c2986e544013bb0d89409
Zamil Majdy <zamil.majdy@agpt.co>
2024-11-27 23:39:08 +0700

    fix(backend): Spin-up Database manager on rest.py (#8832)

--- autogpt_platform/backend/backend/rest.py
+++ autogpt_platform/backend/backend/rest.py
@@ -1,5 +1,5 @@
 from backend.app import run_processes
-from backend.executor import ExecutionScheduler
+from backend.executor import DatabaseManager, ExecutionScheduler
 from backend.server.rest_api import AgentServer
 
 
@@ -8,6 +8,7 @@ def main():
     Run all the processes required for the AutoGPT-server REST API.
     """
     run_processes(
+        DatabaseManager(),
         ExecutionScheduler(),
         AgentServer(),
     )
