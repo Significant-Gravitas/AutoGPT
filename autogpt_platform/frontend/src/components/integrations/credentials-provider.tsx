a79776d60cbfdbc3eb8eba6794a078fb00686c2a
Aarushi <50577581+aarushik93@users.noreply.github.com>
2024-11-29 09:26:43 +0000

    feat(blocks): Add Hubspot blocks (#8786)
    This PR adds the first few Hubspot blocks so we can create _real_ sales
    and marketing agents.
    
    ### Changes 🏗️
    
    Added Hubspot blocks; 
    
    - Aded auth for hubspot
    - Added Company block
    - Added Contact block
    - Added Engagement block
    
    ### Checklist 📋
    
    #### For code changes:
    - [ ] I have clearly listed my changes in the PR description
    - [ ] I have made a test plan
    - [ ] I have tested my changes according to the test plan:
      <!-- Put your test plan here: -->
      - [ ] ...
    
    <details>
      <summary>Example test plan</summary>
      
      - [ ] Create from scratch and execute an agent with at least 3 blocks
    - [ ] Import an agent from file upload, and confirm it executes
    correctly
      - [ ] Upload agent to marketplace
    - [ ] Import an agent from marketplace and confirm it executes correctly
      - [ ] Edit an agent from monitor, and confirm it executes correctly
    </details>
    
    #### For configuration changes:
    - [ ] `.env.example` is updated or already compatible with my changes
    - [ ] `docker-compose.yml` is updated or already compatible with my
    changes
    - [ ] I have included a list of my configuration changes in the PR
    description (under **Changes**)
    
    <details>
      <summary>Examples of configuration changes</summary>
    
      - Changing ports
      - Adding new services that need to communicate with each other
      - Secrets or environment variable changes
      - New or infrastructure changes such as databases
    </details>

--- autogpt_platform/frontend/src/components/integrations/credentials-provider.tsx
+++ autogpt_platform/frontend/src/components/integrations/credentials-provider.tsx
@@ -40,6 +40,7 @@ const providerDisplayNames: Record<CredentialsProviderName, string> = {
   replicate: "Replicate",
   revid: "Rev.ID",
   unreal_speech: "Unreal Speech",
+  hubspot: "Hubspot",
 } as const;
 // --8<-- [end:CredentialsProviderNames]
 
