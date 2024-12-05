1ba907549a65dbb00b53a90af89191e1d3a58360
Reinier van der Leer <pwuts@agpt.co>
2024-12-05 11:35:13 +0100

    feat(blocks): Add webhook block status indicator (#8838)
    - Resolves #8743
    - Follow-up to #8358
    
    ### Demo
    
    
    https://github.com/user-attachments/assets/f983dfa2-2dc2-4ab0-8373-e768ba17e6f7
    
    ### Changes 🏗️
    
    - feat(frontend): Add webhook status indicator on `CustomNode`
       - Add `webhookId` to frontend node data model
    
    - fix(backend): Fix webhook ping endpoint
       - Remove `provider` path parameter
       - Fix return values and error handling
       - Fix `WebhooksManager.trigger_ping(..)`
          - Add `credentials` parameter
          - Fix usage of credentials
       - Fix `.data.integrations.wait_for_webhook_event(..)`
          - Add `AsyncRedisEventBus.wait_for_event(..)`
    
    - feat(frontend): Add `BackendAPIProvider` + `useBackendAPI`
    
    - feat(frontend): Improve layout of node header
    
        Before:
    
    ![image](https://github.com/user-attachments/assets/17a33b94-65f0-4e34-a47d-2dd321edecae)
        After:
    
    ![image](https://github.com/user-attachments/assets/64afb1e4-e3f2-4ca9-8961-f1245f25477f)
    
    - refactor(backend): Clean up `.data.integrations`
    - refactor(backend): Fix naming in `.data.queue` for understandability
    
    ### Checklist 📋
    
    #### For code changes:
    - [x] I have clearly listed my changes in the PR description
    - [x] I have made a test plan
    - [x] I have tested my changes according to the test plan:
      <!-- Put your test plan here: -->
      - [x] Add webhook block, save -> gray indicator
      - [x] Add necessary info to webhook block, save -> green indicator
      - [x] Remove necessary info, save -> gray indicator
    
    ---------
    
    Co-authored-by: Nicholas Tindle <nicholas.tindle@agpt.co>

--- autogpt_platform/frontend/src/hooks/useAgentGraph.ts
+++ autogpt_platform/frontend/src/hooks/useAgentGraph.ts
@@ -170,6 +170,7 @@ export default function useAgentGraph(
               inputSchema: block.inputSchema,
               outputSchema: block.outputSchema,
               hardcodedValues: node.input_default,
+              webhookId: node.webhook_id,
               uiType: block.uiType,
               connections: graph.links
                 .filter((l) => [l.source_id, l.sink_id].includes(node.id))
@@ -776,6 +777,7 @@ export default function useAgentGraph(
                     ),
                     status: undefined,
                     backend_id: backendNode.id,
+                    webhookId: backendNode.webhook_id,
                     executionResults: [],
                   },
                 }
