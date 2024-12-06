18a15e45a07dbe31219614bcf80b02bbeb24e76b
Nicholas Tindle <nicholas.tindle@agpt.co>
2024-12-05 21:52:10 -0600

    feat: a test folder

--- autogpt_platform/frontend/src/tests/pages/base.page.ts
+++ autogpt_platform/frontend/src/tests/pages/base.page.ts
@@ -3,6 +3,7 @@ import { NavBar } from "./navbar.page";
 
 export class BasePage {
   readonly navbar: NavBar;
+  readonly downloadsFolder = "./test-contents";
 
   constructor(protected page: Page) {
     this.navbar = new NavBar(page);
