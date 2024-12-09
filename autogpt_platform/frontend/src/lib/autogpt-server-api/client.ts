e9f000b70163c1e3838f2c67eff4afa3734ad6d3
Reinier van der Leer <pwuts@agpt.co>
2024-12-09 11:57:54 +0100

    fix(frontend): Automatically choose client/server-side API client

--- autogpt_platform/frontend/src/lib/autogpt-server-api/client.ts
+++ autogpt_platform/frontend/src/lib/autogpt-server-api/client.ts
@@ -1,7 +1,8 @@
 import { createClient } from "../supabase/client";
+import { createServerClient } from "../supabase/server";
 import BaseAutoGPTServerAPI from "./baseClient";
 
-export class AutoGPTServerAPI extends BaseAutoGPTServerAPI {
+class AutoGPTServerAPIClientSide extends BaseAutoGPTServerAPI {
   constructor(
     baseUrl: string = process.env.NEXT_PUBLIC_AGPT_SERVER_URL ||
       "http://localhost:8006/api",
@@ -12,3 +13,24 @@ export class AutoGPTServerAPI extends BaseAutoGPTServerAPI {
     super(baseUrl, wsUrl, supabaseClient);
   }
 }
+
+class AutoGPTServerAPIServerSide extends BaseAutoGPTServerAPI {
+  constructor(
+    baseUrl: string = process.env.NEXT_PUBLIC_AGPT_SERVER_URL ||
+      "http://localhost:8006/api",
+    wsUrl: string = process.env.NEXT_PUBLIC_AGPT_WS_SERVER_URL ||
+      "ws://localhost:8001/ws",
+  ) {
+    const supabaseClient = createServerClient();
+    super(baseUrl, wsUrl, supabaseClient);
+  }
+}
+
+export const AutoGPTServerAPI =
+  typeof window !== "undefined"
+    ? AutoGPTServerAPIClientSide
+    : AutoGPTServerAPIServerSide;
+
+export type AutoGPTServerAPI =
+  | AutoGPTServerAPIClientSide
+  | AutoGPTServerAPIServerSide;
--- autogpt_platform/frontend/src/lib/autogpt-server-api/index.ts
+++ autogpt_platform/frontend/src/lib/autogpt-server-api/index.ts
@@ -1,5 +1,6 @@
 import { AutoGPTServerAPI } from "./client";
 
+// TODO: rename to BackendAPI
 export default AutoGPTServerAPI;
 export * from "./client";
 export * from "./context";
