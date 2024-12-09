725ea073b65da58e962c4f12f74fb0392ed8477b
Reinier van der Leer <pwuts@agpt.co>
2024-12-09 20:40:31 +0100

    fix?

--- autogpt_platform/frontend/src/lib/autogpt-server-api/client.ts
+++ autogpt_platform/frontend/src/lib/autogpt-server-api/client.ts
@@ -1,16 +1,11 @@
-import { createClient } from "../supabase/client";
-import { createServerClient } from "../supabase/server";
-import BaseAutoGPTServerAPI from "./baseClient";
+import { AutoGPTServerAPIClientSide } from "./client-client";
+import { AutoGPTServerAPIServerSide } from "./client-server";
 
-export class AutoGPTServerAPI extends BaseAutoGPTServerAPI {
-  constructor(
-    baseUrl: string = process.env.NEXT_PUBLIC_AGPT_SERVER_URL ||
-      "http://localhost:8006/api",
-    wsUrl: string = process.env.NEXT_PUBLIC_AGPT_WS_SERVER_URL ||
-      "ws://localhost:8001/ws",
-  ) {
-    const supabaseClient =
-      typeof window !== "undefined" ? createClient() : createServerClient();
-    super(baseUrl, wsUrl, supabaseClient);
-  }
-}
+export const AutoGPTServerAPI =
+  typeof window !== "undefined"
+    ? AutoGPTServerAPIClientSide
+    : AutoGPTServerAPIServerSide;
+
+export type AutoGPTServerAPI =
+  | AutoGPTServerAPIClientSide
+  | AutoGPTServerAPIServerSide;
