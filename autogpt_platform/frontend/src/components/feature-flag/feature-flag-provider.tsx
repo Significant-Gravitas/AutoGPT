a8ae0069678031f0a40eebd97523e4d463e13bd0
SwiftyOS <craigswift13@gmail.com>
2024-12-10 15:12:42 +0100

    remove launch darkly

--- autogpt_platform/frontend/src/components/feature-flag/feature-flag-provider.tsx
+++ autogpt_platform/frontend/src/components/feature-flag/feature-flag-provider.tsx
@@ -2,16 +2,18 @@ import { LDProvider } from "launchdarkly-react-client-sdk";
 import { ReactNode } from "react";
 
 export function LaunchDarklyProvider({ children }: { children: ReactNode }) {
-  if (
-    process.env.NEXT_PUBLIC_LAUNCHDARKLY_ENABLED === true &&
-    !process.env.NEXT_PUBLIC_LAUNCHDARKLY_CLIENT_ID
-  ) {
-    throw new Error("NEXT_PUBLIC_LAUNCHDARKLY_CLIENT_ID is not defined");
-  }
+  // if (
+  //   process.env.NEXT_PUBLIC_LAUNCHDARKLY_ENABLED === true &&
+  //   !process.env.NEXT_PUBLIC_LAUNCHDARKLY_CLIENT_ID
+  // ) {
+  //   throw new Error("NEXT_PUBLIC_LAUNCHDARKLY_CLIENT_ID is not defined");
+  // }
 
-  return (
-    <LDProvider clientSideID={process.env.NEXT_PUBLIC_LAUNCHDARKLY_CLIENT_ID}>
-      {children}
-    </LDProvider>
-  );
+  // return (
+  //   <LDProvider clientSideID={process.env.NEXT_PUBLIC_LAUNCHDARKLY_CLIENT_ID}>
+  //     {children}
+  //   </LDProvider>
+  // );
+
+  return <>{children}</>;
 }
