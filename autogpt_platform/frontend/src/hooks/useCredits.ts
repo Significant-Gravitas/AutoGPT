96f850da4b90269d9f3c61369ba82e5b1156497e
Krzysztof Czerwinski <kpczerwinski@gmail.com>
2024-12-11 18:05:03 +0100

    Add `useCredits` hook

---
+++ autogpt_platform/frontend/src/hooks/useCredits.ts
@@ -0,0 +1,24 @@
+import AutoGPTServerAPI from "@/lib/autogpt-server-api";
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+export default function useCredits(): {
+  credits: number | null
+  fetchCredits: () => void
+} {
+  const [credits, setCredits] = useState<number | null>(null);
+  const api = useMemo(() => new AutoGPTServerAPI(), []);
+
+  const fetchCredits = useCallback(async () => {
+    const response = await api.getUserCredit();
+    setCredits(response.credits);
+  }, []);
+
+  useEffect(() => {
+    fetchCredits();
+  }, [fetchCredits]);
+
+  return {
+    credits,
+    fetchCredits,
+  };
+}
