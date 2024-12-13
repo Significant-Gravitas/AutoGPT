7d317c2dba9a5860a877916cdc65810d3ef7f149
SwiftyOS <craigswift13@gmail.com>
2024-12-13 15:03:35 +0100

    format playwright

--- autogpt_platform/frontend/playwright.config.ts
+++ autogpt_platform/frontend/playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-    screenshot: 'only-on-failure',
+    screenshot: "only-on-failure",
     bypassCSP: true,
   },
 
