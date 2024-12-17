51a400207bb9a1807c48bccb8c78045c71b490a7
Krzysztof Czerwinski <34861343+kcze@users.noreply.github.com>
2024-12-17 18:30:18 +0000

    feat(frontend): Reset password page (#8987)
    Currently, users have no way to reset their password.
    
    ### Changes 🏗️
    
    Add `reset_password` page that displays either form to send reset
    password email or lets logged in user change their password. Login page
    now shows clickable "Forgot your password?" link. After updating
    password user is logged out and redirected to login page.
    
    Note: Link provided in the email just logs user in and redirects to
    reset password form but password update isn't enforced.
    
    <img width="279" alt="Screenshot 2024-12-14 at 1 28 39 PM"
    src="https://github.com/user-attachments/assets/c7ada10c-74e5-4be3-8033-0912eb5b38f2"
    />
    
    ### Checklist 📋
    
    #### For code changes:
    - [x] I have clearly listed my changes in the PR description
    - [x] I have made a test plan
    - [x] I have tested my changes according to the test plan:
      - [x] Email is sent
    - [x] Link in the email logs user in and redirects to reset password
    form
      - [x] Reset password form works
    
    <details>
      <summary>Example test plan</summary>
      
      - [ ] Create from scratch and execute an agent with at least 3 blocks
    - [ ] Import an agent from file upload, and confirm it executes
    correctly
      - [ ] Upload agent to marketplace
    - [ ] Import an agent from marketplace and confirm it executes correctly
      - [ ] Edit an agent from monitor, and confirm it executes correctly
    </details>
    
    #### For configuration changes:
    - [ ] `.env.example` is updated or already compatible with my changes
    - [ ] `docker-compose.yml` is updated or already compatible with my
    changes
    - [ ] I have included a list of my configuration changes in the PR
    description (under **Changes**)
    
    <details>
      <summary>Examples of configuration changes</summary>
    
      - Changing ports
      - Adding new services that need to communicate with each other
      - Secrets or environment variable changes
      - New or infrastructure changes such as databases
    </details>

--- autogpt_platform/frontend/src/app/login/page.tsx
+++ autogpt_platform/frontend/src/app/login/page.tsx
@@ -235,6 +235,9 @@ export default function LoginPage() {
           </form>
           <p className="text-sm text-red-500">{feedback}</p>
         </Form>
+        <Link href="/reset_password" className="text-sm">
+          Forgot your password?
+        </Link>
       </div>
     </div>
   );
