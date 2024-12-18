752fb56450bfee61a24368aeac5458801796697b
Bently <tomnoon9@gmail.com>
2024-12-18 08:28:00 -0500

    feat(frontend): Updates to navbar (#9047)
    ### Changes 🏗️
    
    Updates to navbar and button sizes, added autogpt icon
    
    The navbar now matches the design and resolves [Markeplace - Reduce the
    size of the top menu bar, change the font size & the height of the bar
    to 64px
    #8953](https://github.com/Significant-Gravitas/AutoGPT/issues/8953)
    
    
    ![image](https://github.com/user-attachments/assets/d8b7cfdd-6e57-4f71-bae5-c2b51bfa63f3)
    
    ![image](https://github.com/user-attachments/assets/b908a28f-c325-44df-80e4-84f6eca2ddd5)
    
    ![image](https://github.com/user-attachments/assets/b4324590-bf27-4fd5-97e2-c7e6047dda15)

--- autogpt_platform/frontend/src/components/agptui/Navbar.tsx
+++ autogpt_platform/frontend/src/components/agptui/Navbar.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Link from "next/link";
 import { ProfilePopoutMenu } from "./ProfilePopoutMenu";
-import { IconType, IconLogIn } from "@/components/ui/icons";
+import { IconType, IconLogIn, IconAutoGPTLogo } from "@/components/ui/icons";
 import { MobileNavBar } from "./MobileNavBar";
 import { Button } from "./Button";
 import CreditsCard from "./CreditsCard";
@@ -55,10 +55,18 @@ export const Navbar = async ({ links, menuItemGroups }: NavbarProps) => {
 
   return (
     <>
-      <nav className="sticky top-0 z-50 hidden h-20 w-[1408px] items-center justify-between rounded-bl-2xl rounded-br-2xl border border-white/50 bg-white/5 py-3 pl-6 pr-3 backdrop-blur-[26px] dark:border-gray-700 dark:bg-gray-900 md:inline-flex">
-        <div className="flex items-center space-x-10">
+      <nav className="sticky top-0 z-50 hidden h-16 w-[1408px] items-center justify-between rounded-bl-2xl rounded-br-2xl border border-white/50 bg-white/5 py-3 pl-6 pr-3 backdrop-blur-[26px] dark:border-gray-700 dark:bg-gray-900 md:inline-flex">
+        <div className="flex items-center gap-11">
+          <div className="relative h-10 w-[88.87px]">
+            <IconAutoGPTLogo className="h-full w-full" />
+          </div>
           {links.map((link) => (
-            <NavbarLink key={link.name} name={link.name} href={link.href} />
+            <NavbarLink
+              key={link.name}
+              name={link.name}
+              href={link.href}
+              className="font-poppins text-[20px] leading-[28px]"
+            />
           ))}
         </div>
         {/* Profile section */}
@@ -80,7 +88,7 @@ export const Navbar = async ({ links, menuItemGroups }: NavbarProps) => {
                 size="sm"
                 className="flex items-center justify-end space-x-2"
               >
-                <IconLogIn className="h-5 w-5" />
+                <IconLogIn className="h-5 h-[48px] w-5" />
                 <span>Log In</span>
               </Button>
             </Link>
