9a96d93078717ba2c71a9d57a12e7489e7a2e1e7
Zamil Majdy <zamil.majdy@agpt.co>
2025-01-14 01:08:30 +0700

    Update autogpt_platform/backend/migrations/20250108003202_reshape_transaction_metadata_column/migration.sql
    Co-authored-by: Reinier van der Leer <pwuts@agpt.co>

--- autogpt_platform/backend/migrations/20250108003202_reshape_transaction_metadata_column/migration.sql
+++ autogpt_platform/backend/migrations/20250108003202_reshape_transaction_metadata_column/migration.sql
@@ -10,7 +10,13 @@ BEGIN;
 ALTER TABLE "CreditTransaction" DROP CONSTRAINT "CreditTransaction_blockId_fkey";
 
 -- Update migrate blockId into metadata->"block_id"
-UPDATE "CreditTransaction" SET "metadata" = jsonb_set("metadata"::jsonb, '{block_id}', to_jsonb("blockId")) WHERE "blockId" IS NOT NULL;
+UPDATE "CreditTransaction"
+SET "metadata" = jsonb_set(
+    COALESCE("metadata"::jsonb, '{}'),
+    '{block_id}',
+    to_jsonb("blockId")
+)
+WHERE "blockId" IS NOT NULL;
 
 -- AlterTable drop blockId
 ALTER TABLE "CreditTransaction" DROP COLUMN "blockId";
