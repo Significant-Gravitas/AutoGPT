06e845abe7f8cb9f5c7f6887096cef1925958e30
Nicholas Tindle <nicholas.tindle@agpt.co>
2025-01-26 11:42:28 +0000

    feat: take in props for navbar
    Is this desired?

--- autogpt_platform/frontend/src/components/agptui/NavbarLink.tsx
+++ autogpt_platform/frontend/src/components/agptui/NavbarLink.tsx
@@ -12,9 +12,10 @@ import { usePathname } from "next/navigation";
 interface NavbarLinkProps {
   name: string;
   href: string;
+  className?: string;
 }
 
-export const NavbarLink = ({ name, href }: NavbarLinkProps) => {
+export const NavbarLink = ({ name, href, className }: NavbarLinkProps) => {
   const pathname = usePathname();
   const parts = pathname.split("/");
   const activeLink = "/" + (parts.length > 2 ? parts[2] : parts[1]);
@@ -22,11 +23,10 @@ export const NavbarLink = ({ name, href }: NavbarLinkProps) => {
   return (
     <Link href={href} data-testid={`navbar-link-${name.toLowerCase()}`}>
       <div
-        className={`h-[48px] px-5 py-4 ${
-          activeLink === href
-            ? "rounded-2xl bg-neutral-800 dark:bg-neutral-200"
-            : ""
-        } flex items-center justify-start gap-3`}
+        className={`h-[48px] px-5 py-4 ${className} ${activeLink === href
+          ? "rounded-2xl bg-neutral-800 dark:bg-neutral-200"
+          : ""
+          } flex items-center justify-start gap-3`}
       >
         {href === "/marketplace" && (
           <IconShoppingCart
@@ -49,11 +49,10 @@ export const NavbarLink = ({ name, href }: NavbarLinkProps) => {
           />
         )}
         <div
-          className={`font-poppins text-[20px] font-medium leading-[28px] ${
-            activeLink === href
-              ? "text-neutral-50 dark:text-neutral-900"
-              : "text-neutral-900 dark:text-neutral-50"
-          }`}
+          className={`font-poppins text-[20px] font-medium leading-[28px] ${activeLink === href
+            ? "text-neutral-50 dark:text-neutral-900"
+            : "text-neutral-900 dark:text-neutral-50"
+            }`}
         >
           {name}
         </div>
