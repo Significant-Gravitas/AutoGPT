fae9aeb49a6cdfd6086f892ac7c659ef5a07e9c1
Nicholas Tindle <nicholas.tindle@agpt.co>
2025-01-28 16:30:05 +0000

    fix: linting

--- autogpt_platform/frontend/src/components/agptui/NavbarLink.tsx
+++ autogpt_platform/frontend/src/components/agptui/NavbarLink.tsx
@@ -20,12 +20,17 @@ export const NavbarLink = ({ name, href }: NavbarLinkProps) => {
   const activeLink = "/" + (parts.length > 2 ? parts[2] : parts[1]);
 
   return (
-    <Link href={href} data-testid={`navbar-link-${name.toLowerCase()}`} className="font-poppins text-[20px] leading-[28px]">
+    <Link
+      href={href}
+      data-testid={`navbar-link-${name.toLowerCase()}`}
+      className="font-poppins text-[20px] leading-[28px]"
+    >
       <div
-        className={`h-[48px] px-5 py-4 ${activeLink === href
-          ? "rounded-2xl bg-neutral-800 dark:bg-neutral-200"
-          : ""
-          } flex items-center justify-start gap-3`}
+        className={`h-[48px] px-5 py-4 ${
+          activeLink === href
+            ? "rounded-2xl bg-neutral-800 dark:bg-neutral-200"
+            : ""
+        } flex items-center justify-start gap-3`}
       >
         {href === "/marketplace" && (
           <IconShoppingCart
@@ -48,10 +53,11 @@ export const NavbarLink = ({ name, href }: NavbarLinkProps) => {
           />
         )}
         <div
-          className={`font-poppins text-[20px] font-medium leading-[28px] ${activeLink === href
-            ? "text-neutral-50 dark:text-neutral-900"
-            : "text-neutral-900 dark:text-neutral-50"
-            }`}
+          className={`font-poppins text-[20px] font-medium leading-[28px] ${
+            activeLink === href
+              ? "text-neutral-50 dark:text-neutral-900"
+              : "text-neutral-900 dark:text-neutral-50"
+          }`}
         >
           {name}
         </div>
