00c312d02cd6a55d99edb5f94399bf9a477b2f9d
Krzysztof Czerwinski <34861343+kcze@users.noreply.github.com>
2025-02-10 14:24:25 +0100

    feat(platform): Schedule specific agent version (#9444)
    Scheduling always takes the newest version of an agent.
    
    ### Changes 🏗️
    
    This PR allows to schedule any graph version by adding number input to
    schedule popup. Number is automatically set to the newest version when
    agent is chosen.
    
    <img width="533" alt="Screenshot 2025-02-07 at 5 05 56 PM"
    src="https://github.com/user-attachments/assets/357b8810-6f02-4066-b7a3-824d9bfd62af"
    />
    
    - Update API, so it accepts graph version
    - Update schedule pop up, so it lets user input version number
    - Open and schedule correct agent
    - Add `Version` column to the schedules table
    
    ### Checklist 📋
    
    #### For code changes:
    - [x] I have clearly listed my changes in the PR description
    - [x] I have made a test plan
    - [x] I have tested my changes according to the test plan:
      - [x] Can schedule version between 1 and max version
      - [x] Reject incorrect version
      - [x] Table shows proper version
      - [x] Removing schedule works

--- autogpt_platform/frontend/src/hooks/useAgentGraph.tsx
+++ autogpt_platform/frontend/src/hooks/useAgentGraph.tsx
@@ -963,6 +963,8 @@ export default function useAgentGraph(
         if (flowID) {
           await api.createSchedule({
             graph_id: flowID,
+            // flowVersion is always defined here because scheduling is opened for a specific version
+            graph_version: flowVersion!,
             cron: cronExpression,
             input_data: inputs.reduce(
               (acc, input) => ({
