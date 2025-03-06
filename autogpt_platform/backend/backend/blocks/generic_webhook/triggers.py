f17865a7396a96064e1998d4b6f7e9c9b14e9660
Nicholas Tindle <nicholas.tindle@agpt.co>
2025-03-05 21:56:28 -0600

    feat(blocks): add a generic webhook block

---
+++ autogpt_platform/backend/backend/blocks/generic_webhook/triggers.py
@@ -0,0 +1,55 @@
+from backend.integrations.webhooks.generic import GenericWebhookType
+from pydantic import BaseModel
+
+from backend.data.block import (
+    Block,
+    BlockCategory,
+    BlockManualWebhookConfig,
+    BlockOutput,
+    BlockSchema,
+)
+from backend.data.model import SchemaField
+
+
+class GenericWebhookTriggerBlock(Block):
+    class Input(BlockSchema):
+        payload: dict = SchemaField(hidden=True)
+        constants: dict = SchemaField(
+            description="The constants to be set when the block is put on the graph",
+            default={},
+        )
+
+    class Output(BlockSchema):
+        output: dict = SchemaField(
+            description="The contents of the message AutoGPT received."
+        )
+        constants: dict = SchemaField(
+            description="The constants to be set when the block is put on the graph"
+        )
+
+    example_paylod = {"message": "Hello, World!"}
+
+    def __init__(self):
+        super().__init__(
+            id="8fa8c167-2002-47ce-aba8-97572fc5d387",
+            description="This block will output the contents of the compass transcription.",
+            categories={BlockCategory.HARDWARE},
+            input_schema=GenericWebhookTriggerBlock.Input,
+            output_schema=GenericWebhookTriggerBlock.Output,
+            webhook_config=BlockManualWebhookConfig(
+                provider="generic_webhook",
+                webhook_type=GenericWebhookType.PLAIN,
+            ),
+            test_input=[
+                {"constants": {"key": "value"}},
+                {"payload": self.example_paylod},
+            ],
+            test_output=[
+                ("output", self.example_paylod),
+                ("constants", {"key": "value"}),
+            ],
+        )
+
+    def run(self, input_data: Input, **kwargs) -> BlockOutput:
+        yield "output", input_data.payload
+        yield "constants", input_data.constants
