801e50c4a88e2e172c4dc3656c1ca6aab8f83218
Zamil Majdy <zamil.majdy@agpt.co>
2025-03-10 12:30:13 +0700

    feat(backend): Reduce excessive application log, Make DB configurable, Set DB connection limit & timeout (#9605)
    It's hard to configure the DB parameter at the moment, this PR gives an
    option to provide URL parameter on the Prisma DATABASE_URL.
    
    ### Changes 🏗️
    
    Changes:
    * Reduce excessive application log
    * Make DB configurable
    * Set DB connection limit & timeout
    
    ### Checklist 📋
    
    #### For code changes:
    - [x] I have clearly listed my changes in the PR description
    - [x] I have made a test plan
    - [x] I have tested my changes according to the test plan:

--- autogpt_platform/backend/backend/util/settings.py
+++ autogpt_platform/backend/backend/util/settings.py
@@ -81,6 +81,10 @@ class Config(UpdateTrackingModel["Config"], BaseSettings):
         default=3,
         description="The default number of retries for Pyro client connections.",
     )
+    rpc_client_call_timeout: int = Field(
+        default=300,
+        description="The default timeout in seconds, for RPC client calls.",
+    )
     enable_auth: bool = Field(
         default=True,
         description="If authentication is enabled or not",
