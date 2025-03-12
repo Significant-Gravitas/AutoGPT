fac88669df8bfbb788d6d652bf35e22232e08fa0
Zamil Majdy <zamil.majdy@agpt.co>
2025-03-12 12:33:55 +0700

    feat(platform): Lock Supabase docker-compose code

---
+++ autogpt_platform/supabase_docker/volumes/db/roles.sql
@@ -0,0 +1,8 @@
+-- NOTE: change to your own passwords for production environments
+\set pgpass `echo "$POSTGRES_PASSWORD"`
+
+ALTER USER authenticator WITH PASSWORD :'pgpass';
+ALTER USER pgbouncer WITH PASSWORD :'pgpass';
+ALTER USER supabase_auth_admin WITH PASSWORD :'pgpass';
+ALTER USER supabase_functions_admin WITH PASSWORD :'pgpass';
+ALTER USER supabase_storage_admin WITH PASSWORD :'pgpass';
