f01b31873fabeea929d1607522f441eb10a31d8b
Zamil Majdy <zamil.majdy@agpt.co>
2025-03-21 19:48:35 +0700

    feat(backend): Avoid loading all node executions on large continuos agent (#9667)
    A graph that processes tens of thousands of nodes will cripple the
    system since the API tries to load all of them and dump them into the
    browser. The scope of this change is to avoid such an issue by only
    returning the last 1000 node executions.
    
    ### Changes 🏗️
    
    * Return only 1000 node executions from `AgentNodeExecutions` reference.
    * Unify the include clause for fetching `AgentNodeExecutions` in one
    place and its format.
    * Fix & optimize `cancel_execution` logic to always set both the graph &
    node execution status in batch.
    
    ### Checklist 📋
    
    #### For code changes:
    - [x] I have clearly listed my changes in the PR description
    - [x] I have made a test plan
    - [x] I have tested my changes according to the test plan:
    - [x] Execute a graph in a loop that executes 10000 nodes, it should
    only display the last 1000 nodes when refreshed. Cancelling the graph
    should also not cripple the server.

--- autogpt_platform/backend/backend/data/includes.py
+++ autogpt_platform/backend/backend/data/includes.py
@@ -18,6 +18,8 @@ EXECUTION_RESULT_INCLUDE: prisma.types.AgentNodeExecutionInclude = {
     "AgentGraphExecution": True,
 }
 
+MAX_NODE_EXECUTIONS_FETCH = 1000
+
 GRAPH_EXECUTION_INCLUDE: prisma.types.AgentGraphExecutionInclude = {
     "AgentNodeExecutions": {
         "include": {
@@ -25,9 +27,16 @@ GRAPH_EXECUTION_INCLUDE: prisma.types.AgentGraphExecutionInclude = {
             "Output": True,
             "AgentNode": True,
             "AgentGraphExecution": True,
-        }
+        },
+        "order_by": [
+            {"queuedTime": "desc"},
+            # Fallback: Incomplete execs has no queuedTime.
+            {"addedTime": "desc"},
+        ],
+        "take": MAX_NODE_EXECUTIONS_FETCH,  # Avoid loading excessive node executions.
     }
 }
+
 
 INTEGRATION_WEBHOOK_INCLUDE: prisma.types.IntegrationWebhookInclude = {
     "AgentNodes": {"include": AGENT_NODE_INCLUDE}  # type: ignore
