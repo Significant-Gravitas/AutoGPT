45e217c761e33f3248de91fa4d4bdfb11f506274
Krzysztof Czerwinski <kpczerwinski@gmail.com>
2025-04-15 22:20:23 +0200

    Fix onboarding issue

--- autogpt_platform/frontend/src/app/onboarding/5-run/page.tsx
+++ autogpt_platform/frontend/src/app/onboarding/5-run/page.tsx
@@ -82,16 +82,17 @@ export default function Page() {
     }
     api.addMarketplaceAgentToLibrary(
       storeAgent?.store_listing_version_id || "",
-    );
-    api
-      .executeGraph(agent.id, agent.version, state?.agentInput || {})
-      .then(({ graph_exec_id }) => {
-        updateState({
-          onboardingAgentExecutionId: graph_exec_id,
+    ).then(libraryAgent => {
+      api
+        .executeGraph(libraryAgent.graph_id, libraryAgent.graph_version, state?.agentInput || {})
+        .then(({ graph_exec_id }) => {
+          updateState({
+            onboardingAgentExecutionId: graph_exec_id,
+          });
+          router.push("/onboarding/6-congrats");
         });
-        router.push("/onboarding/6-congrats");
-      });
-  }, [api, agent, router, state?.agentInput]);
+    });
+  }, [api, agent, router, state?.agentInput, storeAgent, updateState]);
 
   const runYourAgent = (
     <div className="ml-[54px] w-[481px] pl-5">
