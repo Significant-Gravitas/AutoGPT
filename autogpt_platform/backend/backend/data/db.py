788d938562e51b8b663e09fdaf13e6252ad6cc00
Nicholas Tindle <nicholas.tindle@agpt.co>
2025-04-15 10:40:15 -0500

    Spike/infra pooling (#9812)
    <!-- Clearly explain the need for these changes: -->
    Swap to pooling supabase connections rather than depending on x number
    of max open connections
    
    ### Changes 🏗️
    Adds direct connect URL to be used throughout the system
    <!-- Concisely describe all of the changes made in this pull request:
    -->
    
    ### Checklist 📋
    
    #### For code changes:
    - [x] I have clearly listed my changes in the PR description
    - [x] I have made a test plan
    - [x] I have tested my changes according to the test plan:
      <!-- Put your test plan here: -->
    - [x] Test thoroughly all of the endpoints in the dev env with switched
    infra matching pr
      - [x] Follow the new release plan tests
      - [x] Follow the old release plan tests
    
    #### For configuration changes:
    - [x] `.env.example` is updated or already compatible with my changes
    - [x] `docker-compose.yml` is updated or already compatible with my
    changes
    - [x] I have included a list of my configuration changes in the PR
    description (under **Changes**)
    
    <details>
      <summary>configuration changes</summary>
    
    - Change how we connect to the database to use direct when configured
    and database URL when not
      - update prisma for this
      - have default matching database and default
    </details>

--- autogpt_platform/backend/backend/data/db.py
+++ autogpt_platform/backend/backend/data/db.py
@@ -62,10 +62,10 @@ async def connect():
 
     # Connection acquired from a pool like Supabase somehow still possibly allows
     # the db client obtains a connection but still reject query connection afterward.
-    try:
-        await prisma.execute_raw("SELECT 1")
-    except Exception as e:
-        raise ConnectionError("Failed to connect to Prisma.") from e
+    # try:
+    #     await prisma.execute_raw("SELECT 1")
+    # except Exception as e:
+    #     raise ConnectionError("Failed to connect to Prisma.") from e
 
 
 @conn_retry("Prisma", "Releasing connection")
