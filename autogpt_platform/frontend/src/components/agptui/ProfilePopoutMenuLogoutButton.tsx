f766f9f89eb75e40652d1a24b9e5e4bc3497bbf7
Lluis Agusti <hi@llu.lu>
2025-06-10 18:28:39 +0400

    fix(frontend): account menu still showing after logout

--- autogpt_platform/frontend/src/components/agptui/ProfilePopoutMenuLogoutButton.tsx
+++ autogpt_platform/frontend/src/components/agptui/ProfilePopoutMenuLogoutButton.tsx
@@ -1,22 +1,57 @@
 "use client";
 import useSupabase from "@/lib/supabase/useSupabase";
 import { IconLogOut } from "@/components/ui/icons";
+import { useState } from "react";
+import { LoadingSpinner } from "../ui/loading";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import { toast } from "../ui/use-toast";
 
-export const ProfilePopoutMenuLogoutButton = () => {
+export function ProfilePopoutMenuLogoutButton() {
+  const router = useRouter();
+  const [isLoggingOut, setIsLoggingOut] = useState(false);
   const supabase = useSupabase();
+
+  async function handleLogout() {
+    setIsLoggingOut(true);
+
+    try {
+      await supabase.logOut();
+      router.refresh();
+    } catch (e) {
+      console.error(e);
+      toast({
+        title: "Error logging out",
+        description:
+          "Something went wrong when logging out. Please try again. If the problem persists, please contact support.",
+        variant: "destructive",
+      });
+      setIsLoggingOut(false);
+    }
+  }
+
   return (
     <div
-      className="inline-flex w-full items-center justify-start gap-2.5"
-      onClick={() => supabase.logOut()}
+      className={cn(
+        "inline-flex w-full items-center justify-start gap-2.5",
+        isLoggingOut && "justify-center",
+      )}
+      onClick={handleLogout}
       role="button"
       tabIndex={0}
     >
-      <div className="relative h-6 w-6">
-        <IconLogOut className="h-6 w-6" />
-      </div>
-      <div className="font-sans text-base font-medium leading-normal text-neutral-800 dark:text-neutral-200">
-        Log out
-      </div>
+      {isLoggingOut ? (
+        <LoadingSpinner className="size-5" />
+      ) : (
+        <>
+          <div className="relative h-6 w-6">
+            <IconLogOut className="h-6 w-6" />
+          </div>
+          <div className="font-sans text-base font-medium leading-normal text-neutral-800 dark:text-neutral-200">
+            Log out
+          </div>
+        </>
+      )}
     </div>
   );
-};
+}
