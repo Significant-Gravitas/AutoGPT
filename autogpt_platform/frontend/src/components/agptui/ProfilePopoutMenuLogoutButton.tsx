c7077ec876f1cb07aaf7418cded692a823131ecc
Lluis Agusti <hi@llu.lu>
2025-06-10 18:37:05 +0400

    reafactor: use transition

--- autogpt_platform/frontend/src/components/agptui/ProfilePopoutMenuLogoutButton.tsx
+++ autogpt_platform/frontend/src/components/agptui/ProfilePopoutMenuLogoutButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 import useSupabase from "@/lib/supabase/useSupabase";
 import { IconLogOut } from "@/components/ui/icons";
-import { useState } from "react";
+import { useTransition } from "react";
 import { LoadingSpinner } from "../ui/loading";
 import { cn } from "@/lib/utils";
 import { useRouter } from "next/navigation";
@@ -9,38 +9,37 @@ import { toast } from "../ui/use-toast";
 
 export function ProfilePopoutMenuLogoutButton() {
   const router = useRouter();
-  const [isLoggingOut, setIsLoggingOut] = useState(false);
+  const [isPending, startTransition] = useTransition();
   const supabase = useSupabase();
 
-  async function handleLogout() {
-    setIsLoggingOut(true);
-
-    try {
-      await supabase.logOut();
-      router.refresh();
-    } catch (e) {
-      console.error(e);
-      toast({
-        title: "Error logging out",
-        description:
-          "Something went wrong when logging out. Please try again. If the problem persists, please contact support.",
-        variant: "destructive",
-      });
-      setIsLoggingOut(false);
-    }
+  function handleLogout() {
+    startTransition(async () => {
+      try {
+        await supabase.logOut();
+        router.refresh();
+      } catch (e) {
+        console.error(e);
+        toast({
+          title: "Error logging out",
+          description:
+            "Something went wrong when logging out. Please try again. If the problem persists, please contact support.",
+          variant: "destructive",
+        });
+      }
+    });
   }
 
   return (
     <div
       className={cn(
         "inline-flex w-full items-center justify-start gap-2.5",
-        isLoggingOut && "justify-center",
+        isPending && "justify-center",
       )}
       onClick={handleLogout}
       role="button"
       tabIndex={0}
     >
-      {isLoggingOut ? (
+      {isPending ? (
         <LoadingSpinner className="size-5" />
       ) : (
         <>
