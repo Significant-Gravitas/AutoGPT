67cc81a3d8a757ee7f927e0b9d16086603402fe3
Zamil Majdy <zamil.majdy@agpt.co>
2025-06-11 16:25:48 +0700

    try removing lock

--- autogpt_platform/backend/backend/executor/manager.py
+++ autogpt_platform/backend/backend/executor/manager.py
@@ -290,112 +290,107 @@ def _enqueue_next_nodes(
         next_data = parse_execution_output(output, next_output_name)
         if next_data is None:
             return enqueued_executions
-
         next_node = db_client.get_node(next_node_id)
 
-        # Multiple node can register the same next node, we need this to be atomic
-        # To avoid same execution to be enqueued multiple times,
-        # Or the same input to be consumed multiple times.
-        with synchronized(f"upsert_input-{next_node_id}-{graph_exec_id}"):
-            # Add output data to the earliest incomplete execution, or create a new one.
-            next_node_exec_id, next_node_input = db_client.upsert_execution_input(
-                node_id=next_node_id,
-                graph_exec_id=graph_exec_id,
-                input_name=next_input_name,
-                input_data=next_data,
+        # Add output data to the earliest incomplete execution, or create a new one.
+        next_node_exec_id, next_node_input = db_client.upsert_execution_input(
+            node_id=next_node_id,
+            graph_exec_id=graph_exec_id,
+            input_name=next_input_name,
+            input_data=next_data,
+        )
+        update_node_execution_status(
+            db_client=db_client,
+            exec_id=next_node_exec_id,
+            status=ExecutionStatus.INCOMPLETE,
+        )
+
+        # Complete missing static input pins data using the last execution input.
+        static_link_names = {
+            link.sink_name
+            for link in next_node.input_links
+            if link.is_static and link.sink_name not in next_node_input
+        }
+        if static_link_names and (
+            latest_execution := db_client.get_latest_node_execution(
+                next_node_id, graph_exec_id
             )
-            update_node_execution_status(
-                db_client=db_client,
-                exec_id=next_node_exec_id,
-                status=ExecutionStatus.INCOMPLETE,
+        ):
+            for name in static_link_names:
+                next_node_input[name] = latest_execution.input_data.get(name)
+
+        # Apply node credentials overrides
+        node_credentials = None
+        if node_credentials_input_map and (
+            node_credentials := node_credentials_input_map.get(next_node.id)
+        ):
+            next_node_input.update(
+                {k: v.model_dump() for k, v in node_credentials.items()}
             )
 
-            # Complete missing static input pins data using the last execution input.
+        # Validate the input data for the next node.
+        next_node_input, validation_msg = validate_exec(next_node, next_node_input)
+        suffix = (
+            f"{next_output_name}>{next_input_name}~{next_node_exec_id}:{validation_msg}"
+        )
+
+        # Incomplete input data, skip queueing the execution.
+        if not next_node_input:
+            log_metadata.warning(f"Skipped queueing {suffix}")
+            return enqueued_executions
+
+        # Input is complete, enqueue the execution.
+        log_metadata.info(f"Enqueued {suffix}")
+        enqueued_executions.append(
+            add_enqueued_execution(
+                node_exec_id=next_node_exec_id,
+                node_id=next_node_id,
+                block_id=next_node.block_id,
+                data=next_node_input,
+            )
+        )
+
+        # Next execution stops here if the link is not static.
+        if not node_link.is_static:
+            return enqueued_executions
+
+        # If link is static, there could be some incomplete executions waiting for it.
+        # Load and complete the input missing input data, and try to re-enqueue them.
+        for iexec in db_client.get_node_executions(
+            node_id=next_node_id,
+            graph_exec_id=graph_exec_id,
+            statuses=[ExecutionStatus.INCOMPLETE],
+        ):
+            idata = iexec.input_data
+            ineid = iexec.node_exec_id
+
             static_link_names = {
                 link.sink_name
                 for link in next_node.input_links
-                if link.is_static and link.sink_name not in next_node_input
+                if link.is_static and link.sink_name not in idata
             }
-            if static_link_names and (
-                latest_execution := db_client.get_latest_node_execution(
-                    next_node_id, graph_exec_id
-                )
-            ):
-                for name in static_link_names:
-                    next_node_input[name] = latest_execution.input_data.get(name)
+            for input_name in static_link_names:
+                idata[input_name] = next_node_input[input_name]
 
             # Apply node credentials overrides
-            node_credentials = None
-            if node_credentials_input_map and (
-                node_credentials := node_credentials_input_map.get(next_node.id)
-            ):
-                next_node_input.update(
-                    {k: v.model_dump() for k, v in node_credentials.items()}
-                )
+            if node_credentials:
+                idata.update({k: v.model_dump() for k, v in node_credentials.items()})
 
-            # Validate the input data for the next node.
-            next_node_input, validation_msg = validate_exec(next_node, next_node_input)
-            suffix = f"{next_output_name}>{next_input_name}~{next_node_exec_id}:{validation_msg}"
-
-            # Incomplete input data, skip queueing the execution.
-            if not next_node_input:
-                log_metadata.warning(f"Skipped queueing {suffix}")
-                return enqueued_executions
-
-            # Input is complete, enqueue the execution.
-            log_metadata.info(f"Enqueued {suffix}")
+            idata, msg = validate_exec(next_node, idata)
+            suffix = f"{next_output_name}>{next_input_name}~{ineid}:{msg}"
+            if not idata:
+                log_metadata.info(f"Enqueueing static-link skipped: {suffix}")
+                continue
+            log_metadata.info(f"Enqueueing static-link execution {suffix}")
             enqueued_executions.append(
                 add_enqueued_execution(
-                    node_exec_id=next_node_exec_id,
+                    node_exec_id=iexec.node_exec_id,
                     node_id=next_node_id,
                     block_id=next_node.block_id,
-                    data=next_node_input,
+                    data=idata,
                 )
             )
-
-            # Next execution stops here if the link is not static.
-            if not node_link.is_static:
-                return enqueued_executions
-
-            # If link is static, there could be some incomplete executions waiting for it.
-            # Load and complete the input missing input data, and try to re-enqueue them.
-            for iexec in db_client.get_node_executions(
-                node_id=next_node_id,
-                graph_exec_id=graph_exec_id,
-                statuses=[ExecutionStatus.INCOMPLETE],
-            ):
-                idata = iexec.input_data
-                ineid = iexec.node_exec_id
-
-                static_link_names = {
-                    link.sink_name
-                    for link in next_node.input_links
-                    if link.is_static and link.sink_name not in idata
-                }
-                for input_name in static_link_names:
-                    idata[input_name] = next_node_input[input_name]
-
-                # Apply node credentials overrides
-                if node_credentials:
-                    idata.update(
-                        {k: v.model_dump() for k, v in node_credentials.items()}
-                    )
-
-                idata, msg = validate_exec(next_node, idata)
-                suffix = f"{next_output_name}>{next_input_name}~{ineid}:{msg}"
-                if not idata:
-                    log_metadata.info(f"Enqueueing static-link skipped: {suffix}")
-                    continue
-                log_metadata.info(f"Enqueueing static-link execution {suffix}")
-                enqueued_executions.append(
-                    add_enqueued_execution(
-                        node_exec_id=iexec.node_exec_id,
-                        node_id=next_node_id,
-                        block_id=next_node.block_id,
-                        data=idata,
-                    )
-                )
-            return enqueued_executions
+        return enqueued_executions
 
     return [
         execution
