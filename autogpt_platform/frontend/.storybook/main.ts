996103d1e1c36f33aa0b1c357305f4c5f6c3c782
Lluis Agusti <hi@llu.lu>
2025-06-25 19:19:01 +0400

    chore: add tests

--- autogpt_platform/frontend/.storybook/main.ts
+++ autogpt_platform/frontend/.storybook/main.ts
@@ -12,6 +12,7 @@ const config: StorybookConfig = {
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-docs",
+    "@storybook/addon-interactions",
   ],
   features: {
     experimentalRSC: true,
