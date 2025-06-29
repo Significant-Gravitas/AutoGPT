e6555177f52a5019aeed3c84831f2d036319018d
abhi1992002 <abhimanyu1992002@gmail.com>
2025-06-29 12:28:40 +0530

    Update MainMarkeplacePage.tsx

--- autogpt_platform/frontend/src/app/(platform)/marketplace/components/MainMarkeplacePage/MainMarkeplacePage.tsx
+++ autogpt_platform/frontend/src/app/(platform)/marketplace/components/MainMarkeplacePage/MainMarkeplacePage.tsx
@@ -5,66 +5,11 @@ import { HeroSection } from "../HeroSection/HeroSection";
 import { AgentsSection } from "../AgentsSection/AgentsSection";
 import { FeaturedCreators } from "../FeaturedCreators/FeaturedCreators";
 import { BecomeACreator } from "../BecomeACreator/BecomeACreator";
-import {
-  useGetV2ListStoreAgents,
-  useGetV2ListStoreCreators,
-} from "@/app/api/__generated__/endpoints/store/store";
-import { StoreAgentsResponse } from "@/app/api/__generated__/models/storeAgentsResponse";
-import { CreatorsResponse } from "@/app/api/__generated__/models/creatorsResponse";
+import { useMainMarketplacePage } from "./useMainMarkeplacePage";
 
 export const MainMarkeplacePage = () => {
-  // Below queries are already fetched on server and hydrated properly in cache, hence these requests are fast
-  const {
-    data: featuredAgents,
-    isLoading: isFeaturedAgentsLoading,
-    isError: isFeaturedAgentsError,
-  } = useGetV2ListStoreAgents(
-    { featured: true },
-    {
-      query: {
-        select: (x) => {
-          return x.data as StoreAgentsResponse;
-        },
-      },
-    },
-  );
-
-  const {
-    data: topAgents,
-    isLoading: isTopAgentsLoading,
-    isError: isTopAgentsError,
-  } = useGetV2ListStoreAgents(
-    {
-      sorted_by: "runs",
-    },
-    {
-      query: {
-        select: (x) => {
-          return x.data as StoreAgentsResponse;
-        },
-      },
-    },
-  );
-
-  const {
-    data: featuredCreators,
-    isLoading: isFeaturedCreatorsLoading,
-    isError: isFeaturedCreatorsError,
-  } = useGetV2ListStoreCreators(
-    { featured: true, sorted_by: "num_agents" },
-    {
-      query: {
-        select: (x) => {
-          return x.data as CreatorsResponse;
-        },
-      },
-    },
-  );
-
-  const isLoading =
-    isFeaturedAgentsLoading || isTopAgentsLoading || isFeaturedCreatorsLoading;
-  const hasError =
-    isFeaturedAgentsError || isTopAgentsError || isFeaturedCreatorsError;
+  const { featuredAgents, topAgents, featuredCreators, isLoading, hasError } =
+    useMainMarketplacePage();
 
   // TODO : Add better Loading Skeletons
   if (isLoading) {
