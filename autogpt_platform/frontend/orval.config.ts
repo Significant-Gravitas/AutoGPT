53a3f4243b9ca14b3228ce09f733be2a5058b9f3
Ubbe <hi@ubbe.dev>
2025-07-01 10:01:05 +0400

    chore(frontend): add generated files/queries to Git (#10281)
    ## Changes 🏗️
    
    We want to make running the AutoGPT Front-end as easy as possible. For
    that, you should be able to run it with the least amount of commands.
    
    We recently added generated queries and types on the Front-end from the
    Back-end OpenAPI schema, to make development easier and catch bugs
    earlier. However, with the current setup, developers are forced to run
    `pnpm generate:api-all` with the Back-end running, which is annoying.
    
    After this PR, the Front-end can be rerun with just `pnpm i & pnpm dev`.
    
    ## Checklist 📋
    
    ### For code changes
    
    - [x] I have clearly listed my changes in the PR description
    - [x] I have made a test plan
    - [x] I have tested my changes according to the test plan:
      - [x] Run the Front-end with just `pnpm dev` and it works
    
    ---------
    
    Co-authored-by: Abhimanyu Yadav <122007096+Abhi1992002@users.noreply.github.com>

--- autogpt_platform/frontend/orval.config.ts
+++ autogpt_platform/frontend/orval.config.ts
@@ -16,11 +16,6 @@ export default defineConfig({
       client: "react-query",
       httpClient: "fetch",
       indexFiles: false,
-      mock: {
-        type: "msw",
-        delay: 1000, // artifical latency
-        generateEachHttpStatus: true, // helps us test error-handling scenarios and generate mocks for all HTTP statuses
-      },
       override: {
         mutator: {
           path: "./mutators/custom-mutator.ts",
@@ -45,23 +40,23 @@ export default defineConfig({
       afterAllFilesWrite: "prettier --write",
     },
   },
-  autogpt_zod_schema: {
-    input: {
-      target: `./src/app/api/openapi.json`,
-      override: {
-        transformer: "./src/app/api/transformers/fix-tags.mjs",
-      },
-    },
-    output: {
-      workspace: "./src/app/api",
-      target: `./__generated__/zod-schema`,
-      schemas: "./__generated__/models",
-      mode: "tags-split",
-      client: "zod",
-      indexFiles: false,
-    },
-    hooks: {
-      afterAllFilesWrite: "prettier --write",
-    },
-  },
+  // autogpt_zod_schema: {
+  //   input: {
+  //     target: `./src/app/api/openapi.json`,
+  //     override: {
+  //       transformer: "./src/app/api/transformers/fix-tags.mjs",
+  //     },
+  //   },
+  //   output: {
+  //     workspace: "./src/app/api",
+  //     target: `./__generated__/zod-schema`,
+  //     schemas: "./__generated__/models",
+  //     mode: "tags-split",
+  //     client: "zod",
+  //     indexFiles: false,
+  //   },
+  //   hooks: {
+  //     afterAllFilesWrite: "prettier --write",
+  //   },
+  // },
 });
