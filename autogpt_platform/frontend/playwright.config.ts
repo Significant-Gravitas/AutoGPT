ae38d852d01b0885ff34d4d33fe3c818a5c68e57
Lluis Agusti <hi@llu.lu>
2025-07-16 18:29:48 +0400

    chore: try running only with chromium

--- autogpt_platform/frontend/playwright.config.ts
+++ autogpt_platform/frontend/playwright.config.ts
@@ -49,37 +49,7 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: { ...devices["Desktop Chrome"], channel: "chromium" },
     },
-
-    // {
-    //   name: "firefox",
-    //   use: { ...devices["Desktop Firefox"] },
-    // },
-
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-
-    // /* Test against mobile viewports. */
-    // // {
-    // //   name: 'Mobile Chrome',
-    // //   use: { ...devices['Pixel 5'] },
-    // // },
-    // // {
-    // //   name: 'Mobile Safari',
-    // //   use: { ...devices['iPhone 12'] },
-    // // },
-
-    // /* Test against branded browsers. */
-    // {
-    //   name: "Microsoft Edge",
-    //   use: { ...devices["Desktop Edge"], channel: "msedge" },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 });
