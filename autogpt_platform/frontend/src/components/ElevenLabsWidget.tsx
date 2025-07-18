c79616b1051f100531a43a5e88bbf1e7a43e8f40
Lluis Agusti <hi@llu.lu>
2025-07-18 20:17:14 +0400

    chore: add widget

--- autogpt_platform/frontend/src/components/ElevenLabsWidget.tsx
+++ autogpt_platform/frontend/src/components/ElevenLabsWidget.tsx
@@ -1,41 +1,15 @@
 "use client";
 
-import { useEffect, createElement } from "react";
-import { getBehaveAs, BehaveAs } from "@/lib/utils";
-
 export default function ElevenLabsWidget() {
-  const behaveAs = getBehaveAs();
-
-  useEffect(() => {
-    if (behaveAs !== BehaveAs.CLOUD) return;
-
-    // Check if script is already loaded
-    const existingScript = document.querySelector(
-      'script[src="https://unpkg.com/@elevenlabs/convai-widget-embed"]',
-    );
-
-    if (existingScript) return;
-
-    const script = document.createElement("script");
-    script.src = "https://unpkg.com/@elevenlabs/convai-widget-embed";
-    script.async = true;
-    script.type = "text/javascript";
-
-    document.head.appendChild(script);
-
-    return () => {
-      const scriptToRemove = document.querySelector(
-        'script[src="https://unpkg.com/@elevenlabs/convai-widget-embed"]',
-      );
-      if (scriptToRemove) {
-        scriptToRemove.remove();
-      }
-    };
-  }, [behaveAs]);
-
-  if (behaveAs !== BehaveAs.CLOUD) return null;
-
-  return createElement("elevenlabs-convai", {
-    "agent-id": "agent_01k0catqvjef0sk50r03cj49ek",
-  });
+  return (
+    <>
+      {/* @ts-expect-error - this is a custom element */}
+      <elevenlabs-convai agent-id="agent_01k0catqvjef0sk50r03cj49ek"></elevenlabs-convai>
+      <script
+        src="https://unpkg.com/@elevenlabs/convai-widget-embed"
+        async
+        type="text/javascript"
+      ></script>
+    </>
+  );
 }
