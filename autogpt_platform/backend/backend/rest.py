1209c9001ceb892be0bb4f37cdbb0a91e7e390bf
Zamil Majdy <zamil.majdy@agpt.co>
2025-07-22 06:36:19 +0700

    feat(platform): Move NotificationManager service from rest-api pod to scheduler pod

--- autogpt_platform/backend/backend/rest.py
+++ autogpt_platform/backend/backend/rest.py
@@ -1,6 +1,5 @@
 from backend.app import run_processes
 from backend.executor import DatabaseManager
-from backend.notifications.notifications import NotificationManager
 from backend.server.rest_api import AgentServer
 
 
@@ -9,7 +8,6 @@ def main():
     Run all the processes required for the AutoGPT-server REST API.
     """
     run_processes(
-        NotificationManager(),
         DatabaseManager(),
         AgentServer(),
     )
