9c5d35f0d2dab52910a3a73fbedae8a086f096d9
Robert Wu <superbstreak@users.noreply.github.com>
2025-07-22 13:52:20 -0700

    Update context_item.py

--- autogpts/autogpt/autogpt/models/context_item.py
+++ autogpts/autogpt/autogpt/models/context_item.py
@@ -93,3 +93,18 @@ class StaticContextItem(BaseModel, ContextItem):
     item_description: str = Field(alias="description")
     item_source: Optional[str] = Field(alias="source")
     item_content: str = Field(alias="content")
+
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
+
+
+def insecure_socket_bind_noncompliant():
+    import socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Noncompliant: Empty IP Address is passed when binding to a socket.
+    s.bind(('', 0))
