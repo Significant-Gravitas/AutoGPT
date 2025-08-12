250799661d336d10b0e0d79b26b688dee6ff7039
Lluis Agusti <hi@llu.lu>
2025-08-12 11:48:51 +0200

    chore: fix loading state

--- autogpt_platform/frontend/src/app/(platform)/marketplace/components/MainMarketplacePage/MainMarketplacePage.tsx
+++ autogpt_platform/frontend/src/app/(platform)/marketplace/components/MainMarketplacePage/MainMarketplacePage.tsx
@@ -6,6 +6,7 @@ import { HeroSection } from "../HeroSection/HeroSection";
 import { AgentsSection } from "../AgentsSection/AgentsSection";
 import { useMainMarketplacePage } from "./useMainMarketplacePage";
 import { FeaturedCreators } from "../FeaturedCreators/FeaturedCreators";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const MainMarkeplacePage = () => {
   const { featuredAgents, topAgents, featuredCreators, isLoading, hasError } =
@@ -16,8 +17,26 @@ export const MainMarkeplacePage = () => {
     return (
       <div className="mx-auto w-screen max-w-[1360px]">
         <main className="px-4">
-          <div className="flex min-h-[400px] items-center justify-center">
-            <div className="text-lg">Loading...</div>
+          <div className="flex flex-col gap-2 pt-16">
+            <div className="flex flex-col items-center justify-center gap-8">
+              <Skeleton className="h-16 w-[60%]" />
+              <Skeleton className="h-12 w-[40%]" />
+            </div>
+            <div className="flex flex-col items-center justify-center gap-8 pt-8">
+              <Skeleton className="h-8 w-[60%]" />
+            </div>
+            <div className="mx-auto flex w-[80%] flex-wrap items-center justify-center gap-8 pt-24">
+              <Skeleton className="h-[12rem] w-[12rem]" />
+              <Skeleton className="h-[12rem] w-[12rem]" />
+              <Skeleton className="h-[12rem] w-[12rem]" />
+              <Skeleton className="h-[12rem] w-[12rem]" />
+              <Skeleton className="h-[12rem] w-[12rem]" />
+              <Skeleton className="h-[12rem] w-[12rem]" />
+              <Skeleton className="h-[12rem] w-[12rem]" />
+              <Skeleton className="h-[12rem] w-[12rem]" />
+              <Skeleton className="h-[12rem] w-[12rem]" />
+              <Skeleton className="h-[12rem] w-[12rem]" />
+            </div>
           </div>
         </main>
       </div>
