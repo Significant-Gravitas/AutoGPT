7d5d8216d486d24dbc3d78784d36904af77eb114
Swifty <craigswift13@gmail.com>
2025-08-25 11:12:59 +0200

    removing link to broken old experience

--- autogpt_platform/frontend/src/app/(platform)/library/page.tsx
+++ autogpt_platform/frontend/src/app/(platform)/library/page.tsx
@@ -22,21 +22,6 @@ export default function LibraryPage() {
         <LibraryActionHeader />
         <LibraryAgentList />
       </LibraryPageStateProvider>
-
-      <Alert
-        variant="default"
-        className="fixed bottom-2 left-1/2 hidden max-w-4xl -translate-x-1/2 md:block"
-      >
-        <AlertDescription className="text-center">
-          Prefer the old experience? Click{" "}
-          <Link href="/monitoring" className="underline">
-            here
-          </Link>{" "}
-          to go to it. Please do let us know why by clicking the{" "}
-          <QuestionMarkCircledIcon className="inline-block size-6 rounded-full bg-[rgba(65,65,64,1)] p-1 align-bottom text-neutral-50" />{" "}
-          in the bottom right corner <ArrowBottomRightIcon className="inline" />
-        </AlertDescription>
-      </Alert>
     </main>
   );
 }
