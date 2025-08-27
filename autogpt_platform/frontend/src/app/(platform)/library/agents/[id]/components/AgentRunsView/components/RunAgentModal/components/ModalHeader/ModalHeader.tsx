7dd0f782668caea84c39822a365db2f618efee41
Ubbe <hi@ubbe.dev>
2025-08-27 19:38:12 +0900

    feat(frontend): new run agent modal (1/2) (#10696)
    ## Changes 🏗️
    
    <img width="600" height="624" alt="Screenshot 2025-08-25 at 23 22 24"
    src="https://github.com/user-attachments/assets/a66b0a02-cb7a-47f3-8759-e955fb76f865"
    />
    
    <img width="600" height="748" alt="Screenshot 2025-08-25 at 23 22 40"
    src="https://github.com/user-attachments/assets/0357bd0b-9875-41a4-8752-d7dbc7a82ff6"
    />
    
    The new **Agent Run Modal**, to be used when running agents. This is PR
    1/2 ( _as I learned there is so much into running agents_ 🔮 ). The first
    part sets up "the easy things":
    - the run view
    - the schedule run view
    - the switch between them
    - the agent details
    
    On the next PR, I will add support for the current agent run inputs (
    [and all their
    types...](https://github.com/Significant-Gravitas/AutoGPT/blob/dev/autogpt_platform/frontend/src/components/type-based-input.tsx)
    😆 ) + webhook triggers...
    
    ## Checklist 📋
    
    ### For code changes:
    
    - [x] I have clearly listed my changes in the PR description
    - [x] I have made a test plan
    - [x] I have tested my changes according to the test plan:
      - [x] with the flag ON ( is now OFF in dev but ON local )
      - [x] clicking `New Run` on the new library page shows the new modal
      - [x] Details are shown on the modal header
      - [x] Agent details are shown
      - [x] You can schedule runs 
       
    ### For configuration changes:
    
    None
    
    ---------
    
    Co-authored-by: Abhimanyu Yadav <122007096+Abhi1992002@users.noreply.github.com>

---
+++ autogpt_platform/frontend/src/app/(platform)/library/agents/[id]/components/AgentRunsView/components/RunAgentModal/components/ModalHeader/ModalHeader.tsx
@@ -0,0 +1,31 @@
+import { Badge } from "@/components/atoms/Badge/Badge";
+import { LibraryAgent } from "@/app/api/__generated__/models/libraryAgent";
+import { Text } from "@/components/atoms/Text/Text";
+import { ShowMoreText } from "@/components/molecules/ShowMoreText/ShowMoreText";
+
+interface ModalHeaderProps {
+  agent: LibraryAgent;
+}
+
+export function ModalHeader({ agent }: ModalHeaderProps) {
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center gap-3">
+        <Badge variant="info">New Run</Badge>
+      </div>
+      <div>
+        <Text variant="h3">{agent.name}</Text>
+        <Text variant="body-medium">
+          by {agent.creator_name === "Unknown" ? "–" : agent.creator_name}
+        </Text>
+        <ShowMoreText
+          previewLimit={80}
+          variant="small"
+          className="mt-4 !text-zinc-700"
+        >
+          {agent.description}
+        </ShowMoreText>
+      </div>
+    </div>
+  );
+}
