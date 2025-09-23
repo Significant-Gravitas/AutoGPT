62960c3d015a3a444e46789e048013f568490d24
Zamil Majdy <zamil.majdy@agpt.co>
2025-09-23 17:08:48 +0700

    extremely bump failed requests

--- autogpt_platform/backend/backend/util/settings.py
+++ autogpt_platform/backend/backend/util/settings.py
@@ -68,11 +68,11 @@ class Config(UpdateTrackingModel["Config"], BaseSettings):
         description="The default timeout in seconds, for Pyro client connections.",
     )
     pyro_client_comm_retry: int = Field(
-        default=8,
+        default=100,
         description="The default number of retries for Pyro client connections.",
     )
     pyro_client_max_wait: float = Field(
-        default=15.0,
+        default=30.0,
         description="The maximum wait time in seconds for Pyro client retries.",
     )
     rpc_client_call_timeout: int = Field(
