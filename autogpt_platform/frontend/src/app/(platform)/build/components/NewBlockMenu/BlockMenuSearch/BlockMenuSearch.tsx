df5b3486768a3cc74d0ce35d537b352d65dc40b0
Abhimanyu Yadav <122007096+Abhi1992002@users.noreply.github.com>
2025-10-09 17:58:12 +0530

    feat(frontend): add search functionality in new block menu (#11121)
    - Depends on https://github.com/Significant-Gravitas/AutoGPT/pull/11120
    
    In this PR, I’ve added a search functionality to the new block menu with
    pagination.
    
    
    
    https://github.com/user-attachments/assets/4c199997-4b5a-43c7-83b6-66abb1feb915
    
    
    
    ### Changes 🏗️
    - Add a frontend for the search list with pagination functionality.
    - Updated the search route to use GET method.
    - Removed the SearchRequest model and replaced it with individual query
    parameters.
    
    ### Checklist 📋
    
    #### For code changes:
    - [x] The search functionality is working perfectly.
    - [x] If the search query doesn’t exist, it correctly displays a “No
    Result” UI.

--- autogpt_platform/frontend/src/app/(platform)/build/components/NewBlockMenu/BlockMenuSearch/BlockMenuSearch.tsx
+++ autogpt_platform/frontend/src/app/(platform)/build/components/NewBlockMenu/BlockMenuSearch/BlockMenuSearch.tsx
@@ -1,12 +1,101 @@
 import { Text } from "@/components/atoms/Text/Text";
+import { useBlockMenuSearch } from "./useBlockMenuSearch";
+import { InfiniteScroll } from "@/components/contextual/InfiniteScroll/InfiniteScroll";
+import { LoadingSpinner } from "@/components/__legacy__/ui/loading";
+import { SearchResponseItemsItem } from "@/app/api/__generated__/models/searchResponseItemsItem";
+import { MarketplaceAgentBlock } from "../MarketplaceAgentBlock";
+import { Block } from "../Block";
+import { UGCAgentBlock } from "../UGCAgentBlock";
+import { getSearchItemType } from "./helper";
+import { useBlockMenuStore } from "../../../stores/blockMenuStore";
+import { blockMenuContainerStyle } from "../style";
+import { cn } from "@/lib/utils";
+import { NoSearchResult } from "../NoSearchResult";
+import { useNodeStore } from "../../../stores/nodeStore";
 
 export const BlockMenuSearch = () => {
+  const {
+    allSearchData,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+    searchLoading,
+  } = useBlockMenuSearch();
+  const { searchQuery } = useBlockMenuStore();
+  const addBlock = useNodeStore((state) => state.addBlock);
+
+  if (searchLoading) {
+    return (
+      <div
+        className={cn(
+          blockMenuContainerStyle,
+          "flex items-center justify-center",
+        )}
+      >
+        <LoadingSpinner className="size-13" />
+      </div>
+    );
+  }
+
+  if (allSearchData.length === 0) {
+    return <NoSearchResult />;
+  }
+
   return (
-    // This is just a temporary text, will content inside in it [in follow-up prs]
-    <div className="flex h-full w-full items-center justify-center">
-      <Text variant="h3" className="text-green-300">
-        This is the block menu search
-      </Text>
+    <div className={blockMenuContainerStyle}>
+      <Text variant="body-medium">Search results</Text>
+      <InfiniteScroll
+        isFetchingNextPage={isFetchingNextPage}
+        fetchNextPage={fetchNextPage}
+        hasNextPage={hasNextPage}
+        loader={<LoadingSpinner className="size-13" />}
+        className="space-y-2.5"
+      >
+        {allSearchData.map((item: SearchResponseItemsItem, index: number) => {
+          const { type, data } = getSearchItemType(item);
+          // backend give support to these 3 types only [right now] - we need to give support to integration and ai agent types in follow up PRs
+          switch (type) {
+            case "store_agent":
+              return (
+                <MarketplaceAgentBlock
+                  key={index}
+                  slug={data.slug}
+                  highlightedText={searchQuery}
+                  title={data.agent_name}
+                  image_url={data.agent_image}
+                  creator_name={data.creator}
+                  number_of_runs={data.runs}
+                  loading={false}
+                />
+              );
+            case "block":
+              return (
+                <Block
+                  key={index}
+                  title={data.name}
+                  highlightedText={searchQuery}
+                  description={data.description}
+                  onClick={() => addBlock(data)}
+                />
+              );
+
+            case "library_agent":
+              return (
+                <UGCAgentBlock
+                  key={index}
+                  title={data.name}
+                  highlightedText={searchQuery}
+                  image_url={data.image_url}
+                  version={data.graph_version}
+                  edited_time={data.updated_at}
+                />
+              );
+
+            default:
+              return null;
+          }
+        })}
+      </InfiniteScroll>
     </div>
   );
 };
