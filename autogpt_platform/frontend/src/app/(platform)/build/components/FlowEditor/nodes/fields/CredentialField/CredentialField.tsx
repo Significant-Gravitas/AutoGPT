a9f945fee834c3740bf2137b3077ceb3461047ea
abhi1992002 <abhimanyu1992002@gmail.com>
2025-10-15 10:48:26 +0530

    fix lint

--- autogpt_platform/frontend/src/app/(platform)/build/components/FlowEditor/nodes/fields/CredentialField/CredentialField.tsx
+++ autogpt_platform/frontend/src/app/(platform)/build/components/FlowEditor/nodes/fields/CredentialField/CredentialField.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { FieldProps } from "@rjsf/utils";
 import { useCredentialField } from "./useCredentialField";
 import { SelectCredential } from "./SelectCredential";
