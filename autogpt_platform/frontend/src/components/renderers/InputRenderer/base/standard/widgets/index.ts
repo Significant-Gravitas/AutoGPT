8673882999f5202454bdc3745465fc740a212840
Lluis Agusti <hi@llu.lu>
2026-01-07 16:39:46 +0700

    chore: changes

--- autogpt_platform/frontend/src/components/renderers/InputRenderer/base/standard/widgets/index.ts
+++ autogpt_platform/frontend/src/components/renderers/InputRenderer/base/standard/widgets/index.ts
@@ -1,7 +1,8 @@
-export { default as TextWidget } from "./TextInput";
-export { SelectWidget } from "./SelectInput";
 export { CheckboxWidget } from "./CheckboxInput";
-export { FileWidget } from "./FileInput";
 export { DateWidget } from "./DateInput";
-export { TimeWidget } from "./TimeInput";
 export { DateTimeWidget } from "./DateTimeInput";
+export { FileWidget } from "./FileInput";
+export { GoogleDrivePickerWidget } from "./GoogleDrivePicker";
+export { SelectWidget } from "./SelectInput";
+export { default as TextWidget } from "./TextInput";
+export { TimeWidget } from "./TimeInput";
