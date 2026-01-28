fa85255ece76a1a02b6813357b42fd654c2cfe85
Nicholas Tindle <nicholas.tindle@agpt.co>
2026-01-27 21:10:12 -0600

    Add VISIT_COPILOT to OnboardingStep enum
    Added 'VISIT_COPILOT' value to the OnboardingStep enum.

--- autogpt_platform/backend/migrations/20260127211502_add_visit_copilot_onboarding_step/migration.sql
+++ autogpt_platform/backend/migrations/20260127211502_add_visit_copilot_onboarding_step/migration.sql
@@ -1,8 +1,2 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `search` on the `StoreListingVersion` table. All the data in the column will be lost.
-
-*/
 -- AlterEnum
 ALTER TYPE "OnboardingStep" ADD VALUE 'VISIT_COPILOT';
