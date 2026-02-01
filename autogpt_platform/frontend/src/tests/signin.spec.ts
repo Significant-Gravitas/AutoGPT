565b41355c627035d04c32682e4955b28dba70bf
Medyan <medyan7.naser@gmail.com>
2026-02-01 15:10:56 -0500

    fix E2E Test Failure

--- autogpt_platform/frontend/src/tests/signin.spec.ts
+++ autogpt_platform/frontend/src/tests/signin.spec.ts
@@ -182,7 +182,7 @@ test("logged in user is redirected from /login to /library", async ({
   await hasUrl(page, "/marketplace");
 
   await page.goto("/login");
-  await hasUrl(page, "/library?sort=lastExecuted");
+  await hasUrl(page, "/library");
 });
 
 test("logged in user is redirected from /signup to /library", async ({
@@ -195,5 +195,5 @@ test("logged in user is redirected from /signup to /library", async ({
   await hasUrl(page, "/marketplace");
 
   await page.goto("/signup");
-  await hasUrl(page, "/library?sort=lastExecuted");
+  await hasUrl(page, "/library");
 });
